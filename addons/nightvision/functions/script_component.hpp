20d44e4515d837adfc9ca01545028b3bddb30c36
PabstMirror <elephantisaterriblemascot@gmail.com>
2015-01-13 12:28:45 -0600

    Changed \function scrippComp to link to base

--- addons/nightvision/functions/script_component.hpp
+++ addons/nightvision/functions/script_component.hpp
@@ -1,12 +1,1 @@
-#define COMPONENT nightvision
-#include "\z\ace\addons\main\script_mod.hpp"
-
-#ifdef DEBUG_ENABLED_BLANK
-    #define DEBUG_MODE_FULL
-#endif
-
-#ifdef DEBUG_SETTINGS_BLANK
-    #define DEBUG_SETTINGS DEBUG_SETTINGS_BLANK
-#endif
-
-#include "\z\ace\addons\main\script_macros.hpp"
+#include "\z\ace\addons\nightvision\script_component.hpp"
