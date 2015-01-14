1927d6ba36bd1b43c1b2ae7c59fccbfb1f971c43
PabstMirror <elephantisaterriblemascot@gmail.com>
2015-01-14 13:51:03 -0600

    Script Comp Fix

--- addons/nightvision/script_component.hpp
+++ addons/nightvision/script_component.hpp
@@ -1,12 +1,12 @@
 #define COMPONENT nightvision
 #include "\z\ace\addons\main\script_mod.hpp"
 
-#ifdef DEBUG_ENABLED_BLANK
+#ifdef DEBUG_ENABLED_NIGHTVISION
     #define DEBUG_MODE_FULL
 #endif
 
-#ifdef DEBUG_SETTINGS_BLANK
-    #define DEBUG_SETTINGS DEBUG_SETTINGS_BLANK
+#ifdef DEBUG_SETTINGS_NIGHTVISION
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_NIGHTVISION
 #endif
 
 #include "\z\ace\addons\main\script_macros.hpp"
