26801c754c26b6fb5f35a656554f75205323bd87
Glowbal <thomasskooi@live.nl>
2015-01-14 12:43:22 +0100

    Added CBA Macros
    Added CBA macros to the model and picture paths

--- TO_MERGE/cse/medical/CfgWeapons.hpp
+++ TO_MERGE/cse/medical/CfgWeapons.hpp
@@ -8,8 +8,8 @@ class CfgWeapons {
 		count = 1;
 		type = 16;
 		displayName = $STR_ACE_MAG_BANDAGE_BASIC_DISPLAY;
-		picture = "\cse\cse_sys_medical\equipment\img\field_dressing.paa";
-		model = "\cse\cse_sys_medical\equipment\bandages\fielddressing.p3d";
+		picture = QUOTE(PATHTOF(equipment\img\field_dressing.paa));
+		model = QUOTE(PATHTOF(equipment\bandages\fielddressing.p3d));
 		descriptionShort = $STR_ACE_MAG_BANDAGE_BASIC_DESC_SHORT;
 		descriptionUse = $STR_ACE_MAG_BANDAGE_BASIC_DESC_USE;
 		class ItemInfo: InventoryItem_Base_F
@@ -25,7 +25,7 @@ class CfgWeapons {
 		count = 1;
 		type = 16;
 		displayName = $STR_ACE_MAG_PACKING_BANDAGE_DISPLAY;
-		picture = "\cse\cse_sys_medical\equipment\img\packing_bandage.paa";
+		picture = QUOTE(PATHTOF(equipment\img\packing_bandage.paa));
 		descriptionShort = $STR_ACE_MAG_PACKING_BANDAGE_DESC_SHORT;
 		descriptionUse = $STR_ACE_MAG_PACKING_BANDAGE_DESC_USE;
 		class ItemInfo: InventoryItem_Base_F
@@ -33,7 +33,7 @@ class CfgWeapons {
 			mass=1;
 			type=201;
 		};
-		model = "\cse\cse_sys_medical\equipment\bandages\packingbandage.p3d";
+		model = QUOTE(PATHTOF(equipment\bandages\packingbandage.p3d));
 	};
 	class ACE_bandageElastic: ItemCore {
 		scope = 2;
@@ -41,8 +41,8 @@ class CfgWeapons {
 		count = 1;
 		type = 16;
 		displayName = $STR_ACE_MAG_BANDAGE_ELASTIC_DISPLAY;
-		picture = "\cse\cse_sys_medical\equipment\img\bandageElastic.paa";
-		model = "\A3\Structures_F_EPA\Items\Medical\Bandage_F.p3d";
+		picture = QUOTE(PATHTOF(equipment\img\bandageElastic.paa));
+		model = "\A3\Structures_F_EPA\Items\Medical\Bandage_F.p3d));
 		descriptionShort = $STR_ACE_MAG_BANDAGE_ELASTIC_DESC_SHORT;
 		descriptionUse = $STR_ACE_MAG_BANDAGE_ELASTIC_DESC_USE;
 		class ItemInfo: InventoryItem_Base_F
@@ -58,8 +58,8 @@ class CfgWeapons {
 		count = 1;
 		type = 16;
 		displayName = $STR_ACE_MAG_TOURNIQUET_DISPLAY;
-		picture = "\cse\cse_sys_medical\equipment\img\tourniquet.paa";
-		model = "\cse\cse_sys_medical\equipment\Tourniquet.p3d";
+		picture = QUOTE(PATHTOF(equipment\img\tourniquet.paa));
+		model = QUOTE(PATHTOF(equipment\Tourniquet.p3d));
 		descriptionShort = $STR_ACE_MAG_TOURNIQUET_DESC_SHORT;
 		descriptionUse = $STR_ACE_MAG_TOURNIQUET_DESC_USE;
 		class ItemInfo: InventoryItem_Base_F
@@ -75,7 +75,7 @@ class CfgWeapons {
 		count = 1;
 		type = 16;
 		displayName = $STR_ACE_MAG_SPLINT_DISPLAY;
-		picture = "\cse\cse_sys_medical\equipment\img\splint.paa";
+		picture = QUOTE(PATHTOF(equipment\img\splint.paa));
 		descriptionUse = $STR_ACE_MAG_SPLINT_DESC_USE;
 		descriptionShort = $STR_ACE_MAG_SPLINT_DESC_SHORT;
 		class ItemInfo: InventoryItem_Base_F
@@ -91,8 +91,8 @@ class CfgWeapons {
 		count = 1;
 		type = 16;
 		displayName = $STR_ACE_MAG_MORPHINE_DISPLAY;
-		picture = "\cse\cse_sys_medical\equipment\img\morphine.paa";
-		model = "\cse\cse_sys_medical\equipment\Morphinpen.p3d";
+		picture = QUOTE(PATHTOF(equipment\img\morphine.paa));
+		model = QUOTE(PATHTOF(equipment\Morphinpen.p3d));
 		descriptionShort = $STR_ACE_MAG_MORPHINE_DESC_SHORT;
 		descriptionUse = $STR_ACE_MAG_MORPHINE_DESC_USE;
 		class ItemInfo: InventoryItem_Base_F
@@ -107,8 +107,8 @@ class CfgWeapons {
 		count = 1;
 		type = 16;
 		displayName = $STR_ACE_MAG_ATROPINE_DISPLAY;
-		picture = "\cse\cse_sys_medical\equipment\img\atropine.paa";
-		model = "\cse\cse_sys_medical\equipment\Atropin-pen.p3d";
+		picture = QUOTE(PATHTOF(equipment\img\atropine.paa));
+		model = QUOTE(PATHTOF(equipment\Atropin-pen.p3d));
 		descriptionShort = $STR_ACE_MAG_ATROPINE_DESC_SHORT;
 		descriptionUse = $STR_ACE_MAG_ATROPINE_DESC_USE;
 		class ItemInfo: InventoryItem_Base_F
@@ -123,8 +123,8 @@ class CfgWeapons {
 		count = 1;
 		type = 16;
 		displayName = $STR_ACE_MAG_EPINEPHRINE_DISPLAY;
-		picture = "\cse\cse_sys_medical\equipment\img\epinephrine.paa";
-		model = "\cse\cse_sys_medical\equipment\Epipen.p3d";
+		picture = QUOTE(PATHTOF(equipment\img\epinephrine.paa));
+		model = QUOTE(PATHTOF(equipment\Epipen.p3d));
 		descriptionShort = $STR_ACE_MAG_EPINEPHRINE_DESC_SHORT;
 		descriptionUse = $STR_ACE_MAG_EPINEPHRINE_DESC_USE;
 		class ItemInfo: InventoryItem_Base_F
@@ -138,7 +138,7 @@ class CfgWeapons {
 		value = 1;
 		count = 1;
 		displayName = $STR_ACE_MAG_PLASMA_IV;
-		picture = "\cse\cse_sys_medical\equipment\img\plasma_iv.paa";
+		picture = QUOTE(PATHTOF(equipment\img\plasma_iv.paa));
 		descriptionShort = $STR_ACE_MAG_PLASMA_IV_DESC_SHORT;
 		descriptionUse = $STR_ACE_MAG_PLASMA_IV_DESC_USE;
 		class ItemInfo: InventoryItem_Base_F
@@ -157,9 +157,9 @@ class CfgWeapons {
 		scope = 2;
 		value = 1;
 		count = 1;
-		model = "\A3\Structures_F_EPA\Items\Medical\BloodBag_F.p3d";
+		model = "\A3\Structures_F_EPA\Items\Medical\BloodBag_F.p3d));
 		displayName = $STR_ACE_MAG_BLOOD_IV;
-		picture = "\cse\cse_sys_medical\equipment\img\bloodbag.paa";
+		picture = QUOTE(PATHTOF(equipment\img\bloodbag.paa));
 		descriptionShort = $STR_ACE_MAG_BLOOD_IV_DESC_SHORT;
 		descriptionUse = $STR_ACE_MAG_BLOOD_IV_DESC_USE;
 		class ItemInfo: InventoryItem_Base_F
@@ -179,7 +179,7 @@ class CfgWeapons {
 		value = 1;
 		count = 1;
 		displayName = $STR_ACE_MAG_SALINE_IV;
-		picture = "\cse\cse_sys_medical\equipment\img\saline_iv.paa";
+		picture = QUOTE(PATHTOF(equipment\img\saline_iv.paa));
 		descriptionShort = $STR_ACE_MAG_SALINE_IV_DESC_SHORT;
 		descriptionUse = $STR_ACE_MAG_SALINE_IV_DESC_USE;
 		class ItemInfo: InventoryItem_Base_F
@@ -200,7 +200,7 @@ class CfgWeapons {
 		count = 1;
 		type = 16;
 		displayName = $STR_ACE_MAG_QUIKCLOT_DISPLAY;
-		picture = "\cse\cse_sys_medical\equipment\img\quickclot.paa";
+		picture = QUOTE(PATHTOF(equipment\img\quickclot.paa));
 		descriptionShort = $STR_ACE_MAG_QUIKCLOT_DESC_SHORT;
 		descriptionUse = $STR_ACE_MAG_QUIKCLOT_DESC_USE;
 		class ItemInfo: InventoryItem_Base_F
@@ -215,7 +215,7 @@ class CfgWeapons {
 		count = 1;
 		type = 16;
 		displayName = $STR_ACE_MAG_NPA_DISPLAY;
-		picture = "\cse\cse_sys_medical\equipment\img\nasopharyngeal_tube.paa";
+		picture = QUOTE(PATHTOF(equipment\img\nasopharyngeal_tube.paa));
 		descriptionUse = $STR_ACE_MAG_NPA_DESC_USE;
 		descriptionShort = $STR_ACE_MAG_NPA_DESC_SHORT;
 		class ItemInfo: InventoryItem_Base_F
@@ -230,7 +230,7 @@ class CfgWeapons {
 		count = 1;
 		type = 16;
 		displayName = $STR_ACE_MAG_OPA_DISPLAY;
-		picture = "\cse\cse_sys_medical\equipment\img\nasopharyngeal_tube.paa";
+		picture = QUOTE(PATHTOF(equipment\img\nasopharyngeal_tube.paa));
 		descriptionShort = $STR_ACE_MAG_OPA_DESC_SHORT;
 		descriptionUse = $STR_ACE_MAG_OPA_DESC_USE;
 		class ItemInfo: InventoryItem_Base_F
@@ -245,8 +245,8 @@ class CfgWeapons {
 		count = 1;
 		type = 16;
 		displayName = $STR_ACE_MAG_LIQUID_SKIN_DISPLAY;
-		picture = "\cse\cse_sys_medical\equipment\img\liquidSkin.paa";
-		model = "\cse\cse_sys_medical\equipment\skinliquid.p3d";
+		picture = QUOTE(PATHTOF(equipment\img\liquidSkin.paa));
+		model = QUOTE(PATHTOF(equipment\skinliquid.p3d));
 		descriptionShort = $STR_ACE_MAG_LIQUID_SKIN_DESC_SHORT;
 		descriptionUse = $STR_ACE_MAG_LIQUID_SKIN_DESC_USE;
 		class ItemInfo: InventoryItem_Base_F
@@ -261,7 +261,7 @@ class CfgWeapons {
 		count = 1;
 		type = 16;
 		displayName = $STR_ACE_MAG_CHEST_SEAL_DISPLAY;
-		picture = "\cse\cse_sys_medical\equipment\img\chestseal.paa";
+		picture = QUOTE(PATHTOF(equipment\img\chestseal.paa));
 		descriptionShort = $STR_ACE_MAG_CHEST_SEAL_DESC_SHORT;
 		descriptionUse = $STR_ACE_MAG_CHEST_SEAL_DESC_USE;
 		class ItemInfo: InventoryItem_Base_F
@@ -276,8 +276,8 @@ class CfgWeapons {
 		count = 1;
 		type = 16;
 		displayName = $STR_ACE_MAG_AID_KIT_DISPLAY;
-		picture = "\cse\cse_sys_medical\equipment\img\personal_aid_kit.paa";
-		model = "\cse\cse_sys_medical\equipment\Personal-aidkits\MTP.p3d";
+		picture = QUOTE(PATHTOF(equipment\img\personal_aid_kit.paa));
+		model = QUOTE(PATHTOF(equipment\Personal-aidkits\MTP.p3d));
 		descriptionShort = $STR_ACE_MAG_AID_KIT_DESC_SHORT;
 		descriptionUse = $STR_ACE_MAG_AID_KIT_DESC_USE;
 		class ItemInfo: InventoryItem_Base_F
@@ -290,8 +290,8 @@ class CfgWeapons {
 	{
 		scope=2;
 		displayName= $STR_ACE_MAG_SURGICALKIT_DISPLAY;
-		model="\cse\cse_sys_medical\equipment\surgical_kit.p3d";
-		picture="\cse\cse_sys_medical\equipment\img\surgical_kit.paa";
+		model = QUOTE(PATHTOF(equipment\surgical_kit.p3d));
+		picture = QUOTE(PATHTOF(equipment\img\surgical_kit.paa));
 		descriptionShort = $STR_ACE_MAG_SURGICALKIT_DESC_SHORT;
 		descriptionUse = $STR_ACE_MAG_SURGICALKIT_DESC_USE;
 		class ItemInfo: InventoryItem_Base_F
@@ -304,8 +304,8 @@ class CfgWeapons {
 	{
 		scope=2;
 		displayName= $STR_ACE_MAG_BODYBAG_DISPLAY;
-		model="\cse\cse_sys_medical\equipment\bodybagItem.p3d";
-		picture="\cse\cse_sys_medical\equipment\img\bodybag.paa";
+		model = QUOTE(PATHTOF(equipment\bodybagItem.p3d));
+		picture = QUOTE(PATHTOF(equipment\img\bodybag.paa));
 		descriptionShort = $STR_ACE_MAG_BODYBAG_DESC_SHORT;
 		descriptionUse = $STR_ACE_MAG_BODYBAG_DESC_USE;
 		class ItemInfo: InventoryItem_Base_F
