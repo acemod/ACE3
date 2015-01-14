ad47b9a5df83256050d2ef5fcc0218aa7f224805
PabstMirror <elephantisaterriblemascot@gmail.com>
2015-01-14 12:24:09 -0600

    fix scriptComponent.hpp

--- addons/noradio/script_component.hpp
+++ addons/noradio/script_component.hpp
@@ -3,12 +3,12 @@
 
 // #define DEBUG_MODE_FULL
 
-#ifdef DEBUG_ENABLED_COMMON
+#ifdef DEBUG_ENABLED_NORADIO
     #define DEBUG_MODE_FULL
 #endif
 
-#ifdef DEBUG_ENABLED_COMMON
-    #define DEBUG_SETTINGS DEBUG_ENABLED_COMMON
+#ifdef DEBUG_SETTINGS_NORADIO
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_NORADIO
 #endif
 
 #include "\z\ace\addons\main\script_macros.hpp"
