e1c08626becb42534b142e5dfe22241de2cecd49
PabstMirror <elephantisaterriblemascot@gmail.com>
2015-01-14 12:28:44 -0600

    Fix Script Comp

--- addons/grenades/script_component.hpp
+++ addons/grenades/script_component.hpp
@@ -3,12 +3,12 @@
 
 // #define DEBUG_MODE_FULL
 
-#ifdef DEBUG_ENABLED_COMMON
+#ifdef DEBUG_ENABLED_GRENADES
     #define DEBUG_MODE_FULL
 #endif
 
-#ifdef DEBUG_ENABLED_COMMON
-    #define DEBUG_SETTINGS DEBUG_ENABLED_COMMON
+#ifdef DEBUG_SETTINGS_GRENADES
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_GRENADES
 #endif
 
 #include "\z\ace\addons\main\script_macros.hpp"
