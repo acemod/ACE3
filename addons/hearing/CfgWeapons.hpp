a78f5085514bda5ea5bc96c4fb420402c945c960
PabstMirror <elephantisaterriblemascot@gmail.com>
2015-01-13 20:07:31 -0600

    compiling, still need to test

--- addons/hearing/CfgWeapons.hpp
+++ addons/hearing/CfgWeapons.hpp
@@ -5,8 +5,8 @@ class CfgWeapons {
     class ACE_EarBuds: ACE_ItemCore {
         displayName = "$STR_ACE_Hearing_Earbuds_Name";
         descriptionShort = "$STR_ACE_Hearing_Earbuds_Description";
-        model = QUOTE(PATHOF(ACE_earplugs.p3d);
-        picture = QUOTE(PATHOF(UI\ACE_earplugs_x_ca.paa);
+        model = QUOTE(PATHOF(ACE_earplugs.p3d));
+        picture = QUOTE(PATHOF(UI\ACE_earplugs_x_ca.paa));
         scope = 2;
         class ItemInfo: InventoryItem_Base_F {
             mass = 1;
