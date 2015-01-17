e61b828314f9831c6b4c6ce94b2ec07abc690471
Nicolás Badano <nicolas.d.badano@gmail.com>
2015-01-17 01:01:56 -0300

    gforces: moved stuff out of config.cpp

--- addons/gforces/config.cpp
+++ addons/gforces/config.cpp
@@ -14,39 +14,6 @@ class CfgPatches {
 
 #include "CfgEventHandlers.hpp"
 
-class CfgWeapons {
-    class ItemCore;
-    class Uniform_Base: ItemCore {
-        AGM_GForceCoef = 1;
-    };
+#include "CfgWeapons.hpp"
 
-    class U_B_PilotCoveralls: Uniform_Base {
-        AGM_GForceCoef = 0.8;
-    };
-    class U_I_pilotCoveralls: Uniform_Base {
-        AGM_GForceCoef = 0.8;
-    };
-    class U_O_PilotCoveralls: Uniform_Base {
-        AGM_GForceCoef = 0.8;
-    };
-};
-
-class CfgVehicles {
-    class Man;
-    class CAManBase: Man {
-        AGM_GForceCoef = 1;
-    };
-
-    class B_Soldier_05_f;
-    class B_Pilot_F: B_Soldier_05_f {
-        AGM_GForceCoef = 0.55;
-    };
-    class I_Soldier_04_F;
-    class I_pilot_F: I_Soldier_04_F {
-        AGM_GForceCoef = 0.55;
-    };
-    class O_helipilot_F;
-    class O_Pilot_F: O_helipilot_F {
-        AGM_GForceCoef = 0.55;
-    };
-};
+#include "CfgVehicles.hpp"
