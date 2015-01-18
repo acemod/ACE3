058aa8d920792caa56b42803730fca1d5c309566
Nicolás Badano <nicolas.d.badano@gmail.com>
2015-01-18 19:38:58 -0300

    interaction: cba keybinds

--- addons/interaction/config.cpp
+++ addons/interaction/config.cpp
@@ -43,38 +43,6 @@ class ACE_Default_Keys {
     control = 1;
     alt = 0;
   };
-  class openDoor {
-    displayName = "$STR_ACE_Interaction_OpenDoor";
-    condition = QUOTE(!GVAR(isOpeningDoor) && {[2] call FUNC(getDoor) select 1 != ''});
-    statement = QUOTE(call ACE_Interaction_fnc_openDoor);
-    conditionUp = QUOTE( GVAR(isOpeningDoor) );
-    statementUp = QUOTE( GVAR(isOpeningDoor) = false;);
-    key = 57;
-    shift = 0;
-    control = 1;
-    alt = 0;
-  };
-  class tapShoulder {
-    displayName = "$STR_ACE_Interaction_TapShoulder";
-    condition = QUOTE([_player, cursorTarget] call FUNC(canTapShoulder));
-    statement = QUOTE([_player, cursorTarget] call FUNC(tapShoulder););
-    key = 20;
-    shift = 1;
-    control = 0;
-    alt = 0;
-  };
-  class modifierKey {
-    displayName = "$STR_ACE_Interaction_ModifierKey";
-    condition = "";
-    statement = QUOTE(ACE_Modifier = 1;);
-    conditionUp = "";
-    statementUp = QUOTE(ACE_Modifier = 0;);
-    exceptions[] = {"ACE_Drag_isNotDragging"};
-    key = 29;
-    shift = 0;
-    control = 0;
-    alt = 0;
-  };
 };
 
 class ACE_Options {
