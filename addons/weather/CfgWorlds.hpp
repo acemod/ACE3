66d6d4b2be7a3595e4b1325eb44ae8218f4f0136
Nicolás Badano <nicolas.d.badano@gmail.com>
2015-01-21 00:18:39 -0300

    weather: add config data for A3 islands

---
+++ addons/weather/CfgWorlds.hpp
@@ -0,0 +1,17 @@
+class CfgWorlds {
+    class CAWorld;
+
+    class Stratis: CAWorld {
+        AGM_TempMeanJan = 7.4;
+        AGM_TempMeanJul = 25.9;
+        AGM_TempAmplitudeJan = 6.4;
+        AGM_TempAmplitudeJul = 9.2;
+    };
+
+    class Altis: CAWorld {
+        AGM_TempMeanJan = 7.4;
+        AGM_TempMeanJul = 25.9;
+        AGM_TempAmplitudeJan = 6.4;
+        AGM_TempAmplitudeJul = 9.2;
+    };
+};
