2c34c166212b9f32068db99d2d20bab8aa7fe06e
Nicolás Badano <nicolas.d.badano@gmail.com>
2015-01-21 02:28:23 -0300

    weather: weird syntax!

--- addons/weather/CfgWorlds.hpp
+++ addons/weather/CfgWorlds.hpp
@@ -3,17 +3,17 @@ class CfgWorlds {
 
     class Stratis: CAWorld {
         // Source: http://www.iten-online.ch/klima/europa/griechenland/limnos.htm
-        ACE_TempDay   = {10, 10, 12, 16, 21, 26, 29, 28, 25, 20, 15, 11};
-        ACE_TempNight = {4, 4, 6, 8, 13, 17, 20, 20, 16, 12, 8, 6};
+        ACE_TempDay[]   = {10, 10, 12, 16, 21, 26, 29, 28, 25, 20, 15, 11};
+        ACE_TempNight[] = {4, 4, 6, 8, 13, 17, 20, 20, 16, 12, 8, 6};
         // Source: http://www.weather-and-climate.com/average-monthly-Humidity-perc,Limnos,Greece
-        ACE_Humidity = {78, 77, 78, 74, 71, 60, 59, 61, 65, 72, 79, 80};
+        ACE_Humidity[]  = {78, 77, 78, 74, 71, 60, 59, 61, 65, 72, 79, 80};
     };
 
     class Altis: CAWorld {
         // Source: http://www.iten-online.ch/klima/europa/griechenland/limnos.htm
-        ACE_TempDay   = {10, 10, 12, 16, 21, 26, 29, 28, 25, 20, 15, 11};
-        ACE_TempNight = {4, 4, 6, 8, 13, 17, 20, 20, 16, 12, 8, 6};
+        ACE_TempDay[]   = {10, 10, 12, 16, 21, 26, 29, 28, 25, 20, 15, 11};
+        ACE_TempNight[] = {4, 4, 6, 8, 13, 17, 20, 20, 16, 12, 8, 6};
         // Source: http://www.weather-and-climate.com/average-monthly-Humidity-perc,Limnos,Greece
-        ACE_Humidity = {78, 77, 78, 74, 71, 60, 59, 61, 65, 72, 79, 80};
+        ACE_Humidity[]  = {78, 77, 78, 74, 71, 60, 59, 61, 65, 72, 79, 80};
     };
 };
