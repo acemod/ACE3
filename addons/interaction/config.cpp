dc55ea517e4876f2b5557f817a2ada8b3c65f0ec
Nicolás Badano <nicolas.d.badano@gmail.com>
2015-01-24 14:02:47 -0300

    interaction: cba keybinds

--- addons/interaction/config.cpp
+++ addons/interaction/config.cpp
@@ -18,33 +18,6 @@ class CfgPatches {
 
 #include <Menu_Config.hpp>
 
-class ACE_Default_Keys {
-  class openInteractionMenuNew {
-    displayName = "$STR_ACE_Interaction_InteractionMenu";
-    condition = "true";
-    statement = QUOTE(call FUNC(onButtonDown));
-    conditionUp = QUOTE(!isNull (findDisplay 1713999) && {profileNamespace getVariable [ARR_2(QUOTE(QGVAR(AutoCloseMenu)), false)]});
-    statementUp = QUOTE(if (GVAR(MenuType) mod 2 == 0) then {call FUNC(onButtonUp)};);
-    exceptions[] = {"ACE_Drag_isNotDragging", "ACE_Medical_canTreat", "ACE_Interaction_isNotEscorting", "ACE_Interaction_isNotSwimming"};
-    key = 219;
-    shift = 0;
-    control = 0;
-    alt = 0;
-  };
-  class openInteractionMenuSelfNew {
-    displayName = "$STR_ACE_Interaction_InteractionMenuSelf";
-    condition = "true";
-    statement = QUOTE(call FUNC(onButtonDownSelf));
-    conditionUp = QUOTE(!isNull (findDisplay 1713999) && {profileNamespace getVariable [ARR_2(QUOTE(QGVAR(AutoCloseMenu)), false)]});
-    statementUp = QUOTE(if (EGVAR(interaction,MenuType) mod 2 == 1) then {call FUNC(onButtonUp)};);
-    exceptions[] = {"ACE_Drag_isNotDragging", "ACE_Medical_canTreat", "ACE_Interaction_isNotEscorting", "ACE_Interaction_isNotSwimming", "ACE_Common_notOnMap"};
-    key = 219;
-    shift = 0;
-    control = 1;
-    alt = 0;
-  };
-};
-
 class ACE_Options {
   class Interaction_FlowMenu {
     displayName = "$STR_ACE_Interaction_FlowMenu";
