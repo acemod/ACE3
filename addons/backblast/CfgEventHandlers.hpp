44aaf3955b2f59fe18c346f784f4b614552af88b
commy2 <commy-2@gmx.de>
2015-02-15 15:48:40 +0100

    some backblast cleanup

--- addons/backblast/CfgEventHandlers.hpp
+++ addons/backblast/CfgEventHandlers.hpp
@@ -7,11 +7,11 @@ class Extended_PreInit_EventHandlers {
 
 class Extended_FiredNear_EventHandlers {
     class CAManBase {
-        class GVAR(LauncherBackblast) {
-            FiredNear = QUOTE( if (local (_this select 0) && {getNumber (configfile >> 'CfgWeapons' >> _this select 3 >> 'ACE_Backblast_Damage') > 0}) then {_this call FUNC(launcherBackblast)} );
+        class ADDON {
+            firedNear = QUOTE(if (local (_this select 0) && {getNumber (configfile >> 'CfgWeapons' >> _this select 3 >> 'ACE_Backblast_Damage') > 0}) then {_this call DFUNC(launcherBackblast)});
         };
-        class GVAR(TankDangerZone) {
-            FiredNear = QUOTE( if (local (_this select 0) && {getNumber (configfile >> 'CfgWeapons' >> _this select 3 >> 'ACE_DangerZone_Damage') > 0}) then {_this call FUNC(tankDangerZone)} );
+        class ADDON {
+            firedNear = QUOTE(if (local (_this select 0) && {getNumber (configfile >> 'CfgWeapons' >> _this select 3 >> 'ACE_DangerZone_Damage') > 0}) then {_this call DFUNC(tankDangerZone)});
         };
     };
 };
--- addons/backblast/config.cpp
+++ addons/backblast/config.cpp
@@ -6,12 +6,11 @@ class CfgPatches {
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {"ace_common"};
-        author[] = {"commy2", "KoffeinFlummi"};
+        author[] = {"commy2","KoffeinFlummi"};
         authorUrl = "https://github.com/commy2/";
         VERSION_CONFIG;
     };
 };
 
 #include "CfgEventHandlers.hpp"
-
 #include "CfgWeapons.hpp"
