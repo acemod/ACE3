025c45dcccd6b81f026e00b5415031c2f5fdea79
commy2 <commy-2@gmx.de>
2015-03-10 17:18:21 +0100

    remove lockbackpacks code, add sound and camshake, ref #178

--- addons/backpacks/config.cpp
+++ addons/backpacks/config.cpp
@@ -5,7 +5,7 @@ class CfgPatches {
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"ace_common","ace_interaction"};
+        requiredAddons[] = {"ace_common"};
         author[] = {"bux","commy2"};
         authorUrl = "https://github.com/commy2/";
         VERSION_CONFIG;
@@ -13,4 +13,3 @@ class CfgPatches {
 };
 
 #include "CfgEventHandlers.hpp"
-#include "CfgVehicles.hpp"
