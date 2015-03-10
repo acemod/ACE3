b7ed697e1c6bf5394acff132c486b647bac2d9e5
commy2 <commy-2@gmx.de>
2015-03-10 18:05:56 +0100

    add repair PBO

---
+++ addons/repair/script_component.hpp
@@ -0,0 +1,12 @@
+#define COMPONENT repair
+#include "\z\ace\addons\main\script_mod.hpp"
+
+#ifdef DEBUG_ENABLED_REPAIR
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_REPAIR
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_REPAIR
+#endif
+
+#include "\z\ace\addons\main\script_macros.hpp"
