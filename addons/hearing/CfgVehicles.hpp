bed312dcee07b028f94b176455e0e3cd90ce9ead
PabstMirror <elephantisaterriblemascot@gmail.com>
2015-03-11 17:36:40 -0500

    Use MACRO_ADDITEM from main instead of redefining

--- addons/hearing/CfgVehicles.hpp
+++ addons/hearing/CfgVehicles.hpp
@@ -1,9 +1,3 @@
-
-#define MACRO_ADDITEM(ITEM,COUNT) class _xx_##ITEM { \
-    name = #ITEM; \
-    count = COUNT; \
-};
-
 class CfgVehicles {
     class Man;
     class CAManBase: Man {
@@ -41,61 +35,61 @@ class CfgVehicles {
 
     class Box_NATO_Support_F: NATO_Box_Base {
         class TransportItems {
-            MACRO_ADDITEM(ACE_EarBuds,12)
+            MACRO_ADDITEM(ACE_EarBuds,12);
         };
     };
 
     class B_supplyCrate_F: ReammoBox_F {
         class TransportItems {
-            MACRO_ADDITEM(ACE_EarBuds,12)
+            MACRO_ADDITEM(ACE_EarBuds,12);
         };
     };
 
     class Box_East_Support_F: EAST_Box_Base {
         class TransportItems {
-            MACRO_ADDITEM(ACE_EarBuds,12)
+            MACRO_ADDITEM(ACE_EarBuds,12);
         };
     };
 
     class O_supplyCrate_F: B_supplyCrate_F {
         class TransportItems {
-            MACRO_ADDITEM(ACE_EarBuds,12)
+            MACRO_ADDITEM(ACE_EarBuds,12);
         };
     };
 
     class Box_IND_Support_F: IND_Box_Base {
         class TransportItems {
-            MACRO_ADDITEM(ACE_EarBuds,12)
+            MACRO_ADDITEM(ACE_EarBuds,12);
         };
     };
 
     class Box_FIA_Support_F: FIA_Box_Base_F {
         class TransportItems {
-            MACRO_ADDITEM(ACE_EarBuds,12)
+            MACRO_ADDITEM(ACE_EarBuds,12);
         };
     };
 
     class I_supplyCrate_F: B_supplyCrate_F {
         class TransportItems {
-            MACRO_ADDITEM(ACE_EarBuds,12)
+            MACRO_ADDITEM(ACE_EarBuds,12);
         };
     };
 
     class IG_supplyCrate_F: ReammoBox_F {
         class TransportItems {
-            MACRO_ADDITEM(ACE_EarBuds,12)
+            MACRO_ADDITEM(ACE_EarBuds,12);
         };
     };
 
     class C_supplyCrate_F: ReammoBox_F {
         class TransportItems {
-            MACRO_ADDITEM(ACE_EarBuds,12)
+            MACRO_ADDITEM(ACE_EarBuds,12);
         };
     };
 
     class ACE_Box_Misc: Box_NATO_Support_F {
         class TransportItems {
-            MACRO_ADDITEM(ACE_EarBuds,12)
+            MACRO_ADDITEM(ACE_EarBuds,12);
         };
     };
 };
