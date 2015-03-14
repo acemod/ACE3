58212f5ce0a6329b9082ad0af6c30644d97f5fd5
commy2 <commy-2@gmx.de>
2015-03-14 12:05:43 +0100

    add PBO to drag objects

---
+++ addons/dragging/config.cpp
@@ -0,0 +1,15 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"ace_common","ace_interact_menu"};
+        author[] = {"https://github.com/commy2/"};
+        authorUrl = "";
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgEventHandlers.hpp"
---
+++ addons/dragging/script_component.hpp
@@ -0,0 +1,12 @@
+#define COMPONENT dragging
+#include "\z\ace\addons\main\script_mod.hpp"
+
+#ifdef DEBUG_ENABLED_DRAGGING
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_ENABLED_DRAGGING
+    #define DEBUG_SETTINGS DEBUG_ENABLED_DRAGGING
+#endif
+
+#include "\z\ace\addons\main\script_macros.hpp"
