34df28d93892a86b56753e220ea87e7562d928a6
commy2 <commy-2@gmx.de>
2015-03-24 20:28:44 +0100

    cleaning up common

--- addons/common/CfgActions.hpp
+++ addons/common/CfgActions.hpp
@@ -1,5 +1,6 @@
 
 class CfgActions {
+    // fixes pick up action on mine detectors, because all ACE items are of that simulationClass and BI doesn't seem to care
     class None;
     class TakeWeapon: None {
         show = 0;
