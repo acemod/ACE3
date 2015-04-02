b88d1b4cac146233c822c581e7949339ca1c966d
jaynus <walter.pearce@idi-systems.com>
2015-04-02 10:45:52 -0700

    Begin time to live changes for extended range rounds.

---
+++ addons/ballistics/CfgAmmo.hpp
@@ -0,0 +1,11 @@
+class CfgAmmo {
+    class BulletBase;
+    
+    class B_20mm : BulletBase {
+        timeToLive = 20;
+    };
+
+    class B_25mm: BulletBase {
+        timeToLive = 20;
+    };
+};
