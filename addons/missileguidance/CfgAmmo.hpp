f9735057461c78b5a99cd43b3d93a6aeb9e0761f
jaynus <walter.pearce@idi-systems.com>
2015-04-07 08:53:47 -0700

    DAGR and Hellfire modified config values.

--- addons/missileguidance/CfgAmmo.hpp
+++ addons/missileguidance/CfgAmmo.hpp
@@ -7,17 +7,33 @@ class CfgAmmo {
         airLock = 0;
         manualControl = 0;
         timeToLive = 120;
+        
+        hit = 150;
+		indirectHit = 40;
+		indirectHitRange = 2.5;
+		explosive = 0.1; // KE penetrators
+		inittime = 0.05;
+		thrust = 1900;
+		thrusttime = 1;
+		maxspeed = 1030;
+		maneuvrability = 8;
+		sideairfriction = 0.065;
     };
     
     class ACE_Hydra70_DAGR : M_PG_AT {
-        displayName = "ACE Hydra-70 DAGR Missile";
-		displayNameShort = "$STR_A3_CfgMagazines_24Rnd_PG_missiles_dns";
-		descriptionShort = "$STR_A3_CfgMagazines_24Rnd_PG_missiles1";
+        displayName = "ACE_Hydra70_DAGR";
+		displayNameShort = "ACE_Hydra70_DAGR";
+		descriptionShort = "ACE_Hydra70_DAGR";
     };
     
     class ACE_Hellfire_AGM114K : ACE_Hydra70_DAGR {
-        displayName = "$STR_A3_CfgMagazines_24Rnd_PG_missiles0";
-		displayNameShort = "$STR_A3_CfgMagazines_24Rnd_PG_missiles_dns";
-		descriptionShort = "$STR_A3_CfgMagazines_24Rnd_PG_missiles1";
+        displayName = "ACE_Hellfire_AGM114K";
+		displayNameShort = "ACE_Hellfire_AGM114K";
+		descriptionShort = "ACE_Hellfire_AGM114K";
+        
+        hit = 1400;
+        indirectHit = 71;
+		indirectHitRange = 4.5;
+		effectsMissile = "missile2";
     };
 };
