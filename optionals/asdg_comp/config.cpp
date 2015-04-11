876cec35df5b2f0a0859f4a460f2ffbc1ef09a9c
OnkelDisMaster <arrows96@gmx.de>
2015-04-11 21:20:59 +0200

    Update config.cpp

--- optionals/asdg_comp/config.cpp
+++ optionals/asdg_comp/config.cpp
@@ -31,7 +31,7 @@ class asdg_OpticRail1913: asdg_OpticRail
     {
 	ACE_optic_Hamr_2D = 1;
 	ACE_optic_Hamr_PIP = 1;
-        ACE_optic_Arco_2D = 1;
+	ACE_optic_Arco_2D = 1;
 	ACE_optic_Arco_PIP = 1;
 	ACE_optic_MRCO_2D = 1;
 	ACE_optic_SOS_2D = 1;
