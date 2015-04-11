5d90d6fce0010a913f79990f763aae63b72c7a84
MarcBook <marc.o.negele@gmail.com>
2015-04-11 21:39:56 +0200

    Code 2

--- addons/backpacks/CfgVehicles.hpp
+++ addons/backpacks/CfgVehicles.hpp
@@ -1,28 +1,27 @@
-
 class CfgVehicles {
-    class Man;
-    class CAManBase: Man {
-        class ACE_SelfActions {
-            class ACE_Equipment {
+	class Man;
+	class CAManBase: Man {
+		class ACE_SelfActions {
+			class ACE_Equipment {
 				class ACE_BackpackToFront {
 					displayName = "$STR_ACE_Backpack_BackpackToFront";
-                    condition = "(backpack _player != '') and (vehicle _player == _player) and (!(_player getVariable ['ACE_Backpack', [objNull, false]] select 1))";
-                    exceptions[] = {"isNotInside"};
-                    statement = QUOTE([_player] call FUNC(backpackToFront));
-                    showDisabled = 0;
-                    priority = 2.5;
-                    icon = "\A3\ui_f\data\map\VehicleIcons\iconbackpack_ca.paa";
-                    hotkey = "B";
+					condition = "(backpack _player != '') and (vehicle _player == _player) and (!(_player getVariable ['ACE_Backpack', [objNull, false]] select 1))";
+					exceptions[] = {"isNotInside"};
+					statement = QUOTE([_player] call FUNC(backpackToFront));
+					showDisabled = 0;
+					priority = 2.5;
+					icon = "\A3\ui_f\data\map\VehicleIcons\iconbackpack_ca.paa";
+					hotkey = "B";
 				};
 				class ACE_BackpackToBack {
 					displayName = "$STR_ACE_Backpack_BackpackToBack";
-                    condition = "(backpack _player == '') and (vehicle _player == _player) and (_player getVariable ['ACE_Backpack', [objNull, false]] select 1)";
-                    exceptions[] = {"isNotInside"};
-                    statement = QUOTE([_player] call FUNC(backpackToBack));
-                    showDisabled = 0;
-                    priority = 2.5;
-                    icon = "\A3\ui_f\data\map\VehicleIcons\iconbackpack_ca.paa";
-                    hotkey = "B";
+					condition = "(backpack _player == '') and (vehicle _player == _player) and (_player getVariable ['ACE_Backpack', [objNull, false]] select 1)";
+					exceptions[] = {"isNotInside"};
+					statement = QUOTE([_player] call FUNC(backpackToBack));
+					showDisabled = 0;
+					priority = 2.5;
+					icon = "\A3\ui_f\data\map\VehicleIcons\iconbackpack_ca.paa";
+					hotkey = "B";
 				};
 			};
 		};
