aa53a1815c7a27ef1ce1a67a6cfebbfa4fa9c102
jaynus <jaynus@gmail.com>
2015-04-11 10:58:27 -0700

    scaling.

--- addons/javelin/RscInGameUI.hpp
+++ addons/javelin/RscInGameUI.hpp
@@ -28,10 +28,10 @@ class RscInGameUI {
         
         class ACE_TargetingConstrains: RscControlsGroup {
             idc = 699100;
-            x = "SafezoneX-SafezoneW";
-            y = "SafezoneY-SafezoneH";
-            w = "SafezoneW";
-            h = "SafezoneH";
+            x = "SafezoneX";
+            y = "SafezoneY";
+            w = "SafezoneW-SafezoneX";
+            h = "SafezoneH-SafezoneY";
             enabled = 0;
             class VScrollbar {
                 autoScrollSpeed = -1;
