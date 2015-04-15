b307b4bc4bc662929df19b1e815f0c0265db62a7
jaynus <jaynus@gmail.com>
2015-04-14 18:38:52 -0700

    Fixed: Flummi missed 2 isClientSettable renames.

--- addons/missileguidance/ACE_Settings.hpp
+++ addons/missileguidance/ACE_Settings.hpp
@@ -2,14 +2,14 @@ class ACE_Settings {
     class GVAR(enabled) {
         value = 1;
         typeName = "BOOL";
-        isClientSetable = 1;
+        isClientSettable = 1;
         displayName = "$STR_ACE_MissileGuidance";
         description = "$STR_ACE_MissileGuidance_Desc";
     };
     class GVAR(enabledForAI) {
         value = 1;
         typeName = "BOOL";
-        isClientSetable = 1;
+        isClientSettable = 1;
         displayName = "$STR_ACE_MissileGuidance_AI";
         description = "$STR_ACE_MissileGuidance_AI_Desc";
     };
