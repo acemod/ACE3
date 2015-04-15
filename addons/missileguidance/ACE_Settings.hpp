ad95105508cc712beb256d32886c8fc3f2a9eeea
jaynus <jaynus@gmail.com>
2015-04-14 18:25:02 -0700

    Added and enabled AMG for AI.

--- addons/missileguidance/ACE_Settings.hpp
+++ addons/missileguidance/ACE_Settings.hpp
@@ -6,4 +6,11 @@ class ACE_Settings {
         displayName = "$STR_ACE_MissileGuidance";
         description = "$STR_ACE_MissileGuidance_Desc";
     };
+    class GVAR(enabledForAI) {
+        value = 1;
+        typeName = "BOOL";
+        isClientSetable = 1;
+        displayName = "$STR_ACE_MissileGuidance_AI";
+        description = "$STR_ACE_MissileGuidance_AI_Desc";
+    };
 };
