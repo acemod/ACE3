852b4df61df411717f4fbfe057f90af7a31b2398
jaynus <jaynus@gmail.com>
2015-04-18 11:16:18 -0700

    Settings changes. createVehicleLocal derp.

--- addons/medical/ACE_Settings.hpp
+++ addons/medical/ACE_Settings.hpp
@@ -73,11 +73,18 @@ class ACE_Settings {
     };
     class GVAR(allowLitterCreation) {
         typeName = "BOOL";
-        value = true;
+        value = 1;
+    };
+    class GVAR(litterSimulationDetail) {
+        displayName = "$STR_ACE_Medical_litterSimulationDetail";
+        description = "$STR_ACE_Medical_litterSimulationDetail_Desc";
+        typeName = "SCALAR";
+        value = 500;
+        isClientSettable = 1;
     };
     class GVAR(litterCleanUpDelay) {
         typeName = "SCALAR";
-        value = 1800;
+        value = -1;
     };
     class GVAR(medicSetting_PAK) {
         typeName = "SCALAR";
@@ -122,14 +129,6 @@ class ACE_Settings {
         typeName = "SCALAR";
         value = 0;
         values[] = {"$STR_ACE_Medical_painEffect_Flash", "$STR_ACE_Medical_painEffect_Chroma"};
-        isClientSettable = 1;
-    };
-    
-    class GVAR(litterSimulationDetail) {
-        displayName = "$STR_ACE_Medical_litterSimulationDetail";
-        description = "$STR_ACE_Medical_litterSimulationDetail_Desc";
-        typeName = "SCALAR";
-        value = 500;
         isClientSettable = 1;
     };
 };
--- addons/medical/functions/fnc_handleCreateLitter.sqf
+++ addons/medical/functions/fnc_handleCreateLitter.sqf
@@ -2,15 +2,13 @@
 PARAMS_3(_litterClass,_position,_direction);
 private["_litterObject"];
 
-
-
-if (isnil QGVAR(allCreatedLitter)) then {
+if (isNil QGVAR(allCreatedLitter)) then {
     GVAR(allCreatedLitter) = [];
     GVAR(litterPFHRunning) = false;
 };
 
-if((count GVAR(allCreatedLitter)) <= GVAR(litterSimulationDetail) )then {
-    _litterObject = createVehicleLocal [_litterClass, _position, [], 0, "NONE"];
+if((count GVAR(allCreatedLitter)) <= GVAR(litterSimulationDetail) ) then {
+    _litterObject = _litterClass createVehicleLocal _position;
     _litterObject setDir _direction;
 } else {
     // @TODO: We hit max litter items, remove a few of them to work with what we have.
