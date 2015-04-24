9078eed6bbad7c9623bd2207731610687e78f757
ulteq <ulteq@web.de>
2015-04-24 17:15:28 +0200

    Added optional hlcmods_m14 compatibility pbo

---
+++ optionals/hlcmods_m14_comp/CfgWeapons.hpp
@@ -0,0 +1,15 @@
+
+class CfgWeapons
+{
+	class Rifle_Base_F;
+	class hlc_M14_base: Rifle_Base_F
+	{
+		ACE_barrelTwist=12;
+		ACE_barrelLength=22;
+	};
+	class hlc_rifle_m14sopmod: hlc_rifle_M14
+	{
+		ACE_barrelTwist=12;
+		ACE_barrelLength=18;
+    };
+};
