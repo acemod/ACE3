fce91279b575c09251e739a133feae95bb175ea1
bux578 <github@jonathandavid.de>
2015-04-29 22:04:17 +0200

    add ace3 and banana insignia
    `[this,"ACE_insignia_logo"] call BIS_fnc_setUnitInsignia;`
    and
    `[this,"ACE_insignia_banana"] call BIS_fnc_setUnitInsignia;`

--- addons/common/CfgUnitInsignia.hpp
+++ addons/common/CfgUnitInsignia.hpp
@@ -1,9 +1,15 @@
 
 class CfgUnitInsignia {
-    class ACE_insignia {
-        displayName = "ACE 3";
-        author = "kripto202";
-        texture = PATHTOF(data\ace3logo_ca.paa);
+    class ACE_insignia_logo {
+        displayName = "ACE3";
+        author = "$STR_ACE_Common_ACETeam";
+        texture = PATHTOF(data\Insignia_ace3logo_ca.paa);
+        textureVehicle = "";
+    };
+    class ACE_insignia_banana {
+        displayName = "ABE3";
+        author = "$STR_ACE_Common_ACETeam";
+        texture = PATHTOF(data\insignia_banana_ca.paa);
         textureVehicle = "";
     };
 };
