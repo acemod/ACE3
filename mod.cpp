d68e744fe2c63d039acd144e81c4da862e0b7b1d
ViperMaul <vipermaul@gmail.com>
2015-04-30 14:41:45 -0700

    better grammar

--- mod.cpp
+++ mod.cpp
@@ -7,6 +7,6 @@ logo = "logo_ace3_ca.paa";
 logoOver = "logo_ace3_ca.paa";
 tooltip = "ACE3";
 tooltipOwned = "ACE3 Owned";
-overview = "Advanced Combat Environment 3, or ACE3 is a joint effort by the teams behind ACE2, AGM and CSE to improve the realism and authenticity of Arma 3.";
+overview = "Advanced Combat Environment 3, also known as ACE3, is a joint effort by the teams behind ACE2, AGM and CSE to improve the realism and authenticity of Arma 3.";
 author = "ACE3 Team";
 overviewPicture = "logo_ace3_ca.paa";
