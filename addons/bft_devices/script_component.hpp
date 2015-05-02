e305606a34078bb74616abb5b9299cea6df49043
Nils Lichtenfeld <nils.lichtenfeld@gmx.net>
2015-05-02 18:30:57 +0200

    Initial PBO setup for bft_devices component

---
+++ addons/bft_devices/script_component.hpp
@@ -0,0 +1,12 @@
+#define COMPONENT bft_devices
+#include "\z\ace\addons\main\script_mod.hpp"
+
+#ifdef DEBUG_ENABLED_BFT_DEVICES
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_BLANK
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_BFT_DEVICES
+#endif
+
+#include "\z\ace\addons\main\script_macros.hpp"
