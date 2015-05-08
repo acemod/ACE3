5b5ea907d23fef86a230411286e52f202d6310ef
jaynus <jaynus@gmail.com>
2015-05-07 17:02:06 -0700

    Custom fonts for ACE.

---
+++ addons/fonts/config.cpp
@@ -0,0 +1,13 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+	class ADDON {
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = REQUIRED_VERSION;
+		requiredAddons[] = { "ace_common" };
+		version = VERSION;
+	};
+};
+
+#include "CfgFontFamilies.hpp"
---
+++ addons/fonts/script_component.hpp
@@ -0,0 +1,12 @@
+#define COMPONENT fonts
+#include "\idi\clients\acre\Addons\main\script_mod.hpp"
+
+#ifdef DEBUG_ENABLED_FONTS
+	#define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_ENABLED_FONTS
+	#define DEBUG_SETTINGS DEBUG_SETTINGS_FONTS
+#endif
+
+#include "\z\ace\addons\main\script_macros.hpp"
