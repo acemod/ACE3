7201a23d41f23dc35e4e9f936969d73d5c75e659
SAM <simon@agius-muscat.net>
2015-05-09 10:44:01 +0200

    Adjusted post init, removed redundant settings

--- addons/viewdistance/ACE_Settings.hpp
+++ addons/viewdistance/ACE_Settings.hpp
@@ -1,18 +1,5 @@
 class ACE_Settings {
-    class GVAR(changeAllowed) {
-        typeName = "BOOL";
-        value = 1;
-        displayName = "Allow View Distance Changing";
-        description = "Allows clients to be able to change their view distance";
-    };
-    class GVAR(viewDistanceLimit) {
-        typeName = "SCALAR";
-        value = 11; // setting the highest number in the array below means no limit.
-        values[] = {0,1,2,3,4,5,6,7,8,9,10,11}; // correspond to the INDEX values
-        displayName = "Change View Distance Limit";
-        description = "Sets the top limit for all clients";
-    };
-    class GVAR(newViewDistance) {
+    class GVAR(viewDistance) {
         typeName = "SCALAR";
         isClientSettable = 1;
         value = 0; // not sure what to set this to.
--- addons/viewdistance/XEH_postInit.sqf
+++ addons/viewdistance/XEH_postInit.sqf
@@ -1,7 +1,3 @@
 #include "script_component.hpp"
 
-if (!GVAR(changeAllowed)) exitWith {
-    // if viewdistance module is disabled from config, exit here.
-    diag_log format["[ACE]: View Distance is disabled from the config.cpp in the pbo"];
-};  
-[] call FUNC(initViewDistance);
+[] call FUNC(init);
