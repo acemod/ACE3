b2371b1278ddd1b01f03f851be86defba0b79e42
SAM <simon@agius-muscat.net>
2015-05-14 22:21:55 +0200

    Fixed missing nametags config

---
+++ addons/nametags/config.cpp
@@ -0,0 +1,18 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"ace_interaction"};
+        author[] = { "commy2", "esteldunedain" };
+        authorUrl = "https://github.com/commy2/";
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgEventHandlers.hpp"
+#include "CfgVehicles.hpp"
+#include "ACE_Settings.hpp"
+#include <RscTitles.hpp>
