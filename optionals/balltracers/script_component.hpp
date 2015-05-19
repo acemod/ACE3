60e3d8ff6abecdce6704a7d0dd56bfcd783af064
ulteq <ulteq@web.de>
2015-05-19 16:55:55 +0200

    Renamed tracers into balltracers

---
+++ optionals/balltracers/script_component.hpp
@@ -0,0 +1,12 @@
+#define COMPONENT balltracers
+#include "\z\ace\addons\main\script_mod.hpp"
+
+#ifdef DEBUG_ENABLED_SYS_BALLTRACERS
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_SYS_BALLTRACERS
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_SYS_BALLTRACERS
+#endif
+
+#include "\z\ace\addons\main\script_macros.hpp"
