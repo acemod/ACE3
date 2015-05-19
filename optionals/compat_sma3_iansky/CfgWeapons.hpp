458daad5bb07f828c4965e55a9d450853afb3c7f
ulteq <ulteq@web.de>
2015-05-19 20:13:31 +0200

    Elevation and windage can now have different adjustment increments: * ACE_ScopeAdjust_VerticalIncrement * ACE_ScopeAdjust_HorizontalIncrement

--- optionals/compat_sma3_iansky/CfgWeapons.hpp
+++ optionals/compat_sma3_iansky/CfgWeapons.hpp
@@ -6,7 +6,8 @@ class CfgWeapons {
     class iansky_nfbeast : ItemCore {
         ACE_ScopeAdjust_Vertical[] = { -0.9, 34 };
         ACE_ScopeAdjust_Horizontal[] = { -11, 11 };
-        ACE_ScopeAdjust_Increment = 0.1; // Should be 0.2 mrad for elevation
+        ACE_ScopeAdjust_VerticalIncrement = 0.2;
+        ACE_ScopeAdjust_HorizontalIncrement = 0.1;
         class ItemInfo : InventoryOpticsItem_Base_F {
             class OpticsModes {
                 class NF25_MILDOT {
