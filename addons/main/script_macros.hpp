a99c02ff02410c50e719ee2a7c8c563de019efa1
jonpas <jonpas33@gmail.com>
2015-05-27 22:02:03 +0200

    Added ESTRING and ECSTRING for external module references

--- addons/main/script_macros.hpp
+++ addons/main/script_macros.hpp
@@ -23,7 +23,9 @@
 #define PATHTOEF(var1,var2) PATHTOF_SYS(PREFIX,var1,var2)
 
 #define STRING(var1) QUOTE(TRIPLES(STR,ADDON,var1))
+#define ESTRING(var1,var2) QUOTE(TRIPLES(STR,DOUBLES(PREFIX,var1),var2))
 #define CSTRING(var1) QUOTE(TRIPLES($STR,ADDON,var1))
+#define ECSTRING(var1,var2) QUOTE(TRIPLES($STR,DOUBLES(PREFIX,var1),var2))
 
 #define GETVAR_SYS(var1,var2) getVariable [ARR_2(QUOTE(var1),var2)]
 #define SETVAR_SYS(var1,var2) setVariable [ARR_2(QUOTE(var1),var2)]
