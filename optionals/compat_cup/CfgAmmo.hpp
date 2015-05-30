6f1c6ae8c1df4c2a8829b5468200c84e05418de1
ulteq <ulteq@web.de>
2015-05-30 20:33:32 +0200

    Added missing ammo class

--- optionals/compat_cup/CfgAmmo.hpp
+++ optionals/compat_cup/CfgAmmo.hpp
@@ -94,6 +94,19 @@ class CfgAmmo
         ACE_muzzleVelocities[]={650, 716, 750};
         ACE_barrelLengths[]={254.0, 414.02, 508.0};
 	};
+    class B_762x39mm_KLT: BulletBase
+    {
+        ACE_caliber=7.823;
+        ACE_bulletLength=28.956;
+        ACE_bulletMass=7.5816;
+        ACE_ammoTempMuzzleVelocityShifts[]={-26.55, -25.47, -22.85, -20.12, -16.98, -12.80, -7.64, -1.53, 5.96, 15.17, 26.19};
+        ACE_ballisticCoefficients[]={0.275};
+        ACE_velocityBoundaries[]={};
+        ACE_standardAtmosphere="ICAO";
+        ACE_dragModel=1;
+        ACE_muzzleVelocities[]={650, 716, 750};
+        ACE_barrelLengths[]={254.0, 414.02, 508.0};
+    };
     class CUP_B_9x18_Ball: BulletBase
     {
         ACE_caliber=9.271;
