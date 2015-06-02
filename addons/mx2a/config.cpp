f1dbe0fffc0f7a5b836e48fb4a56626b50ba7730
ulteq <ulteq@web.de>
2015-06-02 12:58:47 +0200

    Ported the ACE2 MX-2A thermal imaging device

---
+++ addons/mx2a/config.cpp
@@ -0,0 +1,15 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        units[] = {"ACE_Item_MX2A"};
+        weapons[] = {"ACE_MX2A"};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"ace_main"};
+        author[] = {"Spooner", "tcp"};
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgVehicles.hpp"
+#include "CfgWeapons.hpp"
