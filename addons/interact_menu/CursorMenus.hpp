18940d0787aaef1aea63579f9dfebf6ff05efc14
SzwedzikPL <szwedzikpl@gmail.com>
2015-06-03 06:04:47 +0200

    Interaction menu background options

--- addons/interact_menu/CursorMenus.hpp
+++ addons/interact_menu/CursorMenus.hpp
@@ -6,3 +6,41 @@ class GVAR(cursorMenu) {
     onLoad = QUOTE(uiNamespace setVariable [ARR_2(QUOTE(QGVAR(dlgCursorMenu)),_this select 0)]; uiNamespace setVariable [ARR_2(QUOTE(QGVAR(cursorMenuOpened)),true)]);
     onUnload = QUOTE(uiNamespace setVariable [ARR_2(QUOTE(QGVAR(cursorMenuOpened)),false)]);
 };
+class RscTitles {
+    class RscPicture;
+    class GVAR(menuBackground) {
+        idd = -1;
+        onLoad = QUOTE(uiNamespace setVariable [ARR_2(QUOTE(QGVAR(menuBackground)),_this select 0)]);
+        onUnload = QUOTE(uiNamespace setVariable [ARR_2(QUOTE(QGVAR(menuBackground)),displayNull)]);
+        fadeIn = 0.25;
+        fadeOut = 0.25;
+        movingEnable = false;
+        duration = 10e10;
+        name = QGVAR(menuBackground);
+        class controls {
+            class GVAR(menuBackground)_picutre: RscPicture {
+                idc = -1;
+            };
+        };
+        class controlsBackground {
+            class background {
+                idc = -1;
+                moving = 0;
+                font = "TahomaB";
+                text = "";
+                sizeEx = 0;
+                lineSpacing = 0;
+                access = 0;
+                type = 0;
+                style = 0;
+                size = 1;
+                colorBackground[] = {0, 0, 0, 0.65};
+                colorText[] = {0, 0, 0, 0};
+                x = "safezoneX";
+                y = "safezoneY";
+                w = "safezoneW";
+                h = "safezoneH";
+            };
+        };
+    };
+};
