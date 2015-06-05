04ae03695bf7aecddc4f62ca4b08b81f4d25c8d7
ulteq <ulteq@web.de>
2015-06-05 17:03:09 +0200

    Reverted dragging init.

--- addons/spottingscope/CfgEventHandlers.hpp
+++ addons/spottingscope/CfgEventHandlers.hpp
@@ -9,3 +9,11 @@ class Extended_PostInit_EventHandlers {
         init = QUOTE( call COMPILE_FILE(XEH_postInit) );
     };
 };
+
+class Extended_Init_EventHandlers {
+    class ACE_SpottingScopeObject {
+        class ADDON {
+            init = QUOTE(_this call DEFUNC(dragging,initObject));
+        };
+    };
+};
