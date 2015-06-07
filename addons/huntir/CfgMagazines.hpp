9818f65545b89ebac23e7ea66f404c6d635fb862
ulteq <ulteq@web.de>
2015-06-07 11:58:34 +0200

    Improved HuntIR descriptions

--- addons/huntir/CfgMagazines.hpp
+++ addons/huntir/CfgMagazines.hpp
@@ -3,7 +3,7 @@ class CfgMagazines {
     class 1Rnd_HE_Grenade_shell;
     class ACE_HuntIR_M203: 1Rnd_HE_Grenade_shell {
         displayName = $STR_ACE_HUNTIR_ROUND;
-        displayNameShort = "";
+        displayNameShort = "HuntIR";
         descriptionShort = "";
         picture = PATHTOF(UI\m_m1070_ca.paa);
         ammo = "F_HuntIR";
