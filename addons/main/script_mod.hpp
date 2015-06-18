394bcafc4ac4444897b0ca73cffcbd2fc2a79ffe
ViperMaul <vipermaul@gmail.com>
2015-06-18 15:49:27 -0700

    Set version to 3.1.2

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -5,8 +5,8 @@
 
 #define MAJOR 3
 #define MINOR 1
-#define PATCHLVL 1
-#define BUILD 1
+#define PATCHLVL 2
+#define BUILD 0
 
 #define VERSION MAJOR.MINOR.PATCHLVL.BUILD
 #define VERSION_AR MAJOR,MINOR,PATCHLVL,BUILD
