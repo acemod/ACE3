150f888d854c6cd8292fbc8045190ff204645129
BaerMitUmlaut <florian.foss@gmail.com>
2015-07-07 17:22:36 +0200

    Removed dummy model for now

--- addons/tagging/CfgWeapons.hpp
+++ addons/tagging/CfgWeapons.hpp
@@ -5,7 +5,6 @@ class CfgWeapons {
     class ACE_Spraypaint : ACE_ItemCore {
         displayname = CSTRING(spraypaint);
         descriptionshort = CSTRING(descSpraypaint);
-        model = "\A3\Structures_F_Mark\Items\Sport\AirHorn_01_F.p3d";
         picture = QUOTE(PATHTOF(UI\itemSpraypaint.paa));
         scope = 2;
         class ItemInfo: InventoryItem_Base_F {
