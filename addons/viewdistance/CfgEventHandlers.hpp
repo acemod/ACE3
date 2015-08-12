47fbe9bca509a276deed849ade99874676e45ca2
jonpas <jonpas33@gmail.com>
2015-08-12 20:18:23 +0200

    Changed postInit to clientInit

--- addons/viewdistance/CfgEventHandlers.hpp
+++ addons/viewdistance/CfgEventHandlers.hpp
@@ -6,6 +6,6 @@ class Extended_PreInit_EventHandlers {
 
 class Extended_PostInit_EventHandlers {
     class ADDON {
-        clientInit = QUOTE(call COMPILE_FILE(XEH_postInit));
+        clientInit = QUOTE(call COMPILE_FILE(XEH_clientInit));
     };
 };
