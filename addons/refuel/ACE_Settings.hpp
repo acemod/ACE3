a576ed82eeb38ba525b18f91132385d4974b6863
IngoKauffmann <github@ingokauffmann.de>
2015-08-15 19:26:26 +0200

    Fixed some macro stuff

--- addons/refuel/ACE_Settings.hpp
+++ addons/refuel/ACE_Settings.hpp
@@ -1,9 +1,9 @@
 class ACE_Settings {
     class GVAR(rate) {
-        displayName = LSTRING(RefuelSettings_speed_DisplayName);
-        description = LSTRING(RefuelSettings_speed_Description);
+        displayName = CSTRING(RefuelSettings_speed_DisplayName);
+        description = CSTRING(RefuelSettings_speed_Description);
         value = 1;
         typeName = "SCALAR";
-        values[] = {LSTRING(RefuelSettings_basic), LSTRING(RefuelSettings_advanced)};
+        values[] = {CSTRING(RefuelSettings_basic), CSTRING(RefuelSettings_advanced)};
     };
 };
--- addons/refuel/script_component.hpp
+++ addons/refuel/script_component.hpp
@@ -1,4 +1,12 @@
 #define COMPONENT refuel
 #include "\z\ace\addons\main\script_mod.hpp"
 
+#ifdef DEBUG_ENABLED_REFUEL
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_ENABLED_REFUEL
+    #define DEBUG_SETTINGS DEBUG_ENABLED_REFUEL
+#endif
+
 #include "\z\ace\addons\main\script_macros.hpp"
