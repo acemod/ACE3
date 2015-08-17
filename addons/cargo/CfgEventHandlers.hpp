1725e14442518ec9e553fdd72388d29d8626ba1d
SzwedzikPL <szwedzikpl@gmail.com>
2015-08-17 15:14:57 +0200

    init all vehicles

--- addons/cargo/CfgEventHandlers.hpp
+++ addons/cargo/CfgEventHandlers.hpp
@@ -11,69 +11,9 @@ class Extended_Killed_EventHandlers {
 };
 
 class Extended_Init_EventHandlers {
-    class StaticWeapon {
-        class ADDON {
-            init = QUOTE(_this call DFUNC(initObject));
-        };
-    };
-
-    class ReammoBox_F {
-        class ADDON {
-            init = QUOTE(_this call DFUNC(initObject));
-        };
-    };
-
-    class Cargo_base_F {
+    class All {
         class ADDON {
             init = QUOTE(_this call DFUNC(initObject); _this call DFUNC(initVehicle));
-        };
-    };
-
-    class CargoNet_01_box_F {
-        class ADDON {
-            init = QUOTE(_this call DFUNC(initObject); _this call DFUNC(initVehicle));
-        };
-    };
-
-    class Land_CargoBox_V1_F {
-        class ADDON {
-            init = QUOTE(_this call DFUNC(initObject); _this call DFUNC(initVehicle));
-        };
-    };
-
-    class Land_PaperBox_closed_F {
-        class ADDON {
-            init = QUOTE(_this call DFUNC(initObject); _this call DFUNC(initVehicle));
-        };
-    };
-
-    class Car {
-        class ADDON {
-            init = QUOTE(_this call DFUNC(initVehicle));
-        };
-    };
-
-    class Tank {
-        class ADDON {
-            init = QUOTE(_this call DFUNC(initVehicle));
-        };
-    };
-
-    class Helicopter {
-        class ADDON {
-            init = QUOTE(_this call DFUNC(initVehicle));
-        };
-    };
-
-    class Plane {
-        class ADDON {
-            init = QUOTE(_this call DFUNC(initVehicle));
-        };
-    };
-
-    class Ship_F {
-        class ADDON {
-            init = QUOTE(_this call DFUNC(initVehicle));
         };
     };
 };
