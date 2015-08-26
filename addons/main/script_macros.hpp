a17d94c213b9c0c99c5f89b6d841cad29d40cbf9
Michael Braun <m.braun92@gmail.com>
2015-08-26 14:12:17 +0200

    Removed semicolon of log macro

--- addons/main/script_macros.hpp
+++ addons/main/script_macros.hpp
@@ -97,7 +97,7 @@
 // Time functions for accuracy per frame
 #define ACE_tickTime (ACE_time + (diag_tickTime - ACE_diagTime))
 
-#define ACE_LOG(module,level,message) diag_log text format [QUOTE([ACE] (module) level: %1), message];
+#define ACE_LOG(module,level,message) diag_log text format [QUOTE([ACE] (module) level: %1), message]
 #define ACE_LOGERROR(message) ACE_LOG(COMPONENT,ERROR,message)
 #define ACE_LOGWARNING(message) ACE_LOG(COMPONENT,WARNING,message)
 #define ACE_LOGINFO(message) ACE_LOG(COMPONENT,INFO,message)
