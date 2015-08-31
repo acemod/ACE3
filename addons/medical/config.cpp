a61e4b3a4ef092eec8ce97239e4eff978956652b
commy2 <commy-2@gmx.de>
2015-09-01 00:35:56 +0200

    remove BI blood overlay

--- addons/medical/config.cpp
+++ addons/medical/config.cpp
@@ -22,5 +22,6 @@ class CfgPatches {
 #include "CfgEden.hpp"
 #include "ACE_Medical_Treatments.hpp"
 #include "ACE_Settings.hpp"
+#include "UI\CfgInGameUI.hpp"
 #include "UI\RscTitles.hpp"
 #include "UI\triagecard.hpp"
