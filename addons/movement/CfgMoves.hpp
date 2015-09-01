ec0229d9a9e57ab87dc0ce36fa88179a8a929887
commy2 <commy-2@gmx.de>
2015-09-01 20:33:02 +0200

    don't force free looking while jumping

--- addons/movement/CfgMoves.hpp
+++ addons/movement/CfgMoves.hpp
@@ -71,6 +71,12 @@ class CfgMovesBasic {
 class CfgMovesMaleSdr: CfgMovesBasic {
     class StandBase;
     class States {
+        // jumping. don't force awkward freelook in mid air.
+        class AovrPercMstpSrasWrflDf;
+        class AovrPercMrunSrasWrflDf: AovrPercMstpSrasWrflDf {
+            forceAim = 0;
+        };
+
         // better slow walk with lowered rifle animation
         class AmovPercMstpSrasWrflDnon;
         class AmovPercMrunSrasWrflDf: AmovPercMstpSrasWrflDnon {
