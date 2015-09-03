a187be725540efdfbd03fd2fb7d76b5791442976
commy2 <commy-2@gmx.de>
2015-09-03 13:23:15 +0200

    sort flashbang under explosive grenades

--- addons/grenades/CfgAmmo.hpp
+++ addons/grenades/CfgAmmo.hpp
@@ -85,6 +85,7 @@ class CfgAmmo {
         suppressionRadiusHit = 20;
         typicalSpeed = 22;
         cost = 40;
+        explosive = 1E-7;
         deflecting = 15;
         timeToLive = 6;
         fuseDistance = 2.3;
