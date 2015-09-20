9c61f256bcfec8b78d971939044b8993e05f9575
PabstMirror <elephantisaterriblemascot@gmail.com>
2015-09-20 10:06:24 -0500

    Rem debug

--- addons/repair/script_component.hpp
+++ addons/repair/script_component.hpp
@@ -1,7 +1,7 @@
 #define COMPONENT repair
 #include "\z\ace\addons\main\script_mod.hpp"
 
-#define DEBUG_MODE_FULL
+// #define DEBUG_MODE_FULL
 
 #ifdef DEBUG_ENABLED_REPAIR
     #define DEBUG_MODE_FULL
