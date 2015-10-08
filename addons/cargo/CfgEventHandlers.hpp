8ec5f34247f57424b5d6c6139be1395334abc386
Broström.A | Evul <andreas.brostrom.ce@gmail.com>
2015-10-08 23:07:39 +0200

    Update CfgEventHandlers.hpp

--- addons/cargo/CfgEventHandlers.hpp
+++ addons/cargo/CfgEventHandlers.hpp
@@ -88,11 +88,6 @@ class Extended_InitPost_EventHandlers {
             init = QUOTE(_this call DFUNC(initObject));
         };
     };
-    class RoadCone_F {
-        class ADDON {
-            init = QUOTE(_this call DFUNC(initObject));
-        };
-    };
     class RoadCone_L_F {
         class ADDON {
             init = QUOTE(_this call DFUNC(initObject));
