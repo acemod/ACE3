ca71c09c1acd57891f8fbde6d107aa491f8c446a
P1NGA <pastor399@gmail.com>
2015-10-08 14:59:34 +1100

    Added Modular Artillery Charge System Items

--- addons/artillery/CfgVehicles.hpp
+++ addons/artillery/CfgVehicles.hpp
@@ -2,6 +2,7 @@ class CfgVehicles {
 	class ThingX;
 	class Item_Base_F;
 	class NonStrategic;
+	class ReammoBox_F;
 	
 	class ACE_M1A2_AimingPost_Item_F: Item_Base_F {
         scope = 2;
@@ -183,4 +184,66 @@ class CfgVehicles {
 		picture=PATHTOF(m198_loading_tray\data\ui\pushrod_equip_ca.paa);
 		vehicleClass = "Static";
 	};
+	class ACE_MACS_Sleeve_F: ThingX {   
+		scope = 2;
+        scopeCurator = 2;
+		displayName = "MACS Charge Loading Sleeve";
+		author = ECSTRING(common,ACETeam);
+		model=PATHTOF(modular_charges\macs_charge_sleeve_F.p3d);
+		icon=PATHTOF(m198_loading_tray\data\ui\map_pushrod_ca.paa);
+		picture=PATHTOF(m198_loading_tray\data\ui\pushrod_equip_ca.paa);
+		vehicleClass = "Static";
+	};
+	class ACE_MACS_M231_Pallet_F: ThingX {   
+		scope = 2;
+        scopeCurator = 2;
+		displayName = "MACS M231 Supply Pallet";
+		author = ECSTRING(common,ACETeam);
+		model=PATHTOF(modular_charges\macs_pallet_m231_F.p3d);
+		icon=PATHTOF(m198_loading_tray\data\ui\map_pushrod_ca.paa);
+		picture=PATHTOF(m198_loading_tray\data\ui\pushrod_equip_ca.paa);
+		vehicleClass = "Static";
+			
+		slingLoadCargoMemoryPoints[] = {"slingload_point_01","slingload_point_02","slingload_point_03","slingload_point_04"};
+	};
+	class ACE_MACS_M232_Pallet_F: ACE_MACS_M231_Pallet_F {   
+		scope = 2;
+        scopeCurator = 2;
+		displayName = "MACS M232 Supply Pallet";
+		author = ECSTRING(common,ACETeam);
+		model=PATHTOF(modular_charges\macs_pallet_m232_F.p3d);
+		icon=PATHTOF(m198_loading_tray\data\ui\map_pushrod_ca.paa);
+		picture=PATHTOF(m198_loading_tray\data\ui\pushrod_equip_ca.paa);
+		vehicleClass = "Static";
+	};
+	class ACE_MACS_M231_Bundle_F: ReammoBox_F {   
+		scope = 2;
+        scopeCurator = 2;
+		displayName = "MACS M231 Artillery Charges (Bundle)";
+		author = ECSTRING(common,ACETeam);
+		model=PATHTOF(modular_charges\macs_bundle_m231_F.p3d);
+		icon=PATHTOF(m198_loading_tray\data\ui\map_pushrod_ca.paa);
+		picture=PATHTOF(m198_loading_tray\data\ui\pushrod_equip_ca.paa);
+		vehicleClass = "Static";
+		
+		class TransportItems {
+            MACRO_ADDITEM(ACE_MACS_M231_Charge_F,24);
+        };
+		
+	};
+	class ACE_MACS_M232_Bundle_F: ReammoBox_F {   
+		scope = 2;
+        scopeCurator = 2;
+		displayName = "MACS M232 Artillery Charges (Bundle)";
+		author = ECSTRING(common,ACETeam);
+		model=PATHTOF(modular_charges\macs_bundle_m232_F.p3d);
+		icon=PATHTOF(m198_loading_tray\data\ui\map_pushrod_ca.paa);
+		picture=PATHTOF(m198_loading_tray\data\ui\pushrod_equip_ca.paa);
+		vehicleClass = "Static";
+
+		class TransportItems {
+            MACRO_ADDITEM(ACE_MACS_M232_Charge_F,30);
+        };
+	};
+	
 };
--- addons/artillery/CfgWeapons.hpp
+++ addons/artillery/CfgWeapons.hpp
@@ -31,4 +31,35 @@ class CfgWeapons {
             mass = 20;
         };
     };
+	
+	class ACE_MACS_M231_Charge_F: ACE_ItemCore {
+		scope = 2;
+        scopeCurator = 2;
+        displayName = "MACS M231 Artillery Charge";
+		author = ECSTRING(common,ACETeam);
+		
+		model=PATHTOF(modular_charges\macs_charge_m231_F.p3d);
+		icon=PATHTOF(m1a1_collimator\data\ui\map_collimator_ca.paa);
+		picture=PATHTOF(m1a1_collimator\data\ui\collimator_equip_ca.paa);
+		keepHorizontalPlacement = 1;
+		
+        class ItemInfo: InventoryItem_Base_F {
+            mass = 5;
+        };
+	};
+	class ACE_MACS_M232_Charge_F: ACE_ItemCore {
+		scope = 2;
+        scopeCurator = 2;
+        displayName = "MACS M232 Artillery Charge";
+		author = ECSTRING(common,ACETeam);
+		
+		model=PATHTOF(modular_charges\macs_charge_m232_F.p3d);
+		icon=PATHTOF(m1a1_collimator\data\ui\map_collimator_ca.paa);
+		picture=PATHTOF(m1a1_collimator\data\ui\collimator_equip_ca.paa);
+		keepHorizontalPlacement = 1;
+		
+        class ItemInfo: InventoryItem_Base_F {
+            mass = 20;
+        };
+	};
 };
