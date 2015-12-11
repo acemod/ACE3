44a6ea53c738807a6b4da61bc2f92f017cf2e07f
Ken <kenny19982009@hotmail.com>
2015-12-11 08:08:03 -0600

    Fixed More Tabs

--- optionals/compat_rhs_afrf3/CfgAmmo.hpp
+++ optionals/compat_rhs_afrf3/CfgAmmo.hpp
@@ -128,7 +128,7 @@ class CfgAmmo
     };
     
     class GrenadeHand;
-	class rhs_ammo_rgd5: GrenadeHand {
+    class rhs_ammo_rgd5: GrenadeHand {
 	    ace_frag_enabled = 1;
         ace_frag_metal = 200;
         ace_frag_charge = 110;
@@ -138,7 +138,7 @@ class CfgAmmo
         ace_frag_skip = 0;
         ace_frag_force = 1;
 	};
-	class rhs_ammo_rgn_base: rhs_ammo_rgd5 {
+    class rhs_ammo_rgn_base: rhs_ammo_rgd5 {
 	    ace_frag_enabled = 1;
         ace_frag_metal = 193;
         ace_frag_charge = 97;
@@ -148,27 +148,27 @@ class CfgAmmo
         ace_frag_skip = 0;
         ace_frag_force = 1;
 	};
-	class rhs_ammo_rgn: rhs_ammo_rgn_base {
+    class rhs_ammo_rgn: rhs_ammo_rgn_base {
         ace_frag_enabled = 0;
         ace_frag_skip = 1;
         ace_frag_force = 0;
 	};
-	class rhs_ammo_rgn_sub: rhs_ammo_rgn_base {};
-	class rhs_ammo_rgn_exp: rhs_ammo_rgn_base {};
-	class rhs_ammo_fakel: GrenadeHand {
+    class rhs_ammo_rgn_sub: rhs_ammo_rgn_base {};
+    class rhs_ammo_rgn_exp: rhs_ammo_rgn_base {};
+    class rhs_ammo_fakel: GrenadeHand {
         ace_frag_enabled = 0;
         ace_frag_skip = 1;
         ace_frag_force = 0;
 	};
-	class rhs_ammo_fakels: rhs_ammo_fakel {};
-	class rhs_ammo_zarya2: rhs_ammo_fakels {};
-	class rhs_ammo_plamyam: rhs_ammo_fakels {};
+    class rhs_ammo_fakels: rhs_ammo_fakel {};
+    class rhs_ammo_zarya2: rhs_ammo_fakels {};
+    class rhs_ammo_plamyam: rhs_ammo_fakels {};
 	
-	class RocketBase;
-	class R_PG32V_F: RocketBase {};
-	class rhs_rpg26_rocket: R_PG32V_F {};
-	class rhs_rpg7v2_pg7vl: rhs_rpg26_rocket {};
-	class rhs_rpg7v2_og7v: rhs_rpg7v2_pg7vl {
+    class RocketBase;
+    class R_PG32V_F: RocketBase {};
+    class rhs_rpg26_rocket: R_PG32V_F {};
+    class rhs_rpg7v2_pg7vl: rhs_rpg26_rocket {};
+    class rhs_rpg7v2_og7v: rhs_rpg7v2_pg7vl {
         ace_frag_enabled = 1;
         ace_frag_metal = 400;
         ace_frag_charge = 210;
@@ -178,12 +178,12 @@ class CfgAmmo
         ace_frag_skip = 0;
         ace_frag_force = 1;
 	};
-	class rhs_rpg7v2_tbg7v: rhs_rpg7v2_pg7vl {
+    class rhs_rpg7v2_tbg7v: rhs_rpg7v2_pg7vl {
         ace_frag_enabled = 0;
         ace_frag_skip = 1;
         ace_frag_force = 0;
 	};
-	class rhs_rshg2_rocket: rhs_rpg7v2_tbg7v {
+    class rhs_rshg2_rocket: rhs_rpg7v2_tbg7v {
         ace_frag_enabled = 0;
         ace_frag_skip = 1;
         ace_frag_force = 0;
