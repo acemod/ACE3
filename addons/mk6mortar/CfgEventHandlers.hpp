c3788e803834d4ab3cb40b6330842407479f601b
VKing <kauestad@gmail.com>
2015-12-17 23:35:02 +0100

    Changed to postInit

--- addons/mk6mortar/CfgEventHandlers.hpp
+++ addons/mk6mortar/CfgEventHandlers.hpp
@@ -5,7 +5,7 @@ class Extended_PreInit_EventHandlers {
 };
 class Extended_PostInit_EventHandlers {
     class ADDON {
-        clientInit = QUOTE(call COMPILE_FILE(XEH_clientInit));
+        postInit = QUOTE(call COMPILE_FILE(XEH_postInit));
     };
 };
 class Extended_FiredBIS_EventHandlers {
