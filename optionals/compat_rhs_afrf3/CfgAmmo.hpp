fce245ad76ff3ced26d8b3401d48f5dc26b7e807
VKing <kauestad@gmail.com>
2015-12-18 16:23:07 +0100

    Remove SMAW Spotting Rifle backblast

--- optionals/compat_rhs_afrf3/CfgAmmo.hpp
+++ optionals/compat_rhs_afrf3/CfgAmmo.hpp
@@ -1,10 +1,8 @@
 
-class CfgAmmo
-{
+class CfgAmmo {
     class BulletBase;
     class B_556x45_Ball;
-    class rhs_B_545x39_Ball: B_556x45_Ball
-    {
+    class rhs_B_545x39_Ball: B_556x45_Ball {
         ACE_caliber=5.588;
         ACE_bulletLength=21.59;
         ACE_bulletMass=3.42792;
@@ -12,12 +10,11 @@ class CfgAmmo
         ACE_ballisticCoefficients[]={0.168};
         ACE_velocityBoundaries[]={};
         ACE_standardAtmosphere="ASM";
-        ACE_dragModel=7;
+        ACE_dragModel=7;-
         ACE_muzzleVelocities[]={780, 880, 920};
         ACE_barrelLengths[]={254.0, 414.02, 508.0};
     };
-    class rhs_B_545x39_Ball_Tracer_Green: rhs_B_545x39_Ball
-    {
+    class rhs_B_545x39_Ball_Tracer_Green: rhs_B_545x39_Ball {
         ACE_caliber=5.588;
         ACE_bulletLength=21.59;
         ACE_bulletMass=3.22704;
@@ -30,8 +27,7 @@ class CfgAmmo
         ACE_barrelLengths[]={254.0, 414.02, 508.0};
     };
     class B_762x51_Ball;
-    class rhs_B_762x54_Ball: B_762x51_Ball
-    {
+    class rhs_B_762x54_Ball: B_762x51_Ball {
         ACE_caliber=7.925;
         ACE_bulletLength=28.956;
         ACE_bulletMass=9.8496;
@@ -43,8 +39,7 @@ class CfgAmmo
         ACE_muzzleVelocities[]={700, 800, 820, 833};
         ACE_barrelLengths[]={406.4, 508.0, 609.6, 660.4};
     };
-    class rhs_B_762x54_Ball_Tracer_Green: B_762x51_Ball
-    {
+    class rhs_B_762x54_Ball_Tracer_Green: B_762x51_Ball {
         ACE_caliber=7.925;
         ACE_bulletLength=28.956;
         ACE_bulletMass=9.6552;
@@ -56,8 +51,7 @@ class CfgAmmo
         ACE_muzzleVelocities[]={680, 750, 798, 800};
         ACE_barrelLengths[]={406.4, 508.0, 609.6, 660.4};
     };
-    class rhs_B_762x54_7N1_Ball: rhs_B_762x54_Ball
-    {
+    class rhs_B_762x54_7N1_Ball: rhs_B_762x54_Ball {
         ACE_caliber=7.925;
         ACE_bulletLength=28.956;
         ACE_bulletMass=9.8496;
@@ -69,8 +63,7 @@ class CfgAmmo
         ACE_muzzleVelocities[]={700, 800, 820, 833};
         ACE_barrelLengths[]={406.4, 508.0, 609.6, 660.4};
     };
-    class rhs_B_762x39_Ball: B_762x51_Ball
-    {
+    class rhs_B_762x39_Ball: B_762x51_Ball {
         ACE_caliber=7.823;
         ACE_bulletLength=28.956;
         ACE_bulletMass=7.9704;
@@ -82,8 +75,7 @@ class CfgAmmo
         ACE_muzzleVelocities[]={650, 716, 750};
         ACE_barrelLengths[]={254.0, 414.02, 508.0};
     };
-    class rhs_B_762x39_Tracer: rhs_B_762x39_Ball
-    {
+    class rhs_B_762x39_Tracer: rhs_B_762x39_Ball {
         ACE_caliber=7.823;
         ACE_bulletLength=28.956;
         ACE_bulletMass=7.5816;
@@ -96,8 +88,7 @@ class CfgAmmo
         ACE_barrelLengths[]={254.0, 414.02, 508.0};
     };
     class B_9x21_Ball;
-    class rhs_B_9x19_7N21: B_9x21_Ball
-    {
+    class rhs_B_9x19_7N21: B_9x21_Ball {
         ACE_caliber=9.017;
         ACE_bulletLength=15.494;
         ACE_bulletMass=5.19696;
@@ -109,8 +100,7 @@ class CfgAmmo
         ACE_muzzleVelocities[]={445, 460, 480};
         ACE_barrelLengths[]={101.6, 127.0, 228.6};
     };
-    class rhs_B_9x18_57N181S: B_9x21_Ball
-    {
+    class rhs_B_9x18_57N181S: B_9x21_Ball {
         ACE_caliber=9.271;
         ACE_bulletLength=15.494;
         ACE_bulletMass=6.00048;
