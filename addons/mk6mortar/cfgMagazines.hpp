f75311917c9124070d4f3c2504606fee45d825bd
VKing <kauestad@gmail.com>
2015-12-19 20:49:09 +0100

    Upped mortar round mass

--- addons/mk6mortar/cfgMagazines.hpp
+++ addons/mk6mortar/cfgMagazines.hpp
@@ -7,10 +7,10 @@ class cfgMagazines {
         author = ECSTRING(common,ACETeam);
         displayName = CSTRING(magazine_HE_displayName);
         displayNameShort = "";
-        descriptionShort = CSTRING(magazine_HE_descriptionShort);
+        descriptionShort = CSTRING(magazine_descriptionShort);
         model = PATHTOF(data\l16_ammo_he.p3d)
         picture = PATHTOF(UI\w_l16_ammo_he_ca.paa);
-        mass = 10; // Temporary value; should be increased
+        mass = 50;
     };
     class 8Rnd_82mm_Mo_Smoke_white;
     class ACE_1Rnd_82mm_Mo_Smoke: 8Rnd_82mm_Mo_Smoke_white {
@@ -20,10 +20,10 @@ class cfgMagazines {
         author = ECSTRING(common,ACETeam);
         displayName = CSTRING(magazine_Smoke_displayName);
         displayNameShort = "";
-        descriptionShort = CSTRING(magazine_Smoke_descriptionShort);
+        descriptionShort = CSTRING(magazine_descriptionShort);
         model = PATHTOF(data\l16_ammo_smk_white.p3d)
         picture = PATHTOF(UI\w_l16_ammo_smk_white_ca.paa);
-        mass = 10;
+        mass = 50;
     };
     class 8Rnd_82mm_Mo_Flare_white;
     class ACE_1Rnd_82mm_Mo_Illum: 8Rnd_82mm_Mo_Flare_white {
@@ -33,10 +33,10 @@ class cfgMagazines {
         author = ECSTRING(common,ACETeam);
         displayName = CSTRING(magazine_Illum_displayName);
         displayNameShort = "";
-        descriptionShort = CSTRING(magazine_Illum_descriptionShort);
+        descriptionShort = CSTRING(magazine_descriptionShort);
         model = PATHTOF(data\l16_ammo_illum.p3d)
         picture = PATHTOF(UI\w_l16_ammo_illum_ca.paa);
-        mass = 10;
+        mass = 50;
     };
     class 8Rnd_82mm_Mo_guided;
     class ACE_1Rnd_82mm_Mo_HE_Guided: 8Rnd_82mm_Mo_guided {
@@ -46,10 +46,10 @@ class cfgMagazines {
         author = ECSTRING(common,ACETeam);
         displayName = CSTRING(magazine_HE_Guided_displayName);
         displayNameShort = "";
-        descriptionShort = CSTRING(magazine_HE_Guided_descriptionShort);
+        descriptionShort = CSTRING(magazine_descriptionShort);
         model = PATHTOF(data\l16_ammo_he.p3d)
         picture = PATHTOF(UI\w_l16_ammo_he_ca.paa);
-        mass = 10;
+        mass = 50;
     };
     class 8Rnd_82mm_Mo_LG;
     class ACE_1Rnd_82mm_Mo_HE_LaserGuided: 8Rnd_82mm_Mo_LG {
@@ -59,9 +59,9 @@ class cfgMagazines {
         author = ECSTRING(common,ACETeam);
         displayName = CSTRING(magazine_HE_LaserGuided_displayName);
         displayNameShort = "";
-        descriptionShort = CSTRING(magazine_HE_LaserGuided_descriptionShort);
+        descriptionShort = CSTRING(magazine_descriptionShort);
         model = PATHTOF(data\l16_ammo_he.p3d)
         picture = PATHTOF(UI\w_l16_ammo_he_ca.paa);
-        mass = 10;
+        mass = 50;
     };
 };
