0fb2fa1b8f86723388e1da9de173e5798046c667
VKing <kauestad@gmail.com>
2015-12-31 23:39:09 +0100

    Fix style

--- optionals/compat_rhs_usf3/CfgVehicles.hpp
+++ optionals/compat_rhs_usf3/CfgVehicles.hpp
@@ -31,45 +31,45 @@ class cfgVehicles {
     };
 
     class Heli_light_03_base_F;
-    class RHS_UH1_Base : Heli_light_03_base_F {
+    class RHS_UH1_Base: Heli_light_03_base_F {
         EGVAR(refuel,fuelCapacity) = 1447;
     };
 
     class Heli_Transport_01_base_F;
-    class RHS_UH60_Base : Heli_Transport_01_base_F {
+    class RHS_UH60_Base: Heli_Transport_01_base_F {
         EGVAR(refuel,fuelCapacity) = 1360;
     };
 
     class Heli_Transport_02_base_F;
-    class RHS_CH_47F_base : Heli_Transport_02_base_F {
+    class RHS_CH_47F_base: Heli_Transport_02_base_F {
         EGVAR(refuel,fuelCapacity) = 3914;
     };
 
     class Heli_Attack_01_base_F;
-    class RHS_AH1Z_base : Heli_Attack_01_base_F {
+    class RHS_AH1Z_base: Heli_Attack_01_base_F {
         EGVAR(refuel,fuelCapacity) = 1600;
     };
 
-    class RHS_AH64_base : Heli_Attack_01_base_F {
+    class RHS_AH64_base: Heli_Attack_01_base_F {
         EGVAR(refuel,fuelCapacity) = 1420;
     };
 
     class MBT_01_arty_base_F;
-    class rhsusf_m109tank_base : MBT_01_arty_base_F {
+    class rhsusf_m109tank_base: MBT_01_arty_base_F {
         EGVAR(refuel,fuelCapacity) = 511;
     };
 
     class MRAP_01_base_F;
-    class rhsusf_hmmwe_base : MRAP_01_base_F {
+    class rhsusf_hmmwe_base: MRAP_01_base_F {
         EGVAR(refuel,fuelCapacity) = 95;
     };
 
-    class rhsusf_rg33_base : MRAP_01_base_F {
+    class rhsusf_rg33_base: MRAP_01_base_F {
         EGVAR(refuel,fuelCapacity) = 302;
     };
 
     class Truck_01_base_F;
-    class rhsusf_fmtv_base : Truck_01_base_F {
+    class rhsusf_fmtv_base: Truck_01_base_F {
         EGVAR(refuel,fuelCapacity) = 219;
     };
 
@@ -83,17 +83,17 @@ class cfgVehicles {
         };
     };
     class APC_Tracked_03_base_F;
-    class RHS_M2A2_Base : APC_Tracked_03_base_F {
+    class RHS_M2A2_Base: APC_Tracked_03_base_F {
         EGVAR(refuel,fuelCapacity) = 746;
     };
 
     class Plane_CAS_01_base_F;
-    class RHS_A10 : Plane_CAS_01_base_F {
+    class RHS_A10: Plane_CAS_01_base_F {
         EGVAR(refuel,fuelCapacity) = 6223;
     };
 
     class Plane_Base_F;
-    class RHS_C130J_Base : Plane_Base_F {
+    class RHS_C130J_Base: Plane_Base_F {
         EGVAR(refuel,fuelCapacity) = 25704;
     };
 };
