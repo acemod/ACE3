46d6e92bcabe641405658b8b4811c7856d4e50c6
PabstMirror <pabstmirror@gmail.com>
2016-01-28 23:26:02 -0600

    Add quick debug defines to all script_components

--- addons/attach/script_component.hpp
+++ addons/attach/script_component.hpp
@@ -1,6 +1,11 @@
 #define COMPONENT attach
 #include "\z\ace\addons\main\script_mod.hpp"
 
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define CBA_DEBUG_SYNCHRONOUS
+// #define ENABLE_PERFORMANCE_COUNTERS
+
 #ifdef DEBUG_ENABLED_ATTACH
     #define DEBUG_MODE_FULL
 #endif
