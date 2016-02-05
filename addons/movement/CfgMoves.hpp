b2d050afcafd8d0e3351f77ac222205e46925d34
esteldunedain <nicolas.d.badano@gmail.com>
2016-02-05 09:46:51 -0300

    Close #3188, Close #3260

--- addons/movement/CfgMoves.hpp
+++ addons/movement/CfgMoves.hpp
@@ -77,43 +77,6 @@ class CfgMovesMaleSdr: CfgMovesBasic {
             InterpolateTo[] = {"AovrPercMrunSrasWrflDf",0.22,"AmovPercMrunSlowWrflDf",0.025,"AmovPercMwlkSrasWrflDf",0.025,"AmovPknlMrunSrasWrflDf",0.03,"AmovPercMrunSlowWrflDf_AmovPpneMstpSrasWrflDnon",0.02,"AmovPercMevaSrasWrflDf",0.025,"Unconscious",0.01,"AmovPercMtacSrasWrflDf",0.02,"AmovPercMrunSrasWrflDfl",0.02,"AmovPercMrunSrasWrflDfl_ldst",0.02,"AmovPercMrunSrasWrflDfr",0.02,"AmovPercMrunSrasWrflDfr_ldst",0.02,"AmovPercMstpSrasWrflDnon",0.02,"AmovPercMrunSrasWrflDl",0.02,"AmovPercMrunSrasWrflDbl",0.02,"AmovPercMrunSrasWrflDb",0.02,"AmovPercMrunSrasWrflDbr",0.02,"AmovPercMrunSrasWrflDr",0.02,"AmovPknlMstpSlowWrflDnon_relax",0.1,"AmovPercMrunSrasWrflDf_ldst",0.02,"AmovPercMrunSrasWrflDf",0.02};
         };
 
-        class AmovPercMstpSlowWrflDnon;
-        class AmovPercMwlkSlowWrflDf: AmovPercMstpSlowWrflDnon {
-            speed = 0.3; //0.206897;
-            file = "\A3\anims_f\Data\Anim\Sdr\Mov\Erc\Wlk\Low\Rfl\AmovPercMwlkSlowWrflDf_ver2";
-            leftHandIKCurve[] = {1};
-        };
-        class AmovPercMwlkSlowWrflDfl: AmovPercMwlkSlowWrflDf {
-            leftHandIKCurve[] = {};
-        };
-        class AmovPercMwlkSlowWrflDfr: AmovPercMwlkSlowWrflDf {
-            leftHandIKCurve[] = {};
-        };
-        class AmovPercMwlkSlowWrflDb: AmovPercMwlkSlowWrflDf {
-            leftHandIKCurve[] = {};
-        };
-        class AmovPercMwlkSlowWrflDbl: AmovPercMwlkSlowWrflDf {
-            leftHandIKCurve[] = {};
-        };
-        class AmovPercMwlkSlowWrflDbr: AmovPercMwlkSlowWrflDf {
-            leftHandIKCurve[] = {};
-        };
-        class AmovPercMwlkSlowWrflDl: AmovPercMwlkSlowWrflDf {
-            leftHandIKCurve[] = {};
-        };
-        class AmovPercMwlkSlowWrflDr: AmovPercMwlkSlowWrflDf {
-            leftHandIKCurve[] = {};
-        };
-        class AmovPercMwlkSlowWrflDf_v1: AmovPercMwlkSlowWrflDf {
-            leftHandIKCurve[] = {};
-        };
-        class AidlPercMwlkSrasWrflDf: AmovPercMwlkSlowWrflDf {
-            leftHandIKCurve[] = {};
-        };
-        class AmovPercMtacSlowWrflDf: AmovPercMwlkSlowWrflDf {
-            leftHandIKCurve[] = {};
-        };
-
         // enable optics in prone down stance
         class AadjPpneMstpSrasWrflDup;
         class AadjPpneMstpSrasWrflDdown: AadjPpneMstpSrasWrflDup {
