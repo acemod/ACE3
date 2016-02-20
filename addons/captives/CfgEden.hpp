1b03ee76fd21ea1f71b0bc3380a9a107ad0113ac
gienkov <gienkov.grzegorz@gmail.com>
2016-02-20 14:07:39 +0100

    tabs

--- addons/captives/CfgEden.hpp
+++ addons/captives/CfgEden.hpp
@@ -3,17 +3,17 @@ class Cfg3DEN {
         class AttributeCategories {
             class ace_attributes {
                 class Attributes {
-					class ace_isHandcuffed {
+                    class ace_isHandcuffed {
                         property = QUOTE(ace_isHandcuffed);
                         control = "Checkbox";
                         displayName = CSTRING(ModuleHandcuffed_DisplayName);
                         tooltip = CSTRING(ModuleHandcuffed_Description);
-						expression = QUOTE([ARR_2(_this,_value)]call FUNC(setHandcuffed));
+                        expression = QUOTE([ARR_2(_this,_value)]call FUNC(setHandcuffed));
                         typeName = "BOOL";
                         condition = "objectBrain";
                         defaultValue = false;
                     };
-					class ace_isSurrendered {
+                    class ace_isSurrendered {
                         property = QUOTE(ace_isSurrendered);
                         control = "Checkbox";
                         displayName = CSTRING(ModuleSurrender_DisplayName);
