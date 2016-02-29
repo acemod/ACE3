05c16a5dfb0e95dd83a4530007cc8e9739a84b32
BaerMitUmlaut <florian.foss@gmail.com>
2016-02-29 01:04:52 +0100

    Removed TOH animations

--- addons/fastroping/CfgMoves.hpp
+++ addons/fastroping/CfgMoves.hpp
@@ -8,32 +8,12 @@ class CfgMovesBasic {
 class CfgMovesMaleSdr: CfgMovesBasic {
     class States {
         class Crew;
-        class ACE_freefallLoop: Crew {
-            file = PATHTOF(anim\freefallLoop.rtm);
+        class ACE_FastRoping: Crew {
+            file = QUOTE(PATHTOF(anim\fastroping.rtm));
             interpolateTo[] = {"Unconscious", 1};
             disableWeapons = 1;
             disableWeaponsLong = 1;
             canReload = 0;
-            looped = 1;
-            speed = 0.204082;
-        };
-        class ACE_freefallStart: Crew {
-            file = PATHTOF(anim\freefallStart.rtm);
-            interpolateTo[] = {"Unconscious", 1};
-            disableWeapons = 1;
-            disableWeaponsLong = 1;
-            canReload = 0;
-            looped = 0;
-            speed = 0.61224502;
-        };
-        class ACE_slidingLoop: Crew {
-            file = PATHTOF(anim\slidingLoop.rtm);
-            interpolateTo[] = {"Unconscious", 1};
-            disableWeapons = 1;
-            disableWeaponsLong = 1;
-            canReload = 0;
-            looped = 1;
-            speed = 0.441176;
         };
     };
 };
