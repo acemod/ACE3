f130d6c2530cac2bc6fae3d73744e6bdd2909c02
esteldunedain <nicolas.d.badano@gmail.com>
2016-03-01 18:17:58 -0300

    Increase spare barrel mass to 60 (2.7 Kg)

--- addons/overheating/CfgMagazines.hpp
+++ addons/overheating/CfgMagazines.hpp
@@ -5,7 +5,7 @@ class CfgMagazines {
         picture = QUOTE(PATHTOF(UI\spare_barrel_ca.paa));
         displayName = "ACE Spare Barrel";   //!!!CANNOT be localized!!!: because it is used as part of the magazineDetail string
         count = 1;
-        mass = 40;
+        mass = 60;
         ACE_isUnique = 1;
     };
 };
