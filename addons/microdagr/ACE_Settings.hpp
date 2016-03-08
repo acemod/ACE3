b5e95f394501d8ed6eccafdda36f2dc0233f206e
PabstMirror <pabstmirror@gmail.com>
2016-03-08 01:17:40 -0600

    Add some names/descriptions to settings configs

--- addons/microdagr/ACE_Settings.hpp
+++ addons/microdagr/ACE_Settings.hpp
@@ -1,5 +1,7 @@
 class ACE_Settings {
     class GVAR(MapDataAvailable) {
+        displayName = CSTRING(MapDataAvailable_DisplayName);
+        description = CSTRING(MapDataAvailable_Description);
         value = 2;
         typeName = "SCALAR";
         isClientSettable = 0;
