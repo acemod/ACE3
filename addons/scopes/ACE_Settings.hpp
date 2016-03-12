1b2f4e0f10d5bc2306cc98e6fc9991b6900bf3c3
esteldunedain <nicolas.d.badano@gmail.com>
2016-03-12 17:55:41 -0300

    Add venilla zeroing correction to Scopes

--- addons/scopes/ACE_Settings.hpp
+++ addons/scopes/ACE_Settings.hpp
@@ -5,4 +5,10 @@ class ACE_Settings {
         displayName = CSTRING(Enabled_displayName);
         description = CSTRING(Enabled_description);
     };
+    class GVAR(defaultZeroDistance) {
+        typeName = "SCALAR";
+        value = 100;
+        displayName = CSTRING(defaultZeroDistance_displayName);
+        description = CSTRING(defaultZeroDistance_description);
+    };
 };
