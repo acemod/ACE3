3489dd48bb4851ecb895e21ed08976b737ec4795
jonpas <jonpas33@gmail.com>
2016-03-29 15:51:51 +0200

    Remove unused gestures, Improve stringtabling and remove duplicate

--- addons/gestures/CfgVehicles.hpp
+++ addons/gestures/CfgVehicles.hpp
@@ -12,131 +12,104 @@ class CfgVehicles {
                 icon = PATHTOF(UI\gestures_ca.paa);
 
                 class GVAR(Advance) {
-                    displayName = CSTRING(BIgestureAdvance);
+                    displayName = CSTRING(Advance);
                     condition = QUOTE(canStand _target);
                     statement = QUOTE(_target playActionNow 'gestureAdvance';);
                     showDisabled = 1;
                     priority = 1.9;
                 };
                 class GVAR(Go) {
-                    displayName = CSTRING(BIgestureGo);
+                    displayName = CSTRING(Go);
                     condition = QUOTE(canStand _target);
                     statement = QUOTE(_target playActionNow ([ARR_2('gestureGo','gestureGoB')] select floor random 2););
                     showDisabled = 1;
                     priority = 1.8;
                 };
                 class GVAR(Follow) {
-                    displayName = CSTRING(BIgestureFollow);
+                    displayName = CSTRING(Follow);
                     condition = QUOTE(canStand _target);
                     statement = QUOTE(_target playActionNow 'gestureFollow';);
                     showDisabled = 1;
                     priority = 1.7;
                 };
                 class GVAR(Up) {
-                    displayName = CSTRING(BIgestureUp);
+                    displayName = CSTRING(Up);
                     condition = QUOTE(canStand _target);
                     statement = QUOTE(_target playActionNow 'gestureUp';);
                     showDisabled = 1;
                     priority = 1.5;
                 };
                 class GVAR(CeaseFire) {
-                    displayName = CSTRING(BIgestureCeaseFire);
+                    displayName = CSTRING(CeaseFire);
                     condition = QUOTE(canStand _target);
                     statement = QUOTE(_target playActionNow 'gestureCeaseFire';);
                     showDisabled = 1;
                     priority = 1.3;
                 };
                 class GVAR(Stop) {
-                    displayName = CSTRING(stop);
+                    displayName = CSTRING(Stop);
                     condition = QUOTE(canStand _target);
                     statement = QUOTE(_target playActionNow 'gestureFreeze';); // BI animation - is actualls "stop" in all stances but prone
                     showDisabled = 1;
                     priority = 1.2;
                 };
                 class GVAR(Forward) {
-                    displayName = CSTRING(forward);
+                    displayName = CSTRING(Forward);
                     condition = QUOTE(canStand _target && GVAR(ReloadMutex));
                     statement = QUOTE(QUOTE(QGVAR(forward)) call FUNC(playSignal));
                     showDisabled = 1;
                     priority = 1.9;
                 };
                 class GVAR(Regroup) {
-                    displayName = CSTRING(regroup);
+                    displayName = CSTRING(Regroup);
                     condition = QUOTE(canStand _target && GVAR(ReloadMutex));
                     statement = QUOTE(QUOTE(QGVAR(regroup)) call FUNC(playSignal));
                     showDisabled = 1;
                     priority = 1.8;
                 };
                 class GVAR(Freeze) {
-                    displayName = CSTRING(freeze);
+                    displayName = CSTRING(Freeze);
                     condition = QUOTE(canStand _target && GVAR(ReloadMutex));
                     statement = QUOTE(QUOTE(QGVAR(freeze)) call FUNC(playSignal));
                     showDisabled = 1;
                     priority = 1.7;
                 };
                 class GVAR(Cover) {
-                    displayName = CSTRING(cover);
+                    displayName = CSTRING(Cover);
                     condition = QUOTE(canStand _target && GVAR(ReloadMutex));
                     statement = QUOTE(QUOTE(QGVAR(cover)) call FUNC(playSignal));
                     showDisabled = 1;
                     priority = 1.6;
                 };
                 class GVAR(Point) {
-                    displayName = CSTRING(point);
+                    displayName = CSTRING(Point);
                     condition = QUOTE(canStand _target && GVAR(ReloadMutex));
                     statement = QUOTE(QUOTE(QGVAR(point)) call FUNC(playSignal));
                     showDisabled = 1;
                     priority = 1.5;
                 };
                 class GVAR(Engage) {
-                    displayName = CSTRING(engage);
+                    displayName = CSTRING(Engage);
                     condition = QUOTE(canStand _target && GVAR(ReloadMutex));
                     statement = QUOTE(QUOTE(QGVAR(engage)) call FUNC(playSignal));
                     showDisabled = 1;
                     priority = 1.4;
                 };
                 class GVAR(Hold) {
-                    displayName = CSTRING(hold);
+                    displayName = CSTRING(Hold);
                     condition = QUOTE(canStand _target && GVAR(ReloadMutex));
                     statement = QUOTE(QUOTE(QGVAR(hold)) call FUNC(playSignal));
                     showDisabled = 1;
                     priority = 1.3;
                 };
                 class GVAR(Warning) {
-                    displayName = CSTRING(warning);
+                    displayName = CSTRING(Warning);
                     condition = QUOTE(canStand _target && GVAR(ReloadMutex));
                     statement = QUOTE(QUOTE(QGVAR(warning)) call FUNC(playSignal));
                     showDisabled = 1;
                     priority = 1.2;
                 };
-                /*
-                class class GVAR(Yes) {
-                    displayName = ECSTRING(common,Yes);
-                    condition = QUOTE(canStand _target);
-                    statement = QUOTE(_target playActionNow ([ARR_2('gestureYes','gestureNod')] select floor random 2););
-                    showDisabled = 1;
-                    priority = 1.1;
-                };
-
-                class class GVAR(No) {
-                    displayName = ECSTRING(common,No);
-                    condition = QUOTE(canStand _target);
-                    statement = QUOTE(_target playActionNow 'gestureNo';);
-                    showDisabled = 1;
-                    priority = 1.0;
-                };
-
-                class class GVAR(Hi) {
-                    displayName = CSTRING(Hi);
-                    condition = QUOTE(canStand _target);
-                    statement = QUOTE(_target playActionNow ([ARR_3('gestureHi','gestureHiB','gestureHiC')] select floor random 3););
-                    showDisabled = 1;
-                    priority = 0.9;
-                };
-                */
-
             };
-
         };
     };
 };
--- addons/gestures/key.sqf
+++ addons/gestures/key.sqf
@@ -4,19 +4,20 @@
     _x params ["_currentName","_key"];
 
     private _signalName = format [QGVAR(%1), _currentName];
-    if (_currentName select [0,2] == "BI") then {
-        //Don't add "ace_gestures_" prefix to BI gestures
+
+    //Don't add "ace_gestures_" prefix to BI gestures
+    if (_key == -1) then {
         _signalName = _currentName;
     };
 
-    private _code = (compile format [QUOTE('%1' call FUNC(playSignal);), _signalName]);
+    private _code = compile format [QUOTE('%1' call FUNC(playSignal)), _signalName];
 
     TRACE_4("Adding KeyBind",_currentName,_signalName,_code,_key);
 
     [
         "ACE3 Gestures",
         _currentName,
-        localize format[LSTRING(%1), _currentName],
+        localize format [LSTRING(%1), _currentName],
         _code,
         {false},
         [_key,  [false, (_key != -1), false]],
@@ -25,18 +26,18 @@
 
     false
 } count [
-    ["freeze", DIK_NUMPAD2],
-    ["cover", DIK_NUMPAD3],
-    ["forward", DIK_NUMPAD4],
-    ["regroup", DIK_NUMPAD5],
-    ["engage", DIK_NUMPAD6],
-    ["point", DIK_NUMPAD7],
-    ["hold", DIK_NUMPAD8],
-    ["warning", DIK_NUMPAD9],
-    ["BIgestureGo", -1],
-    ["BIgestureAdvance", -1],
-    ["BIgestureFollow", -1],
-    ["BIgestureUp", -1],
-    ["BIgestureFreeze", -1],
-    ["BIgestureCeaseFire", -1]
+    ["Freeze", DIK_NUMPAD2],
+    ["Cover", DIK_NUMPAD3],
+    ["Forward", DIK_NUMPAD4],
+    ["Regroup", DIK_NUMPAD5],
+    ["Engage", DIK_NUMPAD6],
+    ["Point", DIK_NUMPAD7],
+    ["Hold", DIK_NUMPAD8],
+    ["Warning", DIK_NUMPAD9],
+    ["Go", -1],
+    ["Advance", -1],
+    ["Follow", -1],
+    ["Up", -1],
+    ["Stop", -1],
+    ["CeaseFire", -1]
 ];
