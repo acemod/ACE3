949703557ed118ac76850bb4ccd9308d7492d7af
Glowbal <thomasskooi@live.nl>
2016-04-10 16:12:54 +0200

    Add g300 bft component

---
+++ addons/bft_device_gd300/script_component.hpp
@@ -0,0 +1,17 @@
+#define COMPONENT bft_device_gd300
+#include "\z\ace\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define CBA_DEBUG_SYNCHRONOUS
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_bft_device_gd300
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_bft_device_gd300
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_bft_device_gd300
+#endif
+
+#include "\z\ace\addons\main\script_macros.hpp"
