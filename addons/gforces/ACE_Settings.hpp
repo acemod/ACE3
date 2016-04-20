3adbb59f048cd1c46fbcfd03917e841cad75dec0
PabstMirror <pabstmirror@gmail.com>
2016-04-19 23:04:51 -0500

    Geforce - Change default to aircraft only

--- addons/gforces/ACE_Settings.hpp
+++ addons/gforces/ACE_Settings.hpp
@@ -2,7 +2,7 @@ class ACE_Settings {
     class GVAR(enabledFor) {
         displayName = CSTRING(enabledFor_displayName);
         typeName = "SCALAR";
-        value = 2;
+        value = 1;
         values[] = {ECSTRING(Common,Disabled), CSTRING(enabledFor_onlyAircraft), ECSTRING(Common,Enabled)};
     };
 };
