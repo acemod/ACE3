1d477cc01a26e79aa9cf521f8196a14c6db1d1ea
nic547 <nic547@outlook.com>
2016-04-25 20:10:07 +0200

    Added compat_adr_97 and realisticnames
    p90 cyka blyat

---
+++ optionals/compat_adr_97/CfgWeapons.hpp
@@ -0,0 +1,47 @@
+
+class CfgWeapons {
+
+  class Rifle_Base_F;
+  class SMG_03_TR_BASE: Rifle_Base_F {
+
+  };
+  class SMG_03_TR_black: SMG_03_TR_BASE{
+    displayName = CSTRING(PS90_TR_Black_Name);
+  };
+  class SMG_03_TR_khaki: SMG_03_TR_black {
+    displayName = CSTRING(PS90_TR_Khaki_Name);
+  };
+  class SMG_03_TR_camo: SMG_03_TR_black {
+    displayName = CSTRING(PS90_TR_Camo_Name);
+  };
+  class SMG_03_black: SMG_03_TR_BASE {
+    displayName = CSTRING(PS90_Black_Name);
+  };
+  class SMG_03_khaki: SMG_03_black {
+    displayName = CSTRING(PS90_Khaki_Name);
+  };
+  class SMG_03_camo: SMG_03_black {
+    displayName = CSTRING(PS90_Camo_Name);
+  };
+  class SMG_03C_BASE: SMG_03_TR_BASE {
+
+  };
+  class SMG_03C_TR_black: SMG_03C_BASE {
+    displayName = CSTRING(P90_TR_Black_Name);
+  };
+  class SMG_03C_TR_khaki: SMG_03C_TR_black {
+    displayName = CSTRING(P90_TR_Khaki_Name);
+  };
+  class SMG_03C_TR_camo: SMG_03C_TR_black {
+    displayName = CSTRING(P90_TR_Camo_Name);
+  };
+  class SMG_03C_black: SMG_03C_Base {
+    displayName = CSTRING(P90_Black_Name);
+  };
+  class SMG_03C_khaki: SMG_03C_black {
+    displayName = CSTINRG(P90_Khaki_Name);
+  };
+  class SMG_03C_camo: SMG_03C_black {
+    displayName = CSTRING(P90_Camo_Name);
+  };
+};
---
+++ optionals/compat_adr_97/config.cpp
@@ -0,0 +1,14 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"A3_Weapons_F_Mod"};
+        author[]={"Nic547"};
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgWeapons.hpp"
