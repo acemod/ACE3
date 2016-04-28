b07d64deaedbb889c2f77c8283ecd2b13a1c80cc
FFAAMOD <FFAAMOD@users.noreply.github.com>
2016-04-28 18:46:48 +0200

    Update CfgVehicles.hpp

--- optionals/compat_ffaamod/CfgVehicles.hpp
+++ optionals/compat_ffaamod/CfgVehicles.hpp
@@ -1,111 +1,77 @@
 class DefaultEventhandlers;
-class CfgVehicles
-{
-    // Clases bases
-    class LandVehicle;
-    class Air;
-    class Ship;
-    class Ship_F : Ship
-    {
-        class Eventhandlers;
-    };
-    class Boat_F : Ship_F {};
-    class FlagCarrier;
+class CfgVehicles {
+    class Boat_F;
     class Plane;
-    class Plane_Base_F: Plane
-    {
+    class Plane_Base_F: Plane {
         class Eventhandlers;
     };
-    class UAV: Plane
-    {
+    class UAV: Plane {
         class Eventhandlers;
     };
     class Tank;
-    class Tank_F : Tank
-    {
+    class Tank_F : Tank {
         class Eventhandlers;
-        class Turrets
-        {
+        class Turrets {
             class MainTurret;
         };
     };
     class Helicopter : Air {
         class Turrets;
     };
-    class Helicopter_Base_F : Helicopter
-    {
+    class Helicopter_Base_F : Helicopter {
         class Eventhandlers;
-        class Turrets: Turrets
-        {
+        class Turrets: Turrets {
             class MainTurret;
         };
     };
     class Helicopter_Base_H: Helicopter_Base_F {};
 
     class StaticWeapon;
-    class StaticATWeapon : StaticWeapon
-    {
+    class StaticATWeapon : StaticWeapon {
         class Eventhandlers;
     };
 
     // Clases a modificar
-    class ffaa_ea_hercules_base : Plane_Base_F
-    {
-        class Eventhandlers : Eventhandlers
-        {
+    class ffaa_ea_hercules_base : Plane_Base_F {
+        class Eventhandlers : Eventhandlers {
             delete init;
         };
     };
-    class Boat_Armed_01_base_F: Boat_F
-    {
+    class Boat_Armed_01_base_F: Boat_F {
         class Eventhandlers;
     };
-    class ffaa_ar_lcm : Boat_Armed_01_base_F
-    {
-        class Eventhandlers : Eventhandlers
-        {
+    class ffaa_ar_lcm : Boat_Armed_01_base_F {
+        class Eventhandlers : Eventhandlers {
             delete init;
         };
     };
-    class ffaa_ar_zodiac_hurricane : Boat_Armed_01_base_F
-    {
-        class Eventhandlers : Eventhandlers
-        {
+    class ffaa_ar_zodiac_hurricane : Boat_Armed_01_base_F {
+        class Eventhandlers : Eventhandlers {
             delete init;
         };
     };
-    class ffaa_spike_tripode : StaticATWeapon
-    {
-        class Eventhandlers : Eventhandlers
-        {
+    class ffaa_spike_tripode : StaticATWeapon {
+        class Eventhandlers : Eventhandlers {
             delete init;
             delete fired;
         };
     };
-    class ffaa_et_ch47_base : Helicopter_Base_H
-    {
-        class Eventhandlers : Eventhandlers
-        {
+    class ffaa_et_ch47_base : Helicopter_Base_H {
+        class Eventhandlers : Eventhandlers {
             delete init;
         };
     };
-    class ffaa_famet_cougar_base : Helicopter_Base_H
-    {
-        class Eventhandlers : Eventhandlers
-        {
+    class ffaa_famet_cougar_base : Helicopter_Base_H {
+        class Eventhandlers : Eventhandlers {
             delete init;
         };
     };
-    class ffaa_et_pizarro: Tank_F
-    {
-        class Eventhandlers : Eventhandlers
-        {
+    class ffaa_et_pizarro: Tank_F {
+        class Eventhandlers : Eventhandlers {
             delete init;
         };
-        class Turrets: Turrets
-        {
-            class MainTurret: MainTurret
-            {
+        class Turrets: Turrets {
+            class MainTurret: MainTurret {
                 ACE_fcs_enabled = 1;
                 ACE_fcs_minDistance = 100;
                 ACE_fcs_maxDistance = 2400;
@@ -115,51 +81,37 @@ class CfgVehicles
             };
         };
     };
-    class ffaa_et_toa : Tank_F
-    {
-        class Eventhandlers : Eventhandlers
-        {
+    class ffaa_et_toa : Tank_F {
+        class Eventhandlers : Eventhandlers {
             delete init;
         };
     };
-    class ffaa_et_toa_m2 : ffaa_et_toa
-    {
-        class Eventhandlers : Eventhandlers
-        {
+    class ffaa_et_toa_m2 : ffaa_et_toa {
+        class Eventhandlers : Eventhandlers {
             delete init;
         };
     };
-    class ffaa_et_toa_mando : ffaa_et_toa_m2
-    {
-        class Eventhandlers : Eventhandlers
-        {
+    class ffaa_et_toa_mando : ffaa_et_toa_m2 {
+        class Eventhandlers : Eventhandlers {
             delete init;
         };
     };
-    class ffaa_et_toa_zapador : ffaa_et_toa_m2
-    {
-        class Eventhandlers : Eventhandlers
-        {
+    class ffaa_et_toa_zapador : ffaa_et_toa_m2 {
+        class Eventhandlers : Eventhandlers {
             delete init;
         };
     };
-    class ffaa_et_toa_ambulancia : ffaa_et_toa_m2
-    {
-        class Eventhandlers : Eventhandlers
-        {
+    class ffaa_et_toa_ambulancia : ffaa_et_toa_m2 {
+        class Eventhandlers : Eventhandlers {
             delete init;
         };
     };
-    class ffaa_et_leopardo_base: Tank_F
-    {
-        class Eventhandlers : Eventhandlers
-        {
+    class ffaa_et_leopardo_base: Tank_F {
+        class Eventhandlers : Eventhandlers {
             delete init;
         };
-        class Turrets: Turrets
-        {
-            class MainTurret: MainTurret
-            {
+        class Turrets: Turrets {
+            class MainTurret: MainTurret {
                 ACE_fcs_enabled = 1;
                 ACE_fcs_minDistance = 100;
                 ACE_fcs_maxDistance = 2400;
@@ -170,8 +122,7 @@ class CfgVehicles
         };
     };
     class ffaa_famet_tigre_base: Helicopter_Base_F {
-        class EventHandlers : Eventhandlers
-        {
+        class EventHandlers : Eventhandlers {
             delete init;
             delete fired;
         };
@@ -186,17 +137,13 @@ class CfgVehicles
             };
         };
     };
-    class ffaa_et_searcherIII: UAV
-    {
-        class EventHandlers : Eventhandlers
-        {
+    class ffaa_et_searcherIII: UAV {
+        class EventHandlers : Eventhandlers {
             delete init;
         };
     };
-    class ffaa_UAVStation : StaticWeapon
-    {
-        class EventHandlers : DefaultEventhandlers
-        {
+    class ffaa_UAVStation : StaticWeapon {
+        class EventHandlers : DefaultEventhandlers {
             delete GetIn;
         };
     };
