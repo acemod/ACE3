ec1c376e538122c7c536e1eb0af7cf9465696949
commy2 <commy-2@gmx.de>
2016-04-28 23:41:43 +0200

    using cba settings

--- addons/movement/ACE_Settings.hpp
+++
@@ -1,8 +0,0 @@
-class ACE_Settings {
-    class GVAR(useImperial) {
-        value = 0;
-        typeName = "BOOL";
-        isClientSettable = 1;
-        displayName = CSTRING(UseImperial);
-    };
-};
