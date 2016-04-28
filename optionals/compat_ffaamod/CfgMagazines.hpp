21f2059c16af9badbe1186a7feb8451c60fa7d3d
FFAAMOD <FFAAMOD@users.noreply.github.com>
2016-04-28 18:41:43 +0200

    Update CfgMagazines.hpp

--- optionals/compat_ffaamod/CfgMagazines.hpp
+++ optionals/compat_ffaamod/CfgMagazines.hpp
@@ -1,16 +1,12 @@
-class CfgMagazines
-{
+class CfgMagazines {
     class CA_Magazine;
-    class ffaa_556x45_ameli: CA_Magazine
-    {
+    class ffaa_556x45_ameli: CA_Magazine {
         ACE_isBelt = 1;
     };
-    class ffaa_762x51_mg3: CA_Magazine
-    {
+    class ffaa_762x51_mg3: CA_Magazine {
         ACE_isBelt = 1;
     };
-    class ffaa_556x45_mg4: CA_Magazine
-    {
+    class ffaa_556x45_mg4: CA_Magazine {
         ACE_isBelt = 1;
     };
     class ffaa_mag_c90;
