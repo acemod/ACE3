2b6e4449167d76e0829e68588b74ae36dbc88cd7
FFAAMOD <FFAAMOD@users.noreply.github.com>
2016-04-28 14:24:49 +0200

    Update CfgGlasses.hpp
    Replaced tabs by 4 spaces for indentation

--- optionals/compat_ffaamod/CfgGlasses.hpp
+++ optionals/compat_ffaamod/CfgGlasses.hpp
@@ -6,13 +6,13 @@
 
 class CfgGlasses
 {
-	class None;
-	class ffaa_brilat_item_nbq: None
-	{
-		COMBAT_GOGGLES
-	};
-	class ffaa_Glasses: None
-	{
-		COMBAT_GOGGLES
-	};
+    class None;
+    class ffaa_brilat_item_nbq: None
+    {
+        COMBAT_GOGGLES
+    };
+    class ffaa_Glasses: None
+    {
+        COMBAT_GOGGLES
+    };
 };
