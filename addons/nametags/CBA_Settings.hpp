9ab0898f74e097fd22e15bf549c24c8b1355e2d0
commy2 <commy-2@gmx.de>
2016-05-03 17:14:12 +0200

    stuff

--- addons/nametags/CBA_Settings.hpp
+++ addons/nametags/CBA_Settings.hpp
@@ -24,6 +24,13 @@ class CBA_Settings {
             displayName = CSTRING(ShowPlayerRanks);
             defaultValue = 1;
         };
+        class GVAR(showSoundWaves): CBA_Setting_List_base {
+            displayName = CSTRING(ShowSoundWaves);
+            tooltip = CSTRING(ShowSoundWaves_Desc);
+            values[] = {0,1,2};
+            valueNames[] = {ECSTRING(common,Disabled),CSTRING(NameTagSettings),CSTRING(AlwaysShowAll)};
+            defaultIndex = 1;
+        };
         class GVAR(showVehicleCrewInfo): CBA_Setting_Boolean_base {
             displayName = CSTRING(ShowVehicleCrewInfo);
             defaultValue = 1;
@@ -36,13 +43,6 @@ class CBA_Settings {
             displayName = "";
             defaultValue = 0;
             // not for client
-        };
-        class GVAR(showSoundWaves): CBA_Setting_List_base {
-            displayName = CSTRING(ShowSoundWaves);
-            tooltip = CSTRING(ShowSoundWaves_Desc);
-            values[] = {0,1,2};
-            valueNames[] = {ECSTRING(common,Disabled),CSTRING(NameTagSettings),CSTRING(AlwaysShowAll)};
-            defaultIndex = 1;
         };
         class GVAR(playerNamesViewDistance): CBA_Setting_Slider_base {
             displayName = "";
