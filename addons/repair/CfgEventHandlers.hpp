f6e4bd7a6b42a43f37d6984820691415cdc5cff5
jonpas <jonpas33@gmail.com>
2016-05-12 21:37:54 +0200

    Remove redundant spare part adding

--- addons/repair/CfgEventHandlers.hpp
+++ addons/repair/CfgEventHandlers.hpp
@@ -28,13 +28,11 @@ class Extended_InitPost_EventHandlers {
     class Tank {
         class ADDON {
             init = QUOTE(_this call DFUNC(addRepairActions));
-            serverInit = QUOTE(_this call DFUNC(addSpareParts));
         };
     };
     class Motorcycle {
         class ADDON {
             init = QUOTE(_this call DFUNC(addRepairActions));
-            serverInit = QUOTE(_this call DFUNC(addSpareParts));
         };
     };
     class Helicopter {
