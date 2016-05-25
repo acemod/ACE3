e53c05ec33d8c0f4839aae990fd345bd4f2c471f
voiper <viper.med@gmail.com>
2016-05-25 12:28:19 -0700

    Moved effects into Effects.hpp.

--- addons/map/config.cpp
+++ addons/map/config.cpp
@@ -30,6 +30,7 @@ class RscEdit;
 #include "CfgAmmo.hpp"
 #include "CfgSounds.hpp"
 #include "CfgLights.hpp"
+#include "Effects.hpp"
 
 class RscMapControl {
     maxSatelliteAlpha = 0.5;
@@ -160,42 +161,5 @@ class RscDisplayServerGetReady: RscDisplayGetReady {
         class TopRight: RscControlsGroup {
             #include "MapControls.hpp"
         };
-    };
-};
-
-// "Smoke" effect classes for global flashlight glow
-
-class ACE_FlashlightEffect_White {
-    class Light1 {
-        simulation = "light";
-        type = "ACE_FlashlightLight_White";
-    };
-};
-
-class ACE_FlashlightEffect_Red {
-    class Light1 {
-        simulation = "light";
-        type = "ACE_FlashlightLight_Red";
-    };
-};
-
-class ACE_FlashlightEffect_Blue {
-    class Light1 {
-        simulation = "light";
-        type = "ACE_FlashlightLight_Blue";
-    };
-};
-
-class ACE_FlashlightEffect_Green {
-    class Light1 {
-        simulation = "light";
-        type = "ACE_FlashlightLight_Green";
-    };
-};
-
-class ACE_FlashlightEffect_Yellow {
-    class Light1 {
-        simulation = "light";
-        type = "ACE_FlashlightLight_Yellow";
     };
 };
