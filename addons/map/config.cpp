7c32ca2796b099c710d926d6cd47f3632d0a4094
voiper <viper.med@gmail.com>
2016-05-25 12:05:34 -0700

    Spaces before and after =

--- addons/map/config.cpp
+++ addons/map/config.cpp
@@ -167,35 +167,35 @@ class RscDisplayServerGetReady: RscDisplayGetReady {
 
 class ACE_FlashlightEffect_White {
     class Light1 {
-        simulation="light";
-        type="ACE_FlashlightLight_White";
+        simulation = "light";
+        type = "ACE_FlashlightLight_White";
     };
 };
 
 class ACE_FlashlightEffect_Red {
     class Light1 {
-        simulation="light";
-        type="ACE_FlashlightLight_Red";
+        simulation = "light";
+        type = "ACE_FlashlightLight_Red";
     };
 };
 
 class ACE_FlashlightEffect_Blue {
     class Light1 {
-        simulation="light";
-        type="ACE_FlashlightLight_Blue";
+        simulation = "light";
+        type = "ACE_FlashlightLight_Blue";
     };
 };
 
 class ACE_FlashlightEffect_Green {
     class Light1 {
-        simulation="light";
-        type="ACE_FlashlightLight_Green";
+        simulation = "light";
+        type = "ACE_FlashlightLight_Green";
     };
 };
 
 class ACE_FlashlightEffect_Yellow {
     class Light1 {
-        simulation="light";
-        type="ACE_FlashlightLight_Yellow";
+        simulation = "light";
+        type = "ACE_FlashlightLight_Yellow";
     };
 };
