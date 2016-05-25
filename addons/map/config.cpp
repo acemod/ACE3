66c734124e8f34e5f960edad6f2b79b358ef5a7b
voiper <viper.med@gmail.com>
2016-05-25 10:51:50 -0700

    -Converted flashlight glow object to chemlight type (emits light without sprite) -Fixed issue where glow object was being deleted without being first detached

--- addons/map/config.cpp
+++ addons/map/config.cpp
@@ -29,6 +29,7 @@ class RscEdit;
 #include "CfgVehicles.hpp"
 #include "CfgAmmo.hpp"
 #include "CfgSounds.hpp"
+#include "CfgLights.hpp"
 
 class RscMapControl {
     maxSatelliteAlpha = 0.5;
@@ -159,5 +160,42 @@ class RscDisplayServerGetReady: RscDisplayGetReady {
         class TopRight: RscControlsGroup {
             #include "MapControls.hpp"
         };
+    };
+};
+
+// "Smoke" effect classes for global flashlight glow
+
+class ACE_FlashlightEffect_White {
+    class Light1 {
+        simulation="light";
+        type="ACE_FlashlightLight_White";
+    };
+};
+
+class ACE_FlashlightEffect_Red {
+    class Light1 {
+        simulation="light";
+        type="ACE_FlashlightLight_Red";
+    };
+};
+
+class ACE_FlashlightEffect_Blue {
+    class Light1 {
+        simulation="light";
+        type="ACE_FlashlightLight_Blue";
+    };
+};
+
+class ACE_FlashlightEffect_Green {
+    class Light1 {
+        simulation="light";
+        type="ACE_FlashlightLight_Green";
+    };
+};
+
+class ACE_FlashlightEffect_Yellow {
+    class Light1 {
+        simulation="light";
+        type="ACE_FlashlightLight_Yellow";
     };
 };
