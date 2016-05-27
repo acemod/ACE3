52ea640c4b473afc9593364b4b85b2109cd4f6da
voiperr <voiperr@users.noreply.github.com>
2016-05-27 10:16:15 -0700

    ace_map: More realistic flashlight glow + minor fix (#3824)
    * -Converted flashlight glow object to chemlight type (emits light without sprite)
    -Fixed issue where glow object was being deleted without being first detached
    
    * Cleanup of temp classes.
    
    * Spaces before and after =
    
    * Moved effects into Effects.hpp.

--- addons/map/CfgAmmo.hpp
+++ addons/map/CfgAmmo.hpp
@@ -1,48 +1,54 @@
 class CfgAmmo {
-
-    class FlareCore;
-
-    class FlareBase: FlareCore {};
-    class F_20mm_White: FlareBase {};
-
-    class ACE_FlashlightProxy_White: F_20mm_White {
+    
+    class Chemlight_base;
+    
+    class ACE_FlashlightProxy_White: Chemlight_base {
         model = "\A3\Weapons_f\empty";
-        effectFlare = "FlareShell";
-
-        triggerTime = 0;
-        intensity = 0.5;
-        flareSize = 1;
-        timeToLive = 10e10;
-
-        lightColor[] = {1,1,1,1};
-
-        grenadeBurningSound[] = {};
-        grenadeFireSound[] = {};
-        soundTrigger[] = {};
-        SmokeShellSoundHit1[] = {};
-        SmokeShellSoundHit2[] = {};
-        SmokeShellSoundHit3[] = {};
-        SmokeShellSoundLoop1[] = {};
-        SmokeShellSoundLoop2[] = {};
+        effectsSmoke = "ACE_FlashlightEffect_White";
+        explosionTime = 0.01;
+        timeToLive = 1e10;
+        
+        soundImpactHard1[] = {"",1,1};
+        soundImpactHard2[] = {"",1,1};
+        soundImpactHard3[] = {"",1,1};
+        soundImpactHard4[] = {"",1,1};
+        soundImpactHard5[] = {"",1,1};
+        soundImpactHard6[] = {"",1,1};
+        soundImpactHard7[] = {"",1,1};
+        soundImpactIron1[] = {"",1,1};
+        soundImpactIron2[] = {"",1,1};
+        soundImpactIron3[] = {"",1,1};
+        soundImpactIron4[] = {"",1,1};
+        soundImpactIron5[] = {"",1,1};
+        soundImpactSoft1[] = {"",1,1};
+        soundImpactSoft2[] = {"",1,1};
+        soundImpactSoft3[] = {"",1,1};
+        soundImpactSoft4[] = {"",1,1};
+        soundImpactSoft5[] = {"",1,1};
+        soundImpactSoft6[] = {"",1,1};
+        soundImpactSoft7[] = {"",1,1};
+        soundImpactWater1[] = {"",1,1};
+        soundImpactWater2[] = {"",1,1};
+        soundImpactWater3[] = {"",1,1};
+        soundImpactWoodExt1[] = {"",1,1};
+        soundImpactWoodExt2[] = {"",1,1};
+        soundImpactWoodExt3[] = {"",1,1};
+        soundImpactWoodExt4[] = {"",1,1};
     };
-
+    
     class ACE_FlashlightProxy_Red: ACE_FlashlightProxy_White {
-        intensity = 1;
-        lightColor[] = {1,0,0,1};
+        effectsSmoke = "ACE_FlashlightEffect_Red";
     };
-
-    class ACE_FlashlightProxy_Green: ACE_FlashlightProxy_White {
-        intensity = 1;
-        lightColor[] = {0,1,0,1};
-    };
-
+    
     class ACE_FlashlightProxy_Blue: ACE_FlashlightProxy_White {
-        intensity = 1.5;
-        lightColor[] = {0.25,0.25,1,1};
+        effectsSmoke = "ACE_FlashlightEffect_Blue";
     };
-
+    
+    class ACE_FlashlightProxy_Green: ACE_FlashlightProxy_White {
+        effectsSmoke = "ACE_FlashlightEffect_Green";
+    };
+    
     class ACE_FlashlightProxy_Yellow: ACE_FlashlightProxy_White {
-        intensity = 1;
-        lightColor[] = {1,1,0.5,1};
+        effectsSmoke = "ACE_FlashlightEffect_Yellow";
     };
 };
