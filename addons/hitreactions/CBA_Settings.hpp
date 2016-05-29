690318321c812abcad3dbe4139b3aeaed402cf9b
commy2 <commy-2@gmx.de>
2016-05-29 21:29:11 +0200

    fix broken cfg

--- addons/hitreactions/CBA_Settings.hpp
+++ addons/hitreactions/CBA_Settings.hpp
@@ -3,9 +3,9 @@ class CBA_Setting_Slider_2_base;
 
 class CBA_Settings {
     class PREFIX {
-        ´//displayName = CSTRING(Module_DisplayName);
+        //displayName = CSTRING(Module_DisplayName);
 
-        class GVAR(minDamageToTrigger) {: CBA_Setting_Slider_2_base {
+        class GVAR(minDamageToTrigger): CBA_Setting_Slider_2_base {
             //Minimum mamage needed to trigger falling down while moving. Set to -1 to disable completely.
             defaultValue = 0.1;
         };
