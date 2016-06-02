73215b707daf61bd36145284904e05dc6e55f29d
PabstMirror <pabstmirror@gmail.com>
2016-06-02 14:36:25 -0500

    Remove Line Drawing from MapTools
    And change map tools key to ALT from CTRL

--- addons/maptools/config.cpp
+++ addons/maptools/config.cpp
@@ -12,8 +12,6 @@ class CfgPatches {
     };
 };
 
-#include "ACE_Settings.hpp"
-
 class RscControlsGroup;
 class RscActiveText;
 class RscPicture;
@@ -29,47 +27,3 @@ class RscEdit;
 #include "CfgEventHandlers.hpp"
 #include "CfgVehicles.hpp"
 #include "CfgWeapons.hpp"
-
-
-// REGULAR MAP
-class RscDisplayMainMap {
-    // Create the drawing color selector
-    class controls {
-        class TopRight: RscControlsGroup {
-            #include "MapControls.hpp"
-        };
-    };
-};
-
-// BRIEFING SCREEN
-class RscDisplayGetReady: RscDisplayMainMap {
-    // Create the drawing color selector
-    class controls {
-        class TopRight: RscControlsGroup {
-            #include "MapControls.hpp"
-        };
-    };
-};
-class RscDisplayClientGetReady: RscDisplayGetReady {
-    // Create the drawing color selector
-    class controls {
-        class TopRight: RscControlsGroup {
-            #include "MapControls.hpp"
-        };
-    };
-};
-class RscDisplayServerGetReady: RscDisplayGetReady {
-    // Create the drawing color selector
-    class controls {
-        class TopRight: RscControlsGroup {
-            #include "MapControls.hpp"
-        };
-    };
-};
-
-class ACE_newEvents {
-    drawing_sendbackMarkers = QGVAR(sendbackMarkers);
-    drawing_requestMarkers = QGVAR(requestMarkers);
-    drawing_addLineMarker = QGVAR(addLineMarker);
-    drawing_removeLineMarker = QGVAR(removeLineMarker);
-};
