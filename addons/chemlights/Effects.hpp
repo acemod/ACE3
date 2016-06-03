7e6f02846a57ea6a8bf50a03f8c947b47a2354d8
voiper <viper.med@gmail.com>
2016-06-03 02:59:41 -0700

    Tab fixing.

--- addons/chemlights/Effects.hpp
+++ addons/chemlights/Effects.hpp
@@ -8,50 +8,50 @@ class ACE_HandFlareEffect {
 };
 
 class ACE_ChemlightEffect_Orange {
-	class Light1 {
-		simulation = "light";
-		type = "ACE_ChemlightLight_Orange";
-	};
+    class Light1 {
+        simulation = "light";
+        type = "ACE_ChemlightLight_Orange";
+    };
 };
 
 class ACE_ChemlightEffect_White {
-	class Light1 {
-		simulation = "light";
-		type = "ACE_ChemlightLight_White";
-	};
+    class Light1 {
+        simulation = "light";
+        type = "ACE_ChemlightLight_White";
+    };
 };
 
 class ACE_ChemlightEffect_HiRed {
-	class Light1 {
-		simulation = "light";
-		type = "ACE_ChemlightLight_HiRed";
-	};
+    class Light1 {
+        simulation = "light";
+        type = "ACE_ChemlightLight_HiRed";
+    };
 };
 
 class ACE_ChemlightEffect_HiYellow {
-	class Light1 {
-		simulation = "light";
-		type = "ACE_ChemlightLight_HiYellow";
-	};
+    class Light1 {
+        simulation = "light";
+        type = "ACE_ChemlightLight_HiYellow";
+    };
 };
 
 class ACE_ChemlightEffect_HiOrange {
-	class Light1 {
-		simulation = "light";
-		type = "ACE_ChemlightLight_HiOrange";
-	};
+    class Light1 {
+        simulation = "light";
+        type = "ACE_ChemlightLight_HiOrange";
+    };
 };
 
 class ACE_ChemlightEffect_HiWhite {
-	class Light1 {
-		simulation = "light";
-		type = "ACE_ChemlightLight_HiWhite";
-	};
+    class Light1 {
+        simulation = "light";
+        type = "ACE_ChemlightLight_HiWhite";
+    };
 };
 
 class ACE_ChemlightEffect_IR {
-	class Light1 {
-		simulation = "light";
-		type = "ACE_ChemlightLight_IR";
-	};
+    class Light1 {
+        simulation = "light";
+        type = "ACE_ChemlightLight_IR";
+    };
 };
