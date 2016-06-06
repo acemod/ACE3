36d0b78811244dedcbdf43916eb5c66bee70f914
commy2 <commy-2@gmx.de>
2016-06-06 21:36:29 +0200

    coeficient handling interface sizes was inverted

--- addons/spottingscope/script_component.hpp
+++ addons/spottingscope/script_component.hpp
@@ -21,14 +21,15 @@
 #define IDC_BLACK_LEFT 10002
 #define IDC_BLACK_RIGHT 10003
 
-#define MAGIC_SCOPE_NUMBER 0.0775
+#define MAGIC_SCOPE_NUMBER 0.0235
+//#define MAGIC_SCOPE_NUMBER (uiNamespace getVariable ['magic', 1])
 
-#define POS_X_BASE(size) safezoneX + 0.5 * safezoneW - 0.5 * (size) * (getResolution select 5)
-#define POS_Y_BASE(size) safezoneY + 0.5 * safezoneH - 0.5 * (size) * (getResolution select 5) * 4/3
-#define POS_W_BASE(size) (size) * (getResolution select 5)
-#define POS_H_BASE(size) (size) * (getResolution select 5) * 4/3
+#define POS_X_BASE(size) safezoneX + 0.5 * safezoneW - 0.5 * (size) / (getResolution select 5)
+#define POS_Y_BASE(size) safezoneY + 0.5 * safezoneH - 0.5 * (size) / (getResolution select 5) * 4/3
+#define POS_W_BASE(size) (size) / (getResolution select 5)
+#define POS_H_BASE(size) (size) / (getResolution select 5) * 4/3
 
-#define SIZEX 3.5
+#define SIZEX 1.05
 
 #define POS_X QUOTE(POS_X_BASE(SIZEX))
 #define POS_Y QUOTE(POS_Y_BASE(SIZEX))
