3c85398d876b3b9b04f7aa91ae7b4e497ae28513
VKing <kauestad@gmail.com>
2016-06-12 10:17:02 +0200

    Clean up the sound config

--- addons/minedetector/CfgSounds.hpp
+++ addons/minedetector/CfgSounds.hpp
@@ -1,31 +1,4 @@
 class CfgSounds {
-    class ace_sweep_high {
-        name = "ace_sweep_high";
-        sound[] = {QUOTE(PATHTOF(sounds\sweep_high.ogg)), "db+1", 1};
-        titles[] = {};
-    };
-    class ace_sweep_low {
-        name = "ace_sweep_low";
-        sound[] = {QUOTE(PATHTOF(sounds\sweep_low.ogg)), "db+1", 1};
-        titles[] = {};
-    };
-    class ace_buzz_1 {
-        name = "buzz_2";
-        sound[] = {QUOTE(PATHTOF(sounds\buzz_2.ogg)), "db+0.5", 0.5};
-        titles[] = {};
-    };
-    class ace_buzz_2 {
-        name = "ace_buzz_2";
-        sound[] = {QUOTE(PATHTOF(sounds\buzz_2.ogg)), "db+0.76", 0.7};
-        titles[] = {};
-    };
-    class ace_buzz_3 {
-        name = "ace_buzz_3";
-        sound[] = {QUOTE(PATHTOF(sounds\buzz_2.ogg)), "db+1", 1};
-        titles[] = {};
-    };
-
-
     class ace_detector_1 {
         name = "ace_detector_1";
         sound[] = {QUOTE(PATHTOF(sounds\metal_detector.wav)), "db+1", 1};
