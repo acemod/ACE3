37c4351340a10bfcc398472c9053c0738a1616b2
Glowbal <thomasskooi@live.nl>
2016-06-24 20:56:57 +0200

    Increase required CBA version

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -13,7 +13,7 @@
 
 // MINIMAL required version for the Mod. Components can specify others..
 #define REQUIRED_VERSION 1.56
-#define REQUIRED_CBA_VERSION {2,4,0}
+#define REQUIRED_CBA_VERSION {2,4,1}
 
 #ifdef COMPONENT_BEAUTIFIED
     #define COMPONENT_NAME QUOTE(ACE3 - COMPONENT_BEAUTIFIED)
