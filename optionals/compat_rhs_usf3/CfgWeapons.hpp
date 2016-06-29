9c8c8d52b12c799ac3ffaa6f75be1d9ec8c524fc
J3aLeiNe <bal2.chris@orange.fr>
2016-06-29 19:34:19 +0200

    Update CfgWeapons.hpp

--- optionals/compat_rhs_usf3/CfgWeapons.hpp
+++ optionals/compat_rhs_usf3/CfgWeapons.hpp
@@ -1,6 +1,5 @@
 
-class CfgWeapons 
-{
+class CfgWeapons {
     class Pistol_Base_F;
     class Rifle_Base_F;
     class srifle_EBR_F;
@@ -117,11 +116,11 @@ class CfgWeapons
     class rhs_weap_m4a1;
     class rhs_weap_hk416d10: rhs_weap_m4a1 {
         ACE_barrelTwist = 177.8;
-        ACE_barrelLength = 264;
+        ACE_barrelLength = 254;
     };
     class rhs_weap_hk416d145: rhs_weap_hk416d10 {
         ACE_barrelTwist = 177.8;
-        ACE_barrelLength = 368;
+        ACE_barrelLength = 368.3;
     };
     class rhs_weap_m4a1_blockII;
     class rhs_weap_mk18: rhs_weap_m4a1_blockII {
@@ -134,12 +133,12 @@ class CfgWeapons
     };
     class rhs_weap_lmg_minimi_railed;    //Rifle_Base_F - scope = private;
     class rhs_weap_m249_pip_S: rhs_weap_lmg_minimi_railed {
-        ACE_barrelLength = 349.0;
+        ACE_barrelLength = 414;    //16.3" FN America
         ACE_barrelTwist = 177.8;
         ACE_Overheating_allowSwapBarrel = 1;
     };
     class rhs_weap_m249_pip_L: rhs_weap_lmg_minimi_railed {
-        ACE_barrelLength = 465.0;
+        ACE_barrelLength = 520.7;    //20.5" FN America
         ACE_barrelTwist = 177.8;
         ACE_Overheating_allowSwapBarrel = 1;
     };
@@ -152,10 +151,12 @@ class CfgWeapons
     class rhs_weap_m14ebrri: srifle_EBR_F {
         ACE_barrelTwist = 304.8;
         ACE_barrelLength = 558.8;
+        ACE_Overheating_dispersion = 0.75;
     };
     class rhs_weap_sr25: rhs_weap_m14ebrri {
         ACE_barrelTwist = 285.75;
         ACE_barrelLength = 609.6;
+        ACE_Overheating_dispersion = 0.75;
     };
     class rhs_weap_sr25_ec: rhs_weap_sr25 {
         ACE_barrelTwist = 285.75;
@@ -176,31 +177,52 @@ class CfgWeapons
         ACE_barrelTwist = 160.0;
         ACE_barrelLength = 180.0;     
     };
+    //RHS pistols
     class hgun_ACPC2_F;
     class rhsusf_weap_m1911a1: hgun_ACPC2_F {
         ACE_barrelTwist = 406.4;
         ACE_barrelLength = 127.0;
     };
-
+    class hgun_P07_F;
+    class rhsusf_weap_glock17g4: hgun_P07_F {
+        ACE_barrelTwist = 248.92;
+        ACE_barrelLength = 114.046;
+    };
+    class rhsusf_weap_m9: rhsusf_weap_glock17g4 {
+        ACE_barrelTwist = 248.92;
+        ACE_barrelLength = 124.46;
+    };
+    //RHS sniper scopes
     class rhsusf_acc_sniper_base;
     class rhsusf_acc_LEUPOLDMK4: rhsusf_acc_sniper_base {
-        ACE_ScopeAdjust_Vertical[] = { -4, 30 };
-        ACE_ScopeAdjust_Horizontal[] = { -6, 6 };
+        ACE_ScopeAdjust_Vertical[] = {-4, 30};
+        ACE_ScopeAdjust_Horizontal[] = {-6, 6};
         ACE_ScopeAdjust_VerticalIncrement = 0.1;
         ACE_ScopeAdjust_HorizontalIncrement = 0.1;
     };
     class rhsusf_acc_LEUPOLDMK4_2: rhsusf_acc_sniper_base {
-        ACE_ScopeAdjust_Vertical[] = { -4, 30 };
-        ACE_ScopeAdjust_Horizontal[] = { -6, 6 };
+        ACE_ScopeAdjust_Vertical[] = {-4, 30};
+        ACE_ScopeAdjust_Horizontal[] = {-6, 6};
         ACE_ScopeAdjust_VerticalIncrement = 0.1;
         ACE_ScopeAdjust_HorizontalIncrement = 0.1;
     };
-
-    class rhs_weap_fgm148 : launch_O_Titan_F {
+    class rhsusf_acc_premier: rhsusf_acc_LEUPOLDMK4_2 {
+        ACE_ScopeAdjust_Vertical[] = {-4, 30};
+        ACE_ScopeAdjust_Horizontal[] = {-6, 6};
+        ACE_ScopeAdjust_VerticalIncrement = 0.1;
+        ACE_ScopeAdjust_HorizontalIncrement = 0.1;
+    };
+    class rhsusf_acc_premier_anpvs27: rhsusf_acc_premier {
+        ACE_ScopeAdjust_Vertical[] = {-4, 30};
+        ACE_ScopeAdjust_Horizontal[] = {-6, 6};
+        ACE_ScopeAdjust_VerticalIncrement = 0.1;
+        ACE_ScopeAdjust_HorizontalIncrement = 0.1;
+    };
+    //RHS lauchers
+    class rhs_weap_fgm148: launch_O_Titan_F {
         ace_javelin_enabled = 1;
         weaponInfoType = "ACE_RscOptics_javelin";
         modelOptics = "\z\ace\addons\javelin\data\reticle_titan.p3d";
-
         canLock = 0;
         lockingTargetSound[] = {"",0,1};
         lockedTargetSound[] = {"",0,1};
@@ -224,13 +246,11 @@ class CfgWeapons
 
     class H_PilotHelmetHeli_B;
     class H_CrewHelmetHeli_B;
-    class rhsusf_hgu56p: H_PilotHelmetHeli_B
-    {
+    class rhsusf_hgu56p: H_PilotHelmetHeli_B {
         ace_hearing_protection = 0.85;
         ace_hearing_lowerVolume = 0.75;
     };
-    class rhsusf_hgu56p_mask: H_CrewHelmetHeli_B
-    {
+    class rhsusf_hgu56p_mask: H_CrewHelmetHeli_B {
         ace_hearing_protection = 0.85;
         ace_hearing_lowerVolume = 0.75;
     };
