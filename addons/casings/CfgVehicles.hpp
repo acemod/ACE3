b1d0779f6d13325fbc301c63d38b7f77f12c011f
esteldunedain <nicolas.d.badano@gmail.com>
2016-07-19 23:28:20 -0300

    Switch vanilla casing by Rocko's from ACE2.

--- addons/casings/CfgVehicles.hpp
+++ addons/casings/CfgVehicles.hpp
@@ -24,44 +24,39 @@ class CfgVehicles {
     };
 
     class GVAR(FxCartridge_Small): GVAR(FxCartridge) {
-        model = "\A3\Weapons_f\ammo\cartridge_small";
-        //model = QUOTE(PATHTOF(rocko_9.p3d));
+        model = QUOTE(PATHTOF(models\rocko_9.p3d));
     };
     class GVAR(FxCartridge_9mm): GVAR(FxCartridge_Small) {
     };
     class GVAR(FxCartridge_556): GVAR(FxCartridge) {
-        model = "\A3\weapons_f\ammo\cartridge_65";
-        //model = QPATHTO_M(rocko_556.p3d);
+        model = QPATHTO_M(models\rocko_556.p3d);
     };
     class GVAR(FxCartridge_65): GVAR(FxCartridge) {
-        model = "\A3\weapons_f\ammo\cartridge_65";
+        model = QPATHTO_M(models\rocko_556.p3d);
     };
     class GVAR(FxCartridge_65_caseless): GVAR(FxCartridge) {
         model = "\A3\weapons_f\empty";
     };
     class GVAR(FxCartridge_762): GVAR(FxCartridge) {
-        model = "\A3\weapons_f\ammo\cartridge_762";
-        //model = QUOTE(PATHTOF(rocko_762.p3d));
+        model = QUOTE(PATHTOF(models\rocko_762.p3d));
     };
     class GVAR(FxCartridge_127): GVAR(FxCartridge) {
-        model = "\A3\weapons_f\ammo\cartridge_127";
-        //model = QUOTE(PATHTOF(rocko_127.p3d));
+        model = QUOTE(PATHTOF(models\rocko_127.p3d));
     };
     class GVAR(FxCartridge_slug): GVAR(FxCartridge) {
-        model = "\A3\weapons_f\ammo\cartridge_slug";
-        //model = QUOTE(PATHTOF(rocko_sg.p3d));
+        model = QUOTE(PATHTOF(models\rocko_sg.p3d));
     };
     class GVAR(FxCartridge_338_Ball): GVAR(FxCartridge) {
-        model = "\A3\weapons_f\ammo\cartridge_762";
+        model = QUOTE(PATHTOF(models\rocko_762.p3d));
     };
     class GVAR(FxCartridge_338_NM_Ball): GVAR(FxCartridge) {
-        model = "\A3\weapons_f\ammo\cartridge_762";
+        model = QUOTE(PATHTOF(models\rocko_762.p3d));
     };
     class GVAR(FxCartridge_127x54_APDS): GVAR(FxCartridge) {
-        model = "\A3\weapons_f\ammo\cartridge_127";
+        model = QUOTE(PATHTOF(models\rocko_127.p3d));
     };
     class GVAR(FxCartridge_93x64_Ball): GVAR(FxCartridge) {
-        model = "\A3\weapons_f\ammo\cartridge_762";
+        model = QUOTE(PATHTOF(models\rocko_127.p3d));
     };
 };
 
