fe63eff7d6c0dd031262d274a534e3ef6753be90
esteldunedain <nicolas.d.badano@gmail.com>
2016-07-19 23:45:44 -0300

    Please John and Yoko. Displease Rocko ;)

--- addons/casings/CfgVehicles.hpp
+++ addons/casings/CfgVehicles.hpp
@@ -24,39 +24,39 @@ class CfgVehicles {
     };
 
     class GVAR(FxCartridge_Small): GVAR(FxCartridge) {
-        model = QUOTE(PATHTOF(models\rocko_9.p3d));
+        model = QUOTE(PATHTOF(models\ace_casing_9.p3d));
     };
     class GVAR(FxCartridge_9mm): GVAR(FxCartridge_Small) {
     };
     class GVAR(FxCartridge_556): GVAR(FxCartridge) {
-        model = QPATHTO_M(models\rocko_556.p3d);
+        model = QPATHTO_M(models\ace_casing_556.p3d);
     };
     class GVAR(FxCartridge_65): GVAR(FxCartridge) {
-        model = QPATHTO_M(models\rocko_556.p3d);
+        model = QPATHTO_M(models\ace_casing_556.p3d);
     };
     class GVAR(FxCartridge_65_caseless): GVAR(FxCartridge) {
         model = "\A3\weapons_f\empty";
     };
     class GVAR(FxCartridge_762): GVAR(FxCartridge) {
-        model = QUOTE(PATHTOF(models\rocko_762.p3d));
+        model = QUOTE(PATHTOF(models\ace_casing_762.p3d));
     };
     class GVAR(FxCartridge_127): GVAR(FxCartridge) {
-        model = QUOTE(PATHTOF(models\rocko_127.p3d));
+        model = QUOTE(PATHTOF(models\ace_casing_127.p3d));
     };
     class GVAR(FxCartridge_slug): GVAR(FxCartridge) {
-        model = QUOTE(PATHTOF(models\rocko_sg.p3d));
+        model = QUOTE(PATHTOF(models\ace_casing_sg.p3d));
     };
     class GVAR(FxCartridge_338_Ball): GVAR(FxCartridge) {
-        model = QUOTE(PATHTOF(models\rocko_762.p3d));
+        model = QUOTE(PATHTOF(models\ace_casing_762.p3d));
     };
     class GVAR(FxCartridge_338_NM_Ball): GVAR(FxCartridge) {
-        model = QUOTE(PATHTOF(models\rocko_762.p3d));
+        model = QUOTE(PATHTOF(models\ace_casing_762.p3d));
     };
     class GVAR(FxCartridge_127x54_APDS): GVAR(FxCartridge) {
-        model = QUOTE(PATHTOF(models\rocko_127.p3d));
+        model = QUOTE(PATHTOF(models\ace_casing_127.p3d));
     };
     class GVAR(FxCartridge_93x64_Ball): GVAR(FxCartridge) {
-        model = QUOTE(PATHTOF(models\rocko_127.p3d));
+        model = QUOTE(PATHTOF(models\ace_casing_127.p3d));
     };
 };
 
