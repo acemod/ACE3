e2689c1fc3944f2a96c5f0e3cef02dccfb14f95b
BaerMitUmlaut <BaerMitUmlaut@users.noreply.github.com>
2016-08-30 00:48:53 +0200

    Make ace_medical required

--- addons/medical_ai/config.cpp
+++ addons/medical_ai/config.cpp
@@ -6,7 +6,7 @@ class CfgPatches {
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"ace_common"};
+        requiredAddons[] = {"ace_medical"};
         author = ECSTRING(common,ACETeam);
         authors[] = {"BaerMitUmlaut"};
         url = ECSTRING(main,URL);
