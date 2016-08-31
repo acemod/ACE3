b85021430bb3bd0614c97e161ee297cb196f0850
BaerMitUmlaut <BaerMitUmlaut@users.noreply.github.com>
2016-08-31 21:16:25 +0200

    Remove debug flags

--- addons/advanced_fatigue/script_component.hpp
+++ addons/advanced_fatigue/script_component.hpp
@@ -2,8 +2,8 @@
 #define COMPONENT_BEAUTIFIED Advanced Fatigue
 #include "\z\ace\addons\main\script_mod.hpp"
 
-#define DEBUG_MODE_FULL
-#define DISABLE_COMPILE_CACHE
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
 // #define CBA_DEBUG_SYNCHRONOUS
 // #define ENABLE_PERFORMANCE_COUNTERS
 
