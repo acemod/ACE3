8639150a361142d7bc8007434939675af1978420
Kwinten Guillaume <kwinno@hotmail.com>
2016-09-13 19:47:27 +0200

    This PR adds compatibility for SMA Weapons by Blazenchamber (Ace ammo and huntIR)

---
+++ optionals/compat_sma_weapons/script_component.hpp
@@ -0,0 +1,6 @@
+#define COMPONENT sma_weapons
+#define COMPONENT_BEAUTIFIED Blazenchamber SMA Weapons Mod Compatibility
+
+#include "\z\ace\addons\main\script_mod.hpp"
+
+#include "\z\ace\addons\main\script_macros.hpp"
