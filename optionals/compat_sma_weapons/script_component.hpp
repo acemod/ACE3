332e53537a672e2dc51d07592b770e226d6c1318
Kwinten Guillaume <kwinno@hotmail.com>
2016-09-13 20:01:07 +0200

    fixed script component

--- optionals/compat_sma_weapons/script_component.hpp
+++ optionals/compat_sma_weapons/script_component.hpp
@@ -1,5 +1,5 @@
 #define COMPONENT sma_weapons
-#define COMPONENT_BEAUTIFIED Blazenchamber SMA Weapons Mod Compatibility
+#define COMPONENT_BEAUTIFIED SMA Weapons Compatibility
 
 #include "\z\ace\addons\main\script_mod.hpp"
 
