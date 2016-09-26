cae51966567095c31df1a0d965efb08efc9bd946
commy2 <commy-2@gmx.de>
2016-09-26 21:09:32 +0200

    fix wound translations being lost in limbo

--- addons/medical_damage/ACE_Medical_Injuries.hpp
+++ addons/medical_damage/ACE_Medical_Injuries.hpp
@@ -4,26 +4,26 @@ class ACE_Medical_Injuries {
         // Source: Scarle
         //  Also called scrapes, they occur when the skin is rubbed away by friction against another rough surface (e.g. rope burns and skinned knees).
         class Abrasion {
-            name = ECSTRING(medical,Wounds_Abrasion);
+            name = CSTRING(Abrasion);
             selections[] = {"All"};
             bleedingRate = 0.0001;
             pain = 0.01;
             causes[] = {"falling", "ropeburn", "vehiclecrash", "unknown"};
             minDamage = 0.01;
             class Minor {
-                name = ECSTRING(medical,Wounds_Abrasion_Minor);
+                name = CSTRING(Abrasion_Minor);
                 minDamage = 0.01;
                 maxDamage = 0.2;
                 bleedingRate = 0.0001;
             };
             class Medium {
-                name = ECSTRING(medical,Wounds_Abrasion_Medium);
+                name = CSTRING(Abrasion_Medium);
                 minDamage = 0.2;
                 maxDamage = 0.3;
                 bleedingRate = 0.00015;
             };
             class Large {
-                name = ECSTRING(medical,Wounds_Abrasion_Large);
+                name = CSTRING(Abrasion_Large);
                 minDamage = 0.3;
                 maxDamage = 0.5;
                 bleedingRate = 0.0002;
@@ -31,33 +31,33 @@ class ACE_Medical_Injuries {
         };
         // Occur when an entire structure or part of it is forcibly pulled away, such as the loss of a permanent tooth or an ear lobe. Explosions, gunshots, and animal bites may cause avulsions.
         class Avulsions {
-            name = ECSTRING(medical,Wounds_Avulsion);
+            name = CSTRING(Avulsion);
             selections[] = {"All"};
             bleedingRate = 0.01;
             pain = 0.3;
             causes[] = {"explosive", "vehiclecrash", "grenade", "shell", "bullet", "backblast", "bite"};
             minDamage = 0.2;
             class Minor {
-                name = ECSTRING(medical,Wounds_Avulsion_Minor);
+                name = CSTRING(Avulsion_Minor);
                 minDamage = 0.2;
                 maxDamage = 0.3;
                 bleedingRate = 0.01;
             };
             class Medium {
-                name = ECSTRING(medical,Wounds_Avulsion_Medium);
+                name = CSTRING(Avulsion_Medium);
                 minDamage = 0.3;
                 maxDamage = 0.6;
                 bleedingRate = 0.02;
             };
             class Large {
-                name = ECSTRING(medical,Wounds_Avulsion_Large);
+                name = CSTRING(Avulsion_Large);
                 minDamage = 0.5;
                 bleedingRate = 0.05;
             };
         };
         // Also called bruises, these are the result of a forceful trauma that injures an internal structure without breaking the skin. Blows to the chest, abdomen, or head with a blunt instrument (e.g. a football or a fist) can cause contusions.
         class Contusion {
-            name = ECSTRING(medical,Wounds_Contusion);
+            name = CSTRING(Contusion);
             selections[] = {"All"};
             bleedingRate = 0.0;
             pain = 0.05;
@@ -65,147 +65,147 @@ class ACE_Medical_Injuries {
             minDamage = 0.01;
             maxDamage = 0.1;
             class Minor {
-                name = ECSTRING(medical,Wounds_Contusion_Minor);
+                name = CSTRING(Contusion_Minor);
                 minDamage = 0.01;
                 maxDamage = 0.1;
             };
             class Medium {
-                name = ECSTRING(medical,Wounds_Contusion_Medium);
+                name = CSTRING(Contusion_Medium);
                 minDamage = 0.1;
                 maxDamage = 0.15;
             };
             class Large {
-                name = ECSTRING(medical,Wounds_Contusion_Large);
+                name = CSTRING(Contusion_Large);
                 minDamage = 0.15;
                 maxDamage = 0.2;
             };
         };
         // Occur when a heavy object falls onto a person, splitting the skin and shattering or tearing underlying structures.
         class CrushWound {
-            name = ECSTRING(medical,Wounds_Crush);
+            name = CSTRING(Crush);
             selections[] = {"All"};
             bleedingRate = 0.01;
             pain = 0.1;
             causes[] = {"falling", "vehiclecrash", "punch", "unknown"};
             minDamage = 0.1;
             class Minor {
-                name = ECSTRING(medical,Wounds_Crush_Minor);
+                name = CSTRING(Crush_Minor);
                 minDamage = 0.1;
                 maxDamage = 0.45;
                 bleedingRate = 0.005;
             };
             class Medium {
-                name = ECSTRING(medical,Wounds_Crush_Medium);
+                name = CSTRING(Crush_Medium);
                 minDamage = 0.4;
                 maxDamage = 0.7;
                 bleedingRate = 0.007;
             };
             class Large {
-                name = ECSTRING(medical,Wounds_Crush_Large);
+                name = CSTRING(Crush_Large);
                 minDamage = 0.6;
                 bleedingRate = 0.0095;
             };
         };
         // Slicing wounds made with a sharp instrument, leaving even edges. They may be as minimal as a paper cut or as significant as a surgical incision.
         class Cut {
-            name = ECSTRING(medical,Wounds_Cut);
+            name = CSTRING(Cut);
             selections[] = {"All"};
             bleedingRate = 0.01;
             pain = 0.075;
             causes[] = {"vehiclecrash", "grenade", "explosive", "shell", "backblast", "stab", "unknown"};
             minDamage = 0.1;
             class Minor {
-                name = ECSTRING(medical,Wounds_Cut_Minor);
+                name = CSTRING(Cut_Minor);
                 minDamage = 0.1;
                 maxDamage = 0.3;
                 bleedingRate = 0.005;
             };
             class Medium {
-                name = ECSTRING(medical,Wounds_Cut_Medium);
+                name = CSTRING(Cut_Medium);
                 minDamage = 0.3;
                 maxDamage = 0.65;
                 bleedingRate = 0.02;
             };
             class Large {
-                name = ECSTRING(medical,Wounds_Cut_Large);
+                name = CSTRING(Cut_Large);
                 minDamage = 0.65;
                 bleedingRate = 0.05;
             };
         };
         // Also called tears, these are separating wounds that produce ragged edges. They are produced by a tremendous force against the body, either from an internal source as in childbirth, or from an external source like a punch.
         class Laceration {
-            name = ECSTRING(medical,Wounds_Laceration);
+            name = CSTRING(Laceration);
             selections[] = {"All"};
             bleedingRate = 0.01;
             pain = 0.075;
             causes[] = {"vehiclecrash", "punch"};
             minDamage = 0.01;
             class Minor {
-                name = ECSTRING(medical,Wounds_Laceration_Minor);
+                name = CSTRING(Laceration_Minor);
                 minDamage = 0.1;
                 maxDamage = 0.5;
                 bleedingRate = 0.005;
             };
             class Medium {
-                name = ECSTRING(medical,Wounds_Laceration_Medium);
+                name = CSTRING(Laceration_Medium);
                 minDamage = 0.5;
                 maxDamage = 0.7;
                 bleedingRate = 0.01;
             };
             class Large {
-                name = ECSTRING(medical,Wounds_Laceration_Large);
+                name = CSTRING(Laceration_Large);
                 minDamage = 0.7;
                 bleedingRate = 0.03;
             };
         };
         // Also called velocity wounds, they are caused by an object entering the body at a high speed, typically a bullet or small peices of shrapnel.
         class velocityWound {
-            name = ECSTRING(medical,Wounds_VelocityWound);
+            name = CSTRING(VelocityWound);
             selections[] = {"All"};
             bleedingRate = 0.01;
             pain = 0.2;
             causes[] = {"bullet", "grenade","explosive", "shell", "unknown"};
             minDamage = 0.15;
             class Minor {
-                name = ECSTRING(medical,Wounds_VelocityWound_Minor);
+                name = CSTRING(VelocityWound_Minor);
                 minDamage = 0.15;
                 maxDamage = 0.3;
                 bleedingRate = 0.025;
             };
             class Medium {
-                name = ECSTRING(medical,Wounds_VelocityWound_Medium);
+                name = CSTRING(VelocityWound_Medium);
                 minDamage = 0.3;
                 maxDamage = 0.75;
                 bleedingRate = 0.05;
             };
             class Large {
-                name = ECSTRING(medical,Wounds_VelocityWound_Large);
+                name = CSTRING(VelocityWound_Large);
                 minDamage = 0.75;
                 bleedingRate = 0.1;
             };
         };
         // Deep, narrow wounds produced by sharp objects such as nails, knives, and broken glass.
         class punctureWound {
-            name = ECSTRING(medical,Wounds_PunctureWound);
+            name = CSTRING(PunctureWound);
             selections[] = {"All"};
             bleedingRate = 0.01;
             pain = 0.075;
             causes[] = {"stab", "grenade"};
             minDamage = 0.01;
             class Minor {
-                name = ECSTRING(medical,Wounds_PunctureWound_Minor);
+                name = CSTRING(PunctureWound_Minor);
                 minDamage = 0.01;
                 maxDamage = 0.5;
                 bleedingRate = 0.01;
             };
             class Medium {
-                name = ECSTRING(medical,Wounds_PunctureWound_Medium);
+                name = CSTRING(PunctureWound_Medium);
                 minDamage = 0.5;
                 maxDamage = 0.75;
                 bleedingRate = 0.03;
             };
             class Large {
-                name = ECSTRING(medical,Wounds_PunctureWound_Large);
+                name = CSTRING(PunctureWound_Large);
                 minDamage = 0.65;
                 bleedingRate = 0.08;
             };
@@ -213,7 +213,7 @@ class ACE_Medical_Injuries {
     };
     class fractures {
         class Femur {
-            name = ECSTRING(medical,Wounds_Femur);
+            name = CSTRING(Femur);
             selections[] = {"Head", "Body"};
             pain = 0.2;
             causes[] = {"Bullet", "VehicleCrash", "Backblast", "Explosive", "Shell", "Grenade"};
