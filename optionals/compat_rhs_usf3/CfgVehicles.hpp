97c02c306b280fe45d7964c31730ef597a61bb16
PabstMirror <pabstmirror@gmail.com>
2016-10-05 13:03:52 -0500

    Disable FCS on RHS helicopters (#4509)
    * Disable FCS on RHS helicopters - Close #4495
    
    * Cleanup

--- optionals/compat_rhs_usf3/CfgVehicles.hpp
+++ optionals/compat_rhs_usf3/CfgVehicles.hpp
@@ -111,6 +111,12 @@ class CfgVehicles {
             };
         };
     };
+    class Air;
+    class Helicopter: Air {
+        class Turrets {
+            class MainTurret;
+        };
+    };
 
     class MBT_01_base_F: Tank_F {};
     class rhsusf_m1a1tank_base: MBT_01_base_F {
@@ -143,7 +149,6 @@ class CfgVehicles {
         ace_repair_hitpointGroups[] = {{"era_1_hitpoint", {"era_2_hitpoint", "era_3_hitpoint", "era_4_hitpoint", "era_5_hitpoint", "era_6_hitpoint", "era_7_hitpoint", "era_8_hitpoint", "era_9_hitpoint", "era_10_hitpoint", "era_11_hitpoint", "era_12_hitpoint", "era_13_hitpoint", "era_14_hitpoint", "era_15_hitpoint", "era_16_hitpoint", "era_17_hitpoint", "era_18_hitpoint", "era_19_hitpoint", "era_20_hitpoint", "era_21_hitpoint", "era_22_hitpoint", "era_23_hitpoint", "era_24_hitpoint", "era_25_hitpoint", "era_26_hitpoint", "era_27_hitpoint", "era_28_hitpoint", "era_29_hitpoint", "era_30_hitpoint", "era_31_hitpoint", "era_32_hitpoint", "era_33_hitpoint", "era_34_hitpoint", "era_35_hitpoint", "era_36_hitpoint", "era_37_hitpoint", "era_38_hitpoint", "era_39_hitpoint", "era_40_hitpoint", "era_41_hitpoint", "era_42_hitpoint", "era_43_hitpoint", "era_44_hitpoint", "era_45_hitpoint", "era_46_hitpoint"}}};
     };
 
-    class Helicopter;
     class Helicopter_Base_F: Helicopter {
         class Eventhandlers;
     };
@@ -254,13 +259,28 @@ class CfgVehicles {
         };
     };
 
-    class Heli_Attack_01_base_F;
+    class Heli_Attack_01_base_F: Helicopter_Base_F {};
+
     class RHS_AH1Z_base: Heli_Attack_01_base_F {
         EGVAR(refuel,fuelCapacity) = 1600;
     };
 
+    class RHS_AH1Z: RHS_AH1Z_base {
+        class Turrets: Turrets {
+            class MainTurret: MainTurret {
+                ace_fcs_Enabled = 0;
+            };
+        };
+    };
     class RHS_AH64_base: Heli_Attack_01_base_F {
         EGVAR(refuel,fuelCapacity) = 1420;
+    };
+    class RHS_AH64D: RHS_AH64_base {
+        class Turrets: Turrets {
+            class MainTurret: MainTurret {
+                ace_fcs_Enabled = 0;
+            };
+        };
     };
 
     class MBT_01_arty_base_F;
