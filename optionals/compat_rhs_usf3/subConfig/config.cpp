c8b259ba7cbf6138d20ebf95f3e8b7c7328347a6
PabstMirror <pabstmirror@gmail.com>
2016-10-08 01:14:43 -0500

    Cleanup RHS 40mm huntIR GL config

--- optionals/compat_rhs_usf3/subConfig/config.cpp
+++ optionals/compat_rhs_usf3/subConfig/config.cpp
@@ -1,6 +1,6 @@
-#include "\z\ace\addons\huntir\script_component.hpp"
+#include "\z\ace\addons\compat_rhs_usf3\script_component.hpp"
 #undef COMPONENT
-#define COMPONENT huntir_sub
+#define COMPONENT compat_rhs_usf3_sub
 
 class CfgPatches {
     class ADDON {
@@ -8,7 +8,7 @@ class CfgPatches {
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"ace_huntir"};
+        requiredAddons[] = {"ace_compat_rhs_usf3"};
         author = ECSTRING(common,ACETeam);
         url = ECSTRING(main,URL);
         VERSION_CONFIG;
@@ -16,10 +16,10 @@ class CfgPatches {
 };
 
 class CfgWeapons {
-    class UGL_F;
-    class Rifle_Base_F;
-    class arifle_MX_Base_F: Rifle_Base_F {
-        class GL_3GL_F: UGL_F {
+    class arifle_MX_Base_F;
+    class rhs_weap_m4_Base: arifle_MX_Base_F {
+        class M203_GL;
+        class M320_GL: M203_GL {
             magazines[] += {"ACE_HuntIR_M203"};
         };
     };
