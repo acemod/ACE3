71884778dd2d4f9c61caca133b2e435eb93916a5
commy2 <commy-2@gmx.de>
2016-10-13 01:16:51 +0200

    animation fixes

--- addons/medical_engine/CfgMoves.hpp
+++ addons/medical_engine/CfgMoves.hpp
@@ -13,7 +13,23 @@ class CfgMovesMaleSdr: CfgMovesBasic {
 
         // wake me up inside
         class Unconscious: Default {
-            InterpolateTo[] = {"DeadState",0.1,"AmovPpneMstpSnonWnonDnon",0.2};
+            InterpolateTo[] = {"DeadState",0.1,"ACE_UnconsciousOutProne",0.2};
+        };
+
+        class AmovPpneMstpSnonWnonDnon;
+        class ACE_UnconsciousOutProne: AmovPpneMstpSnonWnonDnon {
+            //file = "\A3\anims_f\Data\Anim\Sdr\dth\pne\stp\ras\Rfl\AdthPpneMstpSrasWrflDnon_1";
+            looped = 0;
+            canBlendStep = 0;
+            blockMobileSwitching = 1;
+            forceAim = 1;
+            interpolationRestart = 1;
+            variantsPlayer[] = {};
+            variantsAI[] = {};
+            useIdles = 0;
+            InterpolateTo[] = {"Unconscious",0.2};
+            ConnectTo[] = {"AmovPpneMstpSnonWnonDnon",0.2};
+            speed = 100;
         };
     };
 };
