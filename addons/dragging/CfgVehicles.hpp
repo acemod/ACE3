07ebb83281bf8d83adca0e31a2949746d8914bb9
jonpas <jonpas33@gmail.com>
2016-10-29 21:37:50 +0200

    Fix PlasticCase drag direction

--- addons/dragging/CfgVehicles.hpp
+++ addons/dragging/CfgVehicles.hpp
@@ -128,7 +128,7 @@ class CfgVehicles {
 
         GVAR(canDrag) = 1;
         GVAR(dragPosition[]) = {0,1.2,0};
-        GVAR(dragDirection) = 0;
+        GVAR(dragDirection) = 270;
     };
     class MetalCase_01_base_F: Items_base_F {
         class EventHandlers {
