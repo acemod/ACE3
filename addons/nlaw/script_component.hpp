76f50d3b7cf3da3eb427dbb53eee409eba2266d2
PabstMirror <pabstmirror@gmail.com>
2016-12-27 23:39:35 -0600

    Disable Debug

--- addons/nlaw/script_component.hpp
+++ addons/nlaw/script_component.hpp
@@ -2,10 +2,10 @@
 #define COMPONENT_BEAUTIFIED NLAW
 #include "\z\ace\addons\main\script_mod.hpp"
 
-#define DRAW_NLAW_INFO
-#define DEBUG_MODE_FULL
-#define DISABLE_COMPILE_CACHE
-#define ENABLE_PERFORMANCE_COUNTERS
+// #define DRAW_NLAW_INFO
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
 
 #ifdef DEBUG_ENABLED_NLAW
     #define DEBUG_MODE_FULL
