860b478452f9691c54569f3c9fbb49554435f113
PabstMirror <pabstmirror@gmail.com>
2017-01-10 14:17:46 -0600

    Add mags sizes for apache

--- addons/hellfire/CfgMagazines.hpp
+++ addons/hellfire/CfgMagazines.hpp
@@ -8,8 +8,14 @@ class CfgMagazines {
         displayNameShort = CSTRING(AGM114K);
         descriptionShort = CSTRING(AGM114K);
     };
+    class 8Rnd_ACE_Hellfire_AGM114K: 6Rnd_ACE_Hellfire_AGM114K {
+        count = 8;
+    };
     class 12Rnd_ACE_Hellfire_AGM114K: 6Rnd_ACE_Hellfire_AGM114K {
         count = 12;
+    };
+    class 16Rnd_ACE_Hellfire_AGM114K: 6Rnd_ACE_Hellfire_AGM114K {
+        count = 16;
     };
     class 24Rnd_ACE_Hellfire_AGM114K: 6Rnd_ACE_Hellfire_AGM114K {
         count = 24;
