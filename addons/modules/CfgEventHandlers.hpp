92fb9db9edc8f18110ab22b01b0c066bf8517228
Neviothr <Neviothr@users.noreply.github.com>
2017-01-11 15:58:42 +0200

    Cleanup

--- addons/modules/CfgEventHandlers.hpp
+++ addons/modules/CfgEventHandlers.hpp
@@ -1,4 +1,3 @@
-
 class Extended_PreStart_EventHandlers {
     class ADDON {
         init = QUOTE(call COMPILE_FILE(XEH_preStart));
@@ -10,7 +9,6 @@ class Extended_PreInit_EventHandlers {
        init = QUOTE(call COMPILE_FILE(XEH_preInit));
     };
 };
-
 
 class Extended_PostInit_EventHandlers {
     class _ACE_modules { // using a _ so it is the first postInit to be executed
