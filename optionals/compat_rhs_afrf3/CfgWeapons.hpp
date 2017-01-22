c9342ee375c0ab3032bf2b43fe7bf5a59c2c9e56
J3aLeiNe <bal2.chris@orange.fr>
2017-01-22 17:59:54 +0100

    Update CfgWeapons.hpp

--- optionals/compat_rhs_afrf3/CfgWeapons.hpp
+++ optionals/compat_rhs_afrf3/CfgWeapons.hpp
@@ -46,7 +46,11 @@ class CfgWeapons {
         ACE_barrelTwist = 195.072;
         ACE_barrelLength = 589.28;
     };
-
+    class rhs_weap_orsis_Base_F;
+    class rhs_weap_t5000: rhs_weap_orsis_Base_F { // http://en.orsis.com/production/catalog/19046/
+        ACE_barrelTwist = 254.0; // 1:10"
+        ACE_barrelLength = 698.5; // 27.5"
+    };
     class rhs_acc_sniper_base;
     class rhs_acc_pso1m2: rhs_acc_sniper_base {
         ACE_ScopeAdjust_Vertical[] = { 0, 0 };
@@ -60,7 +64,23 @@ class CfgWeapons {
         ACE_ScopeAdjust_VerticalIncrement = 0.5;
         ACE_ScopeAdjust_HorizontalIncrement = 0.5;
     };
-
+    class ItemCore;
+    class InventoryOpticsItem_Base_F;
+    class rhs_acc_dh520x56: ItemCore { // http://nightvision.ru/catalog/4/item/35
+        ACE_ScopeHeightAboveRail = 4.0; // ?
+        ACE_ScopeAdjust_Vertical[] = {0, 33};
+        ACE_ScopeAdjust_Horizontal[] = {-9, 9};
+        ACE_ScopeAdjust_VerticalIncrement = 0.1;
+        ACE_ScopeAdjust_HorizontalIncrement = 0.1;
+        class ItemInfo: InventoryOpticsItem_Base_F {
+            class OpticsModes {
+                class dedal_520 {
+                    discreteDistance[] = {100};
+                    discreteDistanceInitIndex = 0;
+                };
+            };
+        };
+    };
     class Launcher_Base_F;
     class rhs_weap_rpg7: Launcher_Base_F {
         ace_reloadlaunchers_enabled = 1;
