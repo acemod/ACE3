3840cbdac48d1e750153bc3585d62e901503298f
BaerMitUmlaut <BaerMitUmlaut@users.noreply.github.com>
2017-04-01 20:31:19 +0200

    Renamed UI to feedback

--- addons/medical_feedback/script_component.hpp
+++ addons/medical_feedback/script_component.hpp
@@ -1,17 +1,17 @@
-#define COMPONENT medical_ui
-#define COMPONENT_BEAUTIFIED Medical UI
+#define COMPONENT medical_feedback
+#define COMPONENT_BEAUTIFIED Medical Feedback
 #include "\z\ace\addons\main\script_mod.hpp"
 
 // #define DEBUG_MODE_FULL
 #define DISABLE_COMPILE_CACHE
 // #define ENABLE_PERFORMANCE_COUNTERS
 
-#ifdef DEBUG_ENABLED_MEDICAL_UI
+#ifdef DEBUG_ENABLED_MEDICAL_FEEDBACK
     #define DEBUG_MODE_FULL
 #endif
 
-#ifdef DEBUG_SETTINGS_MEDICAL_UI
-    #define DEBUG_SETTINGS DEBUG_SETTINGS_MEDICAL_UI
+#ifdef DEBUG_SETTINGS_MEDICAL_FEEDBACK
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_MEDICAL_FEEDBACK
 #endif
 
 #include "\z\ace\addons\main\script_macros.hpp"
