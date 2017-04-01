54291b3b3bb859845c0301f6db27cbc34a62886e
BaerMitUmlaut <BaerMitUmlaut@users.noreply.github.com>
2017-04-01 20:45:05 +0200

    Renamed menu to gui, moved more gui stuff into it

--- addons/medical_gui/script_component.hpp
+++ addons/medical_gui/script_component.hpp
@@ -1,17 +1,17 @@
-#define COMPONENT medical_menu
-#define COMPONENT_BEAUTIFIED Medical Menu
+#define COMPONENT medical_gui
+#define COMPONENT_BEAUTIFIED Medical GUI
 #include "\z\ace\addons\main\script_mod.hpp"
 
 // #define DEBUG_MODE_FULL
 #define DISABLE_COMPILE_CACHE
 // #define ENABLE_PERFORMANCE_COUNTERS
 
-#ifdef DEBUG_ENABLED_MEDICAL_MENU
+#ifdef DEBUG_ENABLED_MEDICAL_GUI
     #define DEBUG_MODE_FULL
 #endif
 
-#ifdef DEBUG_SETTINGS_MEDICAL_MENU
-    #define DEBUG_SETTINGS DEBUG_SETTINGS_MEDICAL_MENU
+#ifdef DEBUG_SETTINGS_MEDICAL_GUI
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_MEDICAL_GUI
 #endif
 
 #include "\z\ace\addons\main\script_macros.hpp"
