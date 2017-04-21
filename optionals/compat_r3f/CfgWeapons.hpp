369db6073e1302ea4ad2c6f3142115616c56bc62
Laid3acK <bal2.chris@orange.fr>
2017-04-21 11:36:15 +0200

    Update CfgWeapons.hpp

--- optionals/compat_r3f/CfgWeapons.hpp
+++ optionals/compat_r3f/CfgWeapons.hpp
@@ -2,71 +2,88 @@ class CfgWeapons {
     class Pistol_Base_F;
     class Rifle_Base_F;
     class R3F_Famas_F1: Rifle_Base_F {
+        ACE_RailHeightAboveBore = 10.6;
         ACE_barrelTwist = 304.8;
         ACE_barrelLength = 488.0;
     };
     class R3F_Famas_surb: R3F_Famas_F1 {
+        ACE_RailHeightAboveBore = 5.4;
         ACE_barrelTwist = 304.8;
         ACE_barrelLength = 403.86;
     };
     class R3F_FRF2: Rifle_Base_F {
+        ACE_RailHeightAboveBore = 2.2;
         ACE_barrelTwist = 304.8;
         ACE_barrelLength = 650.0;
     };
     class R3F_PGM_Hecate_II: Rifle_Base_F {
+        ACE_RailHeightAboveBore = 2.0;
         ACE_barrelTwist = 381.0;
         ACE_barrelLength = 700.0;
     };
     class R3F_M107: Rifle_Base_F {
+        ACE_RailHeightAboveBore = 3.6;
         ACE_barrelTwist = 381.0;
         ACE_barrelLength = 736.6;
     };
     class R3F_TAC50: Rifle_Base_F {
+        ACE_RailHeightAboveBore = 3.2;
         ACE_barrelTwist = 381.0;
         ACE_barrelLength = 736.6;
     };
     class R3F_Minimi: Rifle_Base_F {
+        ACE_RailHeightAboveBore = 4.0;
         ACE_barrelTwist = 177.8;
         ACE_barrelLength = 347.98;
     };
     class R3F_Minimi_762: R3F_Minimi {
+        ACE_RailHeightAboveBore = 4.0;
         ACE_barrelTwist = 304.8;
         ACE_barrelLength = 502.92;
     };
     class R3F_SIG551: Rifle_Base_F {
+        ACE_RailHeightAboveBore = 4.2;
         ACE_barrelTwist = 177.8;
         ACE_barrelLength = 363.0;
     };
     class R3F_HK417M: Rifle_Base_F {
+        ACE_RailHeightAboveBore = 3.4;
         ACE_barrelTwist = 279.4;
         ACE_barrelLength = 406.0;
     };
     class R3F_HK417S_HG: R3F_HK417M {
+        ACE_RailHeightAboveBore = 3.4;
         ACE_barrelTwist = 279.4;
         ACE_barrelLength = 305.0;
     };
     class R3F_HK417L: R3F_HK417M {
+        ACE_RailHeightAboveBore = 3.4;
         ACE_barrelTwist = 279.4;
         ACE_barrelLength = 508.0;
     };
     class R3F_HK416M: Rifle_Base_F {
+        ACE_RailHeightAboveBore = 3.4;
         ACE_barrelTwist = 177.8;
         ACE_barrelLength = 368.3;
     };
     class R3F_HK416M_HG: R3F_HK416M {};
     class R3F_HK416S_HG: R3F_HK416M_HG
+        ACE_RailHeightAboveBore = 3.4;
         ACE_barrelTwist = 177.8;
         ACE_barrelLength = 279.4;
     };
     class R3F_MP5SD: Rifle_Base_F {
+        ACE_RailHeightAboveBore = 4.5;
         ACE_barrelTwist = 254.0;
         ACE_barrelLength = 144.78;
     };
     class R3F_MP5A5: R3F_MP5SD {
+        ACE_RailHeightAboveBore = 4.5;
         ACE_barrelTwist = 254.0;
         ACE_barrelLength = 226.06;
     };
     class R3F_M4S90: Rifle_Base_F {
+        ACE_RailHeightAboveBore = 2.2;
         ACE_twistDirection = 0;
         ACE_barrelTwist = 0;
         ACE_barrelLength = 144.78;
@@ -123,7 +140,7 @@ class CfgWeapons {
             };
         };
     };
-    class R3F_J8_MILDOT: R3F_J8 { // Scope Mounting Rail 30 MOA
+    class R3F_J8_MILDOT: R3F_J8 { // Scope rail 30 MOA
         ACE_ScopeHeightAboveRail = 4.4;
         ACE_ScopeAdjust_Vertical[] = {-2, 18};
         ACE_ScopeAdjust_Horizontal[] = {-10, 10};
@@ -153,7 +170,7 @@ class CfgWeapons {
             };
         };
     };
-    class R3F_J10_MILDOT: R3F_J10 { // Scope Mounting Rail 30 MOA
+    class R3F_J10_MILDOT: R3F_J10 { // Scope rail 30 MOA
         ACE_ScopeHeightAboveRail = 4.4;
         ACE_ScopeAdjust_Vertical[] = {-2, 18};
         ACE_ScopeAdjust_Horizontal[] = {-10, 10};
