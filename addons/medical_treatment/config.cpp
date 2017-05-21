6460676d98ba3191e0f722a9bce14a152ae5f52a
Arcanum <lubos.len@gmail.com>
2017-05-22 01:57:45 +0200

    Fix authors of medical_treatment
    Felt bad with adding myself as only author and found those two here https://github.com/acemod/ACE3/blame/8b1b31fc8d971039e3211586fed2913f9f98dc01/addons/medical_treatment/config.cpp

--- addons/medical_treatment/config.cpp
+++ addons/medical_treatment/config.cpp
@@ -8,7 +8,7 @@ class CfgPatches {
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {"ace_medical"};
         author = ECSTRING(common,ACETeam);
-        authors[] = {"Arcanum"};
+        authors[] = {"Glowbal", "KoffeinFlummi", "Arcanum"};
         url = ECSTRING(main,URL);
         VERSION_CONFIG;
     };
