22e1cdfe350ecb2604d3945b2d6f1f85f850ada5
Arcanum <lubos.len@gmail.com>
2017-05-22 22:49:27 +0200

    Add myself as Author

--- addons/medical/config.cpp
+++ addons/medical/config.cpp
@@ -8,7 +8,7 @@ class CfgPatches {
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {"ace_common"};
         author = ECSTRING(common,ACETeam);
-        authors[] = {"Glowbal", "KoffeinFlummi"};
+        authors[] = {"Glowbal", "KoffeinFlummi","Arcanum417"};
         url = ECSTRING(main,URL);
         VERSION_CONFIG;
     };
