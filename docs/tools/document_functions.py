028b165135d7e8671cf87aab396fc2b9207038c0
SilentSpike <silentspike100+Github@gmail.com>
2017-06-07 23:27:20 +0100

    Use class based approach for better data handling

--- docs/tools/document_functions.py
+++ docs/tools/document_functions.py
@@ -20,137 +20,168 @@ import sys
 import re
 import argparse
 
-class Writer:
-    def __init__(self, path="", debug=False):
-        self.path = path
-        self.debug = debug
+class FunctionFile:
+    def __init__(self, directory="."):
+        self.directory = directory
+
+        # False unless specified in processing
+        self.debug = False
+
+        # Empty until imported from file
+        self.path = ""
+        self.header = ""
+
+        # False until determined from header
+        self.public = False
+
+        # Filepath should only be logged once
         self.logged = False
+
+    def import_header(self, file_path):
+        self.path = file_path
+
+        with open(file_path) as file:
+            code = file.read()
+
+        header_match = re.match(r"\s*/\*.+?\*/", code, re.S)
+        if header_match:
+            self.header = header_match.group(0)
+        else:
+            self.feedback("Missing header", 3)
+
+    def has_header(self):
+        return bool(self.header)
+
+    def process_header(self, debug=False):
+        # Detailed debugging occurs here so value is set
+        self.debug = debug
+
+        # Preemptively cut away the comment characters (and leading/trailing whitespace)
+        self.header_text = "\n".join([x[3:].strip() for x in self.header.splitlines()])
+
+        # Split the header into expected sections
+        self.sections = re.split(r"^(Author|Argument|Return Value|Example|Public)s?:\s?", self.header_text, 0, re.M)
+
+        # Retrieve the raw public text for processing
+        public_raw = self.get_section("Public")
+
+        # If public section is missing we can't continue
+        if not public_raw:
+            self.feedback("Public value undefined", 3)
+            return
+
+        # Determine whether the header is public
+        self.public = self.process_public(public_raw)
+
+        # Don't bother to process the rest if private
+        # Unless in debug mode
+        if not self.public and not self.debug:
+            return
+
+        # Retrieve the raw sections text for processing
+        author_raw = self.get_section("Author")
+        arguments_raw = self.get_section("Argument")
+        return_value_raw = self.get_section("Return Value")
+        example_raw = self.get_section("Example")
+
+        # Author and description are stored in first section
+        self.authors = self.process_author(author_raw)
+        self.description = self.process_description(author_raw)
+
+    def get_section(self, section_name):
+        try:
+            section_text = self.sections[self.sections.index(section_name) + 1]
+            return section_text
+        except ValueError:
+            self.feedback("Missing \"{}\" header section".format(section_name), 2)
+            return ""
+
+    def process_public(self, raw):
+        # Raw just includes an EOL character
+        public_text = raw[:-1]
+
+        if not re.match(r"(Yes|No)", public_text, re.I):
+            self.feedback("Invalid public value \"{}\"".format(public_text), 2)
+
+        return public_text.capitalize() == "Yes"
+
+    def is_public(self):
+        return self.public
+
+    def process_author(self, raw):
+        # Authors are listed on the first line
+        authors_text = raw.splitlines()[0]
+
+        # Seperate authors are divided by commas
+        return authors_text.split(", ")
+
+    def process_description(self, raw):
+        # Just use all the lines after the authors line
+        description_text = "".join(raw.splitlines(1)[1:])
+
+        return description_text
+
 
     def log_file(self, error=False):
         # When in debug mode we only want to see the files with errors
         if not self.debug or error:
             if not self.logged:
-                self.write("Processing... {}".format(self.path), 1)
+                rel_path = os.path.relpath(self.path, self.directory)
+
+                self.write("Processing... {}".format(rel_path), 1)
                 self.logged = True
 
-    def error(self, message):
-        self.log_file(self.debug)
-        self.write("Error: {}".format(message))
+    def feedback(self, message, level=0):
+        priority_str = ["Info","Warning","Error","Aborted"][level]
+
+        self.log_file(level > 0)
+        self.write("{0}: {1}".format(priority_str, message))
 
     def write(self, message, indent=2):
         to_print = ["  "]*indent
         to_print.append(message)
         print("".join(to_print))
 
+# def process_argument(argument, writer):
+#     definition = re.match(r"^(\d+):\s(.+?)\<(\w+?)\>(\s\(default: (.+)\))?$", argument);
 
-def document_public():
-    return
+#     if definition:
+#         index = definition.group(1)
+#         name = definition.group(2)
+#         data_types = definition.group(3).split("/")
+#         default_value = definition.group(5)
+#     else:
+#         writer.error("Malformed argument {}".format(argument))
+#         return Argument()
+#     return Argument(index, Value(name, data_types), default_value)
 
-def process_author(text, writer):
-    authors = text.splitlines()[0]
-    description = "".join([x[3:] for x in text.splitlines(1)[1:]])
-    return [authors, description]
+# def process_arguments(text, writer):
+#     # Remove the header characters
+#     arguments = [x[3:] for x in text.splitlines(1)]
 
-def process_argument(argument, writer):
-    if argument.startswith("None"):
-        return ["","None","",""]
+#     if arguments[0] == "None":
+#         return [Argument()]
 
-    definition = re.match(r"^(\d+):\s(.+)\<([\s\w]+)\>\s?(\(default: ([\w\s]+)\))?", argument);
+#     # Find lines which new arguments start on (for multi-line descriptions)
+#     arg_indices = [i for i, line in enumerate(arguments) if re.match(r"^\d+:", line)]
 
-    if definition:
-        index = definition.group(1)
-        name = definition.group(2)
-        data_types = definition.group(3)
-        default_value = definition.group(5)
-    else:
-        writer.error("Malformed argument")
-        return ["","","",""]
-    return [index, name, data_types, default_value]
+#     return [process_argument(arguments[i], writer) for i in arg_indices]
 
-def process_arguments(text, writer):
-    # Remove the header characters
-    arguments = [x[3:] for x in text.splitlines(1)]
+# def process_return(text, writer):
+#     # Remove header characters and only use first line
+#     text = text.splitlines()[0][3:]
 
-    # Find lines which new arguments start on (for multi-line descriptions)
-    arg_indices = [i for i, line in enumerate(arguments) if re.match(r"^\d+:", line)]
+#     if text == "None":
+#         return ["None", ""]
 
-    return [process_argument(arguments[i], writer) for i in arg_indices]
+#     valid_value = re.match(r"^(.+)\<([\s\w]+)\>", text)
 
-def process_return(text, writer):
-    # Remove header characters and only use first line
-    text = text.splitlines()[0][3:]
+#     if valid_value:
+#         return [valid_value.group(1), valid_value.group(2)]
+#     else:
+#         writer.error("Malformed \"Return Value\" {}".format(text))
 
-    if text.startswith("None"):
-        return ["None", ""]
-
-    valid_value = re.match(r"^(.+)\<([\s\w]+)\>", text)
-
-    if valid_value:
-        return [valid_value.group(1), valid_value.group(2)]
-    else:
-        writer.error("Malformed \"Return Value\"")
-
-    return ["", ""]
-
-def process_example(text, writer):
-    return
-
-def read_func(code, writer):
-    header = re.match(r"\s*/\*.+?\*/", code, re.S)
-    if header:
-        sections = re.split(r"^\s\*\s+(Author|Argument|Return Value|Example|Public)s?:\s?", header.group(), 0, re.I|re.M)
-
-        # Check if public
-        try:
-            public_value = re.match(r"(Yes|No)", sections[sections.index("Public") + 1], re.I)
-
-            if public_value:
-                is_public = public_value.group().lower() == "yes"
-            else:
-                writer.error("Invalid \"Public\" header value")
-                return
-        except ValueError:
-            writer.error("Missing \"Public\" header section")
-            return
-
-        # Log all functions processed
-        writer.log_file()
-
-        # Process the Author: section
-        try:
-            author_text = sections[sections.index("Author") + 1]
-            author, description = process_author(author_text, writer)
-        except ValueError:
-            writer.error("Missing \"Author\" header section")
-            return
-
-        # Process the Arguments: section
-        try:
-            arguments_text = sections[sections.index("Argument") + 1]
-            arguments = process_arguments(arguments_text, writer)
-        except ValueError:
-            writer.error("Missing \"Arguments\" header section")
-            return
-
-        # Process the Return Value: section
-        try:
-            return_text = sections[sections.index("Return Value") + 1]
-            return_value = process_return(return_text, writer)
-        except ValueError:
-            writer.error("Missing \"Return Value\" header section")
-            return
-
-        # Process the Example: section
-        try:
-            example_text = sections[sections.index("Example") + 1]
-            process_example(example_text, writer)
-        except ValueError:
-            writer.error("Missing \"Example\" header section")
-            return
-    else:
-        writer.error("Invalid header")
-
-    return
+#     return ["", ""]
 
 def crawl_dir(directory, debug=False):
     for root, dirs, files in os.walk(directory):
@@ -158,10 +189,17 @@ def crawl_dir(directory, debug=False):
             if file.endswith(".sqf") and file.startswith("fnc_"):
                 file_path = os.path.join(root, file)
 
-                writer = Writer(os.path.relpath(file_path, directory), debug)
+                # Attempt to import the header from file
+                function = FunctionFile(directory)
+                function.import_header(file_path)
 
-                with open(file_path) as f:
-                    read_func(f.read(), writer)
+                # Undergo data extraction and detailed debug
+                if function.has_header():
+                    function.process_header(debug)
+
+                    # Placeholder
+                    if function.is_public() and not debug:
+                        function.feedback("Publicly documented")
 
 def main():
     print("""
