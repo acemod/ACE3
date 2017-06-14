183eaaecdcf7e7ef6c8d08de15c2cac45246b5ad
PabstMirror <pabstmirror@gmail.com>
2017-06-14 08:35:17 -0500

    Cleanup heli pod actions (#5271)
    Close #4783
    - Make taru pods non loadable
    - Make taru pods non draggable
    - Increase interaction range for huron pods

--- addons/cargo/CfgVehicles.hpp
+++ addons/cargo/CfgVehicles.hpp
@@ -327,26 +327,6 @@ class CfgVehicles {
         GVAR(canLoad) = 1;
     };
 
-    // Taru pods
-    class Pod_Heli_Transport_04_base_F;
-    class Land_Pod_Heli_Transport_04_ammo_F: Pod_Heli_Transport_04_base_F {
-        GVAR(space) = 8;
-        GVAR(hasCargo) = 1;
-    };
-    class Land_Pod_Heli_Transport_04_box_F: Pod_Heli_Transport_04_base_F {
-        GVAR(space) = 20;
-        GVAR(hasCargo) = 1;
-    };
-    class Land_Pod_Heli_Transport_04_repair_F: Pod_Heli_Transport_04_base_F {
-        GVAR(space) = 12;
-        GVAR(hasCargo) = 1;
-    };
-    class Pod_Heli_Transport_04_crewed_base_F;
-    class Land_Pod_Heli_Transport_04_medevac_F: Pod_Heli_Transport_04_crewed_base_F {
-        GVAR(space) = 8;
-        GVAR(hasCargo) = 1;
-    };
-
     class StaticMortar;
     class Mortar_01_base_F: StaticMortar {
         GVAR(size) = 2; // 1 = small, 2 = large
@@ -377,6 +357,37 @@ class CfgVehicles {
     };
     class Slingload_01_Base_F: Slingload_base_F { //Huron 20ft containers
         GVAR(canLoad) = 0;
+        GVAR(size) = -1;
+    };
+
+    // Taru pods
+    class Pod_Heli_Transport_04_base_F: Slingload_base_F {
+        GVAR(canLoad) = 0;
+        GVAR(size) = -1;
+    };
+    class Land_Pod_Heli_Transport_04_ammo_F: Pod_Heli_Transport_04_base_F {
+        GVAR(space) = 8;
+        GVAR(hasCargo) = 1;
+    };
+    class Land_Pod_Heli_Transport_04_box_F: Pod_Heli_Transport_04_base_F {
+        GVAR(space) = 20;
+        GVAR(hasCargo) = 1;
+    };
+    class Land_Pod_Heli_Transport_04_repair_F: Pod_Heli_Transport_04_base_F {
+        GVAR(space) = 12;
+        GVAR(hasCargo) = 1;
+    };
+    class Pod_Heli_Transport_04_crewed_base_F: StaticWeapon {
+        GVAR(canLoad) = 0;
+        GVAR(size) = -1;
+    };
+    class Land_Pod_Heli_Transport_04_covered_F: Pod_Heli_Transport_04_crewed_base_F {
+        GVAR(space) = 8;
+        GVAR(hasCargo) = 1;
+    };
+    class Land_Pod_Heli_Transport_04_medevac_F: Pod_Heli_Transport_04_crewed_base_F {
+        GVAR(space) = 8;
+        GVAR(hasCargo) = 1;
     };
 
     //Plastic and metal case
