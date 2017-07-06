1157bd750f62915ad3f59e41a0510a401a492d85
jokoho48 <hoffmann.jonas95@gmail.com>
2017-07-06 23:04:22 +0200

    inherit in CfgSFX from 1 class

--- addons/cookoff/CfgSFX.hpp
+++ addons/cookoff/CfgSFX.hpp
@@ -2,26 +2,17 @@
 class CfgSFX {
     class GVAR(CookOff_low) {
         name = QGVAR(cookoff_low);
-        // Index 4 is percentage chance to play, in theory high pressure is way more likely
         sound[] = {QPATHTOF(sounds\cookoff_low_pressure.ogg),6,1,400,1,0,0,0};
         sounds[] = {"sound"};
         titles[] = {};
         empty[] = {"",0,0,0,0,0,0,0};
     };
-    class GVAR(CookOff_mid) {
+    class GVAR(CookOff_mid): GVAR(CookOff_low) {
         name = QGVAR(cookoff_mid);
-        // Index 4 is percentage chance to play, in theory high pressure is way more likely
         sound[] = {QPATHTOF(sounds\cookoff_mid_pressure.ogg),6,1,400,1,0,0,0};
-        sounds[] = {"sound"};
-        titles[] = {};
-        empty[] = {"",0,0,0,0,0,0,0};
     };
-    class GVAR(CookOff_high) {
+    class GVAR(CookOff_high): GVAR(CookOff_low) {
         name = QGVAR(cookoff_high);
-        // Index 4 is percentage chance to play, in theory high pressure is way more likely
         sound[] = {QPATHTOF(sounds\cookoff_high_pressure.ogg),6,1,400,1,0,0,0};
-        sounds[] = {"sound"};
-        titles[] = {};
-        empty[] = {"",0,0,0,0,0,0,0};
     };
 };
