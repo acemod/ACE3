9457fc9b9b39af8621c11c7c56f3b2a07aab9c28
jokoho48 <hoffmann.jonas95@gmail.com>
2017-07-06 21:12:58 +0200

    Fix the issue that SFX variants are not global what means that different players can get different pressure Cookoff sounds
    to prevent that i split up the sounds in 3 types and used a Weighted select to have the befor used values back

--- addons/cookoff/CfgSFX.hpp
+++ addons/cookoff/CfgSFX.hpp
@@ -1,12 +1,26 @@
 
 class CfgSFX {
-    class GVAR(CookOff) {
-        name = QGVAR(cookoff);
+    class GVAR(CookOff_low) {
+        name = QGVAR(cookoff_low);
         // Index 4 is percentage chance to play, in theory high pressure is way more likely
-        variant0[] = {PATHTOF(sounds\cookoff_low_pressure.ogg),6,1,400,0.1,0,0,0};
-        variant1[] = {PATHTOF(sounds\cookoff_mid_pressure.ogg),6,1,400,0.25,0,0,0};
-        variant2[] = {PATHTOF(sounds\cookoff_high_pressure.ogg),6,1,400,0.65,0,0,0};
-        sounds[] = {"variant0","variant1","variant2"};
+        sound[] = {QPATHTOF(sounds\cookoff_low_pressure.ogg),6,1,400,1,0,0,0};
+        sounds[] = {"sound"};
+        titles[] = {};
+        empty[] = {"",0,0,0,0,0,0,0};
+    };
+    class GVAR(CookOff_mid) {
+        name = QGVAR(cookoff_mid);
+        // Index 4 is percentage chance to play, in theory high pressure is way more likely
+        sound[] = {QPATHTOF(sounds\cookoff_mid_pressure.ogg),6,1,400,1,0,0,0};
+        sounds[] = {"sound"};
+        titles[] = {};
+        empty[] = {"",0,0,0,0,0,0,0};
+    };
+    class GVAR(CookOff_high) {
+        name = QGVAR(cookoff_high);
+        // Index 4 is percentage chance to play, in theory high pressure is way more likely
+        sound[] = {QPATHTOF(sounds\cookoff_high_pressure.ogg),6,1,400,1,0,0,0};
+        sounds[] = {"sound"};
         titles[] = {};
         empty[] = {"",0,0,0,0,0,0,0};
     };
