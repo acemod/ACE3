84061cb593aee91c439dccbed1c8f5a0e95b052a
jonpas <jonpas@users.noreply.github.com>
2017-07-10 21:41:37 +0200

    Add support for Tanoa and Malden Fuel Pumps, Flexible Tanks and Fuel Canister (#5353)
    * Add support for Tanoan fuel pumps, Flexible Tanks and Fuel Canister (as jerry can)
    
    * Add to Land_FuelStation_01_pump_malevil_F
    
    * Add Malden/Jets to required addons
    
    * REQUIRED_VERSION 1.72

--- addons/main/config.cpp
+++ addons/main/config.cpp
@@ -7,6 +7,19 @@ class CfgPatches {
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {
+            "A3_Data_F_Loadorder",
+            "A3_Data_F_Curator_Loadorder",
+            "A3_Data_F_Kart_Loadorder",
+            "A3_Data_F_Bootcamp_Loadorder",
+            "A3_Data_F_Heli_Loadorder",
+            "A3_Data_F_Mark_Loadorder",
+            "A3_Data_F_Exp_A_Loadorder",
+            "A3_Data_F_Exp_B_Loadorder",
+            "A3_Data_F_Exp_Loadorder",
+            "A3_Data_F_Jets_Loadorder",
+            "A3_Data_F_Argo_Loadorder",
+            "A3_Data_F_Patrol_Loadorder",
+
             // Vanilla
             "a3_3den",
             "a3_3den_language",
--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -9,7 +9,7 @@
 #define VERSION_AR MAJOR,MINOR,PATCHLVL,BUILD
 
 // MINIMAL required version for the Mod. Components can specify others..
-#define REQUIRED_VERSION 1.70
+#define REQUIRED_VERSION 1.72
 #define REQUIRED_CBA_VERSION {3,3,1}
 
 #ifdef COMPONENT_BEAUTIFIED
