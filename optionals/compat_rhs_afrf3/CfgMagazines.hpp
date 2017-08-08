059fa8f116a4a9b8a9a4c538ec1cf2817a2bcafb
CoxLedere <CoxLedere@users.noreply.github.com>
2017-08-08 00:37:43 -0700

    RHS AFRF Disposables

--- optionals/compat_rhs_afrf3/CfgMagazines.hpp
+++ optionals/compat_rhs_afrf3/CfgMagazines.hpp
@@ -1,5 +1,6 @@
 class cfgMagazines {
     class VehicleMagazine;
+    class ACE_PreloadedMissileDummy_Base;
     class rhs_30Rnd_545x39_AK;
 
     class rhs_100Rnd_762x54mmR: rhs_30Rnd_545x39_AK {
@@ -13,5 +14,15 @@ class cfgMagazines {
     };
     class rhs_mag_127x108mm_1470 : rhs_mag_127x108mm_50 {
         ace_isbelt = 0;
+    };
+    class ACE_PreloadedMissileDummy_RPG26: ACE_PreloadedMissileDummy_Base
+    {
+        ammo = "rhs_rpg26_rocket";
+        type = "6 * 256";
+        initSpeed = 149;
+    };
+    class ACE_PreloadedMissileDummy_RSHG2: ACE_PreloadedMissileDummy_RPG26
+    {
+        ammo = "rhs_rshg2_rocket";
     };
 };
--- optionals/compat_rhs_afrf3/CfgWeapons.hpp
+++ optionals/compat_rhs_afrf3/CfgWeapons.hpp
@@ -1,4 +1,5 @@
-
+class WeaponSlotsInfo;
+class Eventhandlers;
 class CfgWeapons {
     class hgun_Rook40_F;
     class rhs_weap_pya: hgun_Rook40_F {
@@ -85,6 +86,43 @@ class CfgWeapons {
     class Launcher_Base_F;
     class rhs_weap_rpg7: Launcher_Base_F {
         ace_reloadlaunchers_enabled = 1;
+    };
+    class rhs_weap_rpg26: Launcher_Base_F {
+        ACE_UsedTube = "rhs_weap_rpg26_used";
+        magazines[] = {"ACE_PreloadedMissileDummy_RPG26"};
+        class WeaponSlotsInfo : WeaponSlotsInfo
+        {
+            mass = 64.2;
+        };
+        class Eventhandlers: Eventhandlers {
+            class RHS_DisposableWeapon {
+                fired = "";
+            };
+        };
+    };
+    class rhs_weap_rshg2 : rhs_weap_rpg26 {
+        ACE_UsedTube = "rhs_weap_rshg2_used";
+        magazines[] = {"ACE_PreloadedMissileDummy_RSHG2"};
+        class WeaponSlotsInfo : WeaponSlotsInfo
+        {
+            mass = 90.6;
+        };
+    };
+    class rhs_weap_rpg26_used: rhs_weap_rpg26 {
+        ACE_isUsedLauncher = 1;
+        magazines[] = {"ACE_FiredMissileDummy"};
+        class WeaponSlotsInfo : WeaponSlotsInfo
+        {
+            mass = 57.2;
+        };
+    };
+    class rhs_weap_rshg2_used: rhs_weap_rshg2 {
+        ACE_isUsedLauncher = 1;
+        magazines[] = {"ACE_FiredMissileDummy"};
+        class WeaponSlotsInfo : WeaponSlotsInfo
+        {
+            mass = 83.6;
+        };
     };
 
     #define HEARING_PROTECTION_VICCREW EGVAR(hearing,protection) = 0.85; EGVAR(hearing,lowerVolume) = 0.6;
