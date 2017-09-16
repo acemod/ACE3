1a1fdb7c2c4ffcab614e8bb7b3ffae92c6c71024
PabstMirror <pabstmirror@gmail.com>
2017-09-16 14:24:43 -0500

    Move the hellfire hud to the laser module (#5503)
    * Move the hellfire hud to the laser module
    
    * Fix example

--- addons/hellfire/ACE_GuidanceConfig.hpp
+++ addons/hellfire/ACE_GuidanceConfig.hpp
@@ -1,6 +1,5 @@
 class EGVAR(missileguidance,AttackProfiles) {
-    class hellfire { 
-        // LOBL and LOAL-DIR behaive the same
+    class hellfire {
         name = "LOAL-DIR";
         nameLocked = "LOBL";
         functionName = QFUNC(attackProfile);
@@ -8,10 +7,12 @@ class EGVAR(missileguidance,AttackProfiles) {
     };
     class hellfire_hi: hellfire {
         name = "LOAL-HI";
+        nameLocked = "LOAL-HI";
         GVAR(launchHeightClear) = 304.8; // clear 1000 ft by 1500m
     };
     class hellfire_lo: hellfire_hi {
         name = "LOAL-LO";
+        nameLocked = "LOAL-LO";
         GVAR(launchHeightClear) = 91.5; // clear 300 ft by 600m
     };
 };
