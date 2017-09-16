a215bc969a58396d98eead4c60f87c9af8712c61
Laid3acK <bal2.chris@orange.fr>
2017-09-16 16:59:12 +0200

    Update CfgMagazines.hpp

--- optionals/compat_r3f/CfgMagazines.hpp
+++ optionals/compat_r3f/CfgMagazines.hpp
@@ -1,6 +1,59 @@
 class CfgMagazines {
     class CA_magazine;
     class R3F_securite_mag: CA_magazine {
-		scope = 0;
+		scope = 0; // default 2
     };
+    class R3F_15Rnd_9x19_PAMAS: CA_magazine {
+		initSpeed = 350; // R3F config
+	};
+	class R3F_15Rnd_9x19_HKUSP: CA_magazine {
+		initSpeed = 350; // R3F config
+	};
+	class R3F_30Rnd_9x19_MP5: CA_magazine {
+		initSpeed = 400; // R3F config
+	};
+	class R3F_25Rnd_556x45_FAMAS: CA_magazine {
+		initSpeed = 960; // R3F config
+	};
+	class R3F_25Rnd_556x45_TRACER_FAMAS: R3F_25Rnd_556x45_FAMAS {};
+	class R3F_30Rnd_556x45_FAMAS: CA_magazine {
+		initSpeed = 925; // R3F config
+	};
+	class R3F_30Rnd_556x45_TRACER_FAMAS: R3F_30Rnd_556x45_FAMAS {};
+	class R3F_30Rnd_556x45_HK416: CA_magazine {
+		initSpeed = 850; // R3F config
+	};
+	class R3F_30Rnd_556x45_TRACER_HK416: R3F_30Rnd_556x45_HK416 {};
+	class R3F_30Rnd_556x45_SIG551: CA_magazine {
+		initSpeed = 850; // R3F config
+	};
+	class R3F_30Rnd_556x45_TRACER_SIG551: R3F_30Rnd_556x45_SIG551 {};
+	class R3F_10Rnd_762x51_FRF2: CA_magazine {
+		initSpeed = 850; // R3F config
+	};
+	class R3F_200Rnd_556x45_MINIMI: CA_magazine {
+		initSpeed = 915; // R3F config
+	};
+	class R3F_100Rnd_762x51_MINIMI: CA_magazine {
+		initSpeed = 820; // R3F config
+	};
+	class R3F_20Rnd_762x51_HK417: CA_magazine {
+		initSpeed = 820; // R3F config
+	};
+	class R3F_20Rnd_762x51_TRACER_HK417: R3F_20Rnd_762x51_HK417 {};
+	class R3F_7Rnd_127x99_PGM: CA_magazine {
+		initSpeed = 780; // R3F config
+	};
+	class R3F_7Rnd_127x99_PEI_PGM: R3F_7Rnd_127x99_PGM {
+		initSpeed = 780; // R3F config
+	};
+	class R3F_10Rnd_127x99_M107: CA_magazine {
+		initSpeed = 850; // R3F config
+	};
+	class R3F_10Rnd_127x99_PEI_M107: R3F_10Rnd_127x99_M107 {
+		initSpeed = 850; // R3F config
+	};
+	class R3F_5Rnd_127x99_TAC50: CA_magazine {
+		initSpeed = 820; // R3F config
+	};
 };
