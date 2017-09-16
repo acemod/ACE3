532890e52a352f420015e501de8d6464788386d0
Laid3acK <bal2.chris@orange.fr>
2017-09-16 17:08:37 +0200

    Update CfgMagazines.hpp

--- optionals/compat_r3f/CfgMagazines.hpp
+++ optionals/compat_r3f/CfgMagazines.hpp
@@ -12,48 +12,48 @@ class CfgMagazines {
 	class R3F_30Rnd_9x19_MP5: CA_magazine {
 		initSpeed = 400; // R3F config
 	};
-	class R3F_25Rnd_556x45_FAMAS: CA_magazine {
+	class R3F_25Rnd_556x45_FAMAS: CA_magazine { // AtragMx GunList: 5.56x45mm M855
 		initSpeed = 960; // R3F config
 	};
-	class R3F_25Rnd_556x45_TRACER_FAMAS: R3F_25Rnd_556x45_FAMAS {};
-	class R3F_30Rnd_556x45_FAMAS: CA_magazine {
+	class R3F_25Rnd_556x45_TRACER_FAMAS: R3F_25Rnd_556x45_FAMAS {}; // AtragMx GunList: 5.56x45mm M855
+	class R3F_30Rnd_556x45_FAMAS: CA_magazine { // AtragMx GunList: 5.56x45mm M855
 		initSpeed = 925; // R3F config
 	};
-	class R3F_30Rnd_556x45_TRACER_FAMAS: R3F_30Rnd_556x45_FAMAS {};
-	class R3F_30Rnd_556x45_HK416: CA_magazine {
+	class R3F_30Rnd_556x45_TRACER_FAMAS: R3F_30Rnd_556x45_FAMAS {}; // AtragMx GunList: 5.56x45mm M855
+	class R3F_30Rnd_556x45_HK416: CA_magazine { // AtragMx GunList: 5.56x45mm M855
 		initSpeed = 850; // R3F config
 	};
-	class R3F_30Rnd_556x45_TRACER_HK416: R3F_30Rnd_556x45_HK416 {};
-	class R3F_30Rnd_556x45_SIG551: CA_magazine {
+	class R3F_30Rnd_556x45_TRACER_HK416: R3F_30Rnd_556x45_HK416 {}; // AtragMx GunList: 5.56x45mm M855
+	class R3F_30Rnd_556x45_SIG551: CA_magazine { // AtragMx GunList: 5.56x45mm M855
 		initSpeed = 850; // R3F config
 	};
-	class R3F_30Rnd_556x45_TRACER_SIG551: R3F_30Rnd_556x45_SIG551 {};
-	class R3F_10Rnd_762x51_FRF2: CA_magazine {
+	class R3F_30Rnd_556x45_TRACER_SIG551: R3F_30Rnd_556x45_SIG551 {}; // AtragMx GunList: 5.56x45mm M855
+	class R3F_10Rnd_762x51_FRF2: CA_magazine { // AtragMx GunList: 5.56x45mm M855
 		initSpeed = 850; // R3F config
 	};
-	class R3F_200Rnd_556x45_MINIMI: CA_magazine {
+	class R3F_200Rnd_556x45_MINIMI: CA_magazine { // AtragMx GunList: 5.56x45mm M855
 		initSpeed = 915; // R3F config
 	};
-	class R3F_100Rnd_762x51_MINIMI: CA_magazine {
+	class R3F_100Rnd_762x51_MINIMI: CA_magazine { // AtragMx GunList: 7.62x51mm M80
 		initSpeed = 820; // R3F config
 	};
-	class R3F_20Rnd_762x51_HK417: CA_magazine {
+	class R3F_20Rnd_762x51_HK417: CA_magazine { // AtragMx GunList: 7.62x51mm M80
 		initSpeed = 820; // R3F config
 	};
-	class R3F_20Rnd_762x51_TRACER_HK417: R3F_20Rnd_762x51_HK417 {};
-	class R3F_7Rnd_127x99_PGM: CA_magazine {
+	class R3F_20Rnd_762x51_TRACER_HK417: R3F_20Rnd_762x51_HK417 {}; // AtragMx GunList: 7.62x51mm M80
+	class R3F_7Rnd_127x99_PGM: CA_magazine { // AtragMx GunList: 5.56x45mm M855
 		initSpeed = 780; // R3F config
 	};
-	class R3F_7Rnd_127x99_PEI_PGM: R3F_7Rnd_127x99_PGM {
+	class R3F_7Rnd_127x99_PEI_PGM: R3F_7Rnd_127x99_PGM { // AtragMx GunList: 12.7x99mm
 		initSpeed = 780; // R3F config
 	};
-	class R3F_10Rnd_127x99_M107: CA_magazine {
+	class R3F_10Rnd_127x99_M107: CA_magazine { // AtragMx GunList: 12.7x99mm
 		initSpeed = 850; // R3F config
 	};
-	class R3F_10Rnd_127x99_PEI_M107: R3F_10Rnd_127x99_M107 {
+	class R3F_10Rnd_127x99_PEI_M107: R3F_10Rnd_127x99_M107 { // AtragMx GunList: 12.7x99mm
 		initSpeed = 850; // R3F config
 	};
-	class R3F_5Rnd_127x99_TAC50: CA_magazine {
+	class R3F_5Rnd_127x99_TAC50: CA_magazine { // AtragMx GunList: 12.7x99mm
 		initSpeed = 820; // R3F config
 	};
 };
