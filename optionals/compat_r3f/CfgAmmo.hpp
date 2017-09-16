9833683f75ebe738332dbeed708e2452cec00c9d
Laid3acK <bal2.chris@orange.fr>
2017-09-16 18:01:34 +0200

    Update CfgAmmo.hpp

--- optionals/compat_r3f/CfgAmmo.hpp
+++ optionals/compat_r3f/CfgAmmo.hpp
@@ -67,7 +67,7 @@ class CfgAmmo {
         ACE_bulletLength = 58.674;
         ACE_bulletMass = 41.9256;
         ACE_ammoTempMuzzleVelocityShifts[] = {-26.55, -25.47, -22.85, -20.12, -16.98, -12.80, -7.64, -1.53, 5.96, 15.17, 26.19};
-        ACE_ballisticCoefficients[] = {0.670};
+        ACE_ballisticCoefficients[] = {0.670}; // AtragMx G1 BC
         ACE_velocityBoundaries[] = {};
         ACE_standardAtmosphere = "ASM";
         ACE_dragModel = 1;
@@ -81,7 +81,7 @@ class CfgAmmo {
         ACE_bulletLength = 58.674;
         ACE_bulletMass = 41.9256;
         ACE_ammoTempMuzzleVelocityShifts[] = {-26.55, -25.47, -22.85, -20.12, -16.98, -12.80, -7.64, -1.53, 5.96, 15.17, 26.19};
-        ACE_ballisticCoefficients[] = {0.670};
+        ACE_ballisticCoefficients[] = {0.670}; // AtragMx G1 BC
         ACE_velocityBoundaries[] = {};
         ACE_standardAtmosphere = "ASM";
         ACE_dragModel = 1;
@@ -95,7 +95,7 @@ class CfgAmmo {
         ACE_bulletLength = 58.674;
         ACE_bulletMass = 41.9256;
         ACE_ammoTempMuzzleVelocityShifts[] = {-26.55, -25.47, -22.85, -20.12, -16.98, -12.80, -7.64, -1.53, 5.96, 15.17, 26.19};
-        ACE_ballisticCoefficients[] = {0.670};
+        ACE_ballisticCoefficients[] = {0.670}; // AtragMx G1 BC
         ACE_velocityBoundaries[] = {};
         ACE_standardAtmosphere = "ASM";
         ACE_dragModel = 1;
@@ -109,7 +109,7 @@ class CfgAmmo {
         ACE_bulletLength = 58.674;
         ACE_bulletMass = 41.9256;
         ACE_ammoTempMuzzleVelocityShifts[] = {-26.55, -25.47, -22.85, -20.12, -16.98, -12.80, -7.64, -1.53, 5.96, 15.17, 26.19};
-        ACE_ballisticCoefficients[] = {0.670};
+        ACE_ballisticCoefficients[] = {0.670}; // AtragMx G1 BC
         ACE_velocityBoundaries[] = {};
         ACE_standardAtmosphere = "ASM";
         ACE_dragModel = 1;
@@ -123,7 +123,7 @@ class CfgAmmo {
         ACE_bulletLength = 58.674;
         ACE_bulletMass = 41.9256;
         ACE_ammoTempMuzzleVelocityShifts[] = {-26.55, -25.47, -22.85, -20.12, -16.98, -12.80, -7.64, -1.53, 5.96, 15.17, 26.19};
-        ACE_ballisticCoefficients[] = {0.670};
+        ACE_ballisticCoefficients[] = {0.670}; // AtragMx G1 BC
         ACE_velocityBoundaries[] = {};
         ACE_standardAtmosphere = "ASM";
         ACE_dragModel = 1;
