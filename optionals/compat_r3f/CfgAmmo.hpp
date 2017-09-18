f7af1531d88893540a0d64eb10f54f9b540995a7
Laid3acK <bal2.chris@orange.fr>
2017-09-18 15:35:59 +0200

    spaces

--- optionals/compat_r3f/CfgAmmo.hpp
+++ optionals/compat_r3f/CfgAmmo.hpp
@@ -46,20 +46,20 @@ class CfgAmmo {
     class R3F_762x51_Ball2: R3F_762x51_Ball { // https://github.com/acemod/ACE3/blob/master/addons/ballistics/CfgAmmo.hpp#L152
         typicalSpeed = 850; // R3F config
         airFriction = -0.00100957; // ACE3 value, default -0.001625
-		ACE_caliber = 7.823;
-		ACE_bulletLength = 28.956;
-		ACE_bulletMass = 9.4608;
-		ACE_ammoTempMuzzleVelocityShifts[] = {-26.55, -25.47, -22.85, -20.12, -16.98, -12.80, -7.64, -1.53, 5.96, 15.17, 26.19};
-		ACE_ballisticCoefficients[] = {0.398}; // AtragMx G1 BC
-		ACE_velocityBoundaries[] = {};
-		ACE_standardAtmosphere = "ICAO";
-		ACE_dragModel = 1;
-		ACE_muzzleVelocities[] = {850};
-		ACE_barrelLengths[] = {650};
-	};
+        ACE_caliber = 7.823;
+        ACE_bulletLength = 28.956;
+        ACE_bulletMass = 9.4608;
+        ACE_ammoTempMuzzleVelocityShifts[] = {-26.55, -25.47, -22.85, -20.12, -16.98, -12.80, -7.64, -1.53, 5.96, 15.17, 26.19};
+        ACE_ballisticCoefficients[] = {0.398}; // AtragMx G1 BC
+        ACE_velocityBoundaries[] = {};
+        ACE_standardAtmosphere = "ICAO";
+        ACE_dragModel = 1;
+        ACE_muzzleVelocities[] = {850};
+        ACE_barrelLengths[] = {650};
+    };
     class R3F_762x51_Minimi_Ball: R3F_762x51_Ball { // https://github.com/acemod/ACE3/blob/master/addons/ballistics/CfgAmmo.hpp#L152
         airFriction = -0.00100957; // ACE3 value, default -0.002000
-	};
+    };
     class R3F_127x99_Ball: BulletBase { // https://github.com/acemod/ACE3/blob/master/addons/ballistics/CfgAmmo.hpp#L494
         typicalSpeed = 780; // R3F config
         airFriction = -0.00057503; // ACE3 value, default -0.00086
