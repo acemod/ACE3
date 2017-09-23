45277c6b5d9341b595ac962acebe2b3684bd86b0
jonpas <jonpas33@gmail.com>
2017-09-23 05:21:16 +0200

    Only allow unloading from outside - fix #5525 as discussed

--- addons/medical/CfgVehicles.hpp
+++ addons/medical/CfgVehicles.hpp
@@ -555,7 +555,7 @@ class CfgVehicles {
                 class GVAR(UnLoadPatient) {
                     displayName = CSTRING(UnloadPatient);
                     distance = 5;
-                    condition = QUOTE(_target getVariable [ARR_2(QUOTE(QUOTE(ACE_isUnconscious)), false)] && {vehicle _target != _target});
+                    condition = QUOTE(_target getVariable [ARR_2(QUOTE(QUOTE(ACE_isUnconscious)), false)] && {vehicle _target != _target} && {vehicle _player == _player});
                     statement = QUOTE([ARR_2(_player, _target)] call DFUNC(actionUnloadUnit));
                     showDisabled = 0;
                     priority = 2;
