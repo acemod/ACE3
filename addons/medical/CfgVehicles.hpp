f645f21af7f45ed48dbe61fb33e461935e9b039d
Ozan Eğitmen <ozanegitmen@gmail.com>
2017-09-29 22:26:18 +0300

    Fix spacing

--- addons/medical/CfgVehicles.hpp
+++ addons/medical/CfgVehicles.hpp
@@ -545,7 +545,7 @@ class CfgVehicles {
                 class GVAR(loadPatient) {
                     displayName = CSTRING(LoadPatient);
                     distance = 5;
-                    condition = QUOTE(_target getVariable [ARR_2(QUOTE(QUOTE(ACE_isUnconscious)),false)] && {alive _target} && {vehicle _target == _target});
+                    condition = QUOTE(_target getVariable [ARR_2(QUOTE(QUOTE(ACE_isUnconscious)), false)] && {alive _target} && {vehicle _target == _target});
                     statement = QUOTE([ARR_2(_player, _target)] call DFUNC(actionLoadUnit));
                     showDisabled = 0;
                     priority = 2;
