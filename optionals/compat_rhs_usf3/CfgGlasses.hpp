155b5b8fa58e0999cb1bcd7dc57ee45cd10e3a83
armyinf0703 <armyinf0703@hotmail.com>
2017-10-24 10:14:00 -0400

    fixes shemagh having overlay despite no goggles

--- optionals/compat_rhs_usf3/CfgGlasses.hpp
+++ optionals/compat_rhs_usf3/CfgGlasses.hpp
@@ -1,4 +1,3 @@
-
 class CfgGlasses {
     class G_Combat;
     class rhs_ess_black: G_Combat {
@@ -72,6 +71,8 @@ class CfgGlasses {
         ACE_TintAmount = 8.0;
     };
     class rhsusf_shemagh_gogg_base: G_Combat {
+        ACE_Color[] = {0,0,0};
+        ACE_TintAmount = 16.0;
         ACE_Overlay = "";
         ACE_OverlayDirt = "A3\Ui_f\data\igui\rsctitles\HealthTextures\dust_upper_ca.paa";
         ACE_OverlayCracked = QPATHTOEF(goggles,textures\HUD\Cracked.paa);
@@ -80,6 +81,24 @@ class CfgGlasses {
         ACE_DustPath = QPATHTOEF(goggles,textures\fx\dust\%1.paa);
     };
     class rhsusf_shemagh2_gogg_base: G_Combat {
+        ACE_Color[] = {0,0,0};
+        ACE_TintAmount = 16.0;
+        ACE_Overlay = "";
+        ACE_OverlayDirt = "A3\Ui_f\data\igui\rsctitles\HealthTextures\dust_upper_ca.paa";
+        ACE_OverlayCracked = QPATHTOEF(goggles,textures\HUD\Cracked.paa);
+        ACE_Resistance = 1;
+        ACE_Protection = 0;
+        ACE_DustPath = QPATHTOEF(goggles,textures\fx\dust\%1.paa);
+    };
+        class rhsusf_shemagh_base: G_Combat {
+        ACE_Overlay = "";
+        ACE_OverlayDirt = "A3\Ui_f\data\igui\rsctitles\HealthTextures\dust_upper_ca.paa";
+        ACE_OverlayCracked = QPATHTOEF(goggles,textures\HUD\Cracked.paa);
+        ACE_Resistance = 1;
+        ACE_Protection = 0;
+        ACE_DustPath = QPATHTOEF(goggles,textures\fx\dust\%1.paa);
+    };
+    class rhsusf_shemagh2_base: G_Combat {
         ACE_Overlay = "";
         ACE_OverlayDirt = "A3\Ui_f\data\igui\rsctitles\HealthTextures\dust_upper_ca.paa";
         ACE_OverlayCracked = QPATHTOEF(goggles,textures\HUD\Cracked.paa);
