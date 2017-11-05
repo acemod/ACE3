f38853b9ef24270a8b01fb6d5c0d6805abf61d17
commy2 <commy-2@gmx.de>
2017-11-05 08:35:03 +0100

    use XEH function instead of config

--- addons/medical/CfgEventHandlers.hpp
+++ addons/medical/CfgEventHandlers.hpp
@@ -16,19 +16,3 @@ class Extended_PostInit_EventHandlers {
         init = QUOTE(call COMPILE_FILE(XEH_postInit));
     };
 };
-
-class Extended_Init_EventHandlers {
-    class ACE_bodyBagObject {
-        class ADDON {
-            init = QUOTE(_this call DEFUNC(dragging,initObject));
-        };
-    };
-};
-
-class Extended_Local_EventHandlers {
-    class CAManBase {
-        class ADDON {
-            local = QUOTE(call FUNC(handleLocal));
-        };
-    };
-};
