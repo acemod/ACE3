3729f377ada2cbf78e5e31f3ec4557d96d077dbd
ulteq <ulteq@users.noreply.github.com>
2017-11-13 18:50:24 +0100

    Sort more settings into categories (#5760)

--- addons/winddeflection/ACE_Settings.hpp
+++ addons/winddeflection/ACE_Settings.hpp
@@ -1,17 +1,20 @@
 class ACE_Settings {
     class GVAR(enabled) {
+        category = CSTRING(windDeflection_DisplayName);
         displayName = CSTRING(deflectionModule_DisplayName);
         description = CSTRING(deflectionModule_Description);
         typeName = "BOOL";
         value = 1;
     };
     class GVAR(vehicleEnabled) {
+        category = CSTRING(windDeflection_DisplayName);
         displayName = CSTRING(vehicleEnabled_DisplayName);
         description = CSTRING(vehicleEnabled_Description);
         typeName = "BOOL";
         value = 1;
     };
     class GVAR(simulationInterval) {
+        category = CSTRING(windDeflection_DisplayName);
         displayName = CSTRING(simulationInterval_DisplayName);
         description = CSTRING(simulationInterval_Description);
         typeName = "SCALAR";
@@ -19,6 +22,7 @@ class ACE_Settings {
         sliderSettings[] = {0, 0.2, 0.05, 1};
     };
     class GVAR(simulationRadius) {
+        category = CSTRING(windDeflection_DisplayName);
         displayName = CSTRING(simulationRadius_DisplayName);
         description = CSTRING(simulationRadius_Description);
         typeName = "SCALAR";
