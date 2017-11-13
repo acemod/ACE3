31aa478f289e25bb361854610af77c630aeb115f
ulteq <ulteq@users.noreply.github.com>
2017-11-13 18:50:08 +0100

    Settings - Utilize 'sliderSettings' (#5756)

--- addons/winddeflection/ACE_Settings.hpp
+++ addons/winddeflection/ACE_Settings.hpp
@@ -16,11 +16,13 @@ class ACE_Settings {
         description = CSTRING(simulationInterval_Description);
         typeName = "SCALAR";
         value = 0.05;
+        sliderSettings[] = {0, 0.2, 0.05, 1};
     };
     class GVAR(simulationRadius) {
         displayName = CSTRING(simulationRadius_DisplayName);
         description = CSTRING(simulationRadius_Description);
         typeName = "SCALAR";
         value = 3000;
+        sliderSettings[] = {0, 3000, 3000, 0};
     };
 };
