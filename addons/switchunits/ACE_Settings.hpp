295e6ca66755b562f5909338ee98186dd8a7442d
ulteq <ulteq@web.de>
2017-11-13 16:43:34 +0100

    Sort more settings into categories

--- addons/switchunits/ACE_Settings.hpp
+++ addons/switchunits/ACE_Settings.hpp
@@ -1,39 +1,46 @@
 class ACE_Settings {
     class GVAR(enableSwitchUnits) {
+        category = CSTRING(Module_DisplayName);
         value = 0;
         typeName = "BOOL";
     };
     class GVAR(switchToWest) {
+        category = CSTRING(Module_DisplayName);
         displayName = CSTRING(SwitchToWest_DisplayName);
         description = CSTRING(SwitchToWest_Description);
         value = 0;
         typeName = "BOOL";
     };
     class GVAR(switchToEast) {
+        category = CSTRING(Module_DisplayName);
         displayName = CSTRING(SwitchToEast_DisplayName);
         description = CSTRING(SwitchToEast_Description);
         value = 0;
         typeName = "BOOL";
     };
     class GVAR(switchToIndependent) {
+        category = CSTRING(Module_DisplayName);
         displayName = CSTRING(SwitchToIndependent_DisplayName);
         description = CSTRING(SwitchToIndependent_Description);
         value = 0;
         typeName = "BOOL";
     };
     class GVAR(switchToCivilian) {
+        category = CSTRING(Module_DisplayName);
         displayName = CSTRING(SwitchToCivilian_DisplayName);
         description = CSTRING(SwitchToCivilian_Description);
         value = 0;
         typeName = "BOOL";
     };
     class GVAR(enableSafeZone) {
+        category = CSTRING(Module_DisplayName);
         displayName = CSTRING(EnableSafeZone_DisplayName);
         description = CSTRING(EnableSafeZone_Description);
         value = 1;
         typeName = "BOOL";
     };
     class GVAR(safeZoneRadius) {
+        category = CSTRING(Module_DisplayName);
         displayName = CSTRING(SafeZoneRadius_DisplayName);
         description = CSTRING(SafeZoneRadius_Description);
         value = 100;
--- addons/weather/ACE_Settings.hpp
+++ addons/weather/ACE_Settings.hpp
@@ -1,11 +1,13 @@
 class ACE_Settings {
     class GVAR(enabled) {
+        category = CSTRING(Module_DisplayName);
         displayName = CSTRING(enabled_DisplayName);
         description = CSTRING(enabled_Description);
         typeName = "BOOL";
         value = 1;
     };
     class GVAR(updateInterval) {
+        category = CSTRING(Module_DisplayName);
         displayName = CSTRING(updateInterval_DisplayName);
         description = CSTRING(updateInterval_Description);
         typeName = "SCALAR";
@@ -13,6 +15,7 @@ class ACE_Settings {
         sliderSettings[] = {0, 300, 0, 0};
     };
     class GVAR(windSimulation) {
+        category = CSTRING(Module_DisplayName);
         displayName = CSTRING(windSimulation_DisplayName);
         description = CSTRING(windSimulation_Description);
         typeName = "BOOL";
