d6164cbc50a267aea0540cd84ee62104e56d79c5
ulteq <ulteq@web.de>
2017-11-20 11:29:36 +0100

    Advanced Ballistics - Muzzle variation: Compatibility update

--- optionals/compat_rhs_afrf3/CfgAmmo.hpp
+++ optionals/compat_rhs_afrf3/CfgAmmo.hpp
@@ -55,6 +55,7 @@ class CfgAmmo {
         ACE_caliber=7.925;
         ACE_bulletLength=28.956;
         ACE_bulletMass=9.8496;
+        ACE_muzzleVelocityVariationSD=0.4;
         ACE_ammoTempMuzzleVelocityShifts[]={-26.55, -25.47, -22.85, -20.12, -16.98, -12.80, -7.64, -1.53, 5.96, 15.17, 26.19};
         ACE_ballisticCoefficients[]={0.4};
         ACE_velocityBoundaries[]={};
