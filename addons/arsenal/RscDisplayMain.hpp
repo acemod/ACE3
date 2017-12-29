f2ca48830c0ab876159e85ee9c7dbc0753c7613a
PabstMirror <pabstmirror@gmail.com>
2017-12-29 12:29:26 -0600

    Arsenal - Add Missing semicolons

--- addons/arsenal/RscDisplayMain.hpp
+++ addons/arsenal/RscDisplayMain.hpp
@@ -17,12 +17,12 @@ class RscDisplayMain: RscStandardDisplay {
                     tooltip = CSTRING(Mission_tooltip);
                     y = "(3 *   1.5) *  (pixelH * pixelGrid * 2) +  (pixelH)";
                     onbuttonclick = QUOTE(playMission [ARR_2('','PATHTOF(missions\Arsenal.VR)')]);
-                    animTextureNormal = QPATHTOF(data\buttonMissionMainMenu_ca.paa)
-                    animTextureDisabled = QPATHTOF(data\buttonMissionMainMenu_ca.paa)
-                    animTextureOver = QPATHTOF(data\buttonMissionMainMenuHover_ca.paa)
-                    animTextureFocused = QPATHTOF(data\buttonMissionMainMenuHover_ca.paa)
-                    animTexturePressed = QPATHTOF(data\buttonMissionMainMenu_ca.paa)
-                    animTextureDefault = QPATHTOF(data\buttonMissionMainMenu_ca.paa)
+                    animTextureNormal = QPATHTOF(data\buttonMissionMainMenu_ca.paa);
+                    animTextureDisabled = QPATHTOF(data\buttonMissionMainMenu_ca.paa);
+                    animTextureOver = QPATHTOF(data\buttonMissionMainMenuHover_ca.paa);
+                    animTextureFocused = QPATHTOF(data\buttonMissionMainMenuHover_ca.paa);
+                    animTexturePressed = QPATHTOF(data\buttonMissionMainMenu_ca.paa);
+                    animTextureDefault = QPATHTOF(data\buttonMissionMainMenu_ca.paa);
                 };
                 class FieldManual: Bootcamp {
                     y = "(4 *   1.5) *  (pixelH * pixelGrid * 2) +  (pixelH)";
