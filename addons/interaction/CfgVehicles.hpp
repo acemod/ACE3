743324807d212a67a8ae15cf95c8cbda018fd0a4
Dystopian <sddex@ya.ru>
2018-01-04 01:09:12 +0300

    Add Flip and Push actions to Quad Bike

--- addons/interaction/CfgVehicles.hpp
+++ addons/interaction/CfgVehicles.hpp
@@ -368,6 +368,24 @@ class CfgVehicles {
         };
     };
 
+    class Car_F: Car{};
+    class Quadbike_01_base_F: Car_F {
+        class ACE_Actions: ACE_Actions {
+            class ACE_MainActions: ACE_MainActions {
+                class GVAR(flip) {
+                    displayName = CSTRING(Flip);
+                    condition = QUOTE(1 < (vectorUp _target) vectorDistance (surfaceNormal getPosATL _target) && {0 == {alive _x} count crew _target});
+                    statement = QUOTE([ARR_3(QQGVAR(flip),_target,_target)] call CBA_fnc_targetEvent);
+                };
+                class GVAR(push) {
+                    displayName = CSTRING(Push);
+                    condition = QUOTE(_target call FUNC(canPush));
+                    statement = QUOTE(call FUNC(push));
+                };
+            };
+        };
+    };
+
     class Tank: LandVehicle {
         class ACE_Actions {
             class ACE_MainActions {
