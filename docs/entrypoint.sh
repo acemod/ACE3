6d1dc4216a46cc1023131c21db86ffcd438e9009
bux <github@jonathandavid.de>
2018-01-03 10:28:47 +0100

    finalize docker fix

--- docs/entrypoint.sh
+++ docs/entrypoint.sh
@@ -1,5 +1,3 @@
-if [[ -z "${INC}" ]]; then
-  jekyll serve --future --config _config_dev.yml -H 0.0.0.0 -P 4000
-else
-  jekyll serve --future --config _config_dev.yml -H 0.0.0.0 -P 4000 --incremental
-fi
+#!/bin/sh
+
+jekyll serve --future --config _config_dev.yml -H 0.0.0.0 -P 4000 ${JEKYLLARGS}
