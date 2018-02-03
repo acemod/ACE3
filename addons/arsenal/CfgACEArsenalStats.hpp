360b4f6a8f809d748227cbfd1529f9043717e309
Josuan Albin <josuanalbin@outlook.fr>
2018-02-03 12:46:49 +0100

    Remove uneeded config entries

--- addons/arsenal/CfgACEArsenalStats.hpp
+++ addons/arsenal/CfgACEArsenalStats.hpp
@@ -13,19 +13,15 @@ class CfgACEArsenalStats {
     };
     class bananaPotassium: statBase {
         scope = 2;
-        priority = 0;
         displayName= CSTRING(statPotassium);
         showBar= 1;
-        showText= 0;
         barStatement = "1";
         condition = "((configName (_this select 1)) == 'ACE_Banana')";
         tabs[]= {{}, {7}};
     };
     class mass: statBase {
         scope = 2;
-        priority = 0;
         displayName= "$STR_a3_rscdisplayarsenal_stat_weight";
-        showBar= 0;
         showText= 1;
         textStatement = QUOTE([ARR_2(_this select 0, _this select 1)] call FUNC(statTextStatement_mass));
         tabs[]= {{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14}, {0,1,2,3,4,5,6,7}};
@@ -58,7 +54,6 @@ class CfgACEArsenalStats {
         stats[]= {"maxZeroing"};
         displayName= "$STR_a3_rscdisplayarsenal_stat_range";
         showBar= 1;
-        showText= 0;
         barStatement = QUOTE([ARR_3((_this select 0) select 0, _this select 1, [ARR_3([ARR_2(0, 2500)], [ARR_2(0.01, 1)], false)])] call FUNC(statBarStatement_default));
         tabs[]= {{0,1,2}, {}};
     };
@@ -68,7 +63,6 @@ class CfgACEArsenalStats {
         stats[]= {"hit", "initSpeed"};
         displayName= "$STR_a3_rscdisplayarsenal_stat_impact";
         showBar= 1;
-        showText= 0;
         barStatement = QUOTE([ARR_3(_this select 0, _this select 1, [ARR_3([ARR_2(0, 3.2)], [ARR_2(-1, 1100)], 2006)])] call FUNC(statBarStatement_impact));
         tabs[]= {{0,1,2}, {}};
     };
@@ -78,7 +72,6 @@ class CfgACEArsenalStats {
         stats[]= {"passthrough"};
         displayName= "$STR_a3_rscdisplayarsenal_stat_passthrough";
         showBar= 1;
-        showText= 0;
         barStatement = QUOTE([ARR_3((_this select 0) select 0, _this select 1, [ARR_3([ARR_2(0, 0.63)], [ARR_2(0.01, 1)], false)])] call FUNC(statBarStatement_default));
         tabs[]= {{3,4,6}, {}};
     };
@@ -88,7 +81,6 @@ class CfgACEArsenalStats {
         stats[]= {"armor"};
         displayName= "$STR_a3_rscdisplayarsenal_stat_armor";
         showBar= 1;
-        showText= 0;
         barStatement = QUOTE([ARR_3((_this select 0) select 0, _this select 1, [ARR_3([ARR_2(0, 0.80)], [ARR_2(0.01, 1)], false)])] call FUNC(statBarStatement_default));
         tabs[]= {{3,4,6}, {}};
     };
@@ -98,7 +90,6 @@ class CfgACEArsenalStats {
         stats[]= {"maximumLoad"};
         displayName= "$STR_a3_rscdisplayarsenal_stat_load";
         showBar= 1;
-        showText= 0;
         barStatement = QUOTE([ARR_3((_this select 0) select 0, _this select 1, [ARR_3([ARR_2(0, 500)], [ARR_2(0.01, 1)], false)])] call FUNC(statBarStatement_default));
         tabs[]= {{3,4,5}, {}};
     };
