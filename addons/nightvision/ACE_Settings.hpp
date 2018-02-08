6499d2ee8ecc900bb169423f8074ca9585accb07
PabstMirror <pabstmirror@gmail.com>
2018-02-08 13:27:33 -0600

    NightVision - Add setting for shutter effects
    Also convert to cba settings
    Close #6119

--- addons/nightvision/ACE_Settings.hpp
+++ addons/nightvision/ACE_Settings.hpp
@@ -1,40 +1,17 @@
 class ACE_Settings {
     class GVAR(disableNVGsWithSights) {
-        category = CSTRING(Category);
-        displayName = CSTRING(DisableNVGsWithSights_DisplayName);
-        description = CSTRING(DisableNVGsWithSights_description);
-        typeName = "BOOL";
-        value = 0;
+        movedToSQF = 1;
     };
     class GVAR(fogScaling) {
-        category = CSTRING(Category);
-        displayName = CSTRING(fogScaling_DisplayName);
-        description = CSTRING(fogScaling_Description);
-        typeName = "SCALAR";
-        value = 1;
-        sliderSettings[] = {0, 2, 1, 1};
+        movedToSQF = 1;
     };
     class GVAR(noiseScaling) {
-        category = CSTRING(Category);
-        displayName = CSTRING(noiseScaling_DisplayName);
-        description = CSTRING(noiseScaling_Description);
-        typeName = "SCALAR";
-        value = 1;
-        sliderSettings[] = {0, 2, 1, 1};
+        movedToSQF = 1;
     };
     class GVAR(effectScaling) {
-        category = CSTRING(Category);
-        displayName = CSTRING(effectScaling_DisplayName);
-        description = CSTRING(effectScaling_Description);
-        typeName = "SCALAR";
-        value = 1;
-        sliderSettings[] = {0, 2, 1, 1};
+        movedToSQF = 1;
     };
     class GVAR(aimDownSightsBlur) {
-        category = CSTRING(Category);
-        displayName = CSTRING(aimDownSightsBlur_DisplayName);
-        typeName = "SCALAR";
-        value = 1;
-        sliderSettings[] = {0, 2, 1, 1};
+        movedToSQF = 1;
     };
 };
