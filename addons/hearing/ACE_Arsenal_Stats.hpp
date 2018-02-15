5d2ab9295e08783811b9702c53704d4d31c382c6
PabstMirror <pabstmirror@gmail.com>
2018-02-15 10:16:54 -0600

    Add missing semicolons

--- addons/hearing/ACE_Arsenal_Stats.hpp
+++ addons/hearing/ACE_Arsenal_Stats.hpp
@@ -15,7 +15,7 @@ class EGVAR(arsenal,stats) {
         stats[] = {QGVAR(lowerVolume)};
         displayName= CSTRING(statHearingLowerVolume);
         showBar = 1;
-        barStatement = QUOTE([ARR_3((_this select 0) select 0, _this select 1, [ARR_3([ARR_2(0, 1)], [ARR_2(0.01, 1)], false)])] call EFUNC(arsenal,statBarStatement_default))
+        barStatement = QUOTE([ARR_3((_this select 0) select 0, _this select 1, [ARR_3([ARR_2(0, 1)], [ARR_2(0.01, 1)], false)])] call EFUNC(arsenal,statBarStatement_default));
         tabs[] = {{6}, {}};
     };
 };
