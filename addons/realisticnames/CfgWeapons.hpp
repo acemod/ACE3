dffa8f62cc8b76db474648392084a831a93c0652
rufix <stefan@ruhfass.com>
2018-04-12 17:02:08 +0200

    Vehicle Vorona -> Metis-M

--- addons/realisticnames/CfgWeapons.hpp
+++ addons/realisticnames/CfgWeapons.hpp
@@ -361,6 +361,9 @@ class CfgWeapons {
     class Missile_AGM_01_Plane_CAS_02_F: Missile_AGM_02_Plane_CAS_01_F {
         displayName = "Kh-25MTP";
     };
+    class missiles_Vorona : MissileLauncher {
+        displayName = CSTRING(missiles_vorona);
+    };
 
     // rockets
     class Rocket_04_HE_Plane_CAS_01_F: RocketPods {
