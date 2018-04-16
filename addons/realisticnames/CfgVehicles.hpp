851c5639e31f4ac0e861b4d51622591c30af298a
rufix <stefan@ruhfass.com>
2018-04-17 01:02:28 +0200

    Removed unnecessary inheritance.

--- addons/realisticnames/CfgVehicles.hpp
+++ addons/realisticnames/CfgVehicles.hpp
@@ -186,7 +186,7 @@ class CfgVehicles {
     class O_APC_Wheeled_02_base_F : APC_Wheeled_02_base_F {
         displayName = CSTRING(APC_Wheeled_02_rcws_Name);
     };
-    class APC_Wheeled_02_base_v2_F : APC_Wheeled_02_base_F{};
+    class APC_Wheeled_02_base_v2_F;
     class O_APC_Wheeled_02_rcws_v2_F : APC_Wheeled_02_base_v2_F {
         displayName = CSTRING(APC_Wheeled_02_rcws_Name);
     };
