6cb43d61eb25d0990e6699d2a5dbd92ccbbb5394
SilentSpike <SilentSpike100+Github@gmail.com>
2018-05-08 13:45:32 +0100

    Fix icon paths

--- addons/medical_gui/ui/menu.hpp
+++ addons/medical_gui/ui/menu.hpp
@@ -274,7 +274,7 @@ class GVAR(medicalMenu) {
             colorBackground[] = {1,1,1,1};
             colorPicture[] = {1,1,1,1};
             colorText[] = {1,1,1,1};
-            text = QPATHTOEF(medical,ui\body_background.paa);
+            text = QPATHTOF(ui\ui\body_background.paa);
         };
         class bodyImgHead: bodyImgBackground {
             idc = 50;
@@ -286,28 +286,28 @@ class GVAR(medicalMenu) {
             colorBackground[] = {1,1,1,1};
             colorPicture[] = {1,1,1,0.75};
             colorText[] = {1,1,1,0.75};
-            text = QPATHTOEF(medical,ui\body_head.paa);
+            text = QPATHTOF(ui\ui\body_head.paa);
         };
 
         class bodyImgTorso: bodyImgHead {
             idc = 51;
-            text = QPATHTOEF(medical,ui\body_torso.paa);
+            text = QPATHTOF(ui\ui\body_torso.paa);
         };
         class bodyImgArms_l: bodyImgHead {
             idc = 52;
-            text = QPATHTOEF(medical,ui\body_arm_left.paa);
+            text = QPATHTOF(ui\ui\body_arm_left.paa);
         };
         class bodyImgArms_r: bodyImgHead {
             idc = 53;
-            text = QPATHTOEF(medical,ui\body_arm_right.paa);
+            text = QPATHTOF(ui\ui\body_arm_right.paa);
         };
         class bodyImgLegs_l: bodyImgHead {
             idc = 54;
-            text = QPATHTOEF(medical,ui\body_leg_left.paa);
+            text = QPATHTOF(ui\ui\body_leg_left.paa);
         };
         class bodyImgLegs_r: bodyImgHead {
             idc = 55;
-            text = QPATHTOEF(medical,ui\body_leg_right.paa);
+            text = QPATHTOF(ui\ui\body_leg_right.paa);
         };
 
 
