a42015e2efee32eb0afc40652fe8fbacc5e878f6
shukari <shukari@users.noreply.github.com>
2018-07-09 16:50:45 +0200

    added apex and tacops fences
    informations from #6420

--- addons/logistics_wirecutter/CfgVehicles.hpp
+++ addons/logistics_wirecutter/CfgVehicles.hpp
@@ -11,16 +11,22 @@ class CfgVehicles {
 
     class Land_Net_Fence_4m_F: Wall_F { GVAR(isFence) = 1; };
     class Land_Net_Fence_8m_F: Wall_F { GVAR(isFence) = 1; };
+	class Land_NetFence_01_m_4m_F: Wall_F { GVAR(isFence) = 1; };
+    class Land_NetFence_01_m_8m_F: Wall_F { GVAR(isFence) = 1; };
+	class Land_Wired_Fence_4m_F: Wall_F { GVAR(isFence) = 1; };
+    class Land_Wired_Fence_8m_F: Wall_F { GVAR(isFence) = 1; };
     class Land_New_WiredFence_5m_F: Wall_F { GVAR(isFence) = 1; };
     class Land_New_WiredFence_10m_F: Wall_F { GVAR(isFence) = 1; };
+	class Land_WiredFence_01_4m_F: Wall_F { GVAR(isFence) = 1; };
+    class Land_WiredFence_01_8m_F: Wall_F { GVAR(isFence) = 1; };
+	class Land_WiredFence_01_16m_F: Wall_F { GVAR(isFence) = 1; };
+	class Land_Mil_WiredFence_F: Wall_F { GVAR(isFence) = 1; };
     class Land_SportGround_fence_F: Wall_F { GVAR(isFence) = 1; };
-    class Land_Wired_Fence_4m_F: Wall_F { GVAR(isFence) = 1; };
-    class Land_Wired_Fence_8m_F: Wall_F { GVAR(isFence) = 1; };
     class Land_SportGround_fence_noLC_F: Wall_F { GVAR(isFence) = 1; };
-    class Land_Mil_WiredFence_F: Wall_F { GVAR(isFence) = 1; };
+	class Land_IndFnc_3_F: Wall_F { GVAR(isFence) = 1; };
+	class Land_IndFnc_3_Hole_F: Wall_F { GVAR(isFence) = 1; };
+	class Land_IndFnc_9_F: Wall_F { GVAR(isFence) = 1; };
     class Land_IndFnc_Corner_F: Wall_F { GVAR(isFence) = 1; };
-    class Land_IndFnc_9_F: Wall_F { GVAR(isFence) = 1; };
-    class Land_IndFnc_3_Hole_F: Wall_F { GVAR(isFence) = 1; };
-    class Land_IndFnc_3_F: Wall_F { GVAR(isFence) = 1; };
+	class Land_Slums01_8m: Wall_F { GVAR(isFence) = 1; };
     class Land_Razorwire_F: NonStrategic { GVAR(isFence) = 1; };
 };
