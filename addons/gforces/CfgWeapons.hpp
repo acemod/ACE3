723df888dbe1dfb5eec623d4e3344a373595d265
Will <shukari94@gmail.com>
2018-07-17 17:17:43 +0200

    Geforce, RHS Geforce adjustments and MH6M can now fastrope

--- addons/gforces/CfgWeapons.hpp
+++ addons/gforces/CfgWeapons.hpp
@@ -7,7 +7,13 @@ class CfgWeapons {
     class U_B_PilotCoveralls: Uniform_Base {
         ACE_GForceCoef = 0.8;
     };
+    class U_B_HeliPilotCoveralls: Uniform_Base {
+        ACE_GForceCoef = 0.8;
+    };
     class U_I_pilotCoveralls: Uniform_Base {
+        ACE_GForceCoef = 0.8;
+    };
+    class U_I_HeliPilotCoveralls: Uniform_Base {
         ACE_GForceCoef = 0.8;
     };
     class U_O_PilotCoveralls: Uniform_Base {
