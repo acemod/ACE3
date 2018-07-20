dd88dc3b1ac66b52e454a5410de715d642f63414
Mezilsa <sennar-gutschnitt@gmx.de>
2018-07-20 15:07:50 +0200

    Update ACE_Settings.hpp

--- addons/microdagr/ACE_Settings.hpp
+++ addons/microdagr/ACE_Settings.hpp
@@ -7,12 +7,14 @@ class ACE_Settings {
         isClientSettable = 0;
         values[] = {CSTRING(MapFill_None), CSTRING(MapFill_OnlyRoads), CSTRING(MapFill_Full)};
     };
-        class GVAR(waypointPrecision) {
+     class GVAR(waypointPrecision) {
+           /*
         displayName = CSTRING(WaypointPrecision_DisplayName);
         description = CSTRING(WaypointPrecision_Description);
         value = 2;
         typeName = "SCALAR";
         isClientSettable = 0;
         values[] = {CSTRING(WaypointPrecision_medium), CSTRING(WaypointPrecision_close),CSTRING(WaypointPrecision_exact)};
+        */
 };
 };
