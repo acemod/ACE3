78ee7d46775aa7aec269d9657f57095966d1e36c
commy2 <commy-2@gmx.de>
2018-07-21 09:16:05 +0200

    simplify animations config

--- addons/medical_engine/CfgMoves.hpp
+++ addons/medical_engine/CfgMoves.hpp
@@ -1,19 +1,11 @@
 
-class CfgMovesBasic {
-    class Default;
-};
-
+class CfgMovesBasic;
 class CfgMovesMaleSdr: CfgMovesBasic {
     class States {
         // fixes hand being stuck at rifle which is on the back
         class AmovPknlMstpSrasWrflDnon_AinvPknlMstpSlayWrflDnon;
         class AinvPknlMstpSlayWnonDnon_medicOther: AmovPknlMstpSrasWrflDnon_AinvPknlMstpSlayWrflDnon {
             weaponIK = 0;
-        };
-
-        // wake me up inside
-        class Unconscious: Default {
-            InterpolateTo[] = {"DeadState",0.1};
         };
 
         class AmovPpneMstpSnonWnonDnon;
