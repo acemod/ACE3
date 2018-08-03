86a0d0a375ac3596ae6cfa5a658bee7d502542b3
pokertour <guillaume_35720@hotmail.fr>
2018-08-03 15:40:32 +0200

    New CFGWeapon

---
+++ addons/fastroping/CfgWeapons.hpp
@@ -0,0 +1,60 @@
+class CfgWeapons {
+    class InventoryItem_Base_F;
+	class ACE_ItemCore;
+
+    class ACE_rope12: ACE_ItemCore {
+        scope = 2;
+        picture = QPATHTOF(data\m_rope_ca);
+        model = "\A3\Structures_F_Heli\Items\Tools\Rope_01_F.p3d";
+        displayName = CSTRING(Rope_12_Display);
+        descriptionShort = CSTRING(Rope_12_Desc_Short);
+        descriptionUse = CSTRING(Rope_12_Desc_Use);
+        class ItemInfo: InventoryItem_Base_F {
+            mass = 36;
+        };
+    };
+	class ACE_rope15: ACE_ItemCore {
+        scope = 2;
+        picture = QPATHTOF(data\m_rope_ca);
+        model = "\A3\Structures_F_Heli\Items\Tools\Rope_01_F.p3d";
+        displayName = CSTRING(Rope_15_Display);
+        descriptionShort = CSTRING(Rope_15_Desc_Short);
+        descriptionUse = CSTRING(Rope_15_Desc_Use);
+        class ItemInfo: InventoryItem_Base_F {
+            mass = 45;
+        };
+    };
+    class ACE_rope18: ACE_ItemCore {
+        scope = 2;
+        picture = QPATHTOF(data\m_rope_ca);
+        model = "\A3\Structures_F_Heli\Items\Tools\Rope_01_F.p3d";
+        displayName = CSTRING(Rope_18_Display);
+        descriptionShort = CSTRING(Rope_18_Desc_Short);
+        descriptionUse = CSTRING(Rope_18_Desc_Use);
+        class ItemInfo: InventoryItem_Base_F {
+            mass = 54;
+        };
+    };
+    class ACE_rope27: ACE_ItemCore {
+        scope = 2;
+        picture = QPATHTOF(data\m_rope_ca);
+        model = "\A3\Structures_F_Heli\Items\Tools\Rope_01_F.p3d";
+        displayName = CSTRING(Rope_27_Display);
+        descriptionShort = CSTRING(Rope_27_Desc_Short);
+        descriptionUse = CSTRING(Rope_27_Desc_Use);
+        class ItemInfo: InventoryItem_Base_F {
+            mass = 81;
+        };
+    };
+    class ACE_rope36: ACE_ItemCore {
+        scope = 2;
+        picture = QPATHTOF(data\m_rope_ca);
+        model = "\A3\Structures_F_Heli\Items\Tools\Rope_01_F.p3d";
+        displayName = CSTRING(Rope_36_Display);
+        descriptionShort = CSTRING(Rope_36_Desc_Short);
+        descriptionUse = CSTRING(Rope_36_Desc_Use);
+        class ItemInfo: InventoryItem_Base_F {
+            mass = 108;
+        };
+    };
+};
