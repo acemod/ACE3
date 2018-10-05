0c9706c4a6df4fdc1684e5d9770b9726721b1bcf
BaerMitUmlaut <baermitumlaut@users.noreply.github.com>
2018-10-05 22:28:42 +0200

    Fix slow motion falling of SLAM mines (#2770)

--- addons/explosives/CfgEventHandlers.hpp
+++ addons/explosives/CfgEventHandlers.hpp
@@ -38,3 +38,9 @@ class Extended_DisplayLoad_EventHandlers {
         ADDON = QUOTE(_this call COMPILE_FILE(XEH_missionDisplayLoad));
     };
 };
+
+class Extended_Init_EventHandlers {
+    class ACE_Explosives_Place_SLAM {
+        ADDON = QUOTE(params ['_mine']; if (local _mine) then {_mine setCenterOfMass [ARR_3(0,-0.035,0.05)]; _mine setMass 5});
+    };
+};
