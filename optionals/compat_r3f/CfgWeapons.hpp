54633fccc6c8c948cc5e08025ed09bc68dce618e
Laid3acK <bal2.chris@orange.fr>
2018-11-06 12:30:35 +0100

    update_r3f_v3.6

--- optionals/compat_r3f/CfgWeapons.hpp
+++ optionals/compat_r3f/CfgWeapons.hpp
@@ -23,10 +23,10 @@ class CfgWeapons {
     class R3F_Famas_F1_M203: R3F_Famas_F1 {
         muzzles[] = {"this","Lance_Grenades"};
     };
-    class R3F_Famas_surb: R3F_Famas_F1 {
+    class R3F_Famas_surb: R3F_Famas_F1 { // R3F FAMAS Surbaissé, should be FAMAS Valorisé : http://narval34.free.fr/fiche_tech_famas.pdf
         ACE_RailHeightAboveBore = 5.08219;
-        ACE_barrelTwist = 228.6; // 1:9"
-        ACE_barrelLength = 450.0; // Beretta barrel
+        ACE_barrelTwist = 228.6; // 1:9" FAMAS Surbaissé, should be 1:7" FAMAS Valorisé
+        ACE_barrelLength = 450.0; // 3D model with Beretta barrel : FAMAS Valorisé
     };
     class R3F_Famas_surb_M203: R3F_Famas_surb {
         muzzles[] = {"this","Lance_Grenades"};
@@ -237,6 +237,12 @@ class CfgWeapons {
         muzzles[] = {"this"};
         initSpeed = -1.0; // default 410
     };
+    class R3F_G17: Pistol_Base_F {
+        ACE_barrelTwist = 250.0;
+        ACE_barrelLength = 114.0;
+        muzzles[] = {"this"};
+        initSpeed = -1.0; // default 410
+    };
     class ItemCore;
     class InventoryOpticsItem_Base_F;
     class R3F_AIMPOINT: ItemCore {
@@ -265,6 +271,31 @@ class CfgWeapons {
     };
     class R3F_FELIN_FRF2: ItemCore {
         ACE_ScopeHeightAboveRail = 4.28091;
+        class ItemInfo: InventoryOpticsItem_Base_F {
+            class OpticsModes {
+                class Felin {};
+                class Oeilleton: Felin {
+                    opticsID=2;
+                    opticsDisplayName="";
+                    useModelOptics=0;
+                    opticsPPEffects[]={};
+                    opticsFlare=0;
+                    opticsDisablePeripherialVision=0;
+                    opticsZoomMin=0.25;
+                    opticsZoomMax=1.25;
+                    opticsZoomInit=0.75;
+                    memoryPointCamera="eye_Oeilleton";
+                    visionMode[]={};
+                    discretefov[]={};
+                    discreteDistance[]={200};
+                    discreteDistanceInitIndex=0;
+                    distanceZoomMin=200;
+                    distanceZoomMax=200;
+                    discreteInitIndex=0;
+                    cameraDir="";
+                };
+            };
+        };
     };
     class R3F_J8: ItemCore { // http://www.scrome.com/assets/templates/flexibility/pdf/Scrome_Marksman_Scope_LTE_Datasheet_GB.pdf
         ACE_ScopeHeightAboveRail = 4.474; // Inaccurate BDC reticle, designed to work with the vanilla ballistic and R3F values.
@@ -492,6 +523,6 @@ class ACE_ATragMX_Presets {
     };
     class R3F_HK417L {
         // Profile Name, Muzzle Velocity, Zero Range, Scope Base Angle, AirFriction, Bore Height, Scope Unit, Scope Click Unit, Scope Click Number, Maximum Elevation, Dialed Elevation, Dialed Windage, Mass, Bullet Diameter, Rifle Twist, BC, Drag Model, Atmosphere Model, Muzzle Velocity vs. Temperature Interpolation, C1 Ballistic Coefficient vs. Distance Interpolation
-        preset[] = {"R3F HK417L M80", 820, 100, 0.0884448, -0.00103711, 7.62, 0, 2, 10, 120, 0, 0, 9.461, 7.82, 27.94, 0.398, 1, "ICAO", {{-15, 801},{0, 808},{10, 815},{15, 820},{25, 834},{30, 843},{35, 854}}, {{0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}}};
+        preset[] = {"R3F HK417L M80", 820, 100, 0.0884448, -0.00103711, 7.62, 0, 2, 10, 120, 0, 0, 9.461, 7.82, 27.94, 0.398, 1, "ICAO", {{-15, 801},{0, 808},{10, 815},{15, 820},{25, 834},{30, 843},{35, 854}}, {{200, 0.398}, {400, 0.398}, {600, 0.398}, {800, 0.39}, {1000, 0.383}, {1200, 0.379}, {1400, 0.378}}};
     };
 };
