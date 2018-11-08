c6a7b87756c286189ad433ca0213a4b0eacd0a8f
Laid3acK <bal2.chris@orange.fr>
2018-11-08 12:38:26 +0100

    update_r3f_v3.6

--- optionals/compat_r3f/CfgMagazines.hpp
+++ optionals/compat_r3f/CfgMagazines.hpp
@@ -1,7 +1,7 @@
 class CfgMagazines {
     class CA_magazine;
     class R3F_securite_mag: CA_magazine {
-        scope = 1; // default 2
+        scope = 1; // Game Update 1.84: "Tweaked: Magazines can now be hidden in Virtual Arsenal by setting their scope to 1", R3F default value 2
     };
     class R3F_15Rnd_9x19_PAMAS: CA_magazine {
         initSpeed = 350; // R3F config
