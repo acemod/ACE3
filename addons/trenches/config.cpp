ae7106143b300e5f3009d476bae283fa7761ee46
Salbei <salbei@gruppe-adler.de>
2018-12-01 21:08:49 +0100

    Fixed Tabs

--- addons/trenches/config.cpp
+++ addons/trenches/config.cpp
@@ -1,20 +1,17 @@
 #include "script_component.hpp"
 
 class CfgPatches {
-    class ADDON {
-        name = COMPONENT_NAME;
-        units[] = {"ACE_envelope_small","ACE_envelope_big","ACE_envelope_gigant","ACE_envelope_vehicle","ACE_envelope_short"};
-        weapons[] = {"ACE_EntrenchingTool"};
-        requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"ace_interaction"};
-        author = ECSTRING(common,ACETeam);
-        authors[] = {"Grey", "esteldunedain", "chris579", "Salbei"};
-        url = ECSTRING(main,URL);
-        //VERSION_CONFIG;
-        version = "3.12.3.36";
-    		versionStr = "3.12.3.36";
-    		versionAr[] = {3,12,3,36};
-    };
+  class ADDON {
+    name = COMPONENT_NAME;
+    units[] = {"ACE_envelope_small","ACE_envelope_big","ACE_envelope_gigant","ACE_envelope_vehicle","ACE_envelope_short"};
+    weapons[] = {"ACE_EntrenchingTool"};
+    requiredVersion = REQUIRED_VERSION;
+    requiredAddons[] = {"ace_interaction"};
+    author = ECSTRING(common,ACETeam);
+    authors[] = {"Grey", "esteldunedain", "chris579", "Salbei"};
+    url = ECSTRING(main,URL);
+    VERSION_CONFIG;
+  };
 };
 
 #include "Cfg3DEN.hpp"
