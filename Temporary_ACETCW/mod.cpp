222bed9784974d34dc243400188fb8a2011fa169
Indominusrexi <37421050+Indominusrexi@users.noreply.github.com>
2018-12-01 02:41:51 +0000

    TCW Temp Mod Added in

---
+++ Temporary_ACETCW/mod.cpp
@@ -0,0 +1,12 @@
+name = "Advanced Combat Environment 3.12.4 (TCW Edition)";
+picture = "logo_ace3_ca.paa";
+actionName = "GitHub";
+action = "https://github.com/acemod/ACE3";
+description = "ACE3 - Version 3.12.4 (TCW Edition)";
+logo = "logo_ace3_ca.paa";
+logoOver = "logo_ace3_ca.paa";
+tooltip = "ACE3 : TCW Edition";
+tooltipOwned = "ACE3 Owned";
+overview = "Advanced Combat Environment 3, also known as ACE3, is a joint effort by the teams behind ACE2, AGM and CSE to improve the realism and authenticity of Arma 3. This is a forked branch of the main ACE3 branch for the TCW mod.";
+author = "ACE3 Team";
+overviewPicture = "logo_ace3_ca.paa";
