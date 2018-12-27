669057477dea07c0e39c77765763df5a83cddb30
commy2 <commy-2@gmx.de>
2018-12-27 14:03:42 +0100

    delete conditional PREP, remove spawn

--- addons/trenches/Cfg3DEN.hpp
+++ addons/trenches/Cfg3DEN.hpp
@@ -1,8 +1,8 @@
 class Cfg3DEN {
     class EventHandlers {
         class ADDON {
-            onMissionPreviewEnd = QUOTE([] spawn {waitUntil {!isNil QQFUNC(initTrench3DEN)}; call FUNC(initTrench3DEN)}); // Don't ask me why but this function is nil when this event triggers
-            onMissionLoad = QUOTE([] spawn {waitUntil {!isNil QQFUNC(initTrench3DEN)}; call FUNC(initTrench3DEN)}); // Don't ask me why but this function is nil when this event triggers
+            onMissionPreviewEnd = QUOTE(with uiNamespace do {call FUNC(initTrench3DEN)});
+            onMissionLoad = QUOTE(with uiNamespace do {call FUNC(initTrench3DEN)});
         };
     };
 };
