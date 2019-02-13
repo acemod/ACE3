92239c498b92b3f4830f6a40294df2448a5cdedb
PabstMirror <pabstmirror@gmail.com>
2019-02-13 12:31:54 -0600

    HuntIR - Move magwell compat to huntIR config

--- addons/huntir/CfgMagazineWells.hpp
+++ addons/huntir/CfgMagazineWells.hpp
@@ -2,7 +2,7 @@ class CfgMagazineWells {
     class CBA_40mm_M203 {
         ADDON[] = {"ACE_HuntIR_M203"};
     };
-    class UGL_40x36 { //Vanilla
+    class UGL_40x36 { //Vanilla and RHS [rhsusf\addons\rhsusf_c_weapons\cfgMagazineWells.hpp]
         ADDON[] = {"ACE_HuntIR_M203"};
     };    
 };
