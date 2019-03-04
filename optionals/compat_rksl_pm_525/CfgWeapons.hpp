fc162566a976a1eb9c2058c5b3556a1a423b385b
Dedmen Miller <dedmen@dedmen.de>
2019-03-04 19:36:32 +0100

    Add rksl pm II 5-25x56 compat

---
+++ optionals/compat_rksl_pm_525/CfgWeapons.hpp
@@ -0,0 +1,21 @@
+
+class CfgWeapons {
+    class ItemCore;
+    class InventoryOpticsItem_Base_F;
+
+    class RKSL_optic_PMII_525 : ItemCore {
+        ACE_ScopeHeightAboveRail = 4.2235;
+        ACE_ScopeAdjust_Vertical[] = { -4, 30 };
+        ACE_ScopeAdjust_Horizontal[] = { -6, 6 };
+        ACE_ScopeAdjust_VerticalIncrement = 0.1;
+        ACE_ScopeAdjust_HorizontalIncrement = 0.1;
+        class ItemInfo : InventoryOpticsItem_Base_F {
+            class OpticsModes {
+                class Snip {
+                    discreteDistance[] = { 100 };
+                    discreteDistanceInitIndex = 0;
+                };
+            };
+        };
+    };
+};
---
+++ optionals/compat_rksl_pm_525/config.cpp
@@ -0,0 +1,17 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"RKSL_PMII_525"};
+        author = ECSTRING(common,ACETeam);
+        authors[] = {"Dedmen"};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgWeapons.hpp"
