7ac58737f4b0bfd6dd37c90625cd383f61590ce9
Dedmen Miller <dedmen@dedmen.de>
2019-03-09 19:36:51 +0100

    Add CTS 7290 9-bang flashbang grenade

--- addons/grenades/CfgAmmo.hpp
+++ addons/grenades/CfgAmmo.hpp
@@ -119,6 +119,13 @@ class CfgAmmo {
         effectsSmoke = "ACE_M84FlashbangEffect";
         whistleDist = 0;
     };
+    
+    class ACE_G_CTS9: ACE_G_M84 {
+        GVAR(flashbang) = 1;
+        model = QPATHTOF(models\ACE_CTS_9bang_thrown.p3d);
+        explosionTime = 1.5;
+        timeToLive = 10;
+    };
 
     class Chemlight_base: SmokeShell {
         GVAR(pullPinSound)[] = {"A3\sounds_f\weapons\Other\dry4.wss", 3, 2, 10};
