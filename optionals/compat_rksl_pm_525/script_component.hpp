7e039e9cb893591edba5b54af022062611b7b923
PabstMirror <pabstmirror@gmail.com>
2019-03-10 11:22:10 -0500

    Combine into existing compat

--- optionals/compat_rksl_pm_525/script_component.hpp
+++
@@ -1,6 +0,0 @@
-#define COMPONENT compat_rksl_pm_525
-#define COMPONENT_BEAUTIFIED RKSL PMII 525 Compatibility
-
-#include "\z\ace\addons\main\script_mod.hpp"
-
-#include "\z\ace\addons\main\script_macros.hpp"
