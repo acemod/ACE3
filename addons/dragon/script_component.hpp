34ccb886c3392d3ff5cf9b3ba5b06af06a4c87ee
Brandon-PC\Brandon <brandondanyluk366@gmail.com>
2019-03-14 15:43:31 -0600

    disable debug

--- addons/dragon/script_component.hpp
+++ addons/dragon/script_component.hpp
@@ -2,9 +2,9 @@
 #define COMPONENT_BEAUTIFIED M47 Dragon
 #include "\z\ace\addons\main\script_mod.hpp"
 
-#define DEBUG_MODE_FULL
-#define DISABLE_COMPILE_CACHE
-#define ENABLE_PERFORMANCE_COUNTERS
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
 
 #ifdef DEBUG_ENABLED_DRAGON
     #define DEBUG_MODE_FULL
