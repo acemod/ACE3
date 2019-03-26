8fdd9a6bc093cad869414721da5c4bf42396f08c
jonpas <jonpas33@gmail.com>
2019-03-26 13:03:11 +0100

    Add base HEMTT config

---
+++ tools/setup.sh
@@ -0,0 +1,12 @@
+VERSION=0.5.1
+ARCH=$(uname -m)
+
+echo "=> Downloading HEMTT..."
+wget -q --show-progress https://github.com/synixebrett/HEMTT/releases/download/v$VERSION/hemtt-v$VERSION-$ARCH-unknown-linux-gnu.tar.gz
+
+echo "=> Extracting HEMTT..."
+tar -xzf hemtt-v$VERSION-$ARCH-unknown-linux-gnu.tar.gz -C ../.
+rm hemtt-v$VERSION-$ARCH-unknown-linux-gnu.tar.gz
+
+echo "=> Updating HEMTT..."
+../hemtt update
