e2f2eb97af253b977fc09e35952dc328383a53bb
commy2 <commy-2@gmx.de>
2019-05-05 13:51:58 +0200

    vector IV updates

--- addons/vector/CfgWeapons.hpp
+++ addons/vector/CfgWeapons.hpp
@@ -1,4 +1,3 @@
-
 class CfgWeapons {
     class Binocular;
     class ACE_Vector: Binocular {
@@ -6,13 +5,30 @@ class CfgWeapons {
         displayName = CSTRING(VectorName);
         descriptionShort = CSTRING(VectorDescription);
         model = QPATHTOF(ace_vector.p3d);
-        modelOptics = QPATHTOF(ace_vector_optics.p3d);
         picture = QPATHTOF(UI\ace_vector_x_ca.paa);
         visionMode[] = {"Normal","NVG"};
-        opticsZoomMax = 0.06621;
-        opticsZoomMin = 0.06621;
-        weaponInfoType = "ACE_RscOptics_vector";
+        opticsZoomMax = 0.25/7;
+        opticsZoomMin = 0.25/7;
+        modelOptics = "\x\cba\addons\optics\cba_optic_big_100.p3d";
+        //weaponInfoType = "ACE_RscOptics_vector";
+
+        class CBA_ScriptedOptic {
+            minMagnificationReticleScale[] = {5,1.026};
+            maxMagnificationReticleScale[] = {25,25*1.026};
+
+            reticleDetailTextures[] = {
+                // start at > this magnification, reticleTexture, reticleTextureSize, reticleTextureNight (optional)
+                {0,  PATHTOF(data\reticles\ace_vector_reticle_1_ca.paa), 1},
+                {9,  PATHTOF(data\reticles\ace_vector_reticle_2_ca.paa), 1/2}
+            };
+
+            bodyTexture = QPATHTOF(data\reticles\ace_vector_body_co.paa);
+            //bodyTextureNight = QPATHTOF(data\reticles\ace_vector_body_co.paa);
+            bodyTextureSize = 1.15;
+        };
+        weaponInfoType = "CBA_ScriptedOptic_zooming";
     };
+
     class ACE_VectorDay: ACE_Vector {
         author = ECSTRING(common,ACETeam);
         displayName = CSTRING(VectorDayName);
