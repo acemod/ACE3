40f0affb7cf589b4f410df71339527ab217d8d25
commy2 <commy-2@gmx.de>
2019-05-11 20:10:23 +0200

    fix spotting scope attenuation

--- addons/spottingscope/CfgVehicles.hpp
+++ addons/spottingscope/CfgVehicles.hpp
@@ -27,23 +27,16 @@ class CfgVehicles {
 
     class LandVehicle;
     class StaticWeapon: LandVehicle {
-        class Turrets;
+        class Turrets {
+            class MainTurret;
+        };
+
         class ACE_Actions {
             class ACE_MainActions;
         };
     };
 
-    class StaticATWeapon: StaticWeapon {
-        class Turrets: Turrets {
-            class MainTurret;
-        };
-
-        class ACE_Actions: ACE_Actions {
-            class ACE_MainActions: ACE_MainActions {};
-        };
-    };
-
-    class ACE_SpottingScopeObject: StaticATWeapon {
+    class ACE_SpottingScopeObject: StaticWeapon {
         EGVAR(dragging,canDrag) = 1;
         EGVAR(dragging,dragPosition)[] = {0,1,0};
         EGVAR(dragging,dragDirection) = 0;
@@ -86,6 +79,7 @@ class CfgVehicles {
         accuracy = 0.12;
         cost = 10000;
         icon = "\A3\Static_F_Gamma\data\UI\map_StaticTurret_AT_CA.paa";
+        attenuationEffectType = "";
 
         class SpeechVariants {
             class Default {
