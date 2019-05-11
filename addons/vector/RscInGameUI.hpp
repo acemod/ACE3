43636975ef7cd452f4526fa1e3b7ad33d603e349
commy2 <commy-2@gmx.de>
2019-05-11 13:23:28 +0200

    idc macros, fix illum mode

--- addons/vector/RscInGameUI.hpp
+++ addons/vector/RscInGameUI.hpp
@@ -6,7 +6,7 @@ class RscInGameUI {
     class CBA_ScriptedOptic;
     class ACE_RscOptics_vector: CBA_ScriptedOptic {
         controls[] = {
-            "CA_FOVMode","ScopeBlack","Reticle","BodyNight","BodyDay","TrippleHeadLeft","TrippleHeadRight","Magnification","ActiveDisplayHelper",
+            "CA_FOVMode","ScopeBlack","Reticle","BodyNight","BodyDay","TrippleHeadLeft","TrippleHeadRight",/**/"CA_Zeroing",/**/"Magnification","ActiveDisplayHelper",
             "CA_Distance","CA_Heading","CA_OpticsPitch","CA_Elev","CA_OpticsZoom","CA_VisionMode","ACE_ScriptedDisplayControlsGroup"
         };
 
@@ -15,31 +15,26 @@ class RscInGameUI {
             w = 0;
             h = 0;
         };
-
         class CA_Heading: RscText {
             idc = 156; // azimuth
             w = 0;
             h = 0;
         };
-
         class CA_OpticsPitch: RscText {
             idc = 182; // inclination
             w = 0;
             h = 0;
         };
-
         class CA_Elev: RscText {
             idc = 175; // inclination, more accurate
             w = 0;
             h = 0;
         };
-
         class CA_OpticsZoom: RscText {
             idc = 180; // some kind of zoom
             w = 0;
             h = 0;
         };
-
         class CA_VisionMode: RscText {
             idc = 179; // ???
             w = 0;
@@ -56,16 +51,15 @@ class RscInGameUI {
 
             class controls {
                 class Center: RscPicture {
-                    idc = 1301;
+                    idc = IDC_CENTER;
                     colorText[] = {1,0,0,0.5};
                     x = "0.5 - (0.8 / 16 * safezoneW)/2 - safezoneX";
                     y = "0.5 - (0.8 / 9 * safezoneH)/2 - safezoneY";
                     w = "0.8 / 16 * safezoneW";
                     h = "0.8 / 9 * safezoneH";
                 };
-
                 class Crosshair: Center {
-                    idc = 1302;
+                    idc = IDC_CROSSHAIR;
                     x = "0.5 - (0.7 / 16 * safezoneW)/2 - safezoneX";
                     y = "0.5 - (0.7 / 9 * safezoneH)/2 - safezoneY";
                     w = "0.7 / 16 * safezoneW";
@@ -73,76 +67,64 @@ class RscInGameUI {
                 };
 
                 class Digit0: Center {
-                    idc = 1310;
+                    idc = IDC_DIGIT_0;
                     x = "(0.54 + 0 * 0.02) * safezoneW";
                     y = "0.54 * safezoneH";
                     w = "0.5 / 16 * safezoneW";
                     h = "0.5 / 9 * safezoneH";
                 };
-
                 class Digit1: Digit0 {
-                    idc = 1311;
+                    idc = IDC_DIGIT_1;
                     x = "(0.54 + 1 * 0.02) * safezoneW";
                 };
-
                 class Digit2: Digit0 {
-                    idc = 1312;
+                    idc = IDC_DIGIT_2;
                     x = "(0.54 + 2 * 0.02) * safezoneW";
                 };
-
                 class Digit3: Digit0 {
-                    idc = 1313;
+                    idc = IDC_DIGIT_3;
                     x = "(0.54 + 3 * 0.02) * safezoneW";
                 };
-
                 class Digit4: Digit0 {
-                    idc = 1314;
+                    idc = IDC_DIGIT_4;
                     x = "(0.54 + 4 * 0.02) * safezoneW";
                 };
-
                 class Digit5: Digit0 {
-                    idc = 1315;
+                    idc = IDC_DIGIT_5;
                     x = "(0.35 + 0 * 0.02) * safezoneW";
                 };
-
                 class Digit6: Digit0 {
-                    idc = 1316;
+                    idc = IDC_DIGIT_6;
                     x = "(0.35 + 1 * 0.02) * safezoneW";
                 };
-
                 class Digit7: Digit0 {
-                    idc = 1317;
+                    idc = IDC_DIGIT_7;
                     x = "(0.35 + 2 * 0.02) * safezoneW";
                 };
-
                 class Digit8: Digit0 {
-                    idc = 1318;
+                    idc = IDC_DIGIT_8;
                     x = "(0.35 + 3 * 0.02) * safezoneW";
                 };
-
                 class Digit9: Digit0 {
-                    idc = 1319;
+                    idc = IDC_DIGIT_9;
                     x = "(0.35 + 4 * 0.02) * safezoneW";
                 };
 
                 class DigitE1: Digit0 {
-                    idc = 1321;
+                    idc = IDC_DIGIT_E1;
                     x = "(0.39 + 0 * 0.02) * safezoneW";
                     y = "0.42 * safezoneH";
                 };
-
                 class DigitE2: DigitE1 {
-                    idc = 1322;
+                    idc = IDC_DIGIT_E2;
                     x = "(0.39 + 1 * 0.02) * safezoneW";
                 };
-
                 class DigitE3: DigitE1 {
-                    idc = 1323;
+                    idc = IDC_DIGIT_E3;
                     x = "(0.39 + 2 * 0.02) * safezoneW";
                 };
-
                 class DigitE4: DigitE1 {
-                    idc = 1324;
+                    idc = IDC_DIGIT_E4;
                     x = "(0.39 + 3 * 0.02) * safezoneW";
                 };
             };
