0697344ae14d3db495b1cdb1c1970b88e7a6e37f
PabstMirror <pabstmirror@gmail.com>
2019-06-29 00:06:58 -0500

    oh no tabs

--- optionals/compat_rhs_afrf3/CfgMagazines.hpp
+++ optionals/compat_rhs_afrf3/CfgMagazines.hpp
@@ -20,7 +20,7 @@ class cfgMagazines {
     class RHS_mag_GPD30_30: VehicleMagazine {
         ace_isbelt = 1;
     };
-    
+
     // RHS magazines for crew handled ammo
     class rhs_mag_9M131M;
     class GVAR(mag_9M131M): rhs_mag_9M131M {
@@ -29,7 +29,7 @@ class cfgMagazines {
         count = 1;
         mass = 55;
         model = "rhsafrf\addons\rhs_heavyweapons\atgm\rhs_metis_emptytube.p3d";
-		picture = "\A3\Weapons_F_Tank\Launchers\Vorona\Data\UI\icon_rocket_vorona_HEAT_F_ca.paa";
+        picture = "\A3\Weapons_F_Tank\Launchers\Vorona\Data\UI\icon_rocket_vorona_HEAT_F_ca.paa";
     };
     class rhs_mag_9M131F;
     class GVAR(mag_9M131F): rhs_mag_9M131F {
@@ -38,7 +38,7 @@ class cfgMagazines {
         count = 1;
         mass = 55;
         model = "rhsafrf\addons\rhs_heavyweapons\atgm\rhs_metis_emptytube.p3d";
-		picture = "\A3\Weapons_F_Tank\Launchers\Vorona\Data\UI\icon_rocket_vorona_HEAT_F_ca.paa";
+        picture = "\A3\Weapons_F_Tank\Launchers\Vorona\Data\UI\icon_rocket_vorona_HEAT_F_ca.paa";
     };
     class rhs_mag_9m133;
     class GVAR(mag_9m133): rhs_mag_9m133 {
@@ -47,7 +47,7 @@ class cfgMagazines {
         count = 1;
         mass = 55;
         model = "rhsafrf\addons\rhs_heavyweapons\atgm\rhs_kornet_emptytube.p3d";
-		picture = "\A3\Weapons_F_Tank\Launchers\Vorona\Data\UI\icon_rocket_vorona_HEAT_F_ca.paa";
+        picture = "\A3\Weapons_F_Tank\Launchers\Vorona\Data\UI\icon_rocket_vorona_HEAT_F_ca.paa";
     };
     class rhs_mag_9m133f;
     class GVAR(mag_9m133f): rhs_mag_9m133f {
@@ -56,7 +56,7 @@ class cfgMagazines {
         count = 1;
         mass = 55;
         model = "rhsafrf\addons\rhs_heavyweapons\atgm\rhs_kornet_emptytube.p3d";
-		picture = "\A3\Weapons_F_Tank\Launchers\Vorona\Data\UI\icon_rocket_vorona_HEAT_F_ca.paa";
+        picture = "\A3\Weapons_F_Tank\Launchers\Vorona\Data\UI\icon_rocket_vorona_HEAT_F_ca.paa";
     };
     class rhs_mag_9m1331;
     class GVAR(mag_9m1331): rhs_mag_9m1331 {
@@ -65,7 +65,7 @@ class cfgMagazines {
         count = 1;
         mass = 55;
         model = "rhsafrf\addons\rhs_heavyweapons\atgm\rhs_kornet_emptytube.p3d";
-		picture = "\A3\Weapons_F_Tank\Launchers\Vorona\Data\UI\icon_rocket_vorona_HEAT_F_ca.paa";
+        picture = "\A3\Weapons_F_Tank\Launchers\Vorona\Data\UI\icon_rocket_vorona_HEAT_F_ca.paa";
     };
     class rhs_mag_9m133m2;
     class GVAR(mag_9m133m2): rhs_mag_9m133m2 {
@@ -74,7 +74,7 @@ class cfgMagazines {
         count = 1;
         mass = 55;
         model = "rhsafrf\addons\rhs_heavyweapons\atgm\rhs_kornet_emptytube.p3d";
-		picture = "\A3\Weapons_F_Tank\Launchers\Vorona\Data\UI\icon_rocket_vorona_HEAT_F_ca.paa";
+        picture = "\A3\Weapons_F_Tank\Launchers\Vorona\Data\UI\icon_rocket_vorona_HEAT_F_ca.paa";
     };
     class rhs_mag_PG9V;
     class GVAR(mag_PG9V): rhs_mag_PG9V {
@@ -83,7 +83,7 @@ class cfgMagazines {
         count = 1;
         mass = 80;
         model = "\A3\Structures_F_EPB\Items\Military\Ammobox_rounds_F.p3d";
-		picture = "\A3\Weapons_F_Exp\Launchers\RPG7\Data\UI\icon_rocket_RPG7_CA.paa";
+        picture = "\A3\Weapons_F_Exp\Launchers\RPG7\Data\UI\icon_rocket_RPG7_CA.paa";
     };
     class rhs_mag_PG9N;
     class GVAR(mag_PG9N): rhs_mag_PG9N {
@@ -92,7 +92,7 @@ class cfgMagazines {
         count = 1;
         mass = 80;
         model = "\A3\Structures_F_EPB\Items\Military\Ammobox_rounds_F.p3d";
-		picture = "\A3\Weapons_F_Exp\Launchers\RPG7\Data\UI\icon_rocket_RPG7_CA.paa";
+        picture = "\A3\Weapons_F_Exp\Launchers\RPG7\Data\UI\icon_rocket_RPG7_CA.paa";
     };
     class rhs_mag_PG9VNT;
     class GVAR(mag_PG9VNT): rhs_mag_PG9VNT {
@@ -101,7 +101,7 @@ class cfgMagazines {
         count = 1;
         mass = 80;
         model = "\A3\Structures_F_EPB\Items\Military\Ammobox_rounds_F.p3d";
-		picture = "\A3\Weapons_F_Exp\Launchers\RPG7\Data\UI\icon_rocket_RPG7_CA.paa";
+        picture = "\A3\Weapons_F_Exp\Launchers\RPG7\Data\UI\icon_rocket_RPG7_CA.paa";
     };
     class rhs_mag_OG9VM;
     class GVAR(mag_OG9VM): rhs_mag_OG9VM {
@@ -110,7 +110,7 @@ class cfgMagazines {
         count = 1;
         mass = 80;
         model = "\A3\Structures_F_EPB\Items\Military\Ammobox_rounds_F.p3d";
-		picture = "\A3\Weapons_F_Exp\Launchers\RPG7\Data\UI\icon_rocket_RPG7_CA.paa";
+        picture = "\A3\Weapons_F_Exp\Launchers\RPG7\Data\UI\icon_rocket_RPG7_CA.paa";
     };
     class rhs_mag_OG9V;
     class GVAR(mag_OG9V): rhs_mag_OG9V {
@@ -119,16 +119,16 @@ class cfgMagazines {
         count = 1;
         mass = 80;
         model = "\A3\Structures_F_EPB\Items\Military\Ammobox_rounds_F.p3d";
-		picture = "\A3\Weapons_F_Exp\Launchers\RPG7\Data\UI\icon_rocket_RPG7_CA.paa";
+        picture = "\A3\Weapons_F_Exp\Launchers\RPG7\Data\UI\icon_rocket_RPG7_CA.paa";
     };
-    
+
     class GVAR(mag_VOG30_30): RHS_mag_VOG30_30 {
         scope = 2;
         type = 256;
         count = 30;
         mass = 40;
         model = "\A3\Structures_F_EPB\Items\Military\Ammobox_rounds_F.p3d";
-		picture = QPATHTOEF(csw,UI\ammoBox_50bmg_ca.paa);
+        picture = QPATHTOEF(csw,UI\ammoBox_50bmg_ca.paa);
     };
     class GVAR(mag_GPD30_30): RHS_mag_GPD30_30 {
         scope = 2;
@@ -136,7 +136,7 @@ class cfgMagazines {
         count = 30;
         mass = 40;
         model = "\A3\Structures_F_EPB\Items\Military\Ammobox_rounds_F.p3d";
-		picture = QPATHTOEF(csw,UI\ammoBox_50bmg_ca.paa);
+        picture = QPATHTOEF(csw,UI\ammoBox_50bmg_ca.paa);
     };
     class RHS_mag_VOG17m_30;
     class GVAR(mag_VOG17m_30): RHS_mag_VOG17m_30 {
@@ -145,7 +145,7 @@ class cfgMagazines {
         count = 30;
         mass = 40;
         model = "\A3\Structures_F_EPB\Items\Military\Ammobox_rounds_F.p3d";
-		picture = QPATHTOEF(csw,UI\ammoBox_50bmg_ca.paa);
+        picture = QPATHTOEF(csw,UI\ammoBox_50bmg_ca.paa);
     };
-    
+
 };
