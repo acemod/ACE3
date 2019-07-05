c7ddbd476021b78866e94b14eeee9ac36895d1fd
PabstMirror <pabstmirror@gmail.com>
2019-07-05 15:19:39 -0500

    Medical - add dummy cfgVehicle entries for old modules

--- addons/medical/CfgVehicles.hpp
+++ addons/medical/CfgVehicles.hpp
@@ -1,7 +1,21 @@
 class CfgVehicles {
+    // Left as dumb modules so that old missions don't error about missing vehicles
     class Logic;
-    // Left as dumb logic so that old missions don't error about missing vehicle
-    class ACE_moduleMedicalSettings: Logic {
-        scope = 1;
+    class Module_F: Logic {
+        class EventHandlers;
     };
+    class ACE_moduleMedicalSettings: Module_F {
+        author = ECSTRING(common,ACETeam);
+        scope = 1;
+        displayName = "[ACE] Retired Medical module (will have no effect)";
+        class EventHandlers: EventHandlers {
+            init = "diag_log text format ['[ACE] (Medical) Warning retired module [%1] placed (will have no effect)', typeOf (_this select 0)];";
+        };
+    };
+    class ACE_moduleBasicMedicalSettings: ACE_moduleMedicalSettings {};
+    class ACE_moduleAdvancedMedicalSettings: ACE_moduleMedicalSettings {};
+    class ACE_moduleReviveSettings: ACE_moduleMedicalSettings {};
+    class ACE_moduleAssignMedicRoles: ACE_moduleMedicalSettings {};
+    class ACE_moduleAssignMedicVehicle: ACE_moduleMedicalSettings {};
+    class ACE_moduleAssignMedicalFacility: ACE_moduleMedicalSettings {};
 };
--- addons/medical_treatment/script_component.hpp
+++ addons/medical_treatment/script_component.hpp
@@ -2,9 +2,9 @@
 #define COMPONENT_BEAUTIFIED Medical Treatment
 #include "\z\ace\addons\main\script_mod.hpp"
 
-#define DEBUG_MODE_FULL
-#define DISABLE_COMPILE_CACHE
-#define ENABLE_PERFORMANCE_COUNTERS
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
 
 #ifdef DEBUG_ENABLED_MEDICAL_TREATMENT
     #define DEBUG_MODE_FULL
