71a7ef90f7279a27e8b9dda41c7d8532cf2a4361
Rik <Neciota@users.noreply.github.com>
2019-07-09 15:34:40 +0200

    Update addons/nametags/CfgFactionClasses.hpp
    Co-Authored-By: Dedmen Miller <dedmen@users.noreply.github.com>

--- addons/nametags/CfgFactionClasses.hpp
+++ addons/nametags/CfgFactionClasses.hpp
@@ -1,6 +1,14 @@
 class CfgFactionClasses {
     class OPF_F {
-        GVAR(rankIcons)[] = {QPATHTOF(UI\icons_russia\private_gs.paa), QPATHTOF(UI\icons_russia\corporal_gs.paa), QPATHTOF(UI\icons_russia\sergeant_gs.paa), QPATHTOF(UI\icons_russia\lieutenant_gs.paa), QPATHTOF(UI\icons_russia\captain_gs.paa), QPATHTOF(UI\icons_russia\major_gs.paa), QPATHTOF(UI\icons_russia\colonel_gs.paa)};
+        GVAR(rankIcons)[] = {
+            QPATHTOF(UI\icons_russia\private_gs.paa),
+            QPATHTOF(UI\icons_russia\corporal_gs.paa),
+            QPATHTOF(UI\icons_russia\sergeant_gs.paa),
+            QPATHTOF(UI\icons_russia\lieutenant_gs.paa),
+            QPATHTOF(UI\icons_russia\captain_gs.paa),
+            QPATHTOF(UI\icons_russia\major_gs.paa),
+            QPATHTOF(UI\icons_russia\colonel_gs.paa)
+        };
     };
     class OPF_G_F {
         GVAR(rankIcons)[] = {QPATHTOF(UI\icons_spain\private_gs.paa), QPATHTOF(UI\icons_spain\corporal_gs.paa), QPATHTOF(UI\icons_spain\sergeant_gs.paa), QPATHTOF(UI\icons_spain\lieutenant_gs.paa), QPATHTOF(UI\icons_spain\captain_gs.paa), QPATHTOF(UI\icons_spain\major_gs.paa), QPATHTOF(UI\icons_spain\colonel_gs.paa)};
