cf389fba76a203b057acf26481020b24f6b561ed
Whigital <andr3as.j4nss0n@gmail.com>
2019-07-12 17:04:12 +0200

    [compat_rhs_usf3] ACE Explosives functionality for RHS USAF (#7099)
    * Enable ACE Explosives functionality for RHS USAF explosives
    
    * Update with requested changes
    
    * Reorder requiredAddons as per suggestion

--- optionals/compat_rhs_usf3/CfgMagazines.hpp
+++ optionals/compat_rhs_usf3/CfgMagazines.hpp
@@ -51,4 +51,60 @@ class cfgMagazines {
         ammo = "ACE_Hellfire_AGM114N";
     };
 
+    class rhsusf_m112_mag: CA_Magazine {
+        ace_explosives_DelayTime = 1;
+        ace_explosives_Placeable = 1;
+        ace_explosives_SetupObject = "ACE_Explosives_Place_rhsusf_m112_DemoCharge";
+        useAction = 0;
+        class ACE_Triggers {
+            SupportedTriggers[] = {"Timer", "Command", "MK16_Transmitter", "DeadmanSwitch"};
+            class Timer {
+                FuseTime = 0.5;
+            };
+            class Command {
+                FuseTime = 0.5;
+            };
+            class MK16_Transmitter: Command {};
+            class DeadmanSwitch: Command {};
+        };
+    };
+
+    class rhsusf_m112x4_mag: CA_Magazine {
+        ace_explosives_DelayTime = 1;
+        ace_explosives_Placeable = 1;
+        ace_explosives_SetupObject = "ACE_Explosives_Place_rhsusf_m112x4_DemoCharge";
+        useAction = 0;
+        class ACE_Triggers {
+            SupportedTriggers[] = {"Timer", "Command", "MK16_Transmitter", "DeadmanSwitch"};
+            class Timer {
+                FuseTime = 0.5;
+            };
+            class Command {
+                FuseTime = 0.5;
+            };
+            class MK16_Transmitter: Command {};
+            class DeadmanSwitch: Command {};
+        };
+    };
+
+    class ATMine_Range_Mag;
+    class rhs_mine_M19_mag: ATMine_Range_Mag {
+        ace_explosives_SetupObject = "ACE_Explosives_Place_rhs_mine_M19_Mine";
+        class ACE_Triggers {
+            SupportedTriggers[] = {"PressurePlate"};
+            class PressurePlate {
+                digDistance = 0.075;
+            };
+        };
+    };
+
+    class rhsusf_mine_m14_mag: ATMine_Range_Mag {
+        ace_explosives_SetupObject = "ACE_Explosives_Place_rhsusf_mine_m14_mag_Mine";
+        class ACE_Triggers {
+            SupportedTriggers[] = {"PressurePlate"};
+            class PressurePlate {
+                digDistance = 0.03;
+            };
+        };
+    };
 };
