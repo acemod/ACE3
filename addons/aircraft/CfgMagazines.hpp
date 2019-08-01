539c4a7a1053fbd8999decc924c45b8ed146944b
LorenLuke <LukeLLL@aol.com>
2019-08-01 02:21:28 -0700

    Adds Combat Mix Magazine

--- addons/aircraft/CfgMagazines.hpp
+++ addons/aircraft/CfgMagazines.hpp
@@ -4,6 +4,11 @@ class CfgMagazines {
     class 1000Rnd_Gatling_30mm_Plane_CAS_01_F: VehicleMagazine {
         count = 1170;
     };
+    
+    class ACE_1000Rnd_Gatling_30mm_Plane_CAS_CM: VehicleMagazine {
+        count = 1170;
+        ammo = "ACE_30mm_mix";
+    };
 
     // an extended magazine for the comanche
     class 300Rnd_20mm_shells;
