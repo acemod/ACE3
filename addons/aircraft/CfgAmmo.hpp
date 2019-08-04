e2c6dee3d94f7d291ad39312c7257f25b83d563f
LorenLuke <LukeLLL@aol.com>
2019-08-04 12:35:37 -0700

    Fixes deafness issue

--- addons/aircraft/CfgAmmo.hpp
+++ addons/aircraft/CfgAmmo.hpp
@@ -67,6 +67,7 @@ class CfgAmmo {
         submunitionConeAngle = 0.056; // in degrees, 0.055 ~= 0.001 mils minute, but present
         model = "\A3\Weapons_f\Data\bullettracer\tracer_red.p3d";
         triggerTime = 0.005;
+        ACE_caliber = 1.4;
    };
 
    class ACE_Gatling_30mm_Sub_AP: ACE_Gatling_30mm_Sub_HEI {
