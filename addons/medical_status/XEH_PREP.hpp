d6c39e661f82015b281c098394f73c7c24b76799
PabstMirror <pabstmirror@gmail.com>
2019-09-05 15:56:14 -0500

    Medical StateMachine - Tweak dead state handling (#7061)
    * Medical StateMachine - Tweak dead state handling
    
    - Don't call setDammage inside HandleDamage EH
    - Add death reason on entering death state
    
    * add a getVar disable in case this causes problems
    
    * Cleanup
    
    * cleanup
    
    * Update addons/medical_statemachine/functions/fnc_enteredStateDeath.sqf
    
    Co-Authored-By: Ferran Obon <magnetar5891@gmail.com>

--- addons/medical_status/XEH_PREP.hpp
+++ addons/medical_status/XEH_PREP.hpp
@@ -4,6 +4,7 @@ PREP(getBloodLoss);
 PREP(getBloodPressure);
 PREP(getBloodVolumeChange);
 PREP(getCardiacOutput);
+PREP(handleKilled);
 PREP(hasStableVitals);
 PREP(initUnit);
 PREP(isBeingCarried);
