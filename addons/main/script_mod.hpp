9cf3d1c02d9750e886cf56045151d1e1e7ad4bad
jonpas <jonpas33@gmail.com>
2019-09-06 21:40:58 +0200

    Pretty align

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -5,9 +5,9 @@
 
 #include "script_version.hpp"
 
-#define VERSION MAJOR.MINOR
+#define VERSION     MAJOR.MINOR
 #define VERSION_STR MAJOR.MINOR.PATCHLVL.BUILD
-#define VERSION_AR MAJOR,MINOR,PATCHLVL,BUILD
+#define VERSION_AR  MAJOR,MINOR,PATCHLVL,BUILD
 
 // MINIMAL required version for the Mod. Components can specify others..
 #define REQUIRED_VERSION 1.94
