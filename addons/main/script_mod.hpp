635ee11fd19ec6890b3f7e15a6a0150d1ecc552e
jonpas <jonpas33@gmail.com>
2019-09-06 21:39:10 +0200

    Use CBA Versioning System properly

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -5,7 +5,8 @@
 
 #include "script_version.hpp"
 
-#define VERSION MAJOR.MINOR.PATCHLVL.BUILD
+#define VERSION MAJOR.MINOR
+#define VERSION_STR MAJOR.MINOR.PATCHLVL.BUILD
 #define VERSION_AR MAJOR,MINOR,PATCHLVL,BUILD
 
 // MINIMAL required version for the Mod. Components can specify others..
