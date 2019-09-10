5dbaef4e07ddaf8e5d11f374f4ec02086c4082aa
mharis001 <34453221+mharis001@users.noreply.github.com>
2019-09-10 00:30:56 -0400

    Improve medical statemachine settings (#7154)
    * Improve medical statemachine settings
    
    * Multi-line condition and update fatal injury description
    
    * Check isPlayer in DeathAI transition
    
    Co-Authored-By: PabstMirror <pabstmirror@gmail.com>

--- addons/medical_statemachine/script_component.hpp
+++ addons/medical_statemachine/script_component.hpp
@@ -16,3 +16,7 @@
 
 #include "\z\ace\addons\medical_engine\script_macros_medical.hpp"
 #include "\z\ace\addons\main\script_macros.hpp"
+
+#define FATAL_INJURIES_ALWAYS 0
+#define FATAL_INJURIES_CRDC_ARRST 1
+#define FATAL_INJURIES_NEVER 2
