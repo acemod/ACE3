516145210fda3e6f3ccb456c1ebb1015eab173af
bux <github@jonathandavid.de>
2019-11-21 18:20:05 +0100

    Add Maglite ML300L

--- addons/flashlights/CfgWeapons.hpp
+++ addons/flashlights/CfgWeapons.hpp
@@ -1,5 +1,49 @@
 class CfgWeapons {
 
+    class Pistol_Base_F;
+    class ACE_Flashlight_Maglite_ML300L: Pistol_Base_F {
+        author = ECSTRING(common,ACETeam);
+        displayName = CSTRING(Maglite_ML300L_DisplayName);
+        descriptionShort = CSTRING(Maglite_ML300L_Description);
+        model = QPATHTOF(data\maglite_ml300l.p3d);
+        picture = QPATHTOF(UI\maglite_ml300l_ca.paa);
+        scope = 2;
+
+        magazines[] = {};
+        recoil = "";
+		recoilProne = "";
+		distanceZoomMin = 50;
+		distanceZoomMax = 50;
+        autoFire = 0;
+
+        class FlashLight
+		{
+			color[] = {180,180,190};
+            ambient[] = {0.9,0.81,0.7};
+            intensity = 85;
+            size = 1;
+            innerAngle = 5;
+            outerAngle = 60;
+            coneFadeCoef = 8;
+			position = "flash dir";
+            direction = "flash";
+			useFlare = 1;
+			flareSize = 1.4;
+            flareMaxDistance = 100;
+            dayLight = 1;
+			class Attenuation {
+                start = 0;
+                constant = 0.7;
+                linear = 0.5;
+                quadratic = 0.6;
+                hardLimitStart = 45;
+                hardLimitEnd = 60;
+            };
+            scale[] = {0};
+		};
+    };
+
+
     class ACE_ItemCore;
     class CBA_MiscItem_ItemInfo;
 
