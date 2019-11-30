8435497fe6589fab6e5e4c710f88893589cf5b15
commy2 <commy-2@gmx.de>
2019-11-30 18:05:02 +0100

    fix vehicle config, add gwh

--- addons/flashlights/config.cpp
+++ addons/flashlights/config.cpp
@@ -3,7 +3,12 @@
 class CfgPatches {
     class ADDON {
         name = COMPONENT_NAME;
-        units[] = {};
+        units[] = {
+            "ACE_Flashlight_MX991Item",
+            "ACE_Flashlight_KSF1Item",
+            "ACE_Flashlight_XL50Item",
+            "ACE_Item_Flashlight_Maglite_ML300L"
+        };
         weapons[] = {
             "ACE_Flashlight_MX991",
             "ACE_Flashlight_KSF1",
@@ -13,7 +18,7 @@ class CfgPatches {
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {"ace_interaction"};
         author = ECSTRING(common,ACETeam);
-        authors[] = {"voiper", "bux", "HorribleGoat"};
+        authors[] = {"voiper","bux","HorribleGoat"};
         url = ECSTRING(main,URL);
         VERSION_CONFIG;
     };
