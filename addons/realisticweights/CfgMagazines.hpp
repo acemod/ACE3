16967d0041ee5ca41c6b3e3d5f6627648460d244
Broström.A | Evul <andreas.brostrom.ce@gmail.com>
2020-01-16 17:34:59 +0100

    Removed smoke mass adjustment

--- addons/realisticweights/CfgMagazines.hpp
+++ addons/realisticweights/CfgMagazines.hpp
@@ -6,24 +6,6 @@ class CfgMagazines {
     class HandGrenade_Stone: HandGrenade {
         mass = 10;
     };
-    class SmokeShell: HandGrenade {
-        mass = 14.99;
-    };
-    class SmokeShellBlue: SmokeShell {
-        mass = 11.85;
-    };
-    class SmokeShellGreen: SmokeShell {
-        mass = 11.85;
-    };
-    class SmokeShellOrange: SmokeShell {
-        mass = 11.85;
-    };
-    class SmokeShellPurple: SmokeShell {
-        mass = 11.85;
-    };
-    class SmokeShellYellow: SmokeShell {
-        mass = 11.85;
-    };
     class MiniGrenade: CA_Magazine {
         mass = 3;
     };
