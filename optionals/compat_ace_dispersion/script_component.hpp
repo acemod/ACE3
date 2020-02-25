416046b802cabd6497c12c501301e31076af98f1
Laid3acK <bal2.chris@orange.fr>
2020-02-25 08:54:45 +0100

    compat_dispersion

---
+++ optionals/compat_ace_dispersion/script_component.hpp
@@ -0,0 +1,6 @@
+#define COMPONENT compat_ace_dispersion
+#define COMPONENT_BEAUTIFIED ACE3 Dispersion Compatibility
+
+#include "\z\ace\addons\main\script_mod.hpp"
+
+#include "\z\ace\addons\main\script_macros.hpp"
