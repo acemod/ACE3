9441f30c09267bdd89edc8ec53b06e7c215a341f
Brandon Danyluk <brandondanyluk366@gmail.com>
2020-03-02 21:18:51 -0700

    disable compile cache

--- addons/fire/script_component.hpp
+++ addons/fire/script_component.hpp
@@ -3,7 +3,7 @@
 #include "\z\ace\addons\main\script_mod.hpp"
 
 // #define DEBUG_MODE_FULL
- #define DISABLE_COMPILE_CACHE
+// #define DISABLE_COMPILE_CACHE
 // #define CBA_DEBUG_SYNCHRONOUS
 // #define ENABLE_PERFORMANCE_COUNTERS
 
