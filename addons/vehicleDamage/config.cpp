c2c63e5a67cbb613e6a1a96f699cb25351812a1d
Brandon Danyluk <brandondanyluk366@gmail.com>
2020-03-04 10:46:41 -0700

    remove vanilla config requirements

--- addons/vehicleDamage/config.cpp
+++ addons/vehicleDamage/config.cpp
@@ -7,11 +7,7 @@ class CfgPatches {
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
         // ammo/vehicle config defines touch all of these
-        requiredAddons[] = {
-            "ace_common", "ace_cookoff", "ace_dragon", "ace_hot", "ace_aircraft", "ace_ballistics", "ace_hellfire", "ace_frag", 
-            "A3_Weapons_F", "A3_Weapons_F_Tank", "A3_Weapons_F_Mark", "A3_Weapons_F_Jets", "A3_Weapons_F_Destroyer",
-            "A3_Armor_F", "A3_Armor_F_EPB", "A3_Armor_F_EPC", "A3_Armor_F_Gamma", "A3_Armor_F_Tank"
-        };
+        requiredAddons[] = { "ace_common", "ace_cookoff", "ace_dragon", "ace_hot", "ace_aircraft", "ace_ballistics", "ace_hellfire", "ace_frag" };
         author = ECSTRING(common,ACETeam);
         authors[] = {""};
         url = ECSTRING(main,URL);
