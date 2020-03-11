75610116da3567067d796a6cae2fcb9c8d82a0fe
PabstMirror <pabstmirror@gmail.com>
2020-03-11 10:36:23 -0500

    Arsenal - Fix main menu mission

--- addons/arsenal/missions/Arsenal.VR/CfgEventHandlers.hpp
+++ addons/arsenal/missions/Arsenal.VR/CfgEventHandlers.hpp
@@ -1,11 +1,11 @@
 class Extended_PreInit_EventHandlers {
     class ADDON {
-        init = QUOTE(call COMPILE_FILE(XEH_preInit));
+        init = QUOTE(call compile preprocessFileLineNumbers 'XEH_preInit.sqf');
     };
 };
 
 class Extended_PostInit_EventHandlers {
     class ADDON {
-        init = QUOTE(call COMPILE_FILE(XEH_postInit));
+        init = QUOTE(call compile preprocessFileLineNumbers 'XEH_postInit.sqf');
     };
 };
