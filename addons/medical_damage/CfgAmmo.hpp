6309ffd38720b1d38ebfa67fa09a4baab5ae89a5
Pterolatypus <pterolatypus@gmail.com>
2020-03-18 23:32:05 +0000

    Fixed up inheritance Some classes were not editable; changes moved to relevant subclasses instead

--- addons/medical_damage/CfgAmmo.hpp
+++ addons/medical_damage/CfgAmmo.hpp
@@ -7,19 +7,36 @@ class CfgAmmo {
     class ShotgunBase: ShotgunCore {
         ACE_damageType = "bullet";
     };
+
     class Default;
-    class Grenade: Default {
-        ACE_damageType = "grenade";
-    };
-    class GrenadeCore: Default {
-        ACE_damageType = "grenade";
-    };
-    class TimeBombCore: Default {
-        ACE_damageType = "explosive";
-    };
     class FuelExplosion: Default {
         ACE_damageType = "explosive";
     };
+    class Grenade: Default {
+        ACE_damageType = "grenade";
+    };
+    class GrenadeCore;
+    class GrenadeBase: GrenadeCore {
+        ACE_damageType = "grenade";
+    };
+
+    class MineCore;
+    class MineBase: MineCore {
+        ACE_damageType = "explosive";
+    };
+    class PipeBombCore;
+    class PipeBombBase: PipeBombCore {
+        ACE_damageType = "explosive";
+    };
+    class DirectionalBombCore;
+    class DirectionalBombBase: DirectionalBombCore {
+        ACE_damageType = "explosive";
+    };
+    class BoundingMineCore;
+    class BoundingMineBase: BoundingMineCore {
+        ACE_damageType = "explosive";
+    };
+
     class RocketCore;
     class RocketBase: RocketCore {
         ACE_damageType = "explosive";
@@ -28,19 +45,48 @@ class CfgAmmo {
     class MissileBase: MissileCore {
         ACE_damageType = "explosive";
     };
-    class BombCore: Default {
+
+    class SubmunitionCore;
+    class SubmunitionBase: SubmunitionCore {
         ACE_damageType = "explosive";
     };
+    class SubmunitionBullet: SubmunitionBase {
+        ACE_damageType = "bullet";
+    };
+
     class ShellCore;
     class ShellBase: ShellCore {
         ACE_damageType = "shell";
     };
+
+    // There is no BombBase so we modify these separately
+    class BombCore;
+    class Bo_Mk82: BombCore {
+        ACE_damageType = "explosive";
+    };
+    class ammo_Bomb_LaserGuidedBase: BombCore {
+        ACE_damageType = "explosive";
+    };
+    class BombDemine_01_Ammo_F: BombCore {
+        ACE_damageType = "explosive";
+    };
     
-    // Autocannon rounds
+    // Autocannon rounds are special (#7401)
     class B_19mm_HE: BulletBase {
         ACE_damageType = "explosive";
     };
+    class B_20mm: BulletBase {
+        ACE_damageType = "explosive";
+    };
+    class B_25mm: BulletBase {
+        ACE_damageType = "explosive";
+    };
     class B_35mm_AA: BulletBase {
+        ACE_damageType = "explosive";
+    };
+
+    // These are also special
+    class Gatling_30mm_HE_Plane_CAS_01_F: BulletBase {
         ACE_damageType = "explosive";
     };
 };
