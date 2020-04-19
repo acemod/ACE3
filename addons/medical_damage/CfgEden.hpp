09f6a2dbe77ad3a63c88d59f514637ea02d4f3a4
diwako <diwako@gmail.com>
2020-04-19 23:44:38 +0200

    Change control text when default value should be used

--- addons/medical_damage/CfgEden.hpp
+++ addons/medical_damage/CfgEden.hpp
@@ -6,7 +6,7 @@ class Cfg3DEN {
             private _slider = _ctrlGroup controlsGroupCtrl 100;\
             private _edit = _ctrlGroup controlsGroupCtrl 101;\
             _slider sliderSetPosition _value;\
-            _edit ctrlSetText ([_value, 1, 1] call CBA_fnc_formatNumber);";
+            _edit ctrlSetText (if (_value < 0.1) then {localize ""str_disp_default""} else {[_value, 1, 1] call CBA_fnc_formatNumber});";
             attributeSave = "params [""_ctrlGroup""];\
             sliderPosition (_ctrlGroup controlsGroupCtrl 100); ";
             onLoad = "params [""_ctrlGroup""];\
@@ -17,14 +17,14 @@ class Cfg3DEN {
                 params [""_slider""];\
                 private _edit = (ctrlParentControlsGroup _slider) controlsGroupCtrl 101;\
                 private _value = sliderPosition _slider;\
-                _edit ctrlSetText ([_value, 1, 1] call CBA_fnc_formatNumber);\
+                _edit ctrlSetText (if (_value < 0.1) then {localize ""str_disp_default""} else {[_value, 1, 1] call CBA_fnc_formatNumber});\
             }];\
             _edit ctrlAddEventHandler [""KillFocus"", {\
                 params [""_edit""];\
                 private _slider = (ctrlParentControlsGroup _edit) controlsGroupCtrl 100;\
                 private _value = ((parseNumber ctrlText _edit) min 10) max 0;\
                 _slider sliderSetPosition _value;\
-                _edit ctrlSetText str _value;\
+                _edit ctrlSetText (if (_value < 0.1) then { localize ""str_disp_default"" } else {[_value, 1, 1] call CBA_fnc_formatNumber});\
             }];";
         };
     };
@@ -39,7 +39,7 @@ class Cfg3DEN {
                         tooltip = CSTRING(Eden_threshold_Description);
                         expression = QUOTE(if (_value >= 0.1) then {_this setVariable [ARR_3(QQEGVAR(medical,damageThreshold),_value,true)]});
                         typeName = "NUMBER";
-                        condition = "objectBrain";
+                        condition = "objectControllable";
                         defaultValue = 0;
                     };
                 };
