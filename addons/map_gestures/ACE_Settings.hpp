0e0c15a4f645ab4f3c70aebc9eff51b1c13b85c1
Jonas Hoffmann <hoffmann.jonas95@gmail.com>
2020-05-12 00:51:48 +0200

    Fix Merge Issue in Stringtable
    Redo Settings

---
+++ addons/map_gestures/ACE_Settings.hpp
@@ -0,0 +1,20 @@
+class ACE_Settings {
+    class GVAR(enabled) {
+        movedToSQF = 1;
+    };
+    class GVAR(maxRange) {
+        movedToSQF = 1;
+    };
+    class GVAR(interval) {
+        movedToSQF = 1;
+    };
+    class GVAR(nameTextColor) {
+        movedToSQF = 1;
+    };
+    class GVAR(defaultLeadColor) {
+        movedToSQF = 1;
+    };
+    class GVAR(defaultColor) {
+        movedToSQF = 1;
+    };
+};
--- addons/map_gestures/config.cpp
+++ addons/map_gestures/config.cpp
@@ -14,5 +14,6 @@ class CfgPatches {
     };
 };
 
+#include "ACE_Settings.hpp"
 #include "CfgEventHandlers.hpp"
 #include "CfgVehicles.hpp"
