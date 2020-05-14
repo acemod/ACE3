830633a7a033661d1ccf048b7f68e0939c6af57f
Jonas Hoffmann <hoffmann.jonas95@gmail.com>
2020-05-14 21:18:13 +0200

    fix a copy past script error

--- addons/map_gestures/CfgEventHandlers.hpp
+++ addons/map_gestures/CfgEventHandlers.hpp
@@ -18,7 +18,7 @@ class Extended_PostInit_EventHandlers {
 
 class Extended_DisplayLoad_EventHandlers {
     class RscDisplayCurator {
-        ADDON = QUOTE((_this displayCtrl 50) call FUNC(initDisplayCurator));
+        ADDON = QUOTE(((_this select 0) displayCtrl 50) call FUNC(initDisplayCurator));
     };
     class RscDisplayEGSpectator {
         ADDON = QUOTE((((_this select 0) displayCtrl 63909) controlsGroupCtrl 62609) call FUNC(initDisplaySpectator));
