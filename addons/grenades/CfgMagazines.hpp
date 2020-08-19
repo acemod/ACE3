8cd4cd58b13c0895bc7b61bcf5233304cc719223
Cyruz143 <root.cyruz@gmail.com>
2020-08-19 23:01:39 +0100

    First pass on adding throwable explosives

--- addons/grenades/CfgMagazines.hpp
+++ addons/grenades/CfgMagazines.hpp
@@ -64,7 +64,7 @@ class CfgMagazines {
         ammo = "ACE_G_M84";
         mass = 4;
     };
-    
+
     class ACE_CTS9: HandGrenade {
         author = ECSTRING(common,ACETeam);
         displayname = CSTRING(CTS9_Name);
@@ -130,5 +130,53 @@ class CfgMagazines {
         ammo = "ACE_40mm_Flare_ir";
         displayName = CSTRING(40mm_flare_ir);
         descriptionShort = CSTRING(parachute_flare_ir_description);
+    };
+
+    class CA_Magazine;
+    class ACE_SatchelCharge_Remote_Mag_Throwable: CA_Magazine {
+        author = ECSTRING(common,ACETeam);
+        scope = 1;
+        picture = "\A3\Weapons_f\data\UI\gear_satchel_CA.paa";
+        model = "\A3\Weapons_F\Explosives\satchel_i";
+        descriptionShort = "Type: Charge<br />Rounds: 1<br />Used on: Things that need to die";
+        class Library {
+            libTextDesc = "An explosive satchel that is throwable. 7 second fixed fuse";
+        };
+        descriptionUse = "";
+        type = "2*      256";
+        allowedSlots[] = {901};
+        value = 5;
+        ammo = "ACE_SatchelCharge_Remote_Ammo_Thrown";
+        mass = 80;
+        count = 1;
+        initSpeed = 10;
+        maxLeadSpeed = 0;
+        nameSoundWeapon = "satchelcharge";
+        nameSound = "satchelcharge";
+        weaponPoolAvailable = 0;
+        useActionTitle = "";
+        sound[] = {"A3\sounds_f\dummysound", 0.000316228, 1, 10};
+        displayName = "Explosive Satchel (Throwable)";
+        displayNameShort = "Satchel Charge";
+        useAction = 0;
+        EGVAR(explosives,placeable) = 0;
+        class ACE_Triggers {
+            SupportedTriggers[] = {};
+        };
+    };
+
+    class ACE_DemoCharge_Remote_Mag_Throwable: SatchelCharge_Remote_Mag_Throwable {
+        picture = "\A3\Weapons_F\Data\UI\gear_c4_charge_small_CA.paa";
+        model = "\A3\Weapons_F\explosives\c4_charge_small_d";
+        descriptionShort = "Type: Charge<br />Rounds: 1<br />Used on: Things that need to die";
+        class Library {
+            libTextDesc = "An explosive charge that is throwable. 7 second fixed fuse";
+        };
+        allowedSlots[] = {901, 701};
+        ammo = "ACE_DemoCharge_Remote_Ammo_Thrown";
+        mass = 20;
+        initSpeed = 15;
+        displayName = "Explosive Charge (Throwable)";
+        displayNameShort = "Explosive Charge";
     };
 };
---
+++ addons/grenades/functions/fnc_changeFuse.sqf
@@ -0,0 +1,69 @@
+#include "script_component.hpp"
+/*
+ * Author: Cyruz
+ * Allows conversion of explosive charges in to throwable versions
+ *
+ * Arguments:
+ * None
+ *
+ * Return Value:
+ * None
+ *
+ * Example:
+ * call ace_grenades_fnc_changeFuse
+ *
+ * Public: No
+ */
+
+{
+    _x params ["_mag", "_throwableMag"];
+
+    [
+        _mag,
+        "CONTAINER",
+        "Covert to short fuse",
+        nil,
+        QPATHTOEF(explosives,UI\Place_Explosive_ca.paa),
+        [
+            {true},
+            {
+                params ["", "", "_item", "", "_magArr"];
+                _item isEqualTo (_magArr #0);
+            }
+        ],
+        {
+            params ["_unit", "", "", "", "_magArr"];
+            _unit addMagazine (_magArr #1);
+            false;
+        },
+        true,
+        [_mag,_throwableMag]
+    ] call CBA_fnc_addItemContextMenuOption;
+
+    [
+        _throwableMag,
+        "CONTAINER",
+        "Remove short fuse",
+        nil,
+        QPATHTOEF(explosives,UI\Place_Explosive_ca.paa),
+        [
+            {true},
+            {
+                params ["", "", "_item", "", "_magArr"];
+                _item isEqualTo (_magArr #1);
+            }
+        ],
+        {
+            params ["_unit", "", "", "", "_magArr"];
+            _unit addMagazine (_magArr #0);
+            false;
+        },
+        true,
+        [_mag,_throwableMag]
+    ] call CBA_fnc_addItemContextMenuOption;
+} forEach [
+    ["SatchelCharge_Remote_Mag", "SatchelCharge_Remote_Mag_Throwable"],
+    ["DemoCharge_Remote_Mag", "DemoCharge_Remote_Mag_Throwable"]
+];
+
+call FUNC(changeFuse);
