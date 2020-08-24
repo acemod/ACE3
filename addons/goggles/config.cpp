b4d89e919e89306b79ddcffc220b99f47534807e
PabstMirror <pabstmirror@gmail.com>
2020-08-24 13:31:35 -0500

    Misc - Fix UBCs (#7870)

--- addons/goggles/config.cpp
+++ addons/goggles/config.cpp
@@ -239,7 +239,7 @@ class CfgGlasses {
     class G_AirPurifyingRespirator_02_base_F: G_AirPurifyingRespirator_01_base_F {
         ACE_Overlay = "a3\ui_f_enoch\data\objects\data\optics_APR_02_CA.paa";
     };
-    class G_RegulatorMask_base_F: G_AirPurifyingRespirator_01_base_F {
+    class G_RegulatorMask_base_F: None {
         ACE_Overlay = "a3\ui_f_enoch\data\objects\data\optics_regulator_ca.paa";
     };
 };
