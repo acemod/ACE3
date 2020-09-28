dc15208be75a82391d1434d71528ec74ab27ca74
Dániel Boros <daniel.boros.17@gmail.com>
2020-09-28 16:51:28 +0200

    New custom animation added

--- addons/dragging/CfgMovesBasic.hpp
+++ addons/dragging/CfgMovesBasic.hpp
@@ -4,5 +4,29 @@ class CfgMovesBasic {
         class MoveWithInjuredManDraggerRfl: MoveWithInjuredManDragger {
             Up = "amovpercmstpsraswrfldnon";
         };
+        class MoveWithInjuredManDraggerPst;
+        class ace_MoveWithInjuredManDraggerPst: MoveWithInjuredManDraggerPst {
+            Stop = "ace_dragging_static";
+            StopRelaxed = "ace_dragging_static";
+            WalkLB = "ace_dragging";
+			WalkRB = "ace_dragging";
+			WalkB = "ace_dragging";
+			PlayerWalkLB = "ace_dragging";
+			PlayerWalkRB = "ace_dragging";
+			PlayerWalkB = "ace_dragging";
+			SlowLB = "ace_dragging";
+			SlowRB = "ace_dragging";
+			SlowB = "ace_dragging";
+			PlayerSlowLB = "ace_dragging";
+			PlayerSlowRB = "ace_dragging";
+			PlayerSlowB = "ace_dragging";
+			FastLB = "ace_dragging";
+			FastRB = "ace_dragging";
+			FastB = "ace_dragging";
+            Up = "ace_dragging_static";
+            grabCarry = "Helper_SwitchToCarrynon_pst";
+            Default = "ace_dragging_static";
+			HandGunOn = "ace_dragging_static";
+        };
     };
 };
--- addons/dragging/CfgMovesMaleSdr.hpp
+++ addons/dragging/CfgMovesMaleSdr.hpp
@@ -1,6 +1,7 @@
 class CfgMovesMaleSdr: CfgMovesBasic {
     class InjuredMovedBase;
     class AgonyBaseRfl;
+    class DraggerBase;
 
     class States {
         class AcinPknlMstpSnonWnonDnon_AcinPercMrunSnonWnonDnon: InjuredMovedBase {
@@ -8,6 +9,60 @@ class CfgMovesMaleSdr: CfgMovesBasic {
         };
         class AinjPfalMstpSnonWrflDnon_carried_Up: AgonyBaseRfl {
             speed = -10; // 1/10
+        };
+        class ace_dragging: DraggerBase {
+            actions = "ace_MoveWithInjuredManDraggerPst";
+            file = QPATHTO_T(anim\ace_dragging.rtm);
+            showHandGun = 1;
+            looped = 1;
+            duty = 0.6;
+            interpolationSpeed = 4;
+            disableWeapons = 0;
+            canPullTrigger = 1;
+            limitGunMovement = 0.15;
+            canReload = 0;
+            aimingBody = "aimingUpDefault";
+            aiming = "aimingDefault";
+            aimPrecision = 5;
+            turnSpeed = 0.1;
+            ConnectTo[]=
+            {
+                "ace_dragging_static",
+                0.1
+            };
+            InterpolateTo[]=
+            {
+                "ace_dragging_static",
+                0.1,
+                "AcinPknlMwlkSnonWpstDb_death",
+                0.2,
+                "AcinPknlMwlkSnonWpstDb_agony",
+                0.2
+            };
+        };
+        class ace_dragging_static: ace_dragging {
+            looped = 1;
+            speed = 0;
+			ConnectTo[]=
+			{
+				"ace_dragging_static",
+				0.1
+			};
+            InterpolateTo[]=
+			{
+				/*"AcinPknlMwlkSnonWpstDb_AmovPercMstpSrasWpstDnon",
+				0.15000001,
+				"AcinPknlMstpSnonWpstDnon_AmovPknlMstpSrasWpstDnon",
+				0.1,*/
+				"ace_dragging",
+				0.1,
+				"AcinPknlMwlkSnonWpstDb_death",
+				0.2,
+				"AcinPknlMwlkSnonWpstDb_agony",
+				0.2,
+				"Helper_SwitchToCarrynon_pst",
+				0.1
+			};
         };
     };
 };
