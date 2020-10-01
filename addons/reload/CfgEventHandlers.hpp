32fcddeeb7db32e070ed6d777036954a4682e42c
Jonas Hoffmann <hoffmann.jonas95@gmail.com>
2020-10-01 19:23:28 +0200

    Extract broken calls to function

--- addons/reload/CfgEventHandlers.hpp
+++ addons/reload/CfgEventHandlers.hpp
@@ -20,14 +20,7 @@ class Extended_PostInit_EventHandlers {
 class Extended_Take_EventHandlers {
     class CAManBase {
         class ACE_AmmoIndicatorReload {
-            clientTake = QUOTE( \
-                params [ARR_3('_unit', '_container', '_item')]; \
-                if ( \
-                    _unit == ACE_player \
-                    && {GVAR(DisplayText)} \
-                    && {_container in [ARR_3(uniformContainer _unit, vestContainer _unit, backpackContainer _unit)]} \
-                    && {_item == currentMagazine _unit} \
-                ) then {_unit call DFUNC(displayAmmo)};);
+            clientTake = QUOTE(_this call FUNC(onTake));
         };
     };
 };
