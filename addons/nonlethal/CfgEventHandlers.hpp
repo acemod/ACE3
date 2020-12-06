b99135948ac35b7a786e8f14a1584e714ae352bf
mjc4wilton <mjc4wilton@gmail.com>
2020-12-06 00:30:38 -0500

    Add missing semicolon

--- addons/nonlethal/CfgEventHandlers.hpp
+++ addons/nonlethal/CfgEventHandlers.hpp
@@ -12,4 +12,4 @@ class Extended_PreInit_EventHandlers {
 // Not sure if this is working. systemChats aren't firing for some reason.
 class Extended_Dammaged_EventHandlers {
     ADDON = QUOTE(_this call FUNC(dammaged));
-}
+};
