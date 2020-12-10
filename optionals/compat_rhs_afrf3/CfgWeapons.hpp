93a834f3a5e1e9bc70925c423c9205aef508212e
YetheSamartaka <55753928+YetheSamartaka@users.noreply.github.com>
2020-12-10 12:18:33 +0100

    DF-15 PR - cfgWeapons formating
    DF-15 PR - cfgWeapons formating

--- optionals/compat_rhs_afrf3/CfgWeapons.hpp
+++ optionals/compat_rhs_afrf3/CfgWeapons.hpp
@@ -332,13 +332,14 @@ class CfgWeapons {
         modes[] = {};
         picture = "\rhsafrf\addons\rhs_heavyweapons\data\ico\rhs_Kornet_9M133_2_msv_ca.paa";
     };
+    
     class rhs_uniform_flora;
-	class rhs_uniform_df15: rhs_uniform_flora
-	{
-		ACE_GForceCoef=0.80000001;
-	};
-	class rhs_uniform_df15_tan: rhs_uniform_df15
-	{
-		ACE_GForceCoef=0.80000001;
-	};
+    class rhs_uniform_df15: rhs_uniform_flora
+    {
+        ACE_GForceCoef=0.8;
+    };
+    class rhs_uniform_df15_tan: rhs_uniform_df15
+    {
+        ACE_GForceCoef=0.8;
+    };
 };
