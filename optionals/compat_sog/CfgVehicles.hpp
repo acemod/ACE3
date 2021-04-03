c1d287896731b1e60f4183e62bec049f476e7f6d
BaerMitUmlaut <BaerMitUmlaut@users.noreply.github.com>
2021-04-03 22:38:31 +0200

    Added G-Forces compatibility

--- optionals/compat_sog/CfgVehicles.hpp
+++ optionals/compat_sog/CfgVehicles.hpp
@@ -12,5 +12,5 @@
 }
 
 class CfgVehicles {
-
+	#include "CfgVehicles/units.hpp";
 };
--- optionals/compat_sog/CfgWeapons.hpp
+++ optionals/compat_sog/CfgWeapons.hpp
@@ -2,4 +2,5 @@ class CfgWeapons {
 	#include "CfgWeapons/helmets.hpp";
 	#include "CfgWeapons/launchers.hpp";
 	#include "CfgWeapons/pistols.hpp";
+	#include "CfgWeapons/uniforms.hpp";
 };
