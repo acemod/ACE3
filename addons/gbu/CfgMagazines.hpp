e7919dabd2c81944af64bc2d80c944278cb7a840
Brandon Danyluk <brandondanyluk366@gmail.com>
2021-04-17 01:11:33 -0600

    Add all GBU-12s that exist for all planes

--- addons/gbu/CfgMagazines.hpp
+++ addons/gbu/CfgMagazines.hpp
@@ -1,29 +1,84 @@
 class CfgMagazines {
     class 2Rnd_GBU12_LGB;
+    // GBU
     class GVAR(2Rnd_12): 2Rnd_GBU12_LGB {
-        displayName = "GBU-12 [ACE]";
+        displayName = "2x GBU-12 [ACE]";
+        author = "Brandon (TCVM)";
+        ammo = QGVAR(12);
+    };
+
+    class 4Rnd_Bomb_04_F;
+    class GVAR(4Rnd_GBU12): 4Rnd_Bomb_04_F {
+        displayName = "4x GBU-12 [ACE]";
+        author = "Brandon (TCVM)";
+        ammo = QGVAR(12);
+    };
+
+    class magazine_Bomb_GBU12_x1;
+    class GVAR(gbu12): magazine_Bomb_GBU12_x1 {
+        displayName = "1x GBU-12 [ACE]";
         author = "Brandon (TCVM)";
         ammo = QGVAR(12);
     };
 
     class PylonMissile_1Rnd_Bomb_04_F;
     class GVAR(1_PylonMissile_1Rnd_12): PylonMissile_1Rnd_Bomb_04_F {
-        displayName = "GBU-12 [ACE]";
+        displayName = "1x GBU-12 [ACE]";
         author = "Brandon (TCVM)";
         ammo = QGVAR(12);
         pylonWeapon = QGVAR(12);
     };
 
+    class 2Rnd_GBU12_LGB_MI10;
+    class GVAR(2Rnd_GBU12_LGB_MI10): 2Rnd_GBU12_LGB_MI10 {
+        displayName = "2x GBU-12 [ACE]";
+        author = "Brandon (TCVM)";
+        ammo = QGVAR(12);
+        pylonWeapon = QGVAR(12);
+    };
+
+    class PylonMissile_Bomb_GBU12_x1;
+    class GVAR(PylonMissile_Bomb_GBU12_x1): PylonMissile_Bomb_GBU12_x1 {
+        displayName = "1x GBU-12 [ACE]";
+        author = "Brandon (TCVM)";
+        ammo = QGVAR(12);
+        pylonWeapon = QGVAR(12);
+    };
+
+    class PylonRack_Bomb_GBU12_x2;
+    class GVAR(PylonRack_Bomb_GBU12_x2): PylonRack_Bomb_GBU12_x2 {
+        displayName = "2x GBU-12 [ACE]";
+        author = "Brandon (TCVM)";
+        ammo = QGVAR(12);
+        pylonWeapon = QGVAR(12);
+    };
+
+    // KAB
     class magazine_Bomb_KAB250_x1;
     class GVAR(1Rnd_FAB250): magazine_Bomb_KAB250_x1 {
-        displayName = "KAB-250M-54 [ACE]";
+        displayName = "1x KAB-250M-54 [ACE]";
         author = "Brandon (TCVM)";
         ammo = QGVAR(FAB250);
     };
 
     class PylonMissile_Bomb_KAB250_x1;
     class GVAR(1_PylonMissile_1Rnd_FAB250): PylonMissile_Bomb_KAB250_x1 {
-        displayName = "KAB-250M-54 [ACE]";
+        displayName = "1x KAB-250M-54 [ACE]";
+        author = "Brandon (TCVM)";
+        ammo = QGVAR(FAB250);
+        pylonWeapon = QGVAR(FAB250);
+    };
+
+    class 2Rnd_Bomb_03_F;
+    class GVAR(2Rnd_FAB250): 2Rnd_Bomb_03_F {
+        displayName = "2x KAB-250M-54 [ACE]";
+        author = "Brandon (TCVM)";
+        ammo = QGVAR(FAB250);
+    };
+
+    class PylonMissile_1Rnd_Bomb_03_F;
+    class GVAR(PylonMissile_1Rnd_FAB250): PylonMissile_1Rnd_Bomb_03_F {
+        displayName = "1x KAB-250M-54 [ACE]";
         author = "Brandon (TCVM)";
         ammo = QGVAR(FAB250);
         pylonWeapon = QGVAR(FAB250);
--- addons/gbu/CfgWeapons.hpp
+++ addons/gbu/CfgWeapons.hpp
@@ -2,7 +2,15 @@ class CfgWeapons {
     class weapon_LGBLauncherBase;
     class GVAR(12): weapon_LGBLauncherBase {
         displayName = "GBU-12 [ACE]";
-        magazines[] = { QGVAR(2Rnd_12), QGVAR(1_PylonMissile_1Rnd_12) };
+        magazines[] = {
+            QGVAR(2Rnd_12),
+            QGVAR(2Rnd_GBU12_LGB_MI10),
+            QGVAR(4Rnd_GBU12),
+            QGVAR(gbu12),
+            QGVAR(1_PylonMissile_1Rnd_12),
+            QGVAR(PylonMissile_Bomb_GBU12_x1),
+            QGVAR(PylonRack_Bomb_GBU12_x2)
+        };
 
         EGVAR(laser,canSelect) = 1; // can ace_laser lock (allows switching laser code)
         EGVAR(laser,showHud) = 1; // show attack profile / lock on hud
@@ -10,7 +18,12 @@ class CfgWeapons {
 
     class GVAR(FAB250): weapon_LGBLauncherBase {
         displayName = "FAB-250M-54 [ACE]";
-        magazines[] = {QGVAR(1Rnd_FAB250), QGVAR(1_PylonMissile_1Rnd_FAB250)};
+        magazines[] = {
+            QGVAR(1Rnd_FAB250),
+            QGVAR(2Rnd_FAB250),
+            QGVAR(1_PylonMissile_1Rnd_FAB250),
+            QGVAR(PylonMissile_1Rnd_FAB250)
+        };
 
         EGVAR(laser,canSelect) = 1; // can ace_laser lock (allows switching laser code)
         EGVAR(laser,showHud) = 1; // show attack profile / lock on hud
