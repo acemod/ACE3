cba9dcd8e754e900f376970ae23ec6a096b4657e
Brandon Danyluk <brandondanyluk366@gmail.com>
2021-04-21 12:48:44 -0600

    fix SAM animations

--- addons/sam/CfgVehicles.hpp
+++ addons/sam/CfgVehicles.hpp
@@ -12,6 +12,12 @@ class CfgVehicles {
 	// RIM-162
 	class SAM_System_02_base_F: StaticMGWeapon {
 		displayName = "Mk-29";
+		class AnimationSources {
+            class Missiles_revolving {
+                source = "revolving";
+                weapon = QGVAR(RIM162);
+            };
+        };
 		class Turrets: Turrets {
             class MainTurret: MainTurret {
 				gunnerName = "Mk-29 Operator";
@@ -27,6 +33,12 @@ class CfgVehicles {
 	// Patriot
 	class SAM_System_03_base_F: StaticMGWeapon {
 		displayName = "MIM-104 Patriot";
+		class AnimationSources {
+            class Missiles_revolving {
+                source = "revolving";
+                weapon = QGVAR(patriot);
+            };
+		};
 		class Turrets: Turrets {
             class MainTurret: MainTurret {
 				gunnerName = "MIM-104 Patriot Operator";
@@ -42,6 +54,12 @@ class CfgVehicles {
 	// S-400
 	class SAM_System_04_base_F: StaticMGWeapon {
 		displayName = "S-400";
+		class AnimationSources {
+            class Missiles_revolving {
+                source = "revolving";
+                weapon = QGVAR(s400);
+            };
+		};
 		class Turrets: Turrets {
             class MainTurret: MainTurret {
 				gunnerName = "S-400 Operator";
