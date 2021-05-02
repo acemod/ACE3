7988469fe326e38e5b66bf2bc3f248fb4c35849c
Brandon Danyluk <brandondanyluk366@gmail.com>
2021-05-02 01:37:03 -0600

    localise vikhr

--- addons/vikhr/CfgWeapons.hpp
+++ addons/vikhr/CfgWeapons.hpp
@@ -2,7 +2,7 @@ class CfgWeapons {
     class missiles_SCALPEL;
     class GVAR(9k121): missiles_SCALPEL {
         author = "Brandon (TCVM)";
-        displayName = "Skalpel ATGM";
+        displayName = CSTRING(9k121);
         magazines[] = {
             QGVAR(2Rnd),
             QGVAR(6Rnd),
