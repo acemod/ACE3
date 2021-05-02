72297be13e9022d1d6bdeaec50cad6c44a712efa
Brandon Danyluk <brandondanyluk366@gmail.com>
2021-05-02 01:31:52 -0600

    localise sdb

--- addons/sdb/CfgMagazines.hpp
+++ addons/sdb/CfgMagazines.hpp
@@ -2,13 +2,13 @@ class CfgMagazines {
     class magazine_Bomb_SDB_x1;
     class PylonRack_Bomb_SDB_x4;
     class GVAR(magazine_bomb_SDB_x1): magazine_Bomb_SDB_x1 {
-        displayName = "1x GBU-39 [ACE]";
+        displayName = CSTRING(39_1x);
         author = "Brandon (TCVM)";
         ammo = QGVAR(sdb);
     };
 
     class GVAR(PylonRack_bomb_SDB_x4): PylonRack_Bomb_SDB_x4 {
-        displayName = "4x GBU-39 [ACE]";
+        displayName = CSTRING(39_4x);
         author = "Brandon (TCVM)";
         ammo = QGVAR(sdb);
         pylonWeapon = QGVAR(sdb);
--- addons/sdb/CfgWeapons.hpp
+++ addons/sdb/CfgWeapons.hpp
@@ -2,7 +2,7 @@ class CfgWeapons {
     class weapon_SDBLauncher;
     class GVAR(sdb): weapon_SDBLauncher {
         author = "Brandon (TCVM)";
-        displayName = "GBU-39 [ACE]";
+        displayName = CSTRING(39);
         magazines[] = {
             QGVAR(magazine_bomb_SDB_x1),
             QGVAR(PylonRack_bomb_SDB_x4)
