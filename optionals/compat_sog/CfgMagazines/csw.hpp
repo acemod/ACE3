a6efd1f2dc7840fea3da951f263918e409e21fab
BaerMitUmlaut <BaerMitUmlaut@users.noreply.github.com>
2021-06-18 01:00:47 +0200

    Add CSW pictures, models and stringtable entries

--- optionals/compat_sog/CfgMagazines/csw.hpp
+++ optionals/compat_sog/CfgMagazines/csw.hpp
@@ -4,6 +4,8 @@ class GVAR(tow_missile): vn_missile_tow_mag_x1 {
     type = 256;
     count = 1;
     model = "\vn\static_f_vietnam\tow\vn_static_tow_mag.p3d";
+    picture = QPATHTOF(UI\ammo_1rnd_tow_ca.paa);
+    mass = 563;
 };
 
 class vn_m1919_v_250_mag;
@@ -12,7 +14,9 @@ class GVAR(m1919_250): vn_m1919_v_250_mag {
     type = 256;
     count = 250;
     model = "\vn\objects_f_vietnam\supply\a2_ammo\macv\vn_us_30cal.p3d";
+    picture = QPATHTOF(UI\ammo_250rnd_30cal_ca.paa);
     ACE_isBelt = 1;
+    mass = 170;
 };
 
 class vn_m60_v_200_mag;
@@ -21,7 +25,9 @@ class GVAR(m60_200): vn_m60_v_200_mag {
     type = 256;
     count = 200;
     model = "\vn\objects_f_vietnam\supply\a2_ammo\macv\vn_us_can_30.p3d";
+    picture = QPATHTOF(UI\ammo_200rnd_762mm_ca.paa);
     ACE_isBelt = 1;
+    mass = 187;
 };
 
 class vn_dshkm_v_50_mag;
@@ -30,7 +36,9 @@ class GVAR(dshkm_50): vn_dshkm_v_50_mag {
     type = 256;
     count = 50;
     model = "\vn\objects_f_vietnam\supply\a2_ammo\pavn\vn_pavn_50_can.p3d";
+    picture = QPATHTOF(UI\ammo_50rnd_127mm_ca.paa);
     ACE_isBelt = 1;
+    mass = 170;
 };
 
 class vn_mortar_m29_mag_he_x8;
@@ -38,8 +46,12 @@ class GVAR(81mm_he): vn_mortar_m29_mag_he_x8 {
     scope = 2;
     type = 256;
     count = 1;
-    model = QPATHTOEF(mk6mortar,data\l16_ammo_he.p3d);
+    model = "\vn\static_f_vietnam\mortar_m29\vn_shell_81mm_m374_he_ammo.p3d";
+    picture = QPATHTOF(UI\ammo_1rnd_81mm_ca.paa);
     mass = 91;
+    displayName = CSTRING(Magazine_81mm_HE);
+    displayNameShort = "";
+    descriptionShort = ECSTRING(mk6mortar,magazine_descriptionShort);
 };
 
 class vn_mortar_m29_mag_wp_x8;
@@ -47,8 +59,12 @@ class GVAR(81mm_wp): vn_mortar_m29_mag_wp_x8 {
     scope = 2;
     type = 256;
     count = 1;
-    model = QPATHTOEF(mk6mortar,data\l16_ammo_he.p3d);
+    model = "\vn\static_f_vietnam\mortar_m29\vn_shell_81mm_m374_he_ammo.p3d";
+    picture = QPATHTOF(UI\ammo_1rnd_81mm_ca.paa);
     mass = 91;
+    displayName = CSTRING(Magazine_81mm_WP);
+    displayNameShort = "";
+    descriptionShort = ECSTRING(mk6mortar,magazine_descriptionShort);
 };
 
 class vn_mortar_m29_mag_chem_x8;
@@ -56,8 +72,12 @@ class GVAR(81mm_chem): vn_mortar_m29_mag_chem_x8 {
     scope = 2;
     type = 256;
     count = 1;
-    model = QPATHTOEF(mk6mortar,data\l16_ammo_smk_white.p3d);
+    model = "\vn\static_f_vietnam\mortar_m29\vn_shell_81mm_m374_he_ammo.p3d";
+    picture = QPATHTOF(UI\ammo_1rnd_81mm_ca.paa);
     mass = 91;
+    displayName = CSTRING(Magazine_81mm_Smoke);
+    displayNameShort = "";
+    descriptionShort = ECSTRING(mk6mortar,magazine_descriptionShort);
 };
 
 class vn_mortar_m29_mag_lume_x8;
@@ -65,8 +85,12 @@ class GVAR(81mm_lume): vn_mortar_m29_mag_lume_x8 {
     scope = 2;
     type = 256;
     count = 1;
-    model = QPATHTOEF(mk6mortar,data\l16_ammo_illum.p3d);
+    model = "\vn\static_f_vietnam\mortar_m29\vn_shell_81mm_m374_he_ammo.p3d";
+    picture = QPATHTOF(UI\ammo_1rnd_81mm_ca.paa);
     mass = 91;
+    displayName = CSTRING(Magazine_81mm_Lume);
+    displayNameShort = "";
+    descriptionShort = ECSTRING(mk6mortar,magazine_descriptionShort);
 };
 
 class vn_mortar_m2_mag_he_x8;
@@ -74,8 +98,12 @@ class GVAR(60mm_he): vn_mortar_m2_mag_he_x8 {
     scope = 2;
     type = 256;
     count = 1;
-    model = QPATHTOEF(mk6mortar,data\l16_ammo_he.p3d);
+    model = "\vn\static_f_vietnam\mortar_m2\vn_shell_60mm_m49a2_he_ammo.p3d";
+    picture = QPATHTOF(UI\ammo_1rnd_60mm_ca.paa);
     mass = 37;
+    displayName = CSTRING(Magazine_60mm_HE);
+    displayNameShort = "";
+    descriptionShort = ECSTRING(mk6mortar,magazine_descriptionShort);
 };
 
 class vn_mortar_m2_mag_wp_x8;
@@ -83,8 +111,12 @@ class GVAR(60mm_wp): vn_mortar_m2_mag_wp_x8 {
     scope = 2;
     type = 256;
     count = 1;
-    model = QPATHTOEF(mk6mortar,data\l16_ammo_he.p3d);
+    model = "\vn\static_f_vietnam\mortar_m2\vn_shell_60mm_m302_wp_ammo.p3d";
+    picture = QPATHTOF(UI\ammo_1rnd_60mm_ca.paa);
     mass = 37;
+    displayName = CSTRING(Magazine_60mm_WP);
+    displayNameShort = "";
+    descriptionShort = ECSTRING(mk6mortar,magazine_descriptionShort);
 };
 
 class vn_mortar_m2_mag_lume_x8;
@@ -92,8 +124,12 @@ class GVAR(60mm_lume): vn_mortar_m2_mag_lume_x8 {
     scope = 2;
     type = 256;
     count = 1;
-    model = QPATHTOEF(mk6mortar,data\l16_ammo_illum.p3d);
+    model = "\vn\static_f_vietnam\mortar_m2\vn_shell_60mm_m83_lume_ammo.p3d";
+    picture = QPATHTOF(UI\ammo_1rnd_60mm_ca.paa);
     mass = 37;
+    displayName = CSTRING(Magazine_60mm_Lume);
+    displayNameShort = "";
+    descriptionShort = ECSTRING(mk6mortar,magazine_descriptionShort);
 };
 
 class vn_mortar_type53_mag_he_x8;
@@ -101,8 +137,12 @@ class GVAR(82mm_he): vn_mortar_type53_mag_he_x8 {
     scope = 2;
     type = 256;
     count = 1;
-    model = QPATHTOEF(mk6mortar,data\l16_ammo_he.p3d);
+    model = "\vn\static_f_vietnam\mortar_type53\vn_shell_82mm_o832d_he_ammo.p3d";
+    picture = QPATHTOF(UI\ammo_1rnd_82mm_ca.paa);
     mass = 70;
+    displayName = CSTRING(Magazine_82mm_HE);
+    displayNameShort = "";
+    descriptionShort = ECSTRING(mk6mortar,magazine_descriptionShort);
 };
 
 class vn_mortar_type53_mag_wp_x8;
@@ -110,8 +150,12 @@ class GVAR(82mm_wp): vn_mortar_type53_mag_wp_x8 {
     scope = 2;
     type = 256;
     count = 1;
-    model = QPATHTOEF(mk6mortar,data\l16_ammo_he.p3d);
+    model = "\vn\static_f_vietnam\mortar_type53\vn_shell_82mm_d832_wp_ammo.p3d";
+    picture = QPATHTOF(UI\ammo_1rnd_82mm_ca.paa);
     mass = 70;
+    displayName = CSTRING(Magazine_82mm_WP);
+    displayNameShort = "";
+    descriptionShort = ECSTRING(mk6mortar,magazine_descriptionShort);
 };
 
 class vn_mortar_type53_mag_lume_x8;
@@ -119,6 +163,10 @@ class GVAR(82mm_lume): vn_mortar_type53_mag_lume_x8 {
     scope = 2;
     type = 256;
     count = 1;
-    model = QPATHTOEF(mk6mortar,data\l16_ammo_illum.p3d);
+    model = "\vn\static_f_vietnam\mortar_type53\vn_shell_82mm_s832s_lume_ammo.p3d";
+    picture = QPATHTOF(UI\ammo_1rnd_82mm_ca.paa);
     mass = 70;
+    displayName = CSTRING(Magazine_82mm_Lume);
+    displayNameShort = "";
+    descriptionShort = ECSTRING(mk6mortar,magazine_descriptionShort);
 };
