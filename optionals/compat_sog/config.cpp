3707fc57a4d4a38c2f9f7ada86c9b0b8ab3a20a9
BaerMitUmlaut <BaerMitUmlaut@users.noreply.github.com>
2021-06-19 00:52:42 +0200

    Fill CfgPatches units and weapons arrays for Zeus

--- optionals/compat_sog/config.cpp
+++ optionals/compat_sog/config.cpp
@@ -3,10 +3,35 @@
 class CfgPatches {
     class ADDON {
         name = COMPONENT_NAME;
-        units[] = {};
-        weapons[] = {};
+        units[] = {
+            QGVAR(box_81mm_he),
+            QGVAR(box_81mm_wp),
+            QGVAR(box_81mm_chem),
+            QGVAR(box_81mm_lume),
+            QGVAR(box_60mm_he),
+            QGVAR(box_60mm_wp),
+            QGVAR(box_60mm_lume),
+            QGVAR(box_82mm_he),
+            QGVAR(box_82mm_wp),
+            QGVAR(box_82mm_lume)
+        };
+        weapons[] = {
+            QGVAR(m2_carry),
+            QGVAR(m1919a4_carry),
+            QGVAR(m1919a6_carry),
+            QGVAR(m60_carry),
+            QGVAR(tow_carry),
+            QGVAR(dshkm_carry),
+            QGVAR(dshkm_shield_carry),
+            QGVAR(dshkm_AA_carry),
+            QGVAR(rpd_carry),
+            QGVAR(pk_carry),
+            QGVAR(mortar_m29_carry),
+            QGVAR(mortar_m2_carry),
+            QGVAR(mortar_type53_carry)
+        };
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"vn_weapons", "vn_emm_main_menu", "weapons_f_vietnam_c"};
+        requiredAddons[] = {"vn_weapons", "vn_emm_main_menu", "weapons_f_vietnam_c", "ace_trenches"};
         author = ECSTRING(common,ACETeam);
         authors[] = {"BaerMitUmlaut", "veteran29"};
         url = ECSTRING(main,URL);
