101fe71757e1ebee239b0ad00c5a0352b5d11570
BaerMitUmlaut <BaerMitUmlaut@users.noreply.github.com>
2021-06-21 20:49:15 +0200

    Fix indentation

--- optionals/compat_sog/CfgVehicles.hpp
+++ optionals/compat_sog/CfgVehicles.hpp
@@ -12,12 +12,12 @@
 }
 
 class CfgVehicles {
-	#include "CfgVehicles\boxes.hpp"
+    #include "CfgVehicles\boxes.hpp"
     #include "CfgVehicles\explosives.hpp"
-	#include "CfgVehicles\helicopters.hpp"
-	#include "CfgVehicles\planes.hpp"
-	#include "CfgVehicles\tracked.hpp"
-	#include "CfgVehicles\turrets.hpp"
-	#include "CfgVehicles\units.hpp"
-	#include "CfgVehicles\wheeled.hpp"
+    #include "CfgVehicles\helicopters.hpp"
+    #include "CfgVehicles\planes.hpp"
+    #include "CfgVehicles\tracked.hpp"
+    #include "CfgVehicles\turrets.hpp"
+    #include "CfgVehicles\units.hpp"
+    #include "CfgVehicles\wheeled.hpp"
 };
