36170e979947a317b7e9f00e8ce71f77b1b033d4
PabstMirror <pabstmirror@gmail.com>
2021-06-27 15:27:04 -0500

    Update addons/vehiclelock/CfgMagazines.hpp

--- addons/vehiclelock/CfgMagazines.hpp
+++ addons/vehiclelock/CfgMagazines.hpp
@@ -5,7 +5,7 @@ class CfgMagazines {
         displayName = "ACE Vehicle Key";   //!!!CANNOT be localized!!!: because it is used as part of the magazineDetail string
         descriptionShort = CSTRING(Item_Custom_Description);
         count = 1;
-        mass = 0.1;
+        mass = 0;
         ACE_isUnique = 1;
     };
 };
