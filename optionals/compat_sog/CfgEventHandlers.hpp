f488251721943dc54506a6d636570f0bf8014da2
Björn Dahlgren <bjorn@dahlgren.io>
2021-07-08 23:45:12 +0200

    pboProject fixes for S.O.G. Prairie Fire Compatibility (#8311)
    * Change macros to work with pboproject
    
    * Add placeholders for VN p3d files

--- optionals/compat_sog/CfgEventHandlers.hpp
+++ optionals/compat_sog/CfgEventHandlers.hpp
@@ -13,17 +13,17 @@ class Extended_PreInit_EventHandlers {
 class Extended_InitPost_EventHandlers {
     class GVAR(spiderhole_01_nogeo) {
         class ADDON {
-            init = QUOTE(_this#0 setMass 1e-12);
+            init = QUOTE((_this select 0) setMass 1e-12);
         };
     };
     class GVAR(spiderhole_02_nogeo) {
         class ADDON {
-            init = QUOTE(_this#0 setMass 1e-12);
+            init = QUOTE((_this select 0) setMass 1e-12);
         };
     };
     class GVAR(spiderhole_03_nogeo) {
         class ADDON {
-            init = QUOTE(_this#0 setMass 1e-12);
+            init = QUOTE((_this select 0) setMass 1e-12);
         };
     };
     class vn_bicycle_base {
