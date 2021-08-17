b899429aa4ed39a355bcdfb42386a614f3114325
Filip Maciejewski <veteran29@users.noreply.github.com>
2021-08-17 02:27:58 +0200

    Update addons/advanced_fatigue/RscInGameUI.hpp
    Co-authored-by: PabstMirror <pabstmirror@gmail.com>

--- addons/advanced_fatigue/RscInGameUI.hpp
+++ addons/advanced_fatigue/RscInGameUI.hpp
@@ -9,7 +9,7 @@ class RscInGameUI {
         class GVAR(speedIndicator): RscPictureKeepAspect {
             onLoad = QUOTE(uiNamespace setVariable [ARR_2(QQGVAR(speedIndicator),_this select 0)]);
             x = IGUI_GRID_STANCE_X + IGUI_GRID_STANCE_WAbs / 2 - SPEED_INDICATOR_W / 2;
-            y = IGUI_GRID_STANCE_Y + IGUI_GRID_STANCE_HAbs - SPEED_INDICATOR_Y_OFFSET);
+            y = IGUI_GRID_STANCE_Y + IGUI_GRID_STANCE_HAbs - SPEED_INDICATOR_Y_OFFSET;
             w = SPEED_INDICATOR_W;
             h = SPEED_INDICATOR_H;
         };
