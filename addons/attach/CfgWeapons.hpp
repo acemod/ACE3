78b3d5e746fe7632d8016ebdd4fadf2a9721a7f8
Samuel Glew <samuel.glew@gmail.com>
2021-09-02 19:28:46 +0930

    updating to stringable

--- addons/attach/CfgWeapons.hpp
+++ addons/attach/CfgWeapons.hpp
@@ -18,76 +18,75 @@ class CfgWeapons {
     };
     class ACE_IR_Strobe_Item: ACE_IR_Strobe_Base {
         ACE_attachable_children[] = {"ACE_IR_Strobe_Item_One", "ACE_IR_Strobe_Item_Two","ACE_IR_Strobe_Item_Three","ACE_IR_Strobe_Item_Four"};
-        // ACE_attachable_children[] = {"ACE_IR_Strobe_Item_One"};
         ACE_attachable = "ACE_IR_Strobe_one_Effect";
         scope = 2;
     };
     class ACE_IR_Strobe_Item_One : ACE_IR_Strobe_Base{
-        selectName = "IR Mode 1";
+        selectName = CSTRING(IrStrobe_mode_1);
         ACE_attachable_children[] = { "ACE_IR_Strobe_Item_One_High","ACE_IR_Strobe_Item_One_Medium","ACE_IR_Strobe_Item_One_Low"};
         ACE_attachable = "ACE_IR_Strobe_one_Effect";
     };
     class ACE_IR_Strobe_Item_One_High : ACE_IR_Strobe_Base{
-        selectName = "IR Mode 1 - High";
+        selectName = CSTRING(IrStrobe_high_strength);
         ACE_attachable = "ACE_IR_Strobe_one_Effect";
     };
     class ACE_IR_Strobe_Item_One_Medium : ACE_IR_Strobe_Base{
-        selectName = "IR Mode 1 - Medium";
+        selectName = CSTRING(IrStrobe_med_strength);
         ACE_attachable = "ACE_IR_Strobe_one_medium_Effect";
     };
     class ACE_IR_Strobe_Item_One_Low : ACE_IR_Strobe_Base{
-        selectName = "IR Mode 1 - Low";
+        selectName = CSTRING(IrStrobe_low_strength);
         ACE_attachable = "ACE_IR_Strobe_one_low_Effect";
     };
     class ACE_IR_Strobe_Item_Two : ACE_IR_Strobe_Base{
-        selectName = "IR Mode 2";
+        selectName = CSTRING(IrStrobe_mode_2);
         ACE_attachable_children[] = {"ACE_IR_Strobe_Item_Two_High","ACE_IR_Strobe_Item_Two_Medium","ACE_IR_Strobe_Item_Two_Low"};
         ACE_attachable = "ACE_IR_Strobe_two_Effect";
     };
     class ACE_IR_Strobe_Item_Two_High : ACE_IR_Strobe_Base{
-        selectName = "IR Mode 2 - High";
+        selectName = CSTRING(IrStrobe_high_strength);
         ACE_attachable = "ACE_IR_Strobe_two_Effect";
     };
     class ACE_IR_Strobe_Item_Two_Medium : ACE_IR_Strobe_Base{
-        selectName = "IR Mode 2 - Medium";
+        selectName = CSTRING(IrStrobe_med_strength);
         ACE_attachable = "ACE_IR_Strobe_two_medium_Effect";
     };
     class ACE_IR_Strobe_Item_Two_Low : ACE_IR_Strobe_Base{
-        selectName = "IR Mode 2 - Low";
+        selectName = CSTRING(IrStrobe_low_strength);
         ACE_attachable = "ACE_IR_Strobe_two_low_Effect";
     };
     class ACE_IR_Strobe_Item_Three : ACE_IR_Strobe_Base{
-        selectName = "IR Mode 3";
+        selectName = CSTRING(IrStrobe_mode_3);
         ACE_attachable_children[] = {"ACE_IR_Strobe_Item_Three_High","ACE_IR_Strobe_Item_Three_Medium","ACE_IR_Strobe_Item_Three_Low"};
         ACE_attachable = "ACE_IR_Strobe_three_Effect";
     };
     class ACE_IR_Strobe_Item_Three_High : ACE_IR_Strobe_Base{
-        selectName = "IR Mode 3 - High";
+        selectName = CSTRING(IrStrobe_high_strength);
         ACE_attachable = "ACE_IR_Strobe_three_Effect";
     };
     class ACE_IR_Strobe_Item_Three_Medium : ACE_IR_Strobe_Base{
-        selectName = "IR Mode 3 - Medium";
+        selectName = CSTRING(IrStrobe_med_strength);
         ACE_attachable = "ACE_IR_Strobe_three_medium_Effect";
     };
     class ACE_IR_Strobe_Item_Three_Low : ACE_IR_Strobe_Base{
-        selectName = "IR Mode 3 - Low";
+        selectName = CSTRING(IrStrobe_low_strength);
         ACE_attachable = "ACE_IR_Strobe_three_low_Effect";
     };
     class ACE_IR_Strobe_Item_Four : ACE_IR_Strobe_Base{
-        selectName = "IR Mode 3";
+        selectName = CSTRING(IrStrobe_mode_4);
         ACE_attachable_children[] = {"ACE_IR_Strobe_Item_Four_High","ACE_IR_Strobe_Item_Four_Medium","ACE_IR_Strobe_Item_Four_Low"};
         ACE_attachable = "ACE_IR_Strobe_four_Effect";
     };
     class ACE_IR_Strobe_Item_Four_High : ACE_IR_Strobe_Base{
-        selectName = "IR Mode 4 - High";
+        selectName = CSTRING(IrStrobe_high_strength);
         ACE_attachable = "ACE_IR_Strobe_four_Effect";
     };
     class ACE_IR_Strobe_Item_Four_Medium : ACE_IR_Strobe_Base{
-        selectName = "IR Mode 4 - Medium";
+        selectName = CSTRING(IrStrobe_med_strength);
         ACE_attachable = "ACE_IR_Strobe_four_medium_Effect";
     };
     class ACE_IR_Strobe_Item_Four_Low : ACE_IR_Strobe_Base{
-        selectName = "IR Mode 4 - Low";
+        selectName = CSTRING(IrStrobe_low_strength);
         ACE_attachable = "ACE_IR_Strobe_four_low_Effect";
     };
 };
