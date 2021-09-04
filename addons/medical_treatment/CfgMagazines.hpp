ed6392edc97a518fc8df41cae7309982d32e9e0d
Salluci <69561145+Salluci@users.noreply.github.com>
2021-09-04 01:39:16 -0300

    derp

--- addons/medical_treatment/CfgMagazines.hpp
+++ addons/medical_treatment/CfgMagazines.hpp
@@ -7,6 +7,7 @@ class CfgMagazines {
         model = QPATHTOF(data\surgical_kit.p3d);
         picture = QPATHTOF(ui\surgicalKit_ca.paa);
         descriptionShort = CSTRING(SurgicalKit_Desc_Short);
+        count = SURGICAL_KIT_USES;
         deleteIfEmpty = 1;
         mass = 15;
     };
