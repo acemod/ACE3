862e7dc357199a611c81e368c940a1f0c715d722
jonpas <jonpas33@gmail.com>
2021-10-13 16:31:58 +0200

    Cleanup unused module and faction classes

--- addons/main/CfgModuleCategories.hpp
+++ addons/main/CfgModuleCategories.hpp
@@ -5,10 +5,6 @@ class CfgFactionClasses {
         priority = 2;
         side = 7;
     };
-    class ACEX: ACE {
-        displayName = "ACEX";
-    };
-
     class ACE_Logistics: ACE {
         displayName = CSTRING(Category_Logistics);
     };
--- addons/main/CfgVehicleClasses.hpp
+++ addons/main/CfgVehicleClasses.hpp
@@ -2,7 +2,4 @@ class CfgVehicleClasses {
     class ACE_Logistics_Items {
         displayName = CSTRING(Category_Logistics);
     };
-    class ACEX_Vehicles {
-        displayName = "ACEX Vehicles";
-    };
 };
