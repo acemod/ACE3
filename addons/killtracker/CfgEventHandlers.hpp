72b63f439429b3625ebfcd4817f42965281d1098
PabstMirror <pabstmirror@gmail.com>
2021-10-15 12:53:44 -0500

    Various - use compile_script macro for new addons (#8516)

--- addons/killtracker/CfgEventHandlers.hpp
+++ addons/killtracker/CfgEventHandlers.hpp
@@ -1,5 +1,5 @@
 class Extended_PostInit_EventHandlers {
     class ADDON {
-        init = QUOTE(call COMPILE_FILE(XEH_postInit));
+        init = QUOTE(call COMPILE_SCRIPT(XEH_postInit));
     };
 };
