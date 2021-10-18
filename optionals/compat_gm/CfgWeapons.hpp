ee00b541cabd64e060ab1b35688ac654a37e10e3
ZluskeN <pontux@gmail.com>
2021-10-18 19:59:02 +0200

    GM Compat - Jam Types and Open Bolts
    - Belt-fed Machineguns and SMGs fire from open bolts
    - Fire and Dud Jam types for Grenade Launchers and Flare Guns

--- optionals/compat_gm/CfgWeapons.hpp
+++ optionals/compat_gm/CfgWeapons.hpp
@@ -8,7 +8,10 @@ class CfgWeapons {
         EGVAR(overheating,allowSwapBarrel) = 1;
         EGVAR(overheating,dispersion) = 0.25;
     };
-    class gm_machineGun_base;
+    class gm_rifle_base;
+    class gm_machineGun_base: gm_rifle_base {
+        GVAR(closedBolt) = 0;
+    };
     class gm_mg3_base: gm_machineGun_base {
         EGVAR(overheating,mrbs) = 3000;
         EGVAR(overheating,slowdownFactor) = 1;
@@ -27,6 +30,33 @@ class CfgWeapons {
         EGVAR(overpressure,range) = 10;
     };
 
+    // SUB MACHINE GUNS
+    class gm_mp2_base: gm_rifle_base {
+        GVAR(closedBolt) = 0;
+    };
+    class gm_pm63_base: gm_rifle_base {
+        GVAR(closedBolt) = 0;
+    };
+    
+    // GRENADE LAUNCHERS
+    class gm_hk69a1_base: gm_rifle_base {
+        ace_overheating_jamTypesAllowed = ["Fire","Dud"];
+    };
+    class gm_pallad_d_base: gm_rifle_base {
+        ace_overheating_jamTypesAllowed = ["Fire","Dud"];
+    };
+    
+    // FLARE GUNS
+    class gm_pistol_base;
+    class gm_lp1_base: gm_pistol_base {
+        ace_overheating_jamTypesAllowed = ["Fire","Dud"];
+    };
+    class gm_p2a1_base: gm_pistol_base {
+        ace_overheating_jamTypesAllowed = ["Fire","Dud"];
+    };
+    
+    
+    
 
     // HELMETS
     #define HEARING_PROTECTION_OPEN EGVAR(hearing,protection) = 0; EGVAR(hearing,lowerVolume) = 0;
