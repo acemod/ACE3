27726e8d30158c5f23d10da6f50789a04af7f738
ZluskeN <pontux@gmail.com>
2021-10-19 20:10:00 +0200

    Update CfgWeapons.hpp

--- optionals/compat_sog/CfgWeapons.hpp
+++ optionals/compat_sog/CfgWeapons.hpp
@@ -4,5 +4,5 @@ class CfgWeapons {
     #include "CfgWeapons\launchers.hpp"
     #include "CfgWeapons\pistols.hpp"
     #include "CfgWeapons\uniforms.hpp"
-	#include "CfgWeapons\weapons.hpp"
+    #include "CfgWeapons\weapons.hpp"
 };
