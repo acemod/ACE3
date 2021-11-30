7a590c38bb9ba42cc1ca70176fae1dba17ece0fc
Whigital <andr3as.j4nss0n@gmail.com>
2021-11-30 18:14:47 +0100

    Compats - Add SOG helmet hearing and dust protection

--- optionals/compat_sog/CfgWeapons/helmets.hpp
+++ optionals/compat_sog/CfgWeapons/helmets.hpp
@@ -21,6 +21,13 @@ class vn_b_helmet_sph4_02_01: vn_b_headgear_base {
 class vn_b_helmet_t56_01_01: vn_b_headgear_base {
     HEARING_PROTECTION_VICCREW;
 };
+class vn_b_helmet_svh4_01_01: vn_b_headgear_base {
+    HEARING_PROTECTION_VICCREW;
+};
+class vn_b_helmet_svh4_02_01: vn_b_headgear_base {
+    HEARING_PROTECTION_VICCREW;
+    ACE_Protection = 1;
+};
 
 class vn_o_headgear_base;
 class vn_o_helmet_tsh3_01: vn_o_headgear_base {
