45e543e61e185c1a29408b2495b013a8d56a01ee
Steve Zhao <ampersand38@gmail.com>
2022-01-10 00:19:16 -0500

    remove testing code

--- addons/overheating/XEH_PREP.hpp
+++ addons/overheating/XEH_PREP.hpp
@@ -1,6 +1,3 @@
-// testing, update fncs on the fly
-#undef PREP
-#define PREP(var1) TRIPLES(ADDON,fnc,var1) = { call compile preProcessFileLineNumbers '\MAINPREFIX\PREFIX\SUBPREFIX\COMPONENT_F\functions\DOUBLES(fnc,var1).sqf' }
 
 PREP(calculateCooling);
 PREP(canUnjam);
