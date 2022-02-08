afd116d77b22f033c4fe1a474286fe003fc3351f
Julien <julien.etelain@outlook.com>
2022-02-08 18:37:29 +0100

    X-Cam Taunus data

---
+++ optionals/compat_xcam_taunus/script_component.hpp
@@ -0,0 +1,6 @@
+#define COMPONENT compat_xcam_taunus
+#define COMPONENT_BEAUTIFIED X-Cam Taunus Compatibility
+
+#include "\z\ace\addons\main\script_mod.hpp"
+
+#include "\z\ace\addons\main\script_macros.hpp"
