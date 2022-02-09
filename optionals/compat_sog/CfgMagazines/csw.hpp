a43af828b43533456b957d4a947d63743bb32a36
Mateusz Cyraniak <m.cyraniak@gmail.com>
2022-02-09 01:05:58 +0100

    SOG Compat - Fix M1919 ammo not showing in arsenal (#8815)
    vn_m1919_v_250_mag magazine has scopeArsenal attribute set to 0 so compat needs to address it.

--- optionals/compat_sog/CfgMagazines/csw.hpp
+++ optionals/compat_sog/CfgMagazines/csw.hpp
@@ -11,6 +11,7 @@ class GVAR(tow_missile): vn_missile_tow_mag_x1 {
 class vn_m1919_v_250_mag;
 class GVAR(m1919_250): vn_m1919_v_250_mag {
     scope = 2;
+    scopeArsenal = 2;
     type = 256;
     count = 250;
     model = "\vn\objects_f_vietnam\supply\a2_ammo\macv\vn_us_30cal.p3d";
