ecfde6f200fc0f6e4b596d21427ef681618eda70
James Woods <pterolatypus@gmail.com>
2022-05-12 15:11:12 +0100

    Use texture as icon, remove colour from names

--- addons/tagging/ACE_Tags.hpp
+++ addons/tagging/ACE_Tags.hpp
@@ -1,9 +1,9 @@
 #define GLUE(g1,g2) g1##g2
 #define TAG(name,col) class TRIPLES(ACE,name,col) { \
-        displayName = CSTRING(DOUBLES(name,col)); \
+        displayName = CSTRING(name); \
         requiredItem = QUOTE(GLUE(ACE_Spraypaint,col)); \
         textures[] = {QPATHTOF(UI\tags\col\name.paa)}; \
-        icon = QPATHTOF(UI\icons\iconTagging##col.paa); \
+        icon = QPATHTOF(UI\tags\col\name.paa); \
     } \
 
 class ACE_Tags {
@@ -11,68 +11,68 @@ class ACE_Tags {
         displayName = CSTRING(XBlack);
         requiredItem = "ACE_SpraypaintBlack";
         textures[] = {QPATHTOF(UI\tags\black\0.paa), QPATHTOF(UI\tags\black\1.paa), QPATHTOF(UI\tags\black\2.paa)};
-        icon = QPATHTOF(UI\icons\iconTaggingBlack.paa);
+        icon = QPATHTOF(UI\tags\black\0.paa);
     };
     class ACE_XRed {
         displayName = CSTRING(XRed);
         requiredItem = "ACE_SpraypaintRed";
         textures[] = {QPATHTOF(UI\tags\red\0.paa), QPATHTOF(UI\tags\red\1.paa), QPATHTOF(UI\tags\red\2.paa)};
-        icon = QPATHTOF(UI\icons\iconTaggingRed.paa);
+        icon = QPATHTOF(UI\tags\red\0.paa);
     };
     class ACE_XGreen {
         displayName = CSTRING(XGreen);
         requiredItem = "ACE_SpraypaintGreen";
         textures[] = {QPATHTOF(UI\tags\green\0.paa), QPATHTOF(UI\tags\green\1.paa), QPATHTOF(UI\tags\green\2.paa)};
-        icon = QPATHTOF(UI\icons\iconTaggingGreen.paa);
+        icon = QPATHTOF(UI\tags\green\0.paa);
     };
     class ACE_XBlue {
         displayName = CSTRING(XBlue);
         requiredItem = "ACE_SpraypaintBlue";
         textures[] = {QPATHTOF(UI\tags\blue\0.paa), QPATHTOF(UI\tags\blue\1.paa), QPATHTOF(UI\tags\blue\2.paa)};
-        icon = QPATHTOF(UI\icons\iconTaggingBlue.paa);
+        icon = QPATHTOF(UI\tags\blue\0.paa);
     };
 
-    TAG(ar_u,Black);
-    TAG(ar_d,Black);
-    TAG(ar_l,Black);
-    TAG(ar_r,Black);
-    TAG(circ,Black);
+    TAG(arrow_up,Black);
+    TAG(arrow_down,Black);
+    TAG(arrow_left,Black);
+    TAG(arrow_right,Black);
+    TAG(circle,Black);
     TAG(cross,Black);
-    TAG(diam,Black);
+    TAG(diamond,Black);
     TAG(square,Black);
-    TAG(tri,Black);
-    TAG(tri_inv,Black);
+    TAG(triangle,Black);
+    TAG(triangle_inverted,Black);
 
-    TAG(ar_u,Blue);
-    TAG(ar_d,Blue);
-    TAG(ar_l,Blue);
-    TAG(ar_r,Blue);
-    TAG(circ,Blue);
+    TAG(arrow_up,Blue);
+    TAG(arrow_down,Blue);
+    TAG(arrow_left,Blue);
+    TAG(arrow_right,Blue);
+    TAG(circle,Blue);
     TAG(cross,Blue);
-    TAG(diam,Blue);
+    TAG(diamond,Blue);
     TAG(square,Blue);
-    TAG(tri,Blue);
-    TAG(tri_inv,Blue);
+    TAG(triangle,Blue);
+    TAG(triangle_inverted,Blue);
     
-    TAG(ar_u,Green);
-    TAG(ar_d,Green);
-    TAG(ar_l,Green);
-    TAG(ar_r,Green);
-    TAG(circ,Green);
+    TAG(arrow_up,Green);
+    TAG(arrow_down,Green);
+    TAG(arrow_left,Green);
+    TAG(arrow_right,Green);
+    TAG(circle,Green);
     TAG(cross,Green);
-    TAG(diam,Green);
+    TAG(diamond,Green);
     TAG(square,Green);
-    TAG(tri,Green);
-    TAG(tri_inv,Green);
+    TAG(triangle,Green);
+    TAG(triangle_inverted,Green);
 
-    TAG(ar_u,Red);
-    TAG(ar_d,Red);
-    TAG(ar_l,Red);
-    TAG(ar_r,Red);
-    TAG(circ,Red);
+    TAG(arrow_up,Red);
+    TAG(arrow_down,Red);
+    TAG(arrow_left,Red);
+    TAG(arrow_right,Red);
+    TAG(circle,Red);
     TAG(cross,Red);
-    TAG(diam,Red);
+    TAG(diamond,Red);
     TAG(square,Red);
-    TAG(tri,Red);
-    TAG(tri_inv,Red);
+    TAG(triangle,Red);
+    TAG(triangle_inverted,Red);
 };
