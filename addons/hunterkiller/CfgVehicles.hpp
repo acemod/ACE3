fdbb3698f388c0d960480896b87349cb11b316f1
PabstMirror <pabstmirror@gmail.com>
2022-05-27 15:24:13 -0500

    Add to all 2035 tanks

--- addons/hunterkiller/CfgVehicles.hpp
+++ addons/hunterkiller/CfgVehicles.hpp
@@ -1,7 +1,7 @@
 class CfgVehicles {
     class Tank_F;
-    class MBT_01_base_F: Tank_F { // Merkel Tank
-        ADDON[] = {{{0}, 1}, {{0,0}, 3}};
+    class MBT_01_base_F: Tank_F {
+        ADDON = 1; // same as ADDON[] = {{{0}, 1}, {{0,0}, 3}};
     };
     class MBT_01_arty_base_F: MBT_01_base_F {
         ADDON = 0;
@@ -9,14 +9,16 @@ class CfgVehicles {
     class MBT_01_mlrs_base_F: MBT_01_base_F {
         ADDON = 0;
     };
-
-    class Wheeled_APC_F;
-    class APC_Wheeled_01_base_F: Wheeled_APC_F { // Badger IFV
+    class MBT_02_base_F: Tank_F {
         ADDON = 1;
     };
-
-    class MRAP_03_base_F;
-    class MRAP_03_hmg_base_F: MRAP_03_base_F {
-        ADDON[] = {{{0}, 0}, {{1}, 2}};
+    class MBT_02_arty_base_F: MBT_02_base_F {
+        ADDON = 0;
+    };
+    class MBT_03_base_F: Tank_F {
+        ADDON = 1;
+    };
+    class MBT_04_base_F: Tank_F {
+        ADDON = 1;
     };
 };
