8ef442ca4220231bc8b4eb5730a07e7901f225ab
Samuel Deutsch <sam.e.deutsch@gmail.com>
2022-07-24 00:07:36 -0700

    Added fuel tank volumes for SOG update 1.2 vehicles

--- optionals/compat_sog/CfgVehicles/planes.hpp
+++ optionals/compat_sog/CfgVehicles/planes.hpp
@@ -3,3 +3,13 @@ class Plane_Base_F;
 class vn_air_f4_base: Plane_Base_F {
     EGVAR(refuel,fuelCapacity) = 7548;
 };
+
+// F-100D https://www.supersabre.com/f-100specs.htm
+class vn_air_f100d_base : Plane_Base_F {
+    EGVAR(refuel,fuelCapacity) = 4500;
+};
+
+// MIG-19S https://en.wikipedia.org/wiki/Mikoyan-Gurevich_MiG-19#cite_ref-60
+class vn_air_mig19_base : Plane_Base_F {
+    EGVAR(refuel,fuelCapacity) = 1800;
+};
