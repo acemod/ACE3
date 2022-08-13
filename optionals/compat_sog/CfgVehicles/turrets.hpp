96473a8578a3766e2365b150add65c2359982a8e
Filip Maciejewski <veteran29@users.noreply.github.com>
2022-08-14 01:38:56 +0200

    Add missing continue digging action to spiderholes

--- optionals/compat_sog/CfgVehicles/turrets.hpp
+++ optionals/compat_sog/CfgVehicles/turrets.hpp
@@ -357,6 +357,16 @@ class vn_o_vc_spiderhole_01: StaticWeapon {
     EGVAR(trenches,noGeoClass) = QGVAR(spiderhole_01_nogeo);
     EGVAR(trenches,placementData)[] = {1.5, 1.5, 0.1};
     EGVAR(trenches,grassCuttingPoints)[] = {};
+
+    class ACE_Actions: ACE_Actions {
+        class ACE_MainActions: ACE_MainActions {
+            class ACE_ContinueDiggingTrench {
+                displayName = ECSTRING(trenches,ContinueDiggingTrench);
+                condition = QUOTE([ARR_2(_target,_player)] call EFUNC(trenches,canContinueDiggingTrench));
+                statement = QUOTE([ARR_2(_target,_player)] call EFUNC(trenches,continueDiggingTrench););
+            };
+        };
+    };
 };
 class vn_o_vc_spiderhole_02: vn_o_vc_spiderhole_01 {
     EGVAR(trenches,noGeoClass) = QGVAR(spiderhole_02_nogeo);
