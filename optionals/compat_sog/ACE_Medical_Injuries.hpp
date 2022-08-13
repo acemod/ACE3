5d8ff5a516eafc8efb922765374e02d8c7dbe934
Filip Maciejewski <veteran29@users.noreply.github.com>
2022-08-14 01:55:14 +0200

    Fix parenthesis

--- optionals/compat_sog/ACE_Medical_Injuries.hpp
+++ optionals/compat_sog/ACE_Medical_Injuries.hpp
@@ -8,7 +8,7 @@ class ACE_Medical_Injuries {
         class GVAR(explosive_incendiary): explosive {
             class woundHandlers: woundHandlers {
                 // TODO use function name after bug with the woundHandlers config caching is fixed
-                ADDON = QUOTE({call FUNC(woundsHandlerIncendiary}));
+                ADDON = QUOTE({call FUNC(woundsHandlerIncendiary)});
             };
         };
     };
