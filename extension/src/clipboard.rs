4d9730c41b5367435fd2b76f77fa2398fbd7a789
Brett <brett@mayson.io>
2022-08-30 18:00:06 -0600

    don't test clipboard on linux

--- extension/src/clipboard.rs
+++ extension/src/clipboard.rs
@@ -1,27 +1,36 @@
 use arma_rs::Group;
+
+#[cfg(feature = "clipboard")]
 use clipboard::{ClipboardContext, ClipboardProvider};
 
 pub fn group() -> Group {
-    Group::new()
-        .command("clear", clear)
-        .command("append", append)
-        .command("complete", complete)
+    let mut group = Group::new();
+    if cfg!(feature = "clipboard") {
+        group = group
+            .command("clear", clear)
+            .command("append", append)
+            .command("complete", complete)
+    }
+    group
 }
 
 static mut BUFFER: String = String::new();
 
+#[cfg(feature = "clipboard")]
 pub fn clear() {
     unsafe {
         BUFFER.clear();
     }
 }
 
+#[cfg(feature = "clipboard")]
 pub fn append(text: String) {
     unsafe {
         BUFFER.push_str(&text);
     }
 }
 
+#[cfg(feature = "clipboard")]
 pub fn complete() -> Result<(), String> {
     let mut ctx = ClipboardContext::new().map_err(|e| e.to_string())?;
     ctx.set_contents(unsafe { BUFFER.clone() })
