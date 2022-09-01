cc3bad3c5685468870a7ea3fde3e321abcd2f990
PabstMirror <pabstmirror@gmail.com>
2022-09-01 13:44:19 -0500

    XM157 (NGSW-FC Smart Scope) Framework (#8897)
    * XM157 (NGSW-FC Smart Scope)
    
    * Update addons/common/functions/fnc_rscObjectHelper.sqf
    
    Co-authored-by: Jouni Järvinen <rautamiekka@users.noreply.github.com>
    
    * Update addons/xm157/functions/fnc_weaponInfo_draw.sqf
    
    Co-authored-by: Jouni Järvinen <rautamiekka@users.noreply.github.com>
    
    * Headers
    
    * Handle prone-deploy weapon bank
    
    * Disable Reticle picker for now
    
    * some localizations
    
    * Change font
    
    looks like CUP modifies EtelkaMonospaceProBold for some reason??
    
    * Create xm157-framework.md
    
    * Formating
    
    Co-authored-by: Jouni Järvinen <rautamiekka@users.noreply.github.com>

---
+++ addons/xm157/RscInGameUI.hpp
@@ -0,0 +1,79 @@
+class RscObject;
+class RscControlsGroupNoScrollbars;
+
+class RscText;
+class GVAR(text): RscText {
+    font = "EtelkaMonospacePro";
+    SizeEx = 0.04;
+    colorText[]={1,0.1,0.05,0.95};
+    shadow = 0;
+};
+class GVAR(textMenu): GVAR(text) {
+    SizeEx = 0.09;
+    style = 2+16;
+};
+
+class RscInGameUI {
+    class CBA_ScriptedOptic_zooming;
+    class GVAR(info): CBA_ScriptedOptic_zooming {
+        onLoad = QUOTE(call FUNC(weaponInfo_onLoad));
+        class objects {            
+            class Optic: RscObject { // first focal plane
+                idc = IDC_SCOPE_OBJECT;
+                type = 82;
+                model = "\A3\Misc_F\Helpers\UserTexture1m.p3d";
+                x = 0;
+                y = 0;
+                z = 0;
+                xBack = 0.9;
+                yBack = 0.9;
+                zBack = 0.3;
+                inBack = 0;
+                enableZoom = 1;
+                zoomDuration = 0.001;
+                class Areas {
+                    class usertexture {
+                        selection = "usertexture";
+                        class controls {
+                            class test: RscControlsGroupNoScrollbars {
+                                idc = IDC_SCOPE_GROUP;
+                                x = 0;
+                                y = 0;
+                                w = 1;
+                                h = 4/3;
+                            };
+                        };
+                    };
+                };
+            };
+            class Screen: RscObject {
+                idc = IDC_SCREEN_OBJECT;
+                type = 82;
+                model = "\A3\Misc_F\Helpers\UserTexture1m.p3d";
+                x = 0;
+                y = 0;
+                z = 0;
+                xBack = 0.9;
+                yBack = 0.9;
+                zBack = 0.3;
+                inBack = 1;
+                enableZoom = 1;
+                zoomDuration = 0.001;
+                class Areas {
+                    class usertexture {
+                        selection = "usertexture";
+                        class controls {
+                            class test: RscControlsGroupNoScrollbars {
+                                idc = IDC_SCREEN_GROUP;
+                                x = 0;
+                                y = 0;
+                                w = 1;
+                                h = 4/3;
+                            };
+                        };
+                    };
+                };
+            };
+        };
+    };
+};
