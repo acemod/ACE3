0629035270170faa8778363c84aedc6c343cd21b
jonpas <jonpas33@gmail.com>
2023-04-18 05:18:00 +0200

    Download latest HEMTT and cleanup setup.bat

--- tools/setup.bat
+++ tools/setup.bat
@@ -1,25 +1,16 @@
 ;@Findstr -bv ;@F "%~f0" | powershell -Command - & pause & goto:eof
 
-# Unzip backwards compatibility (Windows 8)
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-function Unzip {
-    param([string]$zipfile, [string]$outpath)
-    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
-}
-
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$client = New-Object Net.WebClient
 Write-Output "=> Downloading ..."
-$client.DownloadFile("https://github.com/BrettMayson/HEMTT/releases/download/v1.3.1/windows-x64.zip", "hemtt.zip")
-$client.dispose()
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-Write-Output "=> Cleaning old ..."
-If (Test-Path "..\hemtt.exe") {
-    Remove-Item -Force "..\hemtt.exe"
-}
+$url = "https://github.com/BrettMayson/HEMTT/releases/latest/download/windows-x64.zip"
+(New-Object Net.WebClient).DownloadFile($url, "hemtt.zip"); Write-Output "$url => hemtt.zip"
 
-Write-Output "=> Extracting ..."
-Unzip "hemtt.zip" "..\."
+Write-Output "`n=> Extracting ..."
+Expand-Archive -Path "hemtt.zip" -DestinationPath "..\." -Force; Write-Output "hemtt.zip"
 Remove-Item "hemtt.zip"
 
-Write-Output "Tools successfully installed to project!"
+Write-Output "`n=> Verifying ..."
+Start-Process -FilePath ..\hemtt.exe -ArgumentList --version -NoNewWindow -Wait
+
+Write-Output "`nTools successfully installed to project!"
