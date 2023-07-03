544640258f8a3572dc19c9db0ec1fcaef1e51420
KJW <100206101+SpicyBagpipes@users.noreply.github.com>
2023-07-03 19:37:36 +0100

    Update script_component.hpp
    arrayIntersect with possible wirecutters

--- addons/logistics_wirecutter/script_component.hpp
+++ addons/logistics_wirecutter/script_component.hpp
@@ -88,7 +88,7 @@
 #define CUT_TIME_ENGINEER 7.5
 
 #define HAS_WIRECUTTER(unit) (\
-    (unit call EFUNC(common,uniqueItems)) findIf {_x isKindOf ["ACE_wirecutter", configFile >> "CfgWeapons"]} != -1 \
+    count (unit call EFUNC(common,uniqueItems)) arrayIntersect GVAR(possibleWirecutters) != -1 \
     || {getNumber ((configOf (backpackContainer unit)) >> QGVAR(hasWirecutter)) == 1} \
     || {getNumber (configFile >> "CfgWeapons" >> (vest unit) >> QGVAR(hasWirecutter)) == 1} \
 )
