d4863f949b198c6d6bc4bfb67e23f0b32b409dc4
Grim <69561145+LinkIsGrim@users.noreply.github.com>
2023-08-02 01:58:52 +0300

    capitalization
    Co-authored-by: Jouni Järvinen <rautamiekka@users.noreply.github.com>

--- addons/compat_rhs_afrf3/CfgMagazines.hpp
+++ addons/compat_rhs_afrf3/CfgMagazines.hpp
@@ -3,18 +3,18 @@ class cfgMagazines {
     class rhs_30Rnd_545x39_7N6_AK;
 
     class rhs_100Rnd_762x54mmR: rhs_30Rnd_545x39_7N6_AK {
-        ace_isbelt = 1;
+        ACE_isBelt = 1;
     };
     class rhs_mag_127x108mm_50: VehicleMagazine {
-        ace_isbelt = 1;
+        ACE_isBelt = 1;
     };
     class rhs_mag_127x108mm_150: rhs_mag_127x108mm_50 {
-        ace_isbelt = 0;
+        ACE_isBelt = 0;
     };
     class rhs_mag_127x108mm_1470: rhs_mag_127x108mm_50 {
-        ace_isbelt = 0;
+        ACE_isBelt = 0;
     };
     class RHS_mag_VOG30_30: VehicleMagazine {
-        ace_isbelt = 1;
+        ACE_isBelt = 1;
     };
 };
--- addons/compat_rhs_usf3/CfgMagazines.hpp
+++ addons/compat_rhs_usf3/CfgMagazines.hpp
@@ -19,16 +19,16 @@ class cfgMagazines {
     class rhs_mag_30Rnd_556x45_M855A1_Stanag;
 
     class rhsusf_100Rnd_556x45_soft_pouch: rhs_mag_30Rnd_556x45_M855A1_Stanag {
-        ace_isbelt = 1;
+        ACE_isBelt = 1;
     };
     class rhsusf_50Rnd_762x51: CA_Magazine {
-        ace_isbelt = 1;
+        ACE_isBelt = 1;
     };
     class rhs_mag_100rnd_127x99_mag: VehicleMagazine {
-        ace_isbelt = 1;
+        ACE_isBelt = 1;
     };
     class RHS_48Rnd_40mm_MK19: VehicleMagazine {
-        ace_isbelt = 1;
+        ACE_isBelt = 1;
     };
 
     class CA_LauncherMagazine;
