0aaef4c0fd6bf931b741761cbffaee23572d2b9c
PabstMirror <pabstmirror@gmail.com>
2023-08-02 12:03:09 -0500

    Update addons/compat_csla/CfgMagazines.hpp
    Co-authored-by: Grim <69561145+LinkIsGrim@users.noreply.github.com>

--- addons/compat_csla/CfgMagazines.hpp
+++ addons/compat_csla/CfgMagazines.hpp
@@ -6,18 +6,16 @@ class CfgMagazines {
     };
 
     class US85_Magazine;
-    class US85_20Rnd_762x51;
-    class US85_30Rnd_556x45;
-    class US85_30Rnd_762x51;
-
     class US85_50Rnd_762x51: US85_Magazine {
         ace_isBelt = 1;
     };
 
+    class US85_20Rnd_762x51;
     class US85_100Rnd_762x51: US85_20Rnd_762x51 {
         ace_isBelt = 1;
     };
 
+    class US85_30Rnd_556x45;
     class US85_200Rnd_556x45: US85_30Rnd_556x45 {
         ace_isBelt = 1;
     };
