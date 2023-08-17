5900e29e3319dec730a652c5611ffb03159f4c75
Hypoxic <60307173+hypoxia125@users.noreply.github.com>
2023-08-17 09:50:32 -0400

    Update addons/fieldmanual/script_component.hpp
    Co-authored-by: jonpas <jonpas33@gmail.com>

--- addons/fieldmanual/script_component.hpp
+++ addons/fieldmanual/script_component.hpp
@@ -1,5 +1,5 @@
 #define COMPONENT fieldmanual
-#define COMPONENT_BEAUTIFIED FieldManual
+#define COMPONENT_BEAUTIFIED Field Manual
 #include "\z\ace\addons\main\script_mod.hpp"
 
 // #define DEBUG_MODE_FULL
