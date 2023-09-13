596535958d7c516d256a05475dd0925125a842af
Will/KJW <100206101+SpicyBagpipes@users.noreply.github.com>
2023-09-13 19:57:22 +0100

    Update CfgWeapons.hpp
    Add handcuff items

--- addons/captives/CfgWeapons.hpp
+++ addons/captives/CfgWeapons.hpp
@@ -14,4 +14,25 @@ class CfgWeapons {
             mass = 1;
         };
     };
+    class ACE_Handcuffs: ACE_CableTie {
+        GVAR(reusable) = 1;
+        displayName = "Handcuffs"; //TODO: Localise
+        descriptionShort = "A pair of reusable handcuffs used for restraining prisoners."; //TODO: Localise
+        //model = QPATHTOF(models\ace_handcuffs.p3d); //No model source of current.
+        //picture = QPATHTOF(UI\ace_handcuffs_ca.paa); //Icon pending.
+        class ItemInfo: ItemInfo {
+            mass = 6.025;
+        };
+    };
+    class ACE_HandcuffKey: ACE_ItemCore {
+        author = ECSTRING(common,ACETeam);
+        GVAR(key) = 1;
+        displayName = "Handcuff Key"; //TODO: Localise
+        descriptionShort = "A key for a handcuff."; //TODO: Localise
+        //model = QPATHTOF(models\ace_handcuffkey.p3d); //No model source of current.
+        //picture = QPATHTOF(UI\ace_handcuffkey_ca.paa); //Icon pending.
+        class ItemInfo: CBA_MiscItem_ItemInfo {
+            mass = 1;
+        };
+    };
 };
