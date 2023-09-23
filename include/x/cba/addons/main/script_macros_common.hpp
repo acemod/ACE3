ab6dd0164ee98bff7655a50bd6c0ac7c5f34e1be
Mike-MF <TyroneMF@hotmail.com>
2023-09-23 18:07:52 +0100

    Compat WS - Realistic Names (#9419)
    * Compat WS - Realistic Names
    
    * use cba subcstring
    
    * cleanup python
    
    * Update script_macros_common.hpp
    
    ---------
    
    Co-authored-by: BrettMayson <brett@mayson.io>
    Co-authored-by: PabstMirror <pabstmirror@gmail.com>

--- include/x/cba/addons/main/script_macros_common.hpp
+++ include/x/cba/addons/main/script_macros_common.hpp
@@ -1275,9 +1275,11 @@ Author:
     #define ELSTRING(var1,var2) QUOTE(TRIPLES(STR,DOUBLES(PREFIX,var1),var2))
     #define CSTRING(var1) QUOTE(TRIPLES($STR,ADDON,var1))
     #define ECSTRING(var1,var2) QUOTE(TRIPLES($STR,DOUBLES(PREFIX,var1),var2))
+    #define SUBCSTRING(var1) QUOTE(TRIPLES($STR,SUBADDON,var1))
 
     #define LLSTRING(var1) localize QUOTE(TRIPLES(STR,ADDON,var1))
     #define LELSTRING(var1,var2) localize QUOTE(TRIPLES(STR,DOUBLES(PREFIX,var1),var2))
+    #define LSUBLSTRING(var1) localize QUOTE(TRIPLES(STR,SUBADDON,var1))
 #endif
 
 
