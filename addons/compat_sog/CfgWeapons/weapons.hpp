ebce9eb8aef52e48751c254b21d85cf78f5217d2
Assanali Mukhanov <22858781+Esmeray6@users.noreply.github.com>
2023-09-28 06:05:20 +0600

    Add barrel swap support to SOG MGs

--- addons/compat_sog/CfgWeapons/weapons.hpp
+++ addons/compat_sog/CfgWeapons/weapons.hpp
@@ -13,6 +13,18 @@ class vn_pk: vn_lmg {
 class vn_m60: vn_lmg {
     EGVAR(overheating,allowSwapBarrel) = 1;
 };
+class vn_mg42: vn_lmg {
+    EGVAR(overheating,allowSwapBarrel) = 1;
+};
+class vn_l4: vn_lmg {
+    EGVAR(overheating,allowSwapBarrel) = 1;
+};
+class vn_m1918: vn_lmg {
+    EGVAR(overheating,allowSwapBarrel) = 1;
+};
+class vn_rpd: vn_lmg {
+    EGVAR(overheating,allowSwapBarrel) = 1;
+};
 
 class vn_smg: vn_rifle {
     EGVAR(overheating,closedBolt) = 0;
