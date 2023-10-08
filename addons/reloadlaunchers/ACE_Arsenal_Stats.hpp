a1fafbd56d21900711fa78fba9616d2fb9f2c7c5
PabstMirror <pabstmirror@gmail.com>
2023-10-08 17:59:32 -0500

    Apply suggestions from code review
    Co-authored-by: Jouni Järvinen <rautamiekka@users.noreply.github.com>
    Co-authored-by: johnb432 <58661205+johnb432@users.noreply.github.com>

--- addons/reloadlaunchers/ACE_Arsenal_Stats.hpp
+++ addons/reloadlaunchers/ACE_Arsenal_Stats.hpp
@@ -4,9 +4,9 @@ class EGVAR(arsenal,stats) {
         scope = 2;
         priority = -1;
         stats[] = {QGVAR(enabled)};
-        displayName= CSTRING(LoadLauncher);
+        displayName = CSTRING(LoadLauncher);
         showText = 1;
-        text = ECSTRING(Common,Enabled);
+        textStatement = QUOTE(localize QUOTE(ELSTRING(Common,Enabled)));
         condition = QUOTE(params[ARR_2('_stat', '_config')]; (getNumber (_config >> _stat select 0)) == 1);
         tabs[] = {{2}, {}};
     };
