1973a1040497cf0529bc9ae5f8387201cc7e9144
Bob-Murphy <50651578+Bob-Murphy@users.noreply.github.com>
2023-10-17 15:07:58 +0200

    Update land.hpp

--- addons/compat_spe/CfgVehicles/land.hpp
+++ addons/compat_spe/CfgVehicles/land.hpp
@@ -1,12 +1,12 @@
 // fuel objects
 
-class SPE_items_base;
-class Land_SPE_Fuel_Barrel_German: SPE_items_base {
+class SPE_Fuel_Barrel_base;
+class SPE_Fuel_Barrel_German_01: SPE_Fuel_Barrel_base {
     transportFuel = 0;
     EGVAR(refuel,hooks)[] = {{0, 0, 0.5}}; // reference is Land_FlexibleTank_01_F
     EGVAR(refuel,fuelCargo) = 300; // reference is Land_FlexibleTank_01_F
 };
-class Land_SPE_Fuel_Barrel_US: SPE_items_base {
+class SPE_Fuel_Barrel_US_01: SPE_Fuel_Barrel_base {
     transportFuel = 0;
     EGVAR(refuel,hooks)[] = {{0, 0, 0.5}}; // reference is Land_FlexibleTank_01_F
     EGVAR(refuel,fuelCargo) = 300; // reference is Land_FlexibleTank_01_F
