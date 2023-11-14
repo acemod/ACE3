4382a94f506257b250294864666fcf9597996b73
Mike-MF <TyroneMF@hotmail.com>
2023-11-14 01:33:21 +0000

    Remove unnecessary BWC

--- addons/compat_cup_weapons/script_component.hpp
+++ addons/compat_cup_weapons/script_component.hpp
@@ -3,7 +3,3 @@
 
 #include "\z\ace\addons\main\script_mod.hpp"
 #include "\z\ace\addons\main\script_macros.hpp"
-
-// Backwards compatibility
-#undef GVAR
-#define GVAR(var) TRIPLES(PREFIX,COMPONENT,var)
