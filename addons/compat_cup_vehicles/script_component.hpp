be79b07cae660dcf3d561f1c922e959db0b6e8e2
Mike-MF <TyroneMF@hotmail.com>
2023-11-14 01:39:49 +0000

    Remove unnecessary BWC

--- addons/compat_cup_vehicles/script_component.hpp
+++ addons/compat_cup_vehicles/script_component.hpp
@@ -4,10 +4,6 @@
 #include "\z\ace\addons\main\script_mod.hpp"
 #include "\z\ace\addons\main\script_macros.hpp"
 
-// Backwards compatibility
-#undef GVAR
-#define GVAR(var) TRIPLES(PREFIX,COMPONENT,var)
-
 // Fries Macro
 #define EQUIP_FRIES_ATTRIBUTE \
     class Attributes { \
