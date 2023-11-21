acemod/ACE3

commit 28e8e7d4ca1168039a47d1cc390f6c126b646a31

Refuel - Add events for fuel source and jerry can creation (#9133)
* Refuel - Added events

* Added ´ace_refuel_fuelSourceInitialized´ and `ace_refuel_jerryCanInitalized´ events
* Updated documentation

* Cleaner implementation && more coherent with analog rearm event

* Rebase merging is weird

addons/refuel/functions/fnc_makeSource.sqf
@@ -56,9 +56,11 @@ if (
     _source setVariable [QGVAR(hooks), _hooks, true];
 };
 
-// check if menu already exists
-if (_fuelCargoConfig != 0 || {!isNil {_source getVariable QGVAR(initSource_jipID)}}) exitWith {};
+// only add if menu doesn't already exist
+if (!(_fuelCargoConfig != 0 && {!isNil {_source getVariable QGVAR(initSource_jipID)}})) then {
+    private _jipID = [QGVAR(initSource), [_source]] call CBA_fnc_globalEventJIP;
+    [_jipID, _source] call CBA_fnc_removeGlobalEventJIP;
+    _source setVariable [QGVAR(initSource_jipID), _jipID];
+};
 
-private _jipID = [QGVAR(initSource), [_source]] call CBA_fnc_globalEventJIP;
-[_jipID, _source] call CBA_fnc_removeGlobalEventJIP;
-_source setVariable [QGVAR(initSource_jipID), _jipID];
+[QGVAR(sourceInitialized), [_source]] call CBA_fnc_globalEvent;

addons/refuel/functions/fnc_makeJerryCan.sqf
@@ -24,9 +24,12 @@ if (isNull _target ||
 
 _target setVariable [QGVAR(jerryCan), true];
 _target setVariable [QGVAR(source), _target];
-
 _target setVariable [QGVAR(capacity), _fuelAmount];
-if (isServer) then { [_target, _fuelAmount] call FUNC(setFuel); };
+
+if (isServer) then {
+    [_target, _fuelAmount] call FUNC(setFuel); // has global effects
+    [QVAR(jerryCanInitalized), [_target]] call CBA_fnc_globalevent;
+};
 
 // Main Action
 private _action = [QGVAR(Refuel),

commit 213c73385697d9cff7ba8e01ad0cd27d03fe2758

Fix issues introduced in #9133

addons/refuel/functions/fnc_makeSource.sqf
@@ -60,10 +60,10 @@ if (
 };
 
 // only add if menu doesn't already exist
-if (!(_fuelCargoConfig != 0 && {!isNil {_source getVariable QGVAR(initSource_jipID)}})) then {
-    private _jipID = [QGVAR(initSource), [_source]] call CBA_fnc_globalEventJIP;
-    [_jipID, _source] call CBA_fnc_removeGlobalEventJIP;
-    _source setVariable [QGVAR(initSource_jipID), _jipID];
-};
+if (_fuelCargoConfig != REFUEL_DISABLED_FUEL || {!isNil {_source getVariable QGVAR(initSource_jipID)}}) exitWith {};
+
+private _jipID = [QGVAR(initSource), [_source]] call CBA_fnc_globalEventJIP;
+[_jipID, _source] call CBA_fnc_removeGlobalEventJIP;
+_source setVariable [QGVAR(initSource_jipID), _jipID];
 
 [QGVAR(sourceInitialized), [_source]] call CBA_fnc_globalEvent;