29d3d4c19fec79eb433701bf6974648ba29efdf1
lambdatiger <spam.stnuocca.sl@gmail.com>
2024-01-15 15:39:27 -0600

    alphabetized PREP instead of categorizing it

--- addons/frag/XEH_PREP.hpp
+++ addons/frag/XEH_PREP.hpp
@@ -1,37 +1,26 @@
-// dev
-#ifdef DEBUG_MODE_DRAW
-PREP(dev_fired);
-PREP(dev_trackHitBox);
-PREP(dev_sphereDraw);
-PREP(dev_addRound);
-PREP(dev_trackObj);
-PREP(dev_drawTrace);
-PREP(dev_clearTraces);
-PREP(dev_switchUnitHandle);
-#endif
-PREP(dev_fragCalcDump);
-PREP(dev_debugAmmo);
-
-// Base
-PREP(initRound);
-
-// Frag
 PREP(addBlackList);
-PREP(initBlackList);
-PREP(shouldFrag);
-PREP(getFragInfo);
+PREP(dev_addRound);
+PREP(dev_clearTraces);
+PREP(dev_debugAmmo);
+PREP(dev_drawTrace);
+PREP(dev_fired);
+PREP(dev_fragCalcDump);
+PREP(dev_trackHitBox);
+PREP(dev_trackObj);
+PREP(dev_sphereDraw);
+PREP(dev_switchUnitHandle);
+PREP(doExplosions);
 PREP(doFrag);
-PREP(doFragTargeted);
 PREP(doFragRandom);
-
-// Spall
-PREP(shouldSpall);
+PREP(doFragTargeted);
+PREP(doReflections);
 PREP(doSpall);
+PREP(findReflections);
 PREP(getMaterialInfo);
 PREP(getSpallInfo);
+PREP(getFragInfo);
+PREP(initBlackList);
 PREP(initMaterialCache);
-
-// Explosive Reflection
-PREP(findReflections);
-PREP(doExplosions);
-PREP(doReflections);
+PREP(initRound);
+PREP(shouldFrag);
+PREP(shouldSpall);
