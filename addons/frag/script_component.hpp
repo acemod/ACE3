7aa17aa91bda47da06a1a9f50efb162ea160bc87
lambdatiger <spam.stnuocca.sl@gmail.com>
2024-01-16 15:38:47 -0600

    Grammatical erros

--- addons/frag/script_component.hpp
+++ addons/frag/script_component.hpp
@@ -29,7 +29,7 @@
 #define ACE_FRAG_ROUND_COEF 0.02828427
 // half of gravity approx 9.81/2
 #define ACE_FRAG_HALF_GRAVITY_APPROX 4.905
-// stop searching at 0.5% chance to hit
+// Lowest chance to hit of 0.5%
 #define ACE_FRAG_MIN_FRAG_HIT_CHANCE 0.005
 #define ACE_FRAG_MIN_GROUND_OFFSET 0.25
 #define ACE_FRAG_LOW_FRAG_COEFF 4
