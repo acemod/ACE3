a022867049fce7f4ca29dfd41e00024258fa8f9d
BrettMayson <brett@mayson.io>
2024-01-18 14:24:39 -0600

    get version from git

--- extension/src/lib.rs
+++ extension/src/lib.rs
@@ -15,15 +15,12 @@ mod fcs;
 #[arma]
 fn init() -> Extension {
     Extension::build()
+        .version(env!("GIT_HASH").to_string())
         .group("artillery", artillery::group())
         .group("ballistics", ballistics::group())
         .group("clipboard", clipboard::group())
         .command("break_line", break_line::break_line)
         .command("fcs", fcs::get_solution)
-        .command("version", || {
-            (option_env!("CARGO_MAKE_GIT_HEAD_LAST_COMMIT_HASH"))
-                .unwrap_or("UNKNOWN")
-                .to_string()
-        })
+        .command("version", || env!("GIT_HASH"))
         .finish()
 }
