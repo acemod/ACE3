2181f1ea1416d4de6ad8075a65b818ea3e59e2ae
Sotirios Pupakis <sotirek@gmail.com>
2024-02-16 02:09:40 +0100

    Add option to disable AI kills tracking in Kill Tracker
    Add an option to disable tracking of kills of AI units when using ACE Kill Tracker. Configurable through Addon Settings

--- addons/killtracker/CfgEventHandlers.hpp
+++ addons/killtracker/CfgEventHandlers.hpp
@@ -3,3 +3,9 @@ class Extended_PostInit_EventHandlers {
         init = QUOTE(call COMPILE_SCRIPT(XEH_postInit));
     };
 };
+
+class Extended_PreInit_EventHandlers {
+    class ADDON {
+        init = QUOTE(call COMPILE_SCRIPT(XEH_preInit));
+    };
+};
---
+++ addons/killtracker/initSettings.inc.sqf
@@ -0,0 +1,10 @@
+[
+    QGVAR(trackAI),
+    "CHECKBOX",
+    [LSTRING(TrackAI_DisplayName), LSTRING(TrackAI_Description)],
+    LSTRING(Category),
+    true,
+    false,
+    {},
+    true
+] call CBA_fnc_addSetting;
