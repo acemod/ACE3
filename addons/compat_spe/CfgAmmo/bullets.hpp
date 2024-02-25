30ce5d6ba714271de88ea6737e32111d0111ec19
Kerc <daza400@gmail.com>
2024-02-25 20:20:20 +1000

    Added ACE_caliber to SPE 50 cal ammo as it was causing extreme deafness

---
+++ addons/compat_spe/CfgAmmo/bullets.hpp
@@ -0,0 +1,19 @@
+class BulletBase;
+class SubmunitionBase;
+class BulletCore;
+class SPE_Bullet_Vehicle_base;
+
+class SPE_B_127x99_Mixed: SubmunitionBase {
+  ACE_caliber = 12.954;
+};
+
+class SPE_B_127x99_Ball: SPE_Bullet_Vehicle_base {
+  ACE_caliber = 12.954;
+};
+class SPE_B_127x99_AP: SPE_B_127x99_Ball {
+  ACE_caliber = 12.954;
+};
+class SPE_B_127x99_API: SPE_B_127x99_Ball {
+  ACE_caliber = 12.954;
+  ace_vehicle_damage_incendiary = 1;
+};
