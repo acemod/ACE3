f6fcc075c41e2488c4ac4856db139f334c784e75
kerckasha <daza400@gmail.com>
2024-02-27 11:55:01 +1000

    Update addons/compat_spe/CfgAmmo/bullets.hpp
    Co-authored-by: johnb432 <58661205+johnb432@users.noreply.github.com>

--- addons/compat_spe/CfgAmmo/bullets.hpp
+++ addons/compat_spe/CfgAmmo/bullets.hpp
@@ -1,16 +1,12 @@
 class SPE_Bullet_Vehicle_base;
 
 class SPE_B_127x99_Mixed: SubmunitionBase {
-  ACE_caliber = 12.954;
+    ACE_caliber = 12.954;
 };
 
 class SPE_B_127x99_Ball: SPE_Bullet_Vehicle_base {
-  ACE_caliber = 12.954;
-};
-class SPE_B_127x99_AP: SPE_B_127x99_Ball {
-  ACE_caliber = 12.954;
+    ACE_caliber = 12.954;
 };
 class SPE_B_127x99_API: SPE_B_127x99_Ball {
-  ACE_caliber = 12.954;
-  ace_vehicle_damage_incendiary = 1;
+    EGVAR(vehicle_damage,incendiary) = 1;
 };
