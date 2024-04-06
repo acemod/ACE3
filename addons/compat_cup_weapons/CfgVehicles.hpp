74c4cf32a878b848e359fa8ac6d8b7f5e2027b9f
PabstMirror <pabstmirror@gmail.com>
2024-04-05 22:44:15 -0500

    Revert "add 💩 so it builds"
    This reverts commit 7471fd4ab3a5f946939616a06cf0aa23e77af3b1.

--- addons/compat_cup_weapons/CfgVehicles.hpp
+++ addons/compat_cup_weapons/CfgVehicles.hpp
@@ -4,7 +4,7 @@ class CfgVehicles {
         EGVAR(casings,model) = "\CUP\Weapons\CUP_Weapons_Ammunition\magazines\cartridge545.p3d";
     };
     class CUP_FxCartridge_939: FxCartridge {
-        EGVAR(casings,model) = "\CUP\Weapons\CUP_Weapons_Ammunition\magazines\cartridge939.p3d";
+        EGVAR(casings,model) ="\CUP\Weapons\CUP_Weapons_Ammunition\magazines\cartridge939.p3d";
     };
     // oicw?
 };
