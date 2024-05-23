acemod/ACE3

commit 28e8e7d4ca1168039a47d1cc390f6c126b646a31

Refuel - Add events for fuel source and jerry can creation (#9133)
* Refuel - Added events

* Added ´ace_refuel_fuelSourceInitialized´ and `ace_refuel_jerryCanInitalized´ events
* Updated documentation

* Cleaner implementation && more coherent with analog rearm event

* Rebase merging is weird

addons/refuel/functions/fnc_makeSource.sqf
@@ -56,9 +56,11 @@ if (
     _source setVariable [QGVAR(hooks), _hooks, true];
 };
 
-// check if menu already exists
-if (_fuelCargoConfig != 0 || {!isNil {_source getVariable QGVAR(initSource_jipID)}}) exitWith {};
+// only add if menu doesn't already exist
+if (!(_fuelCargoConfig != 0 && {!isNil {_source getVariable QGVAR(initSource_jipID)}})) then {
+    private _jipID = [QGVAR(initSource), [_source]] call CBA_fnc_globalEventJIP;
+    [_jipID, _source] call CBA_fnc_removeGlobalEventJIP;
+    _source setVariable [QGVAR(initSource_jipID), _jipID];
+};
 
-private _jipID = [QGVAR(initSource), [_source]] call CBA_fnc_globalEventJIP;
-[_jipID, _source] call CBA_fnc_removeGlobalEventJIP;
-_source setVariable [QGVAR(initSource_jipID), _jipID];
+[QGVAR(sourceInitialized), [_source]] call CBA_fnc_globalEvent;

addons/refuel/functions/fnc_makeJerryCan.sqf
@@ -24,9 +24,12 @@ if (isNull _target ||
 
 _target setVariable [QGVAR(jerryCan), true];
 _target setVariable [QGVAR(source), _target];
-
 _target setVariable [QGVAR(capacity), _fuelAmount];
-if (isServer) then { [_target, _fuelAmount] call FUNC(setFuel); };
+
+if (isServer) then {
+    [_target, _fuelAmount] call FUNC(setFuel); // has global effects
+    [QVAR(jerryCanInitalized), [_target]] call CBA_fnc_globalevent;
+};
 
 // Main Action
 private _action = [QGVAR(Refuel),

commit c0d74ba703fa38f2e86bc69e9793000577f1da85

Refuel - Fix fuel source disabling (#9995)
* Add enable setting

* Exclude man class init

* Fix multiplayer terrain pump fuel sync

* Add terrain pumps positions

* Add vanilla fuel cargo restoring before destroying

* Add Livonia positions by bux

* Fix terrain pumps destruction

* Improve settings init

* Fix double settings category

* Check enabled var in public functions

* Fix fnc_makeSource

* Handle recent CUP Terrains changes

* Update includes

* Fix issues introduced in #9133

* Change warning

Co-authored-by: johnb432 <58661205+johnb432@users.noreply.github.com>

* Optimisations and cleanup

- Use hashmaps wherever possible
- Reduced pump search radius by ~30%
- Sorted pumps alphabetically and sorted positions by "smallest" first, for consistency

* Add init debug trace

* compileScript in dev

* yoda conditions & DFUNC macro

* Wait until CBA settings are ready

* Update Chernarus 2020 and add more maps configs

* Remove vanilla fuel cargo restoring before destroying

* Update fnc_makeSource.sqf

---------

Co-authored-by: Dystopian <sddex@ya.ru>
Co-authored-by: Grim <69561145+LinkIsGrim@users.noreply.github.com>

addons/refuel/functions/fnc_makeSource.sqf
@@ -40,8 +40,23 @@ TRACE_4("makeSource",_source,_fuelCargo,_hooks,_fuelCargoConfig);
 if (
     isNull _source
     || {_fuelCargo < 0 && {!(_fuelCargo in [REFUEL_INFINITE_FUEL, REFUEL_DISABLED_FUEL])}}
-    || {_fuelCargo == REFUEL_DISABLED_FUEL && {_fuelCargoConfig == REFUEL_DISABLED_FUEL}}
 ) exitWith {};
+
+// We might be removing fuel from an object that in config doesn't have fuel, but was given fuel via this function prior
+if (_fuelCargo == REFUEL_DISABLED_FUEL && {_fuelCargoConfig == REFUEL_DISABLED_FUEL}) exitWith {
+    if (isNil {_source getVariable QGVAR(currentFuelCargo)}) exitWith {};
+
+    _source setVariable [QGVAR(currentFuelCargo), nil, true];
+    _source setVariable [QGVAR(capacity), REFUEL_DISABLED_FUEL, true];
+
+    private _jipID = _source getVariable QGVAR(initSource_jipID);
+
+    if (isNil "_jipID") exitWith {};
+
+    _jipID call CBA_fnc_removeGlobalEventJIP;
+
+    _source setVariable [QGVAR(initSource_jipID), nil];
+};
 
 private _capacity = if (_fuelCargo < 0) then {_fuelCargo} else {_fuelCargoConfig max _fuelCargo};
 