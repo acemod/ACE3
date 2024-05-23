b9037e9af990e55cbb9deb2fbc16292766e23611
PabstMirror <pabstmirror@gmail.com>
2024-05-23 17:19:21 -0500

    Refuel - Cleanup compats

--- addons/compat_cup_terrains/CfgVehicles.hpp
+++ addons/compat_cup_terrains/CfgVehicles.hpp
@@ -5,7 +5,6 @@ class CfgVehicles {
     class House_EP1: House {};
 
     class Land_Benzina_schnell: House {
-        transportFuel = 0;
         EGVAR(refuel,hooks)[] = {{-1.5,-3.93,-1.25}, {2.35,-3.93,-1.25}};
         EGVAR(refuel,fuelCargo) = REFUEL_INFINITE_FUEL;
         class ACE_Actions {
@@ -18,22 +17,18 @@ class CfgVehicles {
         };
     };
     class Land_A_FuelStation_Feed: Strategic {
-        transportFuel = 0;
         EGVAR(refuel,hooks)[] = {{-0.34,0,0}, {0.34,0,0}};
         EGVAR(refuel,fuelCargo) = REFUEL_INFINITE_FUEL;
     };
     class Land_Ind_FuelStation_Feed_EP1: House_EP1 {
-        transportFuel = 0;
         EGVAR(refuel,hooks)[] = {{-0.34,0,0}, {0.34,0,0}};
         EGVAR(refuel,fuelCargo) = REFUEL_INFINITE_FUEL;
     };
     class Land_FuelStation_Feed_PMC: Strategic {
-        transportFuel = 0;
         EGVAR(refuel,hooks)[] = {{-0.34,0,0}, {0.34,0,0}};
         EGVAR(refuel,fuelCargo) = REFUEL_INFINITE_FUEL;
     };
     class FuelStation: House_Small_F {
-        transportFuel = 0;
         EGVAR(refuel,hooks)[] = {{1.25, .2, -1.1}};
         EGVAR(refuel,fuelCargo) = REFUEL_INFINITE_FUEL;
         class ACE_Actions {
