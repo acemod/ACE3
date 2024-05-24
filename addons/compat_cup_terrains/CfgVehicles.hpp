34acb85a5510a317a971cfd82e469db381e994ce
PabstMirror <pabstmirror@gmail.com>
2024-05-24 00:27:01 -0500

    add `XEH_INHERITED`

--- addons/compat_cup_terrains/CfgVehicles.hpp
+++ addons/compat_cup_terrains/CfgVehicles.hpp
@@ -1,3 +1,6 @@
+class CBA_Extended_EventHandlers;
+#define XEH_INHERITED class EventHandlers {class CBA_Extended_EventHandlers: CBA_Extended_EventHandlers {};}
+
 class CfgVehicles {
     class House;
     class House_Small_F;
@@ -5,6 +8,7 @@ class CfgVehicles {
     class House_EP1: House {};
 
     class Land_Benzina_schnell: House {
+        XEH_INHERITED;
         EGVAR(refuel,hooks)[] = {{-1.5,-3.93,-1.25}, {2.35,-3.93,-1.25}};
         EGVAR(refuel,fuelCargo) = REFUEL_INFINITE_FUEL;
         class ACE_Actions {
@@ -17,18 +21,22 @@ class CfgVehicles {
         };
     };
     class Land_A_FuelStation_Feed: Strategic {
+        XEH_INHERITED;
         EGVAR(refuel,hooks)[] = {{-0.34,0,0}, {0.34,0,0}};
         EGVAR(refuel,fuelCargo) = REFUEL_INFINITE_FUEL;
     };
     class Land_Ind_FuelStation_Feed_EP1: House_EP1 {
+        XEH_INHERITED;
         EGVAR(refuel,hooks)[] = {{-0.34,0,0}, {0.34,0,0}};
         EGVAR(refuel,fuelCargo) = REFUEL_INFINITE_FUEL;
     };
     class Land_FuelStation_Feed_PMC: Strategic {
+        XEH_INHERITED;
         EGVAR(refuel,hooks)[] = {{-0.34,0,0}, {0.34,0,0}};
         EGVAR(refuel,fuelCargo) = REFUEL_INFINITE_FUEL;
     };
     class FuelStation: House_Small_F {
+        XEH_INHERITED;
         EGVAR(refuel,hooks)[] = {{1.25, .2, -1.1}};
         EGVAR(refuel,fuelCargo) = REFUEL_INFINITE_FUEL;
         class ACE_Actions {
@@ -39,5 +47,10 @@ class CfgVehicles {
                 condition = "true";
             };
         };
+    };
+    class WarfareBBaseStructure;
+    class Base_WarfareBVehicleServicePoint: WarfareBBaseStructure {
+        // "vehicle service point" (a conex /w barrels) - need hooks???
+        XEH_INHERITED;
     };
 };
