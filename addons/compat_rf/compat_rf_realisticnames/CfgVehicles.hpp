534d0b8464771e21c8e86ebbc3ceb8cec9c62a68
Mike-MF <TyroneMF@hotmail.com>
2024-05-26 18:17:54 +0100

    Reaction Forces Compatibility

---
+++ addons/compat_rf/compat_rf_realisticnames/CfgVehicles.hpp
@@ -0,0 +1,56 @@
+class CfgVehicles {
+    class Heli_light_03_dynamicLoadout_base_F;
+    class B_Heli_light_03_dynamicLoadout_RF: Heli_light_03_dynamicLoadout_base_F {
+        displayName = SUBCSTRING(heli_light_03_Name);
+    };
+
+    class Heli_light_03_unarmed_base_F;
+    class B_Heli_light_03_unarmed_RF: Heli_light_03_unarmed_base_F {
+        displayName = SUBCSTRING(heli_light_03_unarmed_Name);
+    };
+
+    class I_Heli_light_03_dynamicLoadout_RF;
+    class I_E_Heli_light_03_dynamicLoadout_RF: I_Heli_light_03_dynamicLoadout_RF {
+        displayName = SUBCSTRING(heli_light_03_Name);
+    };
+
+    class I_Heli_light_03_unarmed_RF;
+    class I_E_Heli_light_03_unarmed_RF: I_Heli_light_03_unarmed_RF {
+        displayName = SUBCSTRING(heli_light_03_unarmed_Name);
+    };
+
+    class Heli_EC_01A_base_RF;
+    class Heli_EC_01A_military_base_RF: Heli_EC_01A_base_RF {
+        displayName = SUBCSTRING(ec_01a_military_Name);
+    };
+
+    class Helicopter_Base_H;
+    class Heli_EC_01_base_RF: Helicopter_Base_H {
+        displayName = SUBCSTRING(ec_01_base_Name);
+    };
+
+    class Heli_EC_01_civ_base_RF: Heli_EC_01_base_RF {
+        displayName = SUBCSTRING(ec_01_Name);
+    };
+
+    class Heli_EC_01A_civ_base_RF: Heli_EC_01A_base_RF {
+        displayName = SUBCSTRING(ec_01a_Name);
+    };
+
+    class Heli_EC_02_base_RF: Heli_EC_01_base_RF {
+        displayName = SUBCSTRING(ec_02_Name);
+    };
+
+    class Heli_EC_03_base_RF: Heli_EC_01_base_RF {
+        displayName = SUBCSTRING(ec_03_Name);
+    };
+
+    class Heli_EC_04_base_RF: Heli_EC_01_base_RF {
+        displayName = SUBCSTRING(ec_04_Name);
+    };
+
+    class O_Truck_03_fuel_F;
+    class C_Truck_03_water_rf: O_Truck_03_fuel_F {
+        displayName = SUBCSTRING(truck_03_water_Name);
+    };
+};
---
+++ addons/compat_rf/compat_rf_realisticnames/config.cpp
@@ -0,0 +1,22 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class SUBADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "RF_Data_Loadorder",
+            "ace_realisticnames"
+        };
+        skipWhenMissingDependencies = 1;
+        author = ECSTRING(common,ACETeam);
+        authors[] = {"Mike"};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgWeapons.hpp"
+#include "CfgVehicles.hpp"
