1609f4195f231eb0af4454de6a0a3e1153984886
Bailey Danyluk <baileydanyluk@gmail.com>
2024-05-27 21:17:27 -0600

    Add SDB weapon

---
+++ addons/sdb/script_component.hpp
@@ -0,0 +1,18 @@
+#define COMPONENT sdb
+#define COMPONENT_BEAUTIFIED Small Diameter Bomb
+#include "\z\ace\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_SDB
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_SDB
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_SDB
+#endif
+
+#include "\z\ace\addons\main\script_macros.hpp"
+
