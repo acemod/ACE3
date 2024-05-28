11520d03f49fec8a704e3035bd2f6a28460fd8dd
Bailey Danyluk <baileydanyluk@gmail.com>
2024-05-27 21:16:15 -0600

    Add Vikhr missile

---
+++ addons/vikhr/CfgAmmo.hpp
@@ -0,0 +1,46 @@
+class CfgAmmo {
+    class M_Scalpel_AT;
+    class GVAR(9k121): M_Scalpel_AT {
+        author = "Dani (TCVM)";
+        maneuvrability = 0;
+
+        irLock = 0;
+        laserLock = 0;
+        manualControl = 0;
+
+        class ace_missileguidance {
+            enabled = 1;
+            showTrail = 1;
+
+            pitchRate = 60;          // Minium flap deflection for guidance
+            yawRate = 60;            // Maximum flap deflection for guidance
+
+            canVanillaLock = 1;          // Can this default vanilla lock? Only applicable to non-cadet mode
+
+            // Guidance type for munitions
+            defaultSeekerType = "SACLOS";
+            seekerTypes[] = { "SACLOS" };
+
+            defaultSeekerLockMode = "LOBL";
+            seekerLockModes[] = { "LOBL" };
+
+            defaultNavigationType = "Line";
+            navigationTypes[] = { "Line" };
+
+            lineGainP = 8;
+            lineGainD = 6;
+
+            seekLastTargetPos = 0;      // seek last target position [if seeker loses LOS of target, continue to last known pos]
+            seekerAngle = 15;           // Angle from the shooter's view that can track the missile
+            seekerAccuracy = 1;         // seeker accuracy multiplier
+
+            seekerMinRange = 30;
+            seekerMaxRange = 12000;      // Range from the missile which the seeker can visually search
+
+            // Attack profile type selection
+            defaultAttackProfile = "BEAM";
+            attackProfiles[] = {"BEAM"};
+        };
+    };
+};
+
---
+++ addons/vikhr/CfgMagazines.hpp
@@ -0,0 +1,53 @@
+class CfgMagazines {
+    class 2Rnd_LG_scalpel;
+    class 6Rnd_LG_scalpel;
+    class 8Rnd_LG_scalpel;
+    class PylonRack_1Rnd_LG_scalpel;
+    class PylonMissile_1Rnd_LG_scalpel;
+    class PylonRack_3Rnd_LG_scalpel;
+    class PylonRack_4Rnd_LG_scalpel;
+
+    class GVAR(2Rnd): 2Rnd_LG_scalpel {
+        author = "Dani (TCVM)";
+        displayName = CSTRING(2x);
+        ammo = QGVAR(9k121);
+    };
+
+    class GVAR(6Rnd): 6Rnd_LG_scalpel {
+        author = "Dani (TCVM)";
+        displayName = CSTRING(6x);
+        ammo = QGVAR(9k121);
+    };
+
+    class GVAR(8Rnd): 8Rnd_LG_scalpel {
+        author = "Dani (TCVM)";
+        displayName = CSTRING(8x);
+        ammo = QGVAR(9k121);
+    };
+
+    class GVAR(PylonRack_1Rnd): PylonRack_1Rnd_LG_scalpel {
+        author = "Dani (TCVM)";
+        displayName = CSTRING(1x);
+        ammo = QGVAR(9k121);
+        pylonWeapon = QGVAR(9k121);
+    };
+    class GVAR(PylonMissile_1Rnd): PylonMissile_1Rnd_LG_scalpel {
+        author = "Dani (TCVM)";
+        displayName = CSTRING(1x);
+        ammo = QGVAR(9k121);
+        pylonWeapon = QGVAR(9k121);
+    };
+    class GVAR(PylonRack_3Rnd): PylonRack_3Rnd_LG_scalpel {
+        author = "Dani (TCVM)";
+        displayName = CSTRING(3x);
+        ammo = QGVAR(9k121);
+        pylonWeapon = QGVAR(9k121);
+    };
+    class GVAR(PylonRack_4Rnd): PylonRack_4Rnd_LG_scalpel {
+        author = "Dani (TCVM)";
+        displayName = CSTRING(4x);
+        ammo = QGVAR(9k121);
+        pylonWeapon = QGVAR(9k121);
+    };
+};
+
