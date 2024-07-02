73a819053338123d350b331c74174f8e00af05c9
mrschick <58027418+mrschick@users.noreply.github.com>
2024-07-02 14:32:31 +0200

    WIP add Claymore Mine Frag and reduce Structure Damage

--- addons/explosives/CfgAmmo.hpp
+++ addons/explosives/CfgAmmo.hpp
@@ -15,6 +15,10 @@ class CfgAmmo {
         GVAR(defuseObjectPosition)[] = {0, 0, 0.038};
         soundActivation[] = {"", 0, 0, 0};
         soundDeactivation[] = {"", 0, 0, 0};
+
+        // Reduce vanilla damage, so that it doesn't destroy buildings
+        hit = 20;
+        indirectHit = 20;
     };
 
     class APERSTripMine_Wire_Ammo: DirectionalBombBase {
