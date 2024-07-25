6cffdf5420104212b51301001b90b76098ddb32a
johnb432 <58661205+johnb432@users.noreply.github.com>
2024-07-25 23:50:58 +0200

    Add PIP config

--- addons/optics/config.cpp
+++ addons/optics/config.cpp
@@ -31,3 +31,11 @@ class CfgPatches {
 #include "CfgJointRails.hpp"
 #include "CfgWeapons.hpp"
 #include "CfgVehicles.hpp"
+
+class CBA_PIPItems {
+    ACE_optic_Hamr_2D = "ACE_optic_Hamr_PIP";
+    ACE_optic_Arco_2D = "ACE_optic_Arco_PIP";
+    ACE_optic_MRCO_2D = "ACE_optic_MRCO_PIP";
+    ACE_optic_SOS_2D  = "ACE_optic_SOS_PIP";
+    ACE_optic_LRPS_2D = "ACE_optic_LRPS_PIP";
+};
