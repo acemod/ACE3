aa355ccc73cfce12607683cb7e95f4f150859600
johnb432 <58661205+johnb432@users.noreply.github.com>
2024-07-27 18:51:37 +0200

    Readd AI stuff
    visionMode is already set

--- addons/optics/CfgWeapons.hpp
+++ addons/optics/CfgWeapons.hpp
@@ -237,5 +237,13 @@ class CfgWeapons {
         opticsZoomMin = 0.056889;     // 5.25x power
         opticsZoomMax = 0.056889;     // 9 px/mil
         modelOptics = "\z\ace\addons\optics\models\NWD_M22_5x"; // 7 degrees horizontal field of view
+        // Fix AI using Binocs on short range - #18737
+        // minRange = 300; // 300 = uses Rangefinder often (runs a few meters, stops, uses RF, repeats)
+        minRange = 500; // 500 = seem almost never use it..?
+        minRangeProbab = 0.001;
+        midRange = 1000;
+        midRangeProbab = 0.01;
+        maxRange = 5000;
+        maxRangeProbab = 0.01;
     };
 };
