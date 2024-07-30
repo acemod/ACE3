5059cb283d11d3fb6f1f9dad5f735c90872281b3
lambdatiger <spam.stnuocca.sl@gmail.com>
2024-07-29 23:17:05 -0500

    accidentally committed script_component.hpp w/ debug enabled

--- addons/frag/script_component.hpp
+++ addons/frag/script_component.hpp
@@ -3,8 +3,8 @@
 #include "\z\ace\addons\main\script_mod.hpp"
 
 // #define LOG_FRAG_INFO
-#define DEBUG_MODE_FULL
-#define DEBUG_MODE_DRAW
+// #define DEBUG_MODE_FULL
+// #define DEBUG_MODE_DRAW
 // #define DISABLE_COMPILE_CACHE
 // #define ENABLE_PERFORMANCE_COUNTERS
 
