beaf4225343f91bc4612824dd4140ea24aa4f369
johnb432 <58661205+johnb432@users.noreply.github.com>
2024-08-08 23:12:41 +0200

    Preemptively overwrite upcoming changes to Aegis

--- addons/compat_aegis/CfgVehicles.hpp
+++ addons/compat_aegis/CfgVehicles.hpp
@@ -12,8 +12,43 @@ class CfgVehicles {
                 // Overwrite the changes Aegis makes for the .338 coax MG on the Slammer/Merkava
                 // The idea is:
                 // 1) keep it as realistic as possible
-                // 2) the TUSK variant keeps the 7.62mm variant, so the base variant should too
-                // 3) easier to overwrite something with skipWhenMissingDependencies than to not overwrite something if another mod is loaded
+                // 2) easier to overwrite something with skipWhenMissingDependencies than to not overwrite something if another mod is loaded
+                weapons[] = {"cannon_120mm", "ACE_LMG_coax_MAG58_mem3"}; // Base 1.82: "cannon_120mm","LMG_coax"
+                magazines[] = {
+                    "24Rnd_120mm_APFSDS_shells_Tracer_Red",
+                    "12Rnd_120mm_HE_shells_Tracer_Red",
+                    "12Rnd_120mm_HEAT_MP_T_Red",
+                    "200Rnd_762x51_Belt_Red",
+                    "200Rnd_762x51_Belt_Red",
+                    "200Rnd_762x51_Belt_Red",
+                    "200Rnd_762x51_Belt_Red",
+                    "200Rnd_762x51_Belt_Red",
+                    "200Rnd_762x51_Belt_Red",
+                    "200Rnd_762x51_Belt_Red",
+                    "200Rnd_762x51_Belt_Red",
+                    "200Rnd_762x51_Belt_Red",
+                    "200Rnd_762x51_Belt_Red",
+                    "200Rnd_762x51_Belt_Red",
+                    "200Rnd_762x51_Belt_Red",
+                    "200Rnd_762x51_Belt_Red",
+                    "200Rnd_762x51_Belt_Red",
+                    "200Rnd_762x51_Belt_Red",
+                    "200Rnd_762x51_Belt_Red",
+                    "200Rnd_762x51_Belt_Red",
+                    "200Rnd_762x51_Belt_Red",
+                    "200Rnd_762x51_Belt_Red",
+                    "200Rnd_762x51_Belt_Red",
+                    "4Rnd_120mm_LG_cannon_missiles" // Aegis adds laser-guided munitions
+                };
+            };
+        };
+    };
+
+    class B_MBT_01_base_F: MBT_01_base_F {};
+    class B_MBT_01_cannon_F: B_MBT_01_base_F {};
+    class B_MBT_01_TUSK_F: B_MBT_01_cannon_F {
+        class Turrets: Turrets {
+            class MainTurret: MainTurret {
                 weapons[] = {"cannon_120mm", "ACE_LMG_coax_MAG58_mem3"}; // Base 1.82: "cannon_120mm","LMG_coax"
                 magazines[] = {
                     "24Rnd_120mm_APFSDS_shells_Tracer_Red",
