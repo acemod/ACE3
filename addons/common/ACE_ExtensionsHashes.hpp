247495ed2c8f6878dba5d05953bce952e7b4a7fe
PabstMirror <pabstmirror@gmail.com>
2024-08-17 13:22:01 -0500

    auto gen hashes on make

---
+++ addons/common/ACE_ExtensionsHashes.hpp
@@ -0,0 +1,6 @@
+class ACE_ExtensionsHashes {
+    class ace {
+        dll = "b2e646f865f48811192bde4877a210a85f05b473";
+        dll_x64 = "6b32d7f9a3aaa027af2bc6cb0b7c61371bdf6706";
+    };
+};
--- tools/getExtensionHash.py
+++ tools/getExtensionHash.py
@@ -7,15 +7,15 @@ addon_base_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 extensions = {}
 
 for file in os.listdir(addon_base_path):
-    path = pathlib.Path(file)
+    path = pathlib.Path(addon_base_path, file)
     extension_type = "dll"
     if path.suffix == ".dll":
         key = path.stem
         if key.endswith("_x64"):
             key = key.removesuffix("_x64")
             extension_type += "_x64"
-
-        with open(file, 'rb') as file_read:
+        print(f"looking at {path}")
+        with open(path, 'rb') as file_read:
             sha1 = hashlib.sha1()
             data = file_read.read()
             sha1.update(data)
@@ -23,10 +23,14 @@ for file in os.listdir(addon_base_path):
             arr[extension_type] = sha1.hexdigest()
             extensions[key] = arr
 
-print(f"class ACE_ExtensionsHashes {{")
-for key, values in extensions.items():
-    print(f"    class {key} {{")
-    for type, hash in values.items():
-        print(f"        {type} = \"{hash}\";")
-    print(f"    }};")
-print(f"}};")
+file_out = pathlib.Path(addon_base_path, "addons", "common", "ACE_ExtensionsHashes.hpp")
+with open(file_out, 'w') as file_write:
+    print(f"class ACE_ExtensionsHashes {{", file=file_write)
+    for key, values in extensions.items():
+        print(f"    class {key} {{", file=file_write)
+        for type, hash in values.items():
+            print(f"        {type} = \"{hash}\";", file=file_write)
+        print(f"    }};", file=file_write)
+    print(f"}};", file=file_write)
+
+print(f"Wrote {len(extensions)} to {file_out}")
