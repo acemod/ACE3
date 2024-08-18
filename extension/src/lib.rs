dc7b46bedbf42016f952528ae96f0c212188aff8
PabstMirror <pabstmirror@gmail.com>
2024-08-17 22:53:04 -0500

    Revert "try always building clipboard"
    This reverts commit f54dc50cc1e1a504acee8447ddf1d3d27e140648.

--- extension/src/lib.rs
+++ extension/src/lib.rs
@@ -3,14 +3,16 @@
 
 //! ACE3 Extension for quick maths and OS APIs
 
-use arma_rs::{arma, Extension};
+use arma_rs::{arma, Extension, Group};
 
 mod artillery;
 pub mod ballistics;
 mod break_line;
-mod clipboard;
 mod common;
 mod fcs;
+
+#[cfg(feature = "clipboard")]
+mod clipboard;
 
 #[arma]
 fn init() -> Extension {
@@ -18,7 +20,14 @@ fn init() -> Extension {
         .version(env!("GIT_HASH").to_string())
         .group("artillery", artillery::group())
         .group("ballistics", ballistics::group())
-        .group("clipboard", clipboard::group())
+        .group(
+            "clipboard",
+            if cfg!(feature = "clipboard") {
+                clipboard::group()
+            } else {
+                Group::new()
+            },
+        )
         .command("break_line", break_line::break_line)
         .command("fcs", fcs::get_solution)
         .command("version", || env!("GIT_HASH"))
