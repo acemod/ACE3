dcf49d77ebf39d4182e28d758c27efedfffc2894
johnb432 <58661205+johnb432@users.noreply.github.com>
2024-08-23 03:00:15 +0200

    CUP Compat - Add interaction anims to CUP vehicles (#10238)
    Add interaction anims to CUP

--- addons/compat_cup_vehicles/compat_cup_vehicles_refuel/CfgVehicles.hpp
+++ addons/compat_cup_vehicles/compat_cup_vehicles_refuel/CfgVehicles.hpp
@@ -28,4 +28,90 @@ class CfgVehicles {
         EGVAR(refuel,hooks)[] = {{-1.09, -0.01, -0.5},{1, -0.01, -0.5}};
         EGVAR(refuel,fuelCargo) = 10000;
     };
+
+    class Car_F;
+    class CUP_LR_Base: Car_F {
+        class EGVAR(interaction,anims) {
+            class selection_jerry {
+                positions[] = {{-0.65, 2.7, -0.55}, {0.65, 2.7, -0.55}, {-1, -0.25, -0.75}, {1, -0.25, -0.75}};
+                items[] = {"Land_CanisterFuel_F", "Land_CanisterFuel_F", "Land_CanisterFuel_F", "Land_CanisterFuel_F", "Land_CanisterFuel_F", "Land_CanisterFuel_F"};
+                name = ECSTRING(refuel,TakeFuelCanister);
+                text = ECSTRING(refuel,TakeFuelCanisterAction);
+            };
+        };
+    };
+    class CUP_LR_MG_Base: CUP_LR_Base {
+        class EGVAR(interaction,anims): EGVAR(interaction,anims) {
+            class selection_jerry: selection_jerry {
+                positions[] = {{-0.55, 2.45, 1.05}, {0.55, 2.45, 1.05}, {-1, -0.53, 0.9}, {1, -0.53, 0.9}};
+            };
+        };
+    };
+    class CUP_LR_SPG9_Base: CUP_LR_Base {
+        class EGVAR(interaction,anims): EGVAR(interaction,anims) {
+            class selection_jerry: selection_jerry {};
+        };
+    };
+    class CUP_LR_AA_Base: CUP_LR_SPG9_Base {
+        class EGVAR(interaction,anims): EGVAR(interaction,anims) {
+            class selection_jerry: selection_jerry {
+                positions[] = {{-0.65, 2.8, -1.1}, {0.65, 2.8, -1.1}, {-1, -0.15, -1.3}, {1, -0.15, -1.3}};
+            };
+        };
+    };
+    class CUP_LR_Ambulance_Base: CUP_LR_Base {
+        class EGVAR(interaction,anims): EGVAR(interaction,anims) {
+            class selection_jerry: selection_jerry {
+                positions[] = {{-0.65, 3.1, -0.55}, {0.65, 3.1, -0.55}};
+                items[] = {"Land_CanisterFuel_F", "Land_CanisterFuel_F", "Land_CanisterFuel_F", "Land_CanisterFuel_F"};
+            };
+        };
+    };
+    class CUP_LR_Special_Base: CUP_LR_Base {
+        class EGVAR(interaction,anims): EGVAR(interaction,anims) {
+            class selection_jerry: selection_jerry {
+                positions[] = {{-0.7, 2.45, 1.05}, {0.55, 2.45, 1.05}};
+                items[] = {"Land_CanisterFuel_F", "Land_CanisterFuel_F", "Land_CanisterFuel_F", "Land_CanisterFuel_F"};
+            };
+        };
+    };
+
+    class CUP_nHMMWV_Base: Car_F {
+        class EGVAR(interaction,anims) {
+            class hide_jerrycans {
+                positions[] = {"(_target selectionPosition 'vhc_jerrycans') vectorAdd [0, 0, 0.15]"};
+                items[] = {"Land_CanisterFuel_F", "Land_CanisterFuel_F"};
+                name = ECSTRING(refuel,TakeFuelCanister);
+                text = ECSTRING(refuel,TakeFuelCanisterAction);
+            };
+        };
+    };
+    // Don't inherit, as it's easier for the main compat
+    class CUP_nM1025_SOV_Base: CUP_nHMMWV_Base {
+        class EGVAR(interaction,anims) {
+            class hide_jerrycans {
+                positions[] = {"(_target selectionPosition 'vhc_jerrycans') vectorAdd [0, 0, 0.15]"};
+                items[] = {"Land_CanisterFuel_F", "Land_CanisterFuel_F"};
+                name = ECSTRING(refuel,TakeFuelCanister);
+                text = ECSTRING(refuel,TakeFuelCanisterAction);
+            };
+            class hide_rear_rack_content {
+                positions[] = {"_target selectionPosition ['vhc_rear_rack_content', 'FireGeometry', 'AveragePoint']"};
+                items[] = {"Land_CanisterFuel_F", "Land_CanisterFuel_F", "Land_CanisterFuel_F", "Land_CanisterFuel_F", "Land_CanisterFuel_F", "Land_CanisterFuel_F"};
+                name = ECSTRING(refuel,TakeFuelCanister);
+                text = ECSTRING(refuel,TakeFuelCanisterAction);
+            };
+        };
+    };
+
+    class CUP_ECVHMMWV_Base: Car_F {
+        class EGVAR(interaction,anims) {
+            class hide_jerrycans {
+                positions[] = {"(_target selectionPosition ['vhc_rear_trunk_door', 'FireGeometry', 'AveragePoint']) vectorAdd [-0.85, -0.22, -0.1]"};
+                items[] = {"Land_CanisterFuel_F", "Land_CanisterFuel_F"};
+                name = ECSTRING(refuel,TakeFuelCanister);
+                text = ECSTRING(refuel,TakeFuelCanisterAction);
+            };
+        };
+    };
 };
