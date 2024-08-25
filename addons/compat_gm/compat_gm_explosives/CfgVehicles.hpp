2b03c44a3f6d6b72a86699d0f22719e67a560827
lambdatiger <spam.stnuocca.sl@gmail.com>
2024-08-24 20:36:18 -0500

    Added GM's localization strings

--- addons/compat_gm/compat_gm_explosives/CfgVehicles.hpp
+++ addons/compat_gm/compat_gm_explosives/CfgVehicles.hpp
@@ -9,8 +9,8 @@ class CfgVehicles {
 
     // CHARGE
     class EGVAR(explosives,Place_gm_explosive_petn): EGVAR(explosives,Place) {
-        displayName = "PETN Charge";
-        model = "gm\gm_weapons\gm_put\gm_explosive_charge_petn";
+        displayName = "$STR_DN_GM_EXPLOSIVE_PETN_CHARGE";
+        model = "\gm\gm_weapons\gm_put\gm_explosive_charge_petn";
         class ACE_Actions: ACE_Actions {
             class ACE_MainActions: ACE_MainActions {
                 position = "[-0.002, 0.022, 0.02]";
@@ -18,8 +18,8 @@ class CfgVehicles {
         };
     };
     class EGVAR(explosives,Place_gm_explosive_plnp): EGVAR(explosives,Place) {
-        displayName = "PLNP Charge";
-        model = "gm\gm_weapons\gm_put\gm_explosive_charge_plnp";
+        displayName = "$STR_DN_GM_EXPLOSIVE_PLNP_CHARGE";
+        model = "\gm\gm_weapons\gm_put\gm_explosive_charge_plnp";
         class ACE_Actions: ACE_Actions {
             class ACE_MainActions: ACE_MainActions {
                 position = "[-0.002, 0.022, 0.02]";
@@ -28,8 +28,8 @@ class CfgVehicles {
     };
     // AT MINE
     class EGVAR(explosives,Place_gm_mine_tm46): EGVAR(explosives,Place) {
-        displayName = "AT Mine TM46";
-        model = "gm\gm_weapons\gm_put\gm_mine_at_tm46";
+        displayName = "$STR_DN_GM_MINE_AT_TM46";
+        model = "\gm\gm_weapons\gm_put\gm_mine_at_tm46";
         class ACE_Actions: ACE_Actions {
             class ACE_MainActions: ACE_MainActions {
                 position = "[-0.002, 0.022, 0.02]";
@@ -37,8 +37,8 @@ class CfgVehicles {
         };
     };
     class EGVAR(explosives,Place_gm_explosive_dm21): EGVAR(explosives,Place) {
-        displayName = "AT Mine DM21";
-        model = "gm\gm_weapons\gm_put\gm_mine_at_dm21";
+        displayName = "$STR_DN_GM_MINE_AT_DM21";
+        model = "\gm\gm_weapons\gm_put\gm_mine_at_dm21";
         class ACE_Actions: ACE_Actions {
             class ACE_MainActions: ACE_MainActions {
                 position = "[-0.002, 0.022, 0.02]";
@@ -46,8 +46,8 @@ class CfgVehicles {
         };
     };
     class EGVAR(explosives,Place_gm_explosive_m111): EGVAR(explosives,Place) {
-        displayName = "MN 111";
-        model = "gm\gm_weapons\gm_launchers\gm_platan\gm_mine_at_mn111_disarmed";
+        displayName = "$STR_DN_GM_MINE_AT_MN111";
+        model = "\gm\gm_weapons\gm_launchers\gm_platan\gm_mine_at_mn111_disarmed";
         class ACE_Actions: ACE_Actions {
             class ACE_MainActions: ACE_MainActions {
                 position = "[-0.002, 0.022, 0.02]";
@@ -56,7 +56,7 @@ class CfgVehicles {
     };
     // AP
     class EGVAR(explosives,Place_gm_explosive_dm31): EGVAR(explosives,Place) {
-        displayName = "AP Mine DM31";
+        displayName = "$STR_DN_GM_MINE_AP_DM31";
         model = "gm\gm_weapons\gm_put\gm_mine_ap_dm31";
         class ACE_Actions: ACE_Actions {
             class ACE_MainActions: ACE_MainActions {
