f58d514be4fbeddafebacbab175700c517ec43bb
mrschick <58027418+mrschick@users.noreply.github.com>
2024-08-28 15:07:41 +0200

    Isolate Compat RHS_AFRF Overpressure

---
+++ addons/compat_rhs_afrf3/compat_rhs_afrf3_overpressure/CfgWeapons.hpp
@@ -0,0 +1,41 @@
+class CfgWeapons {
+    // Man-Portable launchers, rear-facing overpressure
+
+    class Launcher_Base_F;
+    class rhs_weap_rpg7: Launcher_Base_F {
+        EGVAR(overpressure,angle) = 40;
+        EGVAR(overpressure,offset) = 0.9;
+    };
+
+    class rhs_weap_rpg26: Launcher_Base_F {
+        EGVAR(overpressure,range) = 10;
+        EGVAR(overpressure,angle) = 50;
+        EGVAR(overpressure,offset) = 0.65;
+    };
+
+    class rhs_weap_rpg18: rhs_weap_rpg26 {
+        EGVAR(overpressure,angle) = 45;
+        EGVAR(overpressure,offset) = 1;
+    };
+
+    class rhs_weap_strela;
+    class rhs_weap_igla: rhs_weap_strela {
+        EGVAR(overpressure,range) = 6;
+        EGVAR(overpressure,angle) = 40;
+        EGVAR(overpressure,damage) = 0.6;
+        EGVAR(overpressure,offset) = 1.65;
+    };
+
+    // Vehicle cannons, forward-facing overpressure
+
+    class rhs_weap_d81;
+    class rhs_weap_2a70: rhs_weap_d81 { // "Low pressure" 100mm cannon
+        EGVAR(overpressure,range) = 15;
+        EGVAR(overpressure,damage) = 0.5;
+    };
+    class cannon_120mm;
+    class rhs_weap_2a28_base: cannon_120mm { // "Low pressure"
+        EGVAR(overpressure,range) = 15;
+        EGVAR(overpressure,damage) = 0.5;
+    };
+};
