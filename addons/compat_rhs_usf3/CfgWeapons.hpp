318c5c14d87359e50bcca7bbd71316f8d0c8d023
mrschick <58027418+mrschick@users.noreply.github.com>
2024-08-28 15:14:51 +0200

    Isolate Compat RHS_USAF Overpressure

--- addons/compat_rhs_usf3/CfgWeapons.hpp
+++ addons/compat_rhs_usf3/CfgWeapons.hpp
@@ -175,35 +175,14 @@ class CfgWeapons {
     };
 
     // RHS launchers
-    class launch_O_Titan_F;
-
-    class rhs_weap_fim92: launch_O_Titan_F {
-        EGVAR(overpressure,range) = 6;
-        EGVAR(overpressure,angle) = 40;
-        EGVAR(overpressure,damage) = 0.6;
-        EGVAR(overpressure,offset) = 1.45;
-    };
-
     class Launcher_Base_F;
 
     class rhs_weap_smaw: Launcher_Base_F {
         EGVAR(reloadlaunchers,enabled) = 1;
-        EGVAR(overpressure,angle) = 45;
-        EGVAR(overpressure,offset) = 1.3;
     };
 
     class rhs_weap_maaws: Launcher_Base_F {
         EGVAR(reloadlaunchers,enabled) = 1;
-        EGVAR(overpressure,range) = 15;
-        EGVAR(overpressure,angle) = 70;
-        EGVAR(overpressure,damage) = 0.75;
-        EGVAR(overpressure,offset) = 0.95;
-    };
-
-    class rhs_weap_M136: Launcher_Base_F {
-        EGVAR(overpressure,range) = 10;
-        EGVAR(overpressure,angle) = 50;
-        EGVAR(overpressure,offset) = 0.9;
     };
 
     class rhsusf_hgu56p;
