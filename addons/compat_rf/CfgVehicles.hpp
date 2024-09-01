55edf66ec9eb5206cccc3de81ae820dd36fe7f54
Mr. Zorn <dennisAzorn@gmail.com>
2024-09-01 20:57:41 +0200

    macro'fied

--- addons/compat_rf/CfgVehicles.hpp
+++ addons/compat_rf/CfgVehicles.hpp
@@ -3,28 +3,28 @@ class CfgVehicles {
     class Offroad_01_unarmed_base_F;
     class Pickup_01_base_rf : Offroad_01_unarmed_base_F
     {
-        acex_field_rations_offset[] = {-0.04, -2.45, -0.9};
+        EXGVAR(field_rations,offset)[] = {-0.04, -2.45, -0.9};
     };
     
     // Enable Water Source by Default
     class C_IDAP_Pickup_rf;
     class C_IDAP_Pickup_water_rf: C_IDAP_Pickup_rf
     {
-        acex_field_rations_waterSupply = 500;
+        EXGVAR(field_rations,waterSupply) = 500;
     };
     
     class O_Truck_03_fuel_F;
     class C_Truck_03_water_rf: O_Truck_03_fuel_F
     {
-        acex_field_rations_waterSupply = 10000;
-        acex_field_rations_offset[] = {0, -5.05, -0.3}; // Rear
-        //acex_field_rations_offset[] = {1.25,-1.59,-0.58}; // Right Side Latch
+        EXGVAR(field_rations,waterSupply) = 10000;
+        EXGVAR(field_rations,offset)[] = {0, -5.05, -0.3}; // Rear
+        //EXGVAR(field_rations,offset)[] = {1.25,-1.59,-0.58}; // Right Side Latch
     };
 
     class B_Truck_01_fuel_F;
     class C_Truck_01_water_rf : B_Truck_01_fuel_F 
     {
-        acex_field_rations_waterSupply = 10000;
-        acex_field_rations_offset[] = {-0.41,-5.15,-0.3};
+        EXGVAR(field_rations,waterSupply) = 10000;
+        EXGVAR(field_rations,offset)[] = {-0.41,-5.15,-0.3};
     };
 };
