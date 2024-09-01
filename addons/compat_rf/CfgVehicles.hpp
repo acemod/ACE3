7c4aa011e03466d2346543242a81c09786e8e9ec
Mr. Zorn <dennisAzorn@gmail.com>
2024-09-01 20:19:33 +0200

    remove empty line2

--- addons/compat_rf/CfgVehicles.hpp
+++ addons/compat_rf/CfgVehicles.hpp
@@ -1,5 +1,4 @@
 class CfgVehicles {
-
     // applies the offset to all RF Offroads which can have the optional Tank in the back
     class Offroad_01_unarmed_base_F;
     class Pickup_01_base_rf : Offroad_01_unarmed_base_F
