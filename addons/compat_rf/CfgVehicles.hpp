e55ef6c51e04f128d4f50d6c399d494342a9f90b
OverlordZorn <56258612+OverlordZorn@users.noreply.github.com>
2024-09-08 22:45:47 +0200

    Update addons/compat_rf/CfgVehicles.hpp
    Co-authored-by: johnb432 <58661205+johnb432@users.noreply.github.com>

--- addons/compat_rf/CfgVehicles.hpp
+++ addons/compat_rf/CfgVehicles.hpp
@@ -18,7 +18,7 @@ class CfgVehicles {
     };
 
     class B_Truck_01_fuel_F;
-    class C_Truck_01_water_rf : B_Truck_01_fuel_F {
+    class C_Truck_01_water_rf: B_Truck_01_fuel_F {
         EXGVAR(field_rations,waterSupply) = 10000;
         EXGVAR(field_rations,offset)[] = {-0.41, -5.15, -0.3};
     };
