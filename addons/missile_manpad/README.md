0a686e67e28a59adf9798f220a5bc067795f6d75
Bailey Danyluk <baileydanyluk@gmail.com>
2024-09-10 01:20:43 -0600

    update manpad

--- addons/missile_manpad/README.md
+++ addons/missile_manpad/README.md
@@ -1,4 +1,4 @@
-ace_manpad
+ace_missile_manpad
 ===================
 
 Adds missile guidance to all vanilla MANPAD systems 
--- addons/missile_manpad/script_component.hpp
+++ addons/missile_manpad/script_component.hpp
@@ -1,4 +1,4 @@
-#define COMPONENT manpad
+#define COMPONENT missile_manpad
 #define COMPONENT_BEAUTIFIED MANPAD
 #include "\z\ace\addons\main\script_mod.hpp"
 
@@ -6,12 +6,12 @@
 // #define DISABLE_COMPILE_CACHE
 // #define ENABLE_PERFORMANCE_COUNTERS
 
-#ifdef DEBUG_ENABLED_MANPAD
+#ifdef DEBUG_ENABLED_MISSILE_MANPAD
     #define DEBUG_MODE_FULL
 #endif
 
-#ifdef DEBUG_SETTINGS_MANPAD
-    #define DEBUG_SETTINGS DEBUG_SETTINGS_MANPAD
+#ifdef DEBUG_SETTINGS_MISSILE_MANPAD
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_MISSILE_MANPAD
 #endif
 
 #include "\z\ace\addons\main\script_macros.hpp"
