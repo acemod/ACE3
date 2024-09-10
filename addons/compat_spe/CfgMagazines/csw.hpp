1a5bf86439a40f29d5b398d6ecfc5404aa53532b
johnb432 <58661205+johnb432@users.noreply.github.com>
2024-09-10 09:35:40 +0200

    Make all German belt linkable

--- addons/compat_spe/CfgMagazines/csw.hpp
+++ addons/compat_spe/CfgMagazines/csw.hpp
@@ -13,18 +13,5 @@ class SPE_100Rnd_127x99_M2: VehicleMagazine {
 
 // MG34/42
 class SPE_50Rnd_792x57: CA_Magazine {
-    ACE_isBelt = 0;
-};
-class SPE_100Rnd_792x57: SPE_50Rnd_792x57 {
-    ACE_isBelt = 1;
-};
-
-class SPE_50Rnd_792x57_sS;
-class SPE_100Rnd_792x57_sS: SPE_50Rnd_792x57_sS {
-    ACE_isBelt = 1;
-};
-
-class SPE_50Rnd_792x57_SMK;
-class SPE_100Rnd_792x57_SMK: SPE_50Rnd_792x57_SMK {
     ACE_isBelt = 1;
 };
