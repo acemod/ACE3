d51b1399759c7edf05079b48de3ffe0478783d42
Bailey Danyluk <baileydanyluk@gmail.com>
2024-09-10 01:16:19 -0600

    Update macros

--- addons/missile_aim120/script_component.hpp
+++ addons/missile_aim120/script_component.hpp
@@ -1,4 +1,4 @@
-#define COMPONENT aim120
+#define COMPONENT missile_aim120
 #define COMPONENT_BEAUTIFIED AIM-120
 #include "\z\ace\addons\main\script_mod.hpp"
 
@@ -6,12 +6,12 @@
 // #define DISABLE_COMPILE_CACHE
 // #define ENABLE_PERFORMANCE_COUNTERS
 
-#ifdef DEBUG_ENABLED_AIM120
+#ifdef DEBUG_ENABLED_MISSILE_AIM120
     #define DEBUG_MODE_FULL
 #endif
 
-#ifdef DEBUG_SETTINGS_AIM120
-    #define DEBUG_SETTINGS DEBUG_SETTINGS_AIM120
+#ifdef DEBUG_SETTINGS_MISSILE_AIM120
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_MISSILE_AIM120
 #endif
 
 #include "\z\ace\addons\main\script_macros.hpp"
