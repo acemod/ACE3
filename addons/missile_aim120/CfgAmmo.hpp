9d1f5356f55a5f1c75c91d1f845dc9ab79da237a
Bailey Danyluk <baileydanyluk@gmail.com>
2024-09-09 23:18:31 -0600

    use standard missile defines

--- addons/missile_aim120/CfgAmmo.hpp
+++ addons/missile_aim120/CfgAmmo.hpp
@@ -1,3 +1,5 @@
+class EGVAR(missileguidance,type_AMRAAM);
+class EGVAR(missileguidance,type_R77);
 class CfgAmmo {
     class ammo_Missile_AMRAAM_C;
     class M_Zephyr;
@@ -13,40 +15,8 @@ class CfgAmmo {
         flightProfiles[] = {"Direct", "Loft"};
         class Direct {}; // dummy to allow for F cycling of missile mode
         class Loft {};
-        class ace_missileguidance {
+        class ace_missileguidance: EGVAR(missileguidance,type_AMRAAM) {
             enabled = 1;
-
-            pitchRate = 30;          // Minium flap deflection for guidance
-            yawRate = 30;            // Maximum flap deflection for guidance
-
-            canVanillaLock = 1;          // Can this default vanilla lock? Only applicable to non-cadet mode
-
-            // Guidance type for munitions
-            defaultSeekerType = "DopplerRadar";
-            seekerTypes[] = { "DopplerRadar" };
-            lockableTypes[] = {"Air"};
-
-            minimumSpeedFilter = 25;    // filter out targets that have a closing velocity less than this
-            minimumTimeFilter = 0.0001; // filter out targets that are this close to the ground (speed of light)
-            maxTerrainCheck = 16000;    // How far we should check for terrain
-
-            defaultSeekerLockMode = "LOAL";
-            seekerLockModes[] = { "LOAL", "LOBL" };
-
-            defaultNavigationType = "ZeroEffortMiss";
-            navigationTypes[] = { "ZeroEffortMiss" };
-
-            seekLastTargetPos = 0;      // seek last target position [if seeker loses LOS of target, continue to last known pos]
-            seekerAngle = 50;           // Angle from the shooter's view that can track the missile
-            seekerAccuracy = 1;         // seeker accuracy multiplier
-
-            seekerMinRange = 75;
-            seekerMaxRange = 2500;      // Range from the missile which the seeker can visually search
-
-            // Attack profile type selection
-            defaultAttackProfile = "DIR";
-            attackProfiles[] = {"DIR", "LOFT"};
-            useModeForAttackProfile = 1;
         };
     };
 
@@ -61,40 +31,8 @@ class CfgAmmo {
         flightProfiles[] = {"Direct", "Loft"};
         class Direct {}; // dummy to allow for F cycling of missile mode
         class Loft {};
-        class ace_missileguidance {
+        class ace_missileguidance: EGVAR(missileguidance,type_AMRAAM) {
             enabled = 1;
-
-            pitchRate = 30;          // Minium flap deflection for guidance
-            yawRate = 30;            // Maximum flap deflection for guidance
-
-            canVanillaLock = 1;          // Can this default vanilla lock? Only applicable to non-cadet mode
-
-            // Guidance type for munitions
-            defaultSeekerType = "DopplerRadar";
-            seekerTypes[] = { "DopplerRadar" };
-            lockableTypes[] = {"Air"};
-
-            minimumSpeedFilter = 25;    // filter out targets that have a closing velocity less than this
-            minimumTimeFilter = 0.0001; // filter out targets that are this close to the ground (speed of light)
-            maxTerrainCheck = 16000;    // How far we should check for terrain
-
-            defaultSeekerLockMode = "LOAL";
-            seekerLockModes[] = { "LOAL", "LOBL" };
-
-            defaultNavigationType = "ZeroEffortMiss";
-            navigationTypes[] = { "ZeroEffortMiss" };
-
-            seekLastTargetPos = 0;      // seek last target position [if seeker loses LOS of target, continue to last known pos]
-            seekerAngle = 50;           // Angle from the shooter's view that can track the missile
-            seekerAccuracy = 1;         // seeker accuracy multiplier
-
-            seekerMinRange = 75;
-            seekerMaxRange = 2500;      // Range from the missile which the seeker can visually search
-
-            // Attack profile type selection
-            defaultAttackProfile = "DIR";
-            attackProfiles[] = {"DIR", "LOFT"};
-            useModeForAttackProfile = 1;
         };
     };
     
@@ -110,40 +48,13 @@ class CfgAmmo {
         flightProfiles[] = {"Direct", "TopDown"};
         class Direct {}; // dummy to allow for F cycling of missile mode
         class TopDown {};
-        class ace_missileguidance {
+        class ace_missileguidance: EGVAR(missileguidance,type_AMRAAM) {
             enabled = 1;
-
-            pitchRate = 45;          // Minium flap deflection for guidance
-            yawRate = 45;            // Maximum flap deflection for guidance
-
-            canVanillaLock = 1;          // Can this default vanilla lock? Only applicable to non-cadet mode
-
-            // Guidance type for munitions
-            defaultSeekerType = "DopplerRadar";
-            seekerTypes[] = { "DopplerRadar" };
-            lockableTypes[] = {"Air"};
-
+            pitchRate = 45;
+            yawRate = 45;
             minimumSpeedFilter = 15;    // filter out targets that have a closing velocity less than this
             minimumTimeFilter = 0.00005; // filter out targets that are this close to the ground (speed of light)
             maxTerrainCheck = 16000;    // How far we should check for terrain
-
-            defaultSeekerLockMode = "LOAL";
-            seekerLockModes[] = { "LOAL", "LOBL" };
-
-            defaultNavigationType = "ZeroEffortMiss";
-            navigationTypes[] = { "ZeroEffortMiss" };
-
-            seekLastTargetPos = 0;      // seek last target position [if seeker loses LOS of target, continue to last known pos]
-            seekerAngle = 50;           // Angle from the shooter's view that can track the missile
-            seekerAccuracy = 1;         // seeker accuracy multiplier
-
-            seekerMinRange = 75;
-            seekerMaxRange = 2500;      // Range from the missile which the seeker can visually search
-
-            // Attack profile type selection
-            defaultAttackProfile = "DIR";
-            attackProfiles[] = {"DIR", "LOFT"};
-            useModeForAttackProfile = 1;
         };
     };
 
@@ -160,40 +71,8 @@ class CfgAmmo {
         flightProfiles[] = {"Direct", "TopDown"};
         class Direct {}; // dummy to allow for F cycling of missile mode
         class TopDown {};
-        class ace_missileguidance {
+        class ace_missileguidance: EGVAR(missileguidance,type_R77) {
             enabled = 1;
-
-            pitchRate = 40;          // Minium flap deflection for guidance
-            yawRate = 40;            // Maximum flap deflection for guidance
-
-            canVanillaLock = 1;          // Can this default vanilla lock? Only applicable to non-cadet mode
-
-            // Guidance type for munitions
-            defaultSeekerType = "DopplerRadar";
-            seekerTypes[] = { "DopplerRadar" };
-            lockableTypes[] = {"Air"};
-
-            minimumSpeedFilter = 15;    // filter out targets that have a closing velocity less than this
-            minimumTimeFilter = 0.00005; // filter out targets that are this close to the ground (speed of light)
-            maxTerrainCheck = 16000;    // How far we should check for terrain
-
-            defaultSeekerLockMode = "LOAL";
-            seekerLockModes[] = { "LOAL", "LOBL" };
-
-            defaultNavigationType = "ZeroEffortMiss";
-            navigationTypes[] = { "ZeroEffortMiss" };
-
-            seekLastTargetPos = 0;      // seek last target position [if seeker loses LOS of target, continue to last known pos]
-            seekerAngle = 50;           // Angle from the shooter's view that can track the missile
-            seekerAccuracy = 1;         // seeker accuracy multiplier
-
-            seekerMinRange = 75;
-            seekerMaxRange = 2500;      // Range from the missile which the seeker can visually search
-
-            // Attack profile type selection
-            defaultAttackProfile = "DIR";
-            attackProfiles[] = {"DIR", "LOFT"};
-            useModeForAttackProfile = 1;
         };
     };
 };
