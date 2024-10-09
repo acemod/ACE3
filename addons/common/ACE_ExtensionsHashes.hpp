450f6b2f003ca256e37e10caaf8afeaca0d84728
PabstMirror <pabstmirror@gmail.com>
2024-10-08 22:00:29 -0500

    Update ACE_ExtensionsHashes.hpp

--- addons/common/ACE_ExtensionsHashes.hpp
+++ addons/common/ACE_ExtensionsHashes.hpp
@@ -1,6 +1,6 @@
 class ACE_ExtensionsHashes {
     class ace {
-        dll = "eba921ad7b49cdfad1d5d44c0fbe9e0cbc9056dd";
-        dll_x64 = "35140862f100f0a0b588096d017d8fbe313ce25e";
+        dll = "b83c2c9c7c989eaf888c885d13a2fdf4a61d1c5f";
+        dll_x64 = "356a61c4bd2aa13556a8ba0b467c819b3b438d6c";
     };
 };
