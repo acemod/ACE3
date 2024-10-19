144bdd683b5e90d66950120cbff0735daa5c0516
bluefield <59333909+bluefieldcreator@users.noreply.github.com>
2024-10-19 19:11:51 +0200

    Update addons/overpressure/CfgEden.hpp
    Co-authored-by: PabstMirror <pabstmirror@gmail.com>

--- addons/overpressure/CfgEden.hpp
+++ addons/overpressure/CfgEden.hpp
@@ -8,7 +8,7 @@ class Cfg3DEN {
                         tooltip = CSTRING(distanceCoefficient_toolTip);
                         property = QUOTE(overpressureDistanceCoefficient);
                         control = "Edit";
-                        expression = QUOTE(_this setVariable [ARR_3(QQEGVAR(overpressure,distance),_value,true)]);
+                        expression = QUOTE(if (_value >= 0) then { _this setVariable [ARR_3(QQGVAR(distance),_value,true)] });
                         defaultValue = -1;
                         validate = "number";
                         typeName = "NUMBER";
