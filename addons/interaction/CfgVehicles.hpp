d71491cfa8cc6788a04e954d3e4bbe2f098d6623
Timi007 <timi007gp@gmail.com>
2024-11-06 15:22:55 +0100

    Handle currentThrowable returns empty array

--- addons/interaction/CfgVehicles.hpp
+++ addons/interaction/CfgVehicles.hpp
@@ -70,11 +70,11 @@ class CfgVehicles {
 
                 class ACE_PassThrowable {
                     displayName = CSTRING(PassThrowable);
-                    condition = QUOTE([ARR_3(_target,_player,(currentThrowable _player) select 0)] call FUNC(canPassThrowable));
-                    statement = QUOTE([ARR_3(_target,_player,(currentThrowable _player) select 0)] call FUNC(passThrowable));
+                    condition = QUOTE([ARR_3(_target,_player,(currentThrowable _player) param [ARR_2(0,'')])] call FUNC(canPassThrowable));
+                    statement = QUOTE([ARR_3(_target,_player,(currentThrowable _player) param [ARR_2(0,'')])] call FUNC(passThrowable));
                     exceptions[] = {"isNotSwimming"};
                     showDisabled = 0;
-                    modifierFunction = QUOTE(_this select 3 set [ARR_2(2,getText (configFile >> 'CfgMagazines' >> (currentThrowable (_this select 1)) select 0 >> 'picture'))];); // Set picture of the current grenade
+                    modifierFunction = QUOTE(_this select 3 set [ARR_2(2,getText (configFile >> 'CfgMagazines' >> (currentThrowable (_this select 1)) param [ARR_2(0,'HandGrenade')] >> 'picture'))];); // Set picture of the current grenade
                 };
 
                 class ACE_TeamManagement {
