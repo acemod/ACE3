fa7082fcf048e0e8b57a831316edb7574f1c3bb6
jpspider1 <jp.spider1@gmail.com>
2024-11-11 16:19:24 -0500

    ~ Changed: gurney_C of all German shells to 2930

--- addons/compat_spe/CfgAmmo/shells.hpp
+++ addons/compat_spe/CfgAmmo/shells.hpp
@@ -5,7 +5,7 @@ class SPE_50mm_SprGr38_HE: SPE_ShellHE_base
     EGVAR(frag,force) = 1;
     EGVAR(frag,metal) = 1820;
     EGVAR(frag,charge) = 217;
-    EGVAR(frag,gurney_c) = 2440;
+    EGVAR(frag,gurney_c) = 2930;
     EGVAR(frag,gurney_k) = "1/2";
     EGVAR(frag,classes)[] = {"ACE_frag_medium","ACE_frag_small_HD"};
 };
@@ -16,7 +16,7 @@ class SPE_SprGr34_K51_HE: SPE_ShellHE_base
     EGVAR(frag,force) = 1;
     EGVAR(frag,metal) = 5740;
     EGVAR(frag,charge) = 686;
-    EGVAR(frag,gurney_c) = 2440;
+    EGVAR(frag,gurney_c) = 2930;
     EGVAR(frag,gurney_k) = "1/2";
     EGVAR(frag,classes)[] = {"ACE_frag_medium","ACE_frag_medium_HD"};
 };
@@ -28,7 +28,7 @@ class SPE_SprGr34_KWK40_HE: SPE_ShellHE_base
     EGVAR(frag,force) = 1;
     EGVAR(frag,metal) = 5740;
     EGVAR(frag,charge) = 686;
-    EGVAR(frag,gurney_c) = 2440;
+    EGVAR(frag,gurney_c) = 2930;
     EGVAR(frag,gurney_k) = "1/2";
     EGVAR(frag,classes)[] = {"ACE_frag_medium","ACE_frag_medium_HD"};
 };
@@ -40,7 +40,7 @@ class SPE_SprGr42_KwK42_HE: SPE_ShellHE_base
     EGVAR(frag,force) = 1;
     EGVAR(frag,metal) = 5740;
     EGVAR(frag,charge) = 725;
-    EGVAR(frag,gurney_c) = 2440;
+    EGVAR(frag,gurney_c) = 2930;
     EGVAR(frag,gurney_k) = "1/2";
     EGVAR(frag,classes)[] = {"ACE_frag_medium","ACE_frag_medium_HD"};
 };
@@ -52,7 +52,7 @@ class SPE_SprGr_KwK36_HE: SPE_ShellHE_base
     EGVAR(frag,force) = 1;
     EGVAR(frag,metal) = 9000;
     EGVAR(frag,charge) = 900;
-    EGVAR(frag,gurney_c) = 2440;
+    EGVAR(frag,gurney_c) = 2930;
     EGVAR(frag,gurney_k) = "1/2";
     EGVAR(frag,classes)[] = {"ACE_frag_large","ACE_frag_large_HD"};
 };
@@ -64,7 +64,7 @@ class SPE_105mm_StuH42_Gr38_HE: SPE_ShellHE_base
     EGVAR(frag,force) = 1;
     EGVAR(frag,metal) = 14810;
     EGVAR(frag,charge) = 2530;
-    EGVAR(frag,gurney_c) = 2440;
+    EGVAR(frag,gurney_c) = 2930;
     EGVAR(frag,gurney_k) = "1/2";
     EGVAR(frag,classes)[] = {"ACE_frag_large","ACE_frag_large_HD"};
 };
@@ -76,7 +76,7 @@ class SPE_SprGr39_HE: SPE_ShellHE_base
     EGVAR(frag,force) = 1;
     EGVAR(frag,metal) = 9400;
     EGVAR(frag,charge) = 1000;
-    EGVAR(frag,gurney_c) = 2440;
+    EGVAR(frag,gurney_c) = 2930;
     EGVAR(frag,gurney_k) = "1/2";
     EGVAR(frag,classes)[] = {"ACE_frag_large","ACE_frag_large_HD"};
 };
