bc3a340a1f134713940d7ac190e41d2a0fe04498
PabstMirror <pabstmirror@gmail.com>
2024-11-22 20:29:52 -0600

    Frag - de-quote number

--- addons/compat_spe/CfgAmmo/bombs.hpp
+++ addons/compat_spe/CfgAmmo/bombs.hpp
@@ -5,7 +5,7 @@ class SPE_NC250_Bomb: SPE_Bomb_base {
     EGVAR(frag,metal) = 166000;
     EGVAR(frag,charge) = 130000;
     EGVAR(frag,gurney_c) = 2750;
-    EGVAR(frag,gurney_k) = "1/2";
+    EGVAR(frag,gurney_k) = 1/2;
     EGVAR(frag,classes)[] = {"ACE_frag_medium","ACE_frag_large","ACE_frag_huge"};
 };
 class SPE_NC50_Bomb: SPE_Bomb_base {
@@ -14,7 +14,7 @@ class SPE_NC50_Bomb: SPE_Bomb_base {
     EGVAR(frag,metal) = 25000;
     EGVAR(frag,charge) = 24400;
     EGVAR(frag,gurney_c) = 2750;
-    EGVAR(frag,gurney_k) = "1/2";
+    EGVAR(frag,gurney_k) = 1/2;
     EGVAR(frag,classes)[] = {"ACE_frag_medium","ACE_frag_large"};
 };
 class SPE_SC500_Bomb: SPE_Bomb_base {
@@ -23,7 +23,7 @@ class SPE_SC500_Bomb: SPE_Bomb_base {
     EGVAR(frag,metal) = 200000;
     EGVAR(frag,charge) = 275000;
     EGVAR(frag,gurney_c) = 2750;
-    EGVAR(frag,gurney_k) = "1/2";
+    EGVAR(frag,gurney_k) = 1/2;
     EGVAR(frag,classes)[] = {"ACE_frag_medium","ACE_frag_large","ACE_frag_huge"};
 };
 class SPE_US_500lb_Bomb: SPE_Bomb_base {
@@ -32,7 +32,7 @@ class SPE_US_500lb_Bomb: SPE_Bomb_base {
     EGVAR(frag,metal) = 100000;
     EGVAR(frag,charge) = 124000;
     EGVAR(frag,gurney_c) = 2700;
-    EGVAR(frag,gurney_k) = "1/2";
+    EGVAR(frag,gurney_k) = 1/2;
     EGVAR(frag,classes)[] = {"ACE_frag_medium","ACE_frag_large","ACE_frag_huge"};
 };
 class SPE_US_1000lb_Bomb: SPE_Bomb_base {
@@ -41,6 +41,6 @@ class SPE_US_1000lb_Bomb: SPE_Bomb_base {
     EGVAR(frag,metal) = 200000;
     EGVAR(frag,charge) = 158000;
     EGVAR(frag,gurney_c) = 2700;
-    EGVAR(frag,gurney_k) = "1/2";
+    EGVAR(frag,gurney_k) = 1/2;
     EGVAR(frag,classes)[] = {"ACE_frag_medium","ACE_frag_large","ACE_frag_huge"};
 };
