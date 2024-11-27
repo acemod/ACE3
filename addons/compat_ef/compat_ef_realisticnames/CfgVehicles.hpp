e2fe326cd02cabfcbf966e2418041fcc3aecccbf
PabstMirror <pabstmirror@gmail.com>
2024-11-26 19:17:14 -0600

    Add name for EF_ah99

--- addons/compat_ef/compat_ef_realisticnames/CfgVehicles.hpp
+++ addons/compat_ef/compat_ef_realisticnames/CfgVehicles.hpp
@@ -9,4 +9,9 @@ class CfgVehicles {
     class EF_MRAP_01_LAAD_base: EF_MRAP_01_FSV_base {
         displayName = SUBCSTRING(MRAP_01_LAAD);
     };
+
+    class Heli_Attack_01_dynamicLoadout_base_F;
+    class EF_AH99J_dynamicLoadout_base: Heli_Attack_01_dynamicLoadout_base_F {
+        displayName = SUBCSTRING(AH99J);
+    };
 };
