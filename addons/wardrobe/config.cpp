a381dcc579473e558719da2b853f978f7890c4db
OverlordZorn <dennisAzorn@gmail.com>
2024-12-27 14:10:49 +0100

    Update config.cpp

--- addons/wardrobe/config.cpp
+++ addons/wardrobe/config.cpp
@@ -2,33 +2,21 @@
 
 class CfgPatches {
 	class ADDON {
-
-        // Meta information for editor
-		name = ADDON_NAME;
-		author = "$STR_wardrobe_author";
+		name = COMPONENT_NAME;
+		author = ECSTRING(common,ACETeam);
         authors[] = {"OverlordZorn [CVO]"};
-		
-        url = "$STR_mod_URL";
+        url = ECSTRING(main,URL);
 
 		VERSION_CONFIG;
+        requiredVersion = REQUIRED_VERSION;
 
-        // Addon Specific Information
-        // Minimum compatible version. When the game's version is lower, pop-up warning will appear when launching the game.
-        requiredVersion = 2.02;
-
-        // Required addons, used for setting load order.
-        // When any of the addons is missing, pop-up warning will appear when launching the game.
-        requiredAddons[] = {"cba_main", "ace_interact_menu"};
-
-		// Optional. If this is 1, if any of requiredAddons[] entry is missing in your game the entire config will be ignored and return no error (but in rpt) so useful to make a compat Mod (Since Arma 3 2.14)
+        requiredAddons[] = {
+            "ace_interact_menu"
+        };
 		skipWhenMissingDependencies = 1;
-        
-        // List of objects (CfgVehicles classes) contained in the addon. Important also for Zeus content (units and groups)
+
         units[] = {};
-
-        // List of weapons (CfgWeapons classes) contained in the addon.
         weapons[] = {};
-
 	};
 };
 
