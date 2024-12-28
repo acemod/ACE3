6abe9ab2d434bc5e125b6b06a2afeecdb718d6e7
OverlordZorn <dennisAzorn@gmail.com>
2024-12-28 15:34:21 +0100

    fix compat macros

--- addons/compat_sog/compat_sog_wardrobe/script_component.hpp
+++ addons/compat_sog/compat_sog_wardrobe/script_component.hpp
@@ -1,3 +1,5 @@
 #define SUBCOMPONENT wardrobe
 #define SUBCOMPONENT_BEAUTIFIED Wardrobe Compatibility for SOG:PF
 #include "..\script_component.hpp"
+
+#include "\z\ace\addons\wardrobe\script_macros_zrn.hpp"
--- addons/compat_sog/compat_sog_wardrobe_uniform_fix/script_component.hpp
+++ addons/compat_sog/compat_sog_wardrobe_uniform_fix/script_component.hpp
@@ -1,3 +1,5 @@
 #define SUBCOMPONENT wardrobe
 #define SUBCOMPONENT_BEAUTIFIED Wardrobe Compatibility for SOG:PF - Uniform Fix
 #include "..\script_component.hpp"
+
+#include "\z\ace\addons\wardrobe\script_macros_zrn.hpp"
