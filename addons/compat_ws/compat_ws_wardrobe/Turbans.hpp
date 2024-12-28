d91d5c023b370be87b3d41d1306deaed7503ca69
OverlordZorn <dennisAzorn@gmail.com>
2024-12-28 20:26:32 +0100

    Update Turbans.hpp

--- addons/compat_ws/compat_ws_wardrobe/Turbans.hpp
+++ addons/compat_ws/compat_ws_wardrobe/Turbans.hpp
@@ -8,27 +8,40 @@
 class CN(01,COLOR): lxWS_H_turban_01_black { class PVAR(wardrobe) : EGVAR(wardrobe,base) { modifiableTo[] = { QCN(02,COLOR), QCN(03,COLOR), QCN(04,COLOR) }; }; };\
 class CN(02,COLOR): lxWS_H_turban_02_black { class PVAR(wardrobe) : EGVAR(wardrobe,base) { modifiableTo[] = { QCN(01,COLOR), QCN(03,COLOR), QCN(04,COLOR) }; }; };\
 class CN(03,COLOR): lxWS_H_turban_03_black { class PVAR(wardrobe) : EGVAR(wardrobe,base) { modifiableTo[] = { QCN(01,COLOR), QCN(02,COLOR), QCN(04,COLOR) }; }; };\
-class CN(04,COLOR): lxWS_H_turban_04_black { class PVAR(wardrobe) : EGVAR(wardrobe,base) { modifiableTo[] = { QCN(01,COLOR), QCN(02,COLOR), QCN(03,COLOR) }; }; };\
+class CN(04,COLOR): lxWS_H_turban_04_black { class PVAR(wardrobe) : EGVAR(wardrobe,base) { modifiableTo[] = { QCN(01,COLOR), QCN(02,COLOR), QCN(03,COLOR) }; }; };
 
 #define COLORGROUP_SPECIAL(COLOR)\
 class CN(02,COLOR): lxWS_H_turban_02_black { class PVAR(wardrobe) : EGVAR(wardrobe,base) { modifiableTo[] = { QCN(03,COLOR) }; }; };\
-class CN(03,COLOR): lxWS_H_turban_03_black { class PVAR(wardrobe) : EGVAR(wardrobe,base) { modifiableTo[] = { QCN(02,COLOR) }; }; };\
+class CN(03,COLOR): lxWS_H_turban_03_black { class PVAR(wardrobe) : EGVAR(wardrobe,base) { modifiableTo[] = { QCN(02,COLOR) }; }; };
+
+
+lxWS_H_bmask_base // mask standalone
+
 
 // BASECLASS
 class H_Shemag_khk;
-class lxWS_H_turban_01_black : H_Shemag_khk           { class PVAR(wardrobe) : EGVAR(wardrobe,base) { modifiableTo[] = { QCN(02,black), QCN(03,black), QCN(04,black) }; }; };
-class lxWS_H_turban_02_black : lxWS_H_turban_01_black { class PVAR(wardrobe) : EGVAR(wardrobe,base) { modifiableTo[] = { QCN(01,black), QCN(03,black), QCN(04,black) }; }; };
-class lxWS_H_turban_03_black : lxWS_H_turban_01_black { class PVAR(wardrobe) : EGVAR(wardrobe,base) { modifiableTo[] = { QCN(01,black), QCN(02,black), QCN(04,black) }; }; };
-class lxWS_H_turban_04_black : lxWS_H_turban_01_black { class PVAR(wardrobe) : EGVAR(wardrobe,base) { modifiableTo[] = { QCN(01,black), QCN(02,black), QCN(03,black) }; }; };
+
+class H_turban_02_mask_black_lxws : H_Shemag_khk      { class PVAR(wardrobe) : EGVAR(wardrobe,base) { modifiableTo[] = { QCN(01,black), QCN(02,black), QCN(03,black), QCN(04,black) }; components[] = { "lxWS_H_bmask_base" }; }; };
+class lxWS_H_turban_01_black : H_Shemag_khk           { class PVAR(wardrobe) : EGVAR(wardrobe,base) { modifiableTo[] = { QCN(02,black), QCN(03,black), QCN(04,black), "H_turban_02_mask_black_lxws" }; }; };
+class lxWS_H_turban_02_black : lxWS_H_turban_01_black { class PVAR(wardrobe) : EGVAR(wardrobe,base) { modifiableTo[] = { QCN(01,black), QCN(03,black), QCN(04,black), "H_turban_02_mask_black_lxws" }; }; };
+class lxWS_H_turban_03_black : lxWS_H_turban_01_black { class PVAR(wardrobe) : EGVAR(wardrobe,base) { modifiableTo[] = { QCN(01,black), QCN(02,black), QCN(04,black), "H_turban_02_mask_black_lxws" }; }; };
+class lxWS_H_turban_04_black : lxWS_H_turban_01_black { class PVAR(wardrobe) : EGVAR(wardrobe,base) { modifiableTo[] = { QCN(01,black), QCN(02,black), QCN(03,black), "H_turban_02_mask_black_lxws" }; }; };
 
 // VARIATIONS
 COLORGROUP(blue)
 COLORGROUP(blue_una)
 COLORGROUP(green)
 COLORGROUP(red)
-COLORGROUP(sand)
 COLORGROUP(gray)
 COLORGROUP(yellow)
 
 // Special Variations
 COLORGROUP_SPECIAL(green_pattern)
+
+// Super Special Manual blabla
+class H_turban_02_mask_snake_lxws : H_turban_02_mask_black_lxws { class PVAR(wardrobe) : EGVAR(wardrobe,base) { modifiableTo[] = { QCN(01,sand), QCN(02,sand), QCN(03,sand), QCN(04,sand), "H_turban_02_mask_hex_lxws" }; components[] = {"lxWS_H_bmask_hex"}; }; };
+class H_turban_02_mask_hex_lxws: H_turban_02_mask_black_lxws    { class PVAR(wardrobe) : EGVAR(wardrobe,base) { modifiableTo[] = { QCN(01,sand), QCN(02,sand), QCN(03,sand), QCN(04,sand), "H_turban_02_mask_snake_lxws" }; components[] = {"H_bmask_snake_lxws"}; }; };
+class lxWS_H_turban_01_sand: lxWS_H_turban_01_black             { class PVAR(wardrobe) : EGVAR(wardrobe,base) { modifiableTo[] = { QCN(02,sand), QCN(03,sand), QCN(04,sand), "H_turban_02_mask_snake_lxws", "H_turban_02_mask_hex_lxws" }; }; };
+class lxWS_H_turban_02_sand: lxWS_H_turban_02_black             { class PVAR(wardrobe) : EGVAR(wardrobe,base) { modifiableTo[] = { QCN(01,sand), QCN(03,sand), QCN(04,sand), "H_turban_02_mask_snake_lxws", "H_turban_02_mask_hex_lxws" }; }; };
+class lxWS_H_turban_03_sand: lxWS_H_turban_03_black             { class PVAR(wardrobe) : EGVAR(wardrobe,base) { modifiableTo[] = { QCN(01,sand), QCN(02,sand), QCN(04,sand), "H_turban_02_mask_snake_lxws", "H_turban_02_mask_hex_lxws" }; }; };
+class lxWS_H_turban_04_sand: lxWS_H_turban_04_black             { class PVAR(wardrobe) : EGVAR(wardrobe,base) { modifiableTo[] = { QCN(01,sand), QCN(02,sand), QCN(03,sand), "H_turban_02_mask_snake_lxws", "H_turban_02_mask_hex_lxws" }; }; };
