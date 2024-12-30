fe99af78ec7bcf0c94952e646573b8b349b2eb52
OverlordZorn <dennisAzorn@gmail.com>
2024-12-30 15:18:56 +0100

    macro conflict

--- addons/wardrobe/CfgSounds.hpp
+++ addons/wardrobe/CfgSounds.hpp
@@ -1,8 +1,10 @@
-#define SOUND(var1,dur)\
-class GVAR(DOUBLES(var1,dur))\
+// Duration in this case is 10th of secounds. so 10 -> 1 sec, 15 -> 1.5sec, ...
+
+#define ENTRY(base,dur)\
+class GVAR(DOUBLES(base,dur))\
 {\
-    name = Q(var1 dur);\
-    sound[] = { QPATHTOF(data\DOUBLES(var1,dur)##.ogg), 1, 1, 100 };\
+    name = Q(base dur);\
+    sound[] = { QPATHTOF(data\DOUBLES(base,dur)##.ogg), 1, 1, 100 };\
     titles[] = { 0, "" };\
     forceTitles = 0;\
     titlesStructured = 0;\
@@ -12,11 +14,11 @@ class GVAR(DOUBLES(var1,dur))\
 class CfgSounds
 {
 	sounds[] = {}; // OFP required it filled, now it can be empty or absent depending on the game's version
-    SOUND(fabric,06)
-    SOUND(fabric,07)
-    SOUND(fabric,16)
-    SOUND(fabric,20)
-    SOUND(fabric,25)
-    
-    SOUND(helmet_visor,05)
+    ENTRY(fabric,06)
+    ENTRY(fabric,07)
+    ENTRY(fabric,16)
+    ENTRY(fabric,20)
+    ENTRY(fabric,25)
+
+    ENTRY(helmet_visor,05)
 };
