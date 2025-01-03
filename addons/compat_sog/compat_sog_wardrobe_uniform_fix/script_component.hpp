5f0bbb287fe506db67bcb5490c9e3444c41b555c
OverlordZorn <dennisAzorn@gmail.com>
2025-01-03 11:59:39 +0100

    pruned SUBCOMPONENT_BEAUTIFIED

--- addons/compat_sog/compat_sog_wardrobe_uniform_fix/script_component.hpp
+++ addons/compat_sog/compat_sog_wardrobe_uniform_fix/script_component.hpp
@@ -1,5 +1,5 @@
 #define SUBCOMPONENT wardrobe_uniform_fix
-#define SUBCOMPONENT_BEAUTIFIED Wardrobe Compatibility for SOG:PF - Uniform Fix
+#define SUBCOMPONENT_BEAUTIFIED Wardrobe
 #include "..\script_component.hpp"
 
 #include "\z\ace\addons\wardrobe\script_macros_wardrobe.hpp"
