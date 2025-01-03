f59a1f2395df5ddb1ba85c10a839b45281b05331
OverlordZorn <dennisAzorn@gmail.com>
2025-01-03 02:55:22 +0100

    gm/config

--- addons/compat_gm/compat_gm_wardrobe/Helmets.hpp
+++ addons/compat_gm/compat_gm_wardrobe/Helmets.hpp
@@ -6,9 +6,33 @@ class gm_ge_headgear_psh77_cover_up_base;
 class gm_ge_headgear_psh77_down_base;
 class gm_ge_headgear_psh77_up_base;
 
-class gm_ge_bgs_headgear_psh77_cover_down_smp : gm_ge_headgear_psh77_cover_down_base { class ace_wardrobe : EGVAR(wardrobe,base_H_visor_down) { modifiableTo[] = { "gm_ge_bgs_headgear_psh77_cover_up_smp"   }; }; };
-class gm_ge_bgs_headgear_psh77_cover_up_smp   : gm_ge_headgear_psh77_cover_up_base   { class ace_wardrobe : EGVAR(wardrobe,base_H_visor_up)   { modifiableTo[] = { "gm_ge_bgs_headgear_psh77_cover_down_smp" }; }; };
-class gm_ge_bgs_headgear_psh77_cover_down_str : gm_ge_headgear_psh77_cover_down_base { class ace_wardrobe : EGVAR(wardrobe,base_H_visor_down) { modifiableTo[] = { "gm_ge_bgs_headgear_psh77_cover_up_str"   }; }; };
-class gm_ge_bgs_headgear_psh77_cover_up_str   : gm_ge_headgear_psh77_cover_up_base   { class ace_wardrobe : EGVAR(wardrobe,base_H_visor_up)   { modifiableTo[] = { "gm_ge_bgs_headgear_psh77_cover_down_str" }; }; };
-class gm_ge_headgear_psh77_down_oli           : gm_ge_headgear_psh77_down_base       { class ace_wardrobe : EGVAR(wardrobe,base_H_visor_down) { modifiableTo[] = { "gm_ge_headgear_psh77_up_oli"             }; }; };
-class gm_ge_headgear_psh77_up_oli             : gm_ge_headgear_psh77_up_base         { class ace_wardrobe : EGVAR(wardrobe,base_H_visor_up)   { modifiableTo[] = { "gm_ge_headgear_psh77_down_oli"           }; }; };
+class gm_ge_bgs_headgear_psh77_cover_down_smp : gm_ge_headgear_psh77_cover_down_base {
+    class ace_wardrobe : EGVAR(wardrobe,base_H_visor_down) { 
+        modifiableTo[] = { "gm_ge_bgs_headgear_psh77_cover_up_smp" };
+    };
+};
+class gm_ge_bgs_headgear_psh77_cover_up_smp : gm_ge_headgear_psh77_cover_up_base {
+    class ace_wardrobe : EGVAR(wardrobe,base_H_visor_up)   { 
+        modifiableTo[] = { "gm_ge_bgs_headgear_psh77_cover_down_smp" };
+    };
+};
+class gm_ge_bgs_headgear_psh77_cover_down_str : gm_ge_headgear_psh77_cover_down_base {
+    class ace_wardrobe : EGVAR(wardrobe,base_H_visor_down) { 
+        modifiableTo[] = { "gm_ge_bgs_headgear_psh77_cover_up_str" };
+    };
+};
+class gm_ge_bgs_headgear_psh77_cover_up_str : gm_ge_headgear_psh77_cover_up_base {
+    class ace_wardrobe : EGVAR(wardrobe,base_H_visor_up)   { 
+        modifiableTo[] = { "gm_ge_bgs_headgear_psh77_cover_down_str" };
+    };
+};
+class gm_ge_headgear_psh77_down_oli : gm_ge_headgear_psh77_down_base {
+    class ace_wardrobe : EGVAR(wardrobe,base_H_visor_down) { 
+        modifiableTo[] = { "gm_ge_headgear_psh77_up_oli" };
+    };
+};
+class gm_ge_headgear_psh77_up_oli : gm_ge_headgear_psh77_up_base {
+    class ace_wardrobe : EGVAR(wardrobe,base_H_visor_up)   { 
+        modifiableTo[] = { "gm_ge_headgear_psh77_down_oli" };
+    };
+};
