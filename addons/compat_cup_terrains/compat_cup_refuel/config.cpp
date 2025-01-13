4a65e327e952f2937404151ffd543e11407288a2
johnb432 <58661205+johnb432@users.noreply.github.com>
2025-01-13 13:41:43 -0800

    Compat CUP Terrains - Add sitting compatibility (#10645)
    Co-authored-by: Grim <69561145+LinkIsGrim@users.noreply.github.com>

--- addons/compat_cup_terrains/compat_cup_refuel/config.cpp
+++ addons/compat_cup_terrains/compat_cup_refuel/config.cpp
@@ -1,26 +1,27 @@
 #include "script_component.hpp"
+#include "\z\ace\addons\refuel\defines.hpp"
 
 class CfgPatches {
-    class ADDON {
+    class SUBADDON {
         name = COMPONENT_NAME;
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {
-            "CUP_CAStructures_E_Misc_Misc_Interier",
-            "CUP_CAStructures_E_Misc_Misc_Market",
-            "CUP_Editor_Buildings_Config",
-            "CUP_Editor_Structures_Config",
-            "CUP_Misc3_Config",
-            "ace_common"
+            "CABuildings",
+            "CAStructuresHouse_A_FuelStation",
+            "CAStructures_E_Ind_Ind_FuelStation",
+            "CAStructures_PMC_FuelStation",
+            "CUP_Buildings_Config",
+            "ace_refuel"
         };
         skipWhenMissingDependencies = 1;
         author = ECSTRING(common,ACETeam);
         authors[] = {"Community Upgrade Project", "Mike"};
         url = ECSTRING(main,URL);
         VERSION_CONFIG;
+        addonRootClass = QUOTE(ADDON);
     };
 };
 
 #include "CfgVehicles.hpp"
-#include "CfgEventHandlers.hpp"
