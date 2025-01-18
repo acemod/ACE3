557a6e6db0fa95961ac96376834b1f8969baba10
Timi007 <timi007gp@gmail.com>
2025-01-18 19:18:17 +0100

    Add editor previews

--- addons/flags/CfgVehicles.hpp
+++ addons/flags/CfgVehicles.hpp
@@ -30,6 +30,7 @@ class CfgVehicles {
         author = ECSTRING(common,ACETeam);
 
         displayName = CSTRING(White);
+        editorPreview = QPATHTOF(data\editorpreviews\ace_flags_carrier_white.jpg);
 
         class EventHandlers {
             class CBA_Extended_EventHandlers: CBA_Extended_EventHandlers {}; // Required for ACE_Actions
@@ -51,23 +52,30 @@ class CfgVehicles {
     };
     class GVAR(carrier_red): GVAR(carrier_white) {
         displayName = CSTRING(Red);
+        editorPreview = QPATHTOF(data\editorpreviews\ace_flags_carrier_red.jpg);
     };
     class GVAR(carrier_blue): GVAR(carrier_white) {
         displayName = CSTRING(Blue);
+        editorPreview = QPATHTOF(data\editorpreviews\ace_flags_carrier_blue.jpg);
     };
     class GVAR(carrier_green): GVAR(carrier_white) {
         displayName = CSTRING(Green);
+        editorPreview = QPATHTOF(data\editorpreviews\ace_flags_carrier_green.jpg);
     };
     class GVAR(carrier_yellow): GVAR(carrier_white) {
         displayName = CSTRING(Yellow);
+        editorPreview = QPATHTOF(data\editorpreviews\ace_flags_carrier_yellow.jpg);
     };
     class GVAR(carrier_orange): GVAR(carrier_white) {
         displayName = CSTRING(Orange);
+        editorPreview = QPATHTOF(data\editorpreviews\ace_flags_carrier_orange.jpg);
     };
     class GVAR(carrier_purple): GVAR(carrier_white) {
         displayName = CSTRING(Purple);
+        editorPreview = QPATHTOF(data\editorpreviews\ace_flags_carrier_purple.jpg);
     };
     class GVAR(carrier_black): GVAR(carrier_white) {
         displayName = CSTRING(Black);
+        editorPreview = QPATHTOF(data\editorpreviews\ace_flags_carrier_black.jpg);
     };
 };
