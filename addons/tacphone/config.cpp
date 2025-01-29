14b60947236aef60f2c7b3773d1f68dcdf076657
Will/KJW <100206101+SpicyBagpipes@users.noreply.github.com>
2025-01-29 22:27:02 +0000

    Initial

---
+++ addons/tacphone/config.cpp
@@ -0,0 +1,17 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {};
+        author = ECSTRING(common,ACETeam);
+        authors[] = {"KJW"};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgEventHandlers.hpp"
