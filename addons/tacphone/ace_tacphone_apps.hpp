e11f1eda44dc0646bcea2f69d3cda7249f0d28cb
Will/KJW <100206101+SpicyBagpipes@users.noreply.github.com>
2025-01-30 00:19:21 +0000

    More bits & bobs, still unorganised, still unfinished

---
+++ addons/tacphone/ace_tacphone_apps.hpp
@@ -0,0 +1,14 @@
+class GVAR(apps) {
+    class BFT { // Name TBD
+        scope = 2;
+        displayName = "Battlespace Management";
+        displayNameShort = "BM";
+        icon = "\A3\ui_f\data\map\markers\nato\c_unknown.paa"; // 1:1 aspect ratio. Image TBD
+    };
+    class Messaging {
+        scope = 2;
+        displayName = "Beacon";
+        displayNameShort = "BCN";
+        icon = "\A3\ui_f\data\map\markers\nato\c_ship.paa"; // Image TBD
+    };
+};
