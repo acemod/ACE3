1b50a0ea49c7a562f15fe4dbbd7014109bedd931
Will/KJW <100206101+SpicyBagpipes@users.noreply.github.com>
2025-01-30 02:20:30 +0000

    Proper application handling

--- addons/tacphone/ace_tacphone_apps.hpp
+++ addons/tacphone/ace_tacphone_apps.hpp
@@ -4,11 +4,13 @@ class GVAR(apps) {
         displayName = "Battlespace Management";
         displayNameShort = "BM";
         icon = "\A3\ui_f\data\map\markers\nato\c_unknown.paa"; // 1:1 aspect ratio. Image TBD
+        function = QGVAR(loadBFT);
     };
     class Messaging {
         scope = 2;
         displayName = "Beacon";
         displayNameShort = "BCN";
         icon = "\A3\ui_f\data\map\markers\nato\c_ship.paa"; // Image TBD
+        function = QGVAR(loadMessaging);
     };
 };
