7a5e5404922db1f13e97a85dbdacf12300706579
Will/KJW <100206101+SpicyBagpipes@users.noreply.github.com>
2025-01-31 19:07:29 +0000

    Change scope handling
    `scope = 2` now dictates something should show in homescreen, `scope = 1` just means app is accessible.

--- addons/tacphone/apps/homescreen/ace_tacphone_apps.hpp
+++ addons/tacphone/apps/homescreen/ace_tacphone_apps.hpp
@@ -1,6 +1,6 @@
 class EGVAR(tacphone,apps) {
     class Homescreen { // Name TBD
-        scope = 2;
+        scope = 1;
         displayName = "Home Screen";
         displayNameShort = "HS";
         icon = "\A3\ui_f\data\map\markers\nato\c_unknown.paa"; // 1:1 aspect ratio. Image TBD
