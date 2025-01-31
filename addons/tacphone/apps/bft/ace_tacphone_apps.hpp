ff4af34150139b8aa1755ea6a4946c77abf12b23
Will/KJW <100206101+SpicyBagpipes@users.noreply.github.com>
2025-01-31 22:03:42 +0000

    Fix that

--- addons/tacphone/apps/bft/ace_tacphone_apps.hpp
+++ addons/tacphone/apps/bft/ace_tacphone_apps.hpp
@@ -4,6 +4,8 @@ class EGVAR(tacphone,apps) {
         displayName = "Battlespace Management";
         displayNameShort = "BM";
         icon = "\A3\ui_f\data\map\markers\nato\c_unknown.paa"; // 1:1 aspect ratio. Image TBD
-        EGVAR(tacphone,createApp) = QFUNC(loadBFT);
+
+        EGVAR(tacphone,createApp) = QFUNC(createApp);
+        EGVAR(tacphone,onClose) = QFUNC(onClose);
     };
 };
