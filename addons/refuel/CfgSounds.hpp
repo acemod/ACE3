c1f9c1a13a9f89ce9768d78a7c104c9e20cba858
OverlordZorn <dennisAzorn@gmail.com>
2025-03-25 20:45:52 +0100

    finetune range

--- addons/refuel/CfgSounds.hpp
+++ addons/refuel/CfgSounds.hpp
@@ -3,13 +3,13 @@ class CfgSounds
     class GVAR(nozzle_stop)
     {
         name = QGVAR(nozzle_stop);
-        sound[] = {QPATHTOF(sounds\nozzle_stop.ogg), 1, 1, 50};
+        sound[] = {QPATHTOF(sounds\nozzle_stop.ogg), 1, 1, 25};
         titles[] = {};
     };
     class GVAR(nozzle_start)
     {
         name = QGVAR(nozzle_start);
-        sound[] = {QPATHTOF(sounds\nozzle_start.ogg), 1, 1, 25};
+        sound[] = {QPATHTOF(sounds\nozzle_start.ogg), 1, 1, 10};
         titles[] = {};
     };
 };
