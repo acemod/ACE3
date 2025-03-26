978f8d0540a71012cdbbfe09d1e338e6b5131bf3
OverlordZorn <dennisAzorn@gmail.com>
2025-03-26 12:41:59 +0100

    increased range
    when people walk away and walk back, the sound remains active instead of being dropped due to being out of range.

--- addons/refuel/CfgSounds.hpp
+++ addons/refuel/CfgSounds.hpp
@@ -1,12 +1,12 @@
 class CfgSounds {
     class GVAR(nozzle_stop) {
         name = QGVAR(nozzle_stop);
-        sound[] = {QPATHTOF(sounds\nozzle_stop.ogg), 1, 1, 25};
+        sound[] = {QPATHTOF(sounds\nozzle_stop.ogg), 1, 1, 50};
         titles[] = {};
     };
     class GVAR(nozzle_start) {
         name = QGVAR(nozzle_start);
-        sound[] = {QPATHTOF(sounds\nozzle_start.ogg), 1, 1, 10};
+        sound[] = {QPATHTOF(sounds\nozzle_start.ogg), 0.8, 1, 50};
         titles[] = {};
     };
 };
