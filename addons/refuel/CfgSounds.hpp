5d47b834f7900e5085cf6eaea98aab82464a5fdb
OverlordZorn <56258612+OverlordZorn@users.noreply.github.com>
2025-03-26 11:33:26 +0100

    Update addons/refuel/CfgSounds.hpp
    Co-authored-by: Mike-MF <TyroneMF@hotmail.com>

--- addons/refuel/CfgSounds.hpp
+++ addons/refuel/CfgSounds.hpp
@@ -1,13 +1,10 @@
-class CfgSounds
-{
-    class GVAR(nozzle_stop)
-    {
+class CfgSounds {
+    class GVAR(nozzle_stop) {
         name = QGVAR(nozzle_stop);
         sound[] = {QPATHTOF(sounds\nozzle_stop.ogg), 1, 1, 25};
         titles[] = {};
     };
-    class GVAR(nozzle_start)
-    {
+    class GVAR(nozzle_start) {
         name = QGVAR(nozzle_start);
         sound[] = {QPATHTOF(sounds\nozzle_start.ogg), 1, 1, 10};
         titles[] = {};
