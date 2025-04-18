b1706883269ab87a43a74f1e409a3e7b6f1712cf
PabstMirror <pabstmirror@gmail.com>
2025-04-17 21:05:08 -0500

    Compat CUP - Add artillerytables ranging to D-30 AT (#10870)

---
+++ addons/compat_cup_weapons/CfgVehicles.hpp
@@ -0,0 +1,8 @@
+class CfgVehicles {
+    class CUP_D30_base;
+    class CUP_D30_AT_base: CUP_D30_base {
+        EGVAR(artillerytables,showRangetable) = 1;
+        EGVAR(artillerytables,showGunLaying) = 1;
+        EGVAR(artillerytables,applyCorrections) = 0;
+    };
+};
