289b21db4469eed2813dfde9a5fd7a7c650275a3
OverlordZorn <dennisAzorn@gmail.com>
2025-04-28 15:17:04 +0200

    _hasDocument -> varName

--- addons/wardrobe/script_macros_wardrobe.hpp
+++ addons/wardrobe/script_macros_wardrobe.hpp
@@ -1,7 +1,7 @@
 // REPLACE
 #define INTEL_PRE(varName,className)\
 private varName = _magazineDetails findIf { _x select 0 == QUOTE(className) } > -1;\
-if (varName) then { _hasDocument = [_player, QUOTE(className)] call CBA_fnc_getMagazineIndex apply { [_x] call FUNC(getIndexFromMagID) } }
+if (varName) then { varName = [_player, QUOTE(className)] call CBA_fnc_getMagazineIndex apply { [_x] call FUNC(getIndexFromMagID) } }
 
 #define INTEL_POST(varName,className)\
 if (varName isEqualType []) then { { [_x, varName select _forEachIndex] call FUNC(setIndexForMagID); } forEach ( [_player, QUOTE(className)] call CBA_fnc_getMagazineIndex ); }
