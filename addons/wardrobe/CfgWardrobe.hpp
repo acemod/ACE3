06be6c9da56b153ab7f3851087f88adff67c39dd
OverlordZorn <dennisAzorn@gmail.com>
2025-05-05 14:48:58 +0200

    vanilla compat complete

--- addons/wardrobe/CfgWardrobe.hpp
+++ addons/wardrobe/CfgWardrobe.hpp
@@ -10,25 +10,5 @@ class CfgGlasses {
 class ADDON {
     #include "BaseClasses.hpp"
     #include "Uniforms.hpp"
-
-    // Balaclava, black
-    class G_Balaclava_blk: EGVAR(wardrobe,base) {
-        class modifiableTo {
-            class G_Balaclava_lowprofile;
-            class G_Balaclava_combat;
-        };
-        components[] = { "G_Balaclava_blk" };
-    };
-    class G_Balaclava_combat: EGVAR(wardrobe,base) {
-        class modifiableTo {
-            class G_Balaclava_blk;
-        };
-        components[] = {"G_Balaclava_blk", "G_Combat"};
-    };
-    class G_Balaclava_lowprofile: EGVAR(wardrobe,base) {
-        class modifiableTo {
-            class G_Balaclava_blk;
-        };
-        components[] = { "G_Balaclava_blk", "G_Lowprofile" };
-    };
+    #include "Facewear.hpp"
 };
