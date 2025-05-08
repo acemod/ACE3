2c07a6fba361d1adfcc83d07af1e160ac60d886a
Andy <andx667@protonmail.com>
2025-05-08 16:11:45 +0200

    add AFRF Headgear and Uniforms

--- addons/compat_rhs_afrf3/compat_rhs_afrf3_wardrobe/CfgWardrobe_Uniforms.hpp
+++ addons/compat_rhs_afrf3/compat_rhs_afrf3_wardrobe/CfgWardrobe_Uniforms.hpp
@@ -1,19 +1,54 @@
-// Gloves
-rhs_uniform_6sh122_v1
-rhs_uniform_6sh122_gloves_v1
+// Uniforms with Gloves
+class rhs_uniform_6sh122_v1: EGVAR(wardrobe,base_U_gloves_off) {
+  class modifiableTo { class rhs_uniform_6sh122_gloves_v1; };
+  components[] = {};
+};
+class rhs_uniform_6sh122_gloves_v1: EGVAR(wardrobe,base_U_gloves_on) {
+  class modifiableTo { class rhs_uniform_6sh122_v1; };
+  components[] = {};
+};
 
-rhs_uniform_6sh122_v2
-rhs_uniform_6sh122_gloves_v2
+class rhs_uniform_6sh122_v2: EGVAR(wardrobe,base_U_gloves_off) {
+  class modifiableTo { class rhs_uniform_6sh122_gloves_v2; };
+  components[] = {};
+};
+class rhs_uniform_6sh122_gloves_v2: EGVAR(wardrobe,base_U_gloves_on) {
+  class modifiableTo { class rhs_uniform_6sh122_v2; };
+  components[] = {};
+};
 
-rhs_uniform_gorka_r_g
-rhs_uniform_gorka_r_g_gloves
+class rhs_uniform_gorka_r_g: EGVAR(wardrobe,base_U_gloves_off){
+  class modifiableTo { class rhs_uniform_gorka_r_g_gloves; };
+  components[] = {};
+};
+class rhs_uniform_gorka_r_g_gloves: EGVAR(wardrobe,base_U_gloves_on) {
+  class modifiableTo { class rhs_uniform_gorka_r_g; };
+  components[] = {};
+};
 
-rhs_uniform_gorka_r_y
-rhs_uniform_gorka_r_y_gloves
+class rhs_uniform_gorka_r_y: EGVAR(wardrobe,base_U_gloves_off){
+  class modifiableTo { class rhs_uniform_gorka_r_y_gloves; };
+  components[] = {};
+};
+class rhs_uniform_gorka_r_y_gloves: EGVAR(wardrobe,base_U_gloves_on) {
+  class modifiableTo { class rhs_uniform_gorka_r_y; };
+  components[] = {};
+};
 
-rhs_uniform_vkpo
-rhs_uniform_vkpo_gloves
+class rhs_uniform_vkpo: EGVAR(wardrobe,base_U_gloves_off) {
+  class modifiableTo { class rhs_uniform_vkpo_gloves; };
+  components[] = {};
+};
+class rhs_uniform_vkpo_gloves: EGVAR(wardrobe,base_U_gloves_on) {
+  class modifiableTo { class rhs_uniform_vkpo; };
+  components[] = {};
+};
 
-rhs_uniform_vkpo_alt
-rhs_uniform_vkpo_gloves_alt
-
+class rhs_uniform_vkpo_alt: EGVAR(wardrobe,base_U_gloves_off) {
+  class modifiableTo { class rhs_uniform_vkpo_gloves_alt; };
+  components[] = {};
+};
+class rhs_uniform_vkpo_gloves_alt: EGVAR(wardrobe,base_U_gloves_on) {
+  class modifiableTo { class rhs_uniform_vkpo_alt; };
+  components[] = {};
+};
