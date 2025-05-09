f51c7e37634ed770dc19f6f4f7f96e986abb53a1
Andy <andx667@protonmail.com>
2025-05-09 09:53:20 +0200

    change to macros

--- addons/compat_rhs_saf3/compat_rhs_saf3_wardrobe/CfgWardrobe_Headgear.hpp
+++ addons/compat_rhs_saf3/compat_rhs_saf3_wardrobe/CfgWardrobe_Headgear.hpp
@@ -1,63 +1,16 @@
 // SAF requires RHS USAF, so we can use their Googles
-class rhssaf_helmet_m97_black_nocamo: EGVAR(wardrobe,base_H_goggles_off) {
-    class modifiableTo { class rhssaf_helmet_m97_black_nocamo_black_ess_bare; };
-    components[] = {};
-};
-class rhssaf_helmet_m97_black_nocamo_black_ess_bare: EGVAR(wardrobe,base_H_goggles_on) {
-    class modifiableTo { class rhssaf_helmet_m97_black_nocamo; };
-    components[] = {"rhs_ess_black"}; 
-};
+// Combat Helmets
+HELMET_GOGGLES(rhssaf_helmet_m97_black_nocamo,rhssaf_helmet_m97_black_nocamo_black_ess_bare,rhs_ess_black);
+HELMET_GOGGLES(rhssaf_helmet_m97_nostrap_blue,rhssaf_helmet_m97_nostrap_blue_tan_ess_bare,rhs_ess_black);
+HELMET_GOGGLES(rhssaf_helmet_m97_digital,rhssaf_helmet_m97_digital_black_ess_bare,rhs_ess_black);
+HELMET_GOGGLES(rhssaf_helmet_m97_md2camo,rhssaf_helmet_m97_md2camo_black_ess_bare,rhs_ess_black);
+HELMET_GOGGLES(rhssaf_helmet_m97_oakleaf,rhssaf_helmet_m97_oakleaf_black_ess_bare,rhs_ess_black);
+HELMET_GOGGLES(rhssaf_helmet_m97_olive_nocamo,rhssaf_helmet_m97_olive_nocamo_black_ess_bare,rhs_ess_black);
 
-class rhssaf_helmet_m97_nostrap_blue: EGVAR(wardrobe,base_H_goggles_off) {
-    class modifiableTo { class rhssaf_helmet_m97_nostrap_blue_tan_ess_bare; };
-    components[] = {};
-};
-class rhssaf_helmet_m97_nostrap_blue_tan_ess_bare: EGVAR(wardrobe,base_H_goggles_on) {
-    class modifiableTo { class rhssaf_helmet_m97_nostrap_blue; };
-    components[] = {"rhs_ess_black"};
-};
-
-class rhssaf_helmet_m97_digital: EGVAR(wardrobe,base_H_goggles_off) {
-    class modifiableTo { class rhssaf_helmet_m97_digital_black_ess_bare; };
-    components[] = {};
-};
-class rhssaf_helmet_m97_digital_black_ess_bare: EGVAR(wardrobe,base_H_goggles_on) {
-    class modifiableTo { class rhssaf_helmet_m97_digital; };
-    components[] = {"rhs_ess_black"};
-};
-
-class rhssaf_helmet_m97_md2camo: EGVAR(wardrobe,base_H_goggles_off) {
-    class modifiableTo { class rhssaf_helmet_m97_md2camo_black_ess_bare; };
-    components[] = {};
-};
-class rhssaf_helmet_m97_md2camo_black_ess_bare: EGVAR(wardrobe,base_H_goggles_on) {
-    class modifiableTo { class rhssaf_helmet_m97_md2camo; };
-    components[] = {"rhs_ess_black"};
-};
-
-class rhssaf_helmet_m97_oakleaf: EGVAR(wardrobe,base_H_goggles_off) {
-    class modifiableTo { class rhssaf_helmet_m97_oakleaf_black_ess_bare; };
-    components[] = {};
-};
-class rhssaf_helmet_m97_oakleaf_black_ess_bare: EGVAR(wardrobe,base_H_goggles_on) {
-    class modifiableTo { class rhssaf_helmet_m97_oakleaf; };
-    components[] = {"rhs_ess_black"};
-};
-
-class rhssaf_helmet_m97_olive_nocamo: EGVAR(wardrobe,base_H_goggles_off) {
-    class modifiableTo { class rhssaf_helmet_m97_olive_nocamo_black_ess_bare; };
-    components[] = {};
-};
-class rhssaf_helmet_m97_olive_nocamo_black_ess_bare: EGVAR(wardrobe,base_H_goggles_on) {
-    class modifiableTo { class rhssaf_helmet_m97_olive_nocamo; };
-    components[] = {"rhs_ess_black"};
-};
-
+//Special Case
 class rhssaf_helmet_m97_woodland: EGVAR(wardrobe,base) {
     class modifiableTo { class rhssaf_helmet_m97_woodland_black_ess_bare; class rhssaf_helmet_m97_veil_woodland; };
     components[] = {};
-    //alternativeDisplayName = ECSTRING(wardrobe,gogglesOn);
-    //alternativeDisplayName = ECSTRING(wardrobe,maskOn);
 };
 class rhssaf_helmet_m97_woodland_black_ess_bare: EGVAR(wardrobe,base) {
     class modifiableTo { class rhssaf_helmet_m97_woodland; };
