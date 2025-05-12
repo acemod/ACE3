359d0c524d36dafb115889f4c27c37a02e615706
Andx <andx667@protonmail.com>
2025-05-12 23:15:02 +0200

    use macros

--- addons/compat_cup_units/CfgVehicles.hpp
+++ addons/compat_cup_units/CfgVehicles.hpp
@@ -4,6 +4,6 @@ class CfgVehicles {
         EGVAR(trenches,entrenchingTool) = 1;
     };
     class CUP_B_RUS_Patrol_bag_6E5_Summer: CUP_B_RUS_Patrol_bag_base {
-        ace_trenches_entrenchingTool = 1;
+         EGVAR(trenches,entrenchingTool) = 1;
     };
 };
