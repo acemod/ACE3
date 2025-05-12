c182bf44bf039e71eea9cf5bc71e031e639206c5
OverlordZorn <dennisAzorn@gmail.com>
2025-05-12 21:38:59 +0200

    remove others

--- addons/compat_rhs_afrf3/compat_rhs_afrf3_wardrobe/script_component.hpp
+++
@@ -1,5 +0,0 @@
-#define SUBCOMPONENT wardrobe
-#define SUBCOMPONENT_BEAUTIFIED Wardrobe
-#include "..\script_component.hpp"
-
-#include "\z\ace\addons\wardrobe\script_macros_wardrobe.hpp"
--- addons/compat_rhs_gref3/compat_rhs_gref3_wardrobe/CfgWardrobe.hpp
+++
@@ -1,5 +0,0 @@
-class ace_wardrobe {
-    #include "\z\ace\addons\wardrobe\BaseClasses_Import.hpp"
-
-    #include "CfgWardrobe_Headgear.hpp"
-};
