027e8e9239ca5cd78945260d7630e826d2e59209
Andx <17100110+Andx667@users.noreply.github.com>
2025-05-13 17:03:32 +0200

    Compat CUP Units - Add `trenches` compatibility (#10916)
    * recreate PR
    
    * delete vehicles
    
    * add vehicles

---
+++ addons/compat_cup_units/CfgVehicles.hpp
@@ -0,0 +1,9 @@
+class CfgVehicles {
+    class CUP_B_RUS_Patrol_bag_base;
+    class CUP_B_RUS_Patrol_bag_6E5_Desert: CUP_B_RUS_Patrol_bag_base {
+        EGVAR(trenches,entrenchingTool) = 1;
+    };
+    class CUP_B_RUS_Patrol_bag_6E5_Summer: CUP_B_RUS_Patrol_bag_base {
+         EGVAR(trenches,entrenchingTool) = 1;
+    };
+};
