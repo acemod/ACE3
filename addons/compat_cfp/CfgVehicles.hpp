6bb36954b1a121a2c9607c91394416e30c696370
Andx <17100110+Andx667@users.noreply.github.com>
2025-05-13 08:57:20 +0200

    Update addons/compat_cfp/CfgVehicles.hpp
    Co-authored-by: johnb432 <58661205+johnb432@users.noreply.github.com>

--- addons/compat_cfp/CfgVehicles.hpp
+++ addons/compat_cfp/CfgVehicles.hpp
@@ -1,6 +1,6 @@
 class CfgVehicles {
     class Bag_Base;
     class CFP_Cutters: Bag_Base {
-        ace_logistics_wirecutter_hasWirecutter = 1;
+        EGVAR(logistics_wirecutter,hasWirecutter) = 1;
     };
 };
