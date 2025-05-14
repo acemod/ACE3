d7b95656de33e052dfbb569e8e75d5b8eb6f9988
Andy <andx667@protonmail.com>
2025-05-14 10:03:08 +0200

    initial commit

---
+++ addons/compat_3cb_equipment/config.cpp
@@ -0,0 +1,18 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"ace_common", "UK3CB_BAF_Equipment_Alice_Pack", "UK3CB_BAF_Equipment_Backpacks"};
+        skipWhenMissingDependencies = 1;
+        author = ECSTRING(common,ACETeam);
+        authors[] = {"Andx"};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgVehicles.hpp"
