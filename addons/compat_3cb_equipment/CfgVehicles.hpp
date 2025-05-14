8806d69a745f6ff3fdf855a86e35ed42b69f2c64
Andy <andx667@protonmail.com>
2025-05-14 10:16:01 +0200

    fix double definition

--- addons/compat_3cb_equipment/CfgVehicles.hpp
+++ addons/compat_3cb_equipment/CfgVehicles.hpp
@@ -3,7 +3,6 @@ class CfgVehicles {
     class UK3CB_BAF_B_Bergen_Base;
     class UK3CB_BAF_B_rt1523g_Base;
 
-    //Entrenching Tools
     class UK3CB_BAF_B_Bergen_MTP_Rifleman_H_A: UK3CB_BAF_B_Bergen_Base {
         EGVAR(trenches,entrenchingTool) = 1;
     };
@@ -12,19 +11,15 @@ class CfgVehicles {
     };
     class UK3CB_BAF_B_Bergen_MTP_Engineer_H_A: UK3CB_BAF_B_Bergen_Base {
         EGVAR(trenches,entrenchingTool) = 1;
+        EGVAR(logistics_wirecutter,hasWirecutter) = 1;
+    };
+    class UK3CB_BAF_B_Bergen_MTP_Engineer_L_A: UK3CB_BAF_B_Bergen_Base {
+        EGVAR(logistics_wirecutter,hasWirecutter) = 1;        
     };
     class UK3CB_BAF_B_Bergen_MTP_PointMan_H_A: UK3CB_BAF_B_Bergen_Base {
         EGVAR(trenches,entrenchingTool) = 1;
     };
     class UK3CB_BAF_B_Bergen_MTP_Radio_H_B: UK3CB_BAF_B_rt1523g_Base {
         EGVAR(trenches,entrenchingTool) = 1;
-    };
-
-    //Wirecutter
-    class UK3CB_BAF_B_Bergen_MTP_Engineer_H_A: UK3CB_BAF_B_Bergen_Base {
-        EGVAR(logistics_wirecutter,hasWirecutter) = 1;
-    };
-    class UK3CB_BAF_B_Bergen_MTP_Engineer_L_A: UK3CB_BAF_B_Bergen_Base {
-        EGVAR(logistics_wirecutter,hasWirecutter) = 1;        
     };
 };
