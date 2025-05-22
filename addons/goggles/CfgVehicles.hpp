ce5ff599e0f58a578a137bef740b06a1fd0fb203
JetfireBlack <42004605+JetfireBlack@users.noreply.github.com>
2025-05-23 00:05:08 +0200

    Adding respirator equipment self interactions

--- addons/goggles/CfgVehicles.hpp
+++ addons/goggles/CfgVehicles.hpp
@@ -9,6 +9,24 @@ class CfgVehicles {
                     statement = QUOTE(call FUNC(clearGlasses));
                     exceptions[] = {"isNotInside", "isNotSitting", "isNotSwimming", "isNotEscorting"};
                 };
+				class GVAR(ConnectRespirator) {
+					displayName = CSTRING(connectRespirator);
+					condition = QUOTE('combo' call FUNC(checkRespiratorConditions));
+					statement = QUOTE(call FUNC(connectRespirator));
+                    exceptions[] = {"isNotInside", "isNotSitting", "isNotSwimming", "isNotEscorting"};
+				};
+				class GVAR(toggleMask) {
+					displayName = CSTRING(ToggleMask);
+					condition = QUOTE('mask' call FUNC(checkRespiratorConditions));
+					statement = QUOTE(call FUNC(toggleMask));
+                    exceptions[] = {"isNotInside", "isNotSitting", "isNotSwimming", "isNotEscorting"};
+				};
+				class GVAR(hideHose) {
+					displayName = CSTRING(HideHose);
+					condition = QUOTE('hose' call FUNC(checkRespiratorConditions));
+					statement = QUOTE(call FUNC(hideHose));
+                    exceptions[] = {"isNotInside", "isNotSitting", "isNotSwimming", "isNotEscorting"};
+				};
             };
         };
     };
