35c38e7d8eb54c4d62697c376a3d8e1af044e8a0
OverlordZorn <dennisAzorn@gmail.com>
2025-06-04 22:45:57 +0200

    sort prep a->z

--- addons/wardrobe/XEH_PREP.hpp
+++ addons/wardrobe/XEH_PREP.hpp
@@ -1,31 +1,24 @@
-PREP(isModifiable);
-PREP(compareComponents);
+PREP(addActions);
+PREP(addActionsCondition);
+PREP(addActionsChildren);
+PREP(addActionsIcon);
+PREP(addActionsName);
+PREP(cacheGet);
+PREP(cacheSet);
 PREP(canModifyTo);
-// ace_intel
+PREP(clearOnClosedInteractionMenu);
+PREP(compareComponents);
+PREP(enableContextMenu);
 PREP(getIndexFromMagID);
-PREP(setIndexForMagID);
-// getItems
 PREP(getItemsAll);
 PREP(getItemsModifiableAll);
 PREP(getItemsModifiableCurrent);
 PREP(getItemsModifiableTo);
-// Replace
-PREP(replace);
-PREP(replaceOther);
-PREP(replaceContainer);
-// Compat
-PREP(spareBarrelUpdateMagIDs);
-// Ace Actions
-PREP(addActions);
-PREP(addActionsCondition);
-PREP(addActionsChildren);
-PREP(addActionsName);
-PREP(addActionsIcon);
-// Cache
-PREP(cacheGet);
-PREP(cacheSet);
-PREP(clearOnClosedInteractionMenu);
-// CBA Context Menu
-PREP(enableContextMenu);
+PREP(isModifiable);
 PREP(nextVariantDo);
 PREP(nextVariantGet);
+PREP(replace);
+PREP(replaceContainer);
+PREP(replaceOther);
+PREP(setIndexForMagID);
+PREP(spareBarrelUpdateMagIDs);
