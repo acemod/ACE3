b09a57ebbe451e1a26b31a267524572eeecaaa53
OverlordZorn <dennisAzorn@gmail.com>
2025-06-05 12:10:02 +0200

    rm ()

--- addons/compat_cup_units/compat_cup_wardrobe/CfgWardrobe.hpp
+++ addons/compat_cup_units/compat_cup_wardrobe/CfgWardrobe.hpp
@@ -1,6 +1,6 @@
 class ace_wardrobe {
-    IMPORT_BASE_CLASSES();
-    
+    IMPORT_BASE_CLASSES;
+
     #include "CfgWardrobe_Uniforms.hpp"
     #include "CfgWardrobe_Headgear.hpp"
     #include "CfgWardrobe_Goggles.hpp"
