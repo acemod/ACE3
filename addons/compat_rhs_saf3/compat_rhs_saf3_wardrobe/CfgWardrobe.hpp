6e3065678dbf0fa7f6f9d8168c0d71d3e6754c9e
OverlordZorn <dennisAzorn@gmail.com>
2025-06-05 12:04:42 +0200

    IMPORT_BASE_CLASSES();

--- addons/compat_rhs_saf3/compat_rhs_saf3_wardrobe/CfgWardrobe.hpp
+++ addons/compat_rhs_saf3/compat_rhs_saf3_wardrobe/CfgWardrobe.hpp
@@ -1,6 +1,4 @@
 class ace_wardrobe {
-    #include "\z\ace\addons\wardrobe\BaseClasses_Import.hpp"
-
-    // #include "CfgWardrobe_Turbans.hpp"
+    IMPORT_BASE_CLASSES();
     #include "CfgWardrobe_Headgear.hpp"
 };
