c3466fabaf3141de2b94e3d3ccfd26a3635c1c82
OverlordZorn <dennisAzorn@gmail.com>
2025-06-05 11:32:05 +0200

    altDisplayName-> alternativeActionName

--- addons/wardrobe/BaseClasses.hpp
+++ addons/wardrobe/BaseClasses.hpp
@@ -18,7 +18,7 @@ class GVAR(base) {
 
     // These will be read from the target class, so for example, the uniformclass with the rolled up sleaves, it should say "Roll Up Sleeves"
     alternativePicture = "";
-    alternativeDisplayName = "";
+    alternativeActionName = "";
 
     duration = 1; // Minimum value: 1 - Anything above will produce a progressbar
 
@@ -27,26 +27,26 @@ class GVAR(base) {
 
 // Common base class for uniforms with sleeves up/down variants
 class GVAR(base_U_sleeves_up): GVAR(base) {
-    alternativeDisplayName = CSTRING(sleevesUp);  
+    alternativeActionName = CSTRING(sleevesUp);  
 };
 class GVAR(base_U_sleeves_down): GVAR(base) {
-    alternativeDisplayName = CSTRING(sleevesDown);
+    alternativeActionName = CSTRING(sleevesDown);
 };
 
 // Common base class for uniforms with gloves on/off variants
 class GVAR(base_U_gloves_on): GVAR(base) {
-    alternativeDisplayName = CSTRING(glovesOn); 
+    alternativeActionName = CSTRING(glovesOn); 
 };
 class GVAR(base_U_gloves_off): GVAR(base) {
-    alternativeDisplayName = CSTRING(glovesOff);
+    alternativeActionName = CSTRING(glovesOff);
 };
 
 // Common base class for uniforms who are open/closed in the front
 class GVAR(base_U_jacket_open): GVAR(base) {
-    alternativeDisplayName = CSTRING(jacketOpen); 
+    alternativeActionName = CSTRING(jacketOpen); 
 };
 class GVAR(base_U_jacket_closed): GVAR(base) {
-    alternativeDisplayName = CSTRING(jacketClose);
+    alternativeActionName = CSTRING(jacketClose);
 };
 
 // Common base class for helmets with a visor that can be flipped up or down
@@ -57,7 +57,7 @@ class GVAR(base_H_visor_up): GVAR(base) {
     sound[] = {CN_SOUND(helmet_visor,05)};
     sound_timing = 0;
 
-    alternativeDisplayName = CSTRING(visorUp);
+    alternativeActionName = CSTRING(visorUp);
 };
 
 class GVAR(base_H_visor_down): GVAR(base) {
@@ -67,28 +67,28 @@ class GVAR(base_H_visor_down): GVAR(base) {
     sound[] = {CN_SOUND(helmet_visor,05)};
     sound_timing = 0;
 
-    alternativeDisplayName = CSTRING(visorDown);
+    alternativeActionName = CSTRING(visorDown);
 };
 
 // Common base class for headgear with goggles that can be used as a facewear item
 class GVAR(base_H_goggles_on): GVAR(base) {
     gesture = "GestureWipeFace";
-    alternativeDisplayName = CSTRING(gogglesOn);
+    alternativeActionName = CSTRING(gogglesOn);
 };
 
 class GVAR(base_H_goggles_off): GVAR(base) {
     gesture = "GestureWipeFace";
-    alternativeDisplayName = CSTRING(gogglesOff);
+    alternativeActionName = CSTRING(gogglesOff);
 };
 
 
 // Common base class for headgear with a mask on/off variant
 class GVAR(base_H_mask_on): GVAR(base) {
     gesture = "GestureWipeFace";
-    alternativeDisplayName = CSTRING(maskOn);
+    alternativeActionName = CSTRING(maskOn);
 };
 
 class GVAR(base_H_mask_off): GVAR(base) {
     gesture = "GestureWipeFace";
-    alternativeDisplayName = CSTRING(maskOff);
+    alternativeActionName = CSTRING(maskOff);
 };
--- addons/wardrobe/functions/fnc_addActionsName.sqf
+++ addons/wardrobe/functions/fnc_addActionsName.sqf
@@ -1,7 +1,7 @@
 #include "../script_component.hpp"
 /*
  * Author: OverlordZorn
- * This function checks if the target item has an directional or alternative display name for the action, if not, it returns the displayName property.
+ * This function checks if the target item has an directional or alternative action name for the action, if not, it returns the displayName property.
  *
  * Arguments:
  * 0: Current variant <CONFIG>
@@ -22,11 +22,11 @@ private _classOrigin = configName _cfgOrigin;
 private _classTarget = configName _cfgTarget;
 
 // Check if there is a "directional Display Name", meaning if there is a specific string when changing from a certain class to a certain class
-private _directional = getText (configFile >> QUOTE(ADDON) >> _classOrigin >> "modifiableTo" >> _classTarget >> "directionalDisplayName");
+private _directional = getText (configFile >> QUOTE(ADDON) >> _classOrigin >> "modifiableTo" >> _classTarget >> "directionalActionName");
 if (_directional isNotEqualTo "") exitWith { _directional };
 
 // Check if there is an "alternative Display Name", defined by the ace_wardrobe base class
-private _alternative = getText (configFile >> QUOTE(ADDON) >> _classTarget >> "alternativeDisplayName");
+private _alternative = getText (configFile >> QUOTE(ADDON) >> _classTarget >> "alternativeActionName");
 if (_alternative isNotEqualTo "") exitWith { _alternative };
 
 // return
