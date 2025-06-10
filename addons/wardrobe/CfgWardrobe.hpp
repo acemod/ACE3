9aec18719a9e5444fa45a743a15a95e9dac42958
OverlordZorn <dennisAzorn@gmail.com>
2025-06-10 15:44:13 +0200

    rename files to common pattern

--- addons/wardrobe/CfgWardrobe.hpp
+++ addons/wardrobe/CfgWardrobe.hpp
@@ -1,5 +1,5 @@
 class ADDON {
-    #include "BaseClasses.hpp"
-    #include "Uniforms.hpp"
-    #include "Facewear.hpp"
+    #include "CfgWardrobe_BaseClasses.hpp"
+    #include "CfgWardrobe_Uniforms.hpp"
+    #include "CfgWardrobe_Facewear.hpp"
 };
