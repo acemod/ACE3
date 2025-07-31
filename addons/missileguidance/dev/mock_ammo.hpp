99740dc82d1883169e1d3d35bf622303880d21aa
PabstMirror <pabstmirror@gmail.com>
2025-07-31 13:49:22 -0500

    Update mock_ammo.hpp

--- addons/missileguidance/dev/mock_ammo.hpp
+++ addons/missileguidance/dev/mock_ammo.hpp
@@ -7,9 +7,12 @@ class GVAR(mock_a_Malyutka): M_Titan_AA_static {
     manualControl = 0;
     thrust = 80;
     maxSpeed = 100;
-    airFriction = 0.5;
-    sideairfriction = 0.14;
+    airFriction = 0.01;
+    sideairfriction = 0.5;
+    maneuvrability = 0;
     class ADDON: GVAR(type_Malyutka) {
+        pitchRate = 5;          // Minium flap deflection for guidance
+        yawRate = 5;            // Maximum flap deflection for guidance
         enabled = 1;
     };
 };
@@ -24,8 +27,9 @@ class GVAR(mock_a_blowpipe): M_Titan_AA_static {
     maxSpeed = 500;
     timeToLive = 15;
     thrustTime = 15;
-    airFriction = 0.5;
-    sideairfriction = 0.14;
+    airFriction = 0.01;
+    sideairfriction = 0.5;
+    maneuvrability = 0;
     class ADDON: GVAR(type_Blowpipe) {
         pitchRate = 5;          // Minium flap deflection for guidance
         yawRate = 5;            // Maximum flap deflection for guidance
