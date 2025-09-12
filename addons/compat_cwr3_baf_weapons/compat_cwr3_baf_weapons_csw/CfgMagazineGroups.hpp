5c4b1f23cf340a5707ae686d4ca941f128d70606
Drofseh <Drofseh@users.noreply.github.com>
2025-09-12 11:49:16 -0700

    add baf csw with deployable L7A2

---
+++ addons/compat_cwr3_baf_weapons/compat_cwr3_baf_weapons_csw/CfgMagazineGroups.hpp
@@ -0,0 +1,125 @@
+class ace_csw_groups {
+    class 150Rnd_762x51_Box {
+        150Rnd_762x51_Box = 1;
+    };
+    class 150Rnd_762x51_Box_Tracer {
+        150Rnd_762x51_Box_Tracer = 1;
+    };
+    class BWA3_120Rnd_762x51 {
+        BWA3_120Rnd_762x51 = 1;
+    };
+    class BWA3_120Rnd_762x51_Tracer {
+        BWA3_120Rnd_762x51_Tracer = 1;
+    };
+    class BWA3_120Rnd_762x51_Tracer_soft {
+        BWA3_120Rnd_762x51_Tracer_soft = 1;
+    };
+    class BWA3_120Rnd_762x51_soft {
+        BWA3_120Rnd_762x51_soft = 1;
+    };
+    class CUP_100Rnd_TE4_LRT4_Green_Tracer_762x51_Belt_M {
+        CUP_100Rnd_TE4_LRT4_Green_Tracer_762x51_Belt_M = 1;
+    };
+    class CUP_100Rnd_TE4_LRT4_Red_Tracer_762x51_Belt_M {
+        CUP_100Rnd_TE4_LRT4_Red_Tracer_762x51_Belt_M = 1;
+    };
+    class CUP_100Rnd_TE4_LRT4_White_Tracer_762x51_Belt_M {
+        CUP_100Rnd_TE4_LRT4_White_Tracer_762x51_Belt_M = 1;
+    };
+    class CUP_100Rnd_TE4_LRT4_Yellow_Tracer_762x51_Belt_M {
+        CUP_100Rnd_TE4_LRT4_Yellow_Tracer_762x51_Belt_M = 1;
+    };
+    class OFrP_AANF1_50Rnd_762x51 {
+        OFrP_AANF1_50Rnd_762x51 = 1;
+    };
+    class OFrP_Minimi_100Rnd_762x51 {
+        OFrP_Minimi_100Rnd_762x51 = 1;
+    };
+    class cwr3_100rnd_m60_wt_m {
+        cwr3_100rnd_m60_wt_m = 1;
+    };
+    class cwr3_120rnd_mg3_wt_m {
+        cwr3_120rnd_mg3_wt_m = 1;
+    };
+    class hlc_100Rnd_762x51_B_M60E4 {
+        hlc_100Rnd_762x51_B_M60E4 = 1;
+    };
+    class hlc_100Rnd_762x51_Barrier_M60E4 {
+        hlc_100Rnd_762x51_Barrier_M60E4 = 1;
+    };
+    class hlc_100Rnd_762x51_M_M60E4 {
+        hlc_100Rnd_762x51_M_M60E4 = 1;
+    };
+    class hlc_100Rnd_762x51_Mdim_M60E4 {
+        hlc_100Rnd_762x51_Mdim_M60E4 = 1;
+    };
+    class hlc_100Rnd_762x51_T_M60E4 {
+        hlc_100Rnd_762x51_T_M60E4 = 1;
+    };
+    class hlc_200Rnd_762x51_B_M60E4 {
+        hlc_200Rnd_762x51_B_M60E4 = 1;
+    };
+    class hlc_200Rnd_762x51_Barrier_M60E4 {
+        hlc_200Rnd_762x51_Barrier_M60E4 = 1;
+    };
+    class hlc_200Rnd_762x51_M_M60E4 {
+        hlc_200Rnd_762x51_M_M60E4 = 1;
+    };
+    class hlc_200Rnd_762x51_Mdim_M60E4 {
+        hlc_200Rnd_762x51_Mdim_M60E4 = 1;
+    };
+    class hlc_200Rnd_762x51_T_M60E4 {
+        hlc_200Rnd_762x51_T_M60E4 = 1;
+    };
+    class sfp_50Rnd_762x51_ksp58 {
+        sfp_50Rnd_762x51_ksp58 = 1;
+    };
+    class sfp_50Rnd_762x51_ksp58_ap {
+        sfp_50Rnd_762x51_ksp58_ap = 1;
+    };
+    class sfp_50Rnd_762x51_ksp58_irTracer {
+        sfp_50Rnd_762x51_ksp58_irTracer = 1;
+    };
+    class sfp_100Rnd_762x51_ksp58 {
+        sfp_100Rnd_762x51_ksp58 = 1;
+    };
+    class sfp_249Rnd_762x51_ksp58 {
+        sfp_249Rnd_762x51_ksp58 = 1;
+    };
+    class sp_fwa_50Rnd_762 {
+        sp_fwa_50Rnd_762 = 1;
+    };
+    class sp_fwa_50Rnd_762_mag {
+        sp_fwa_50Rnd_762_mag = 1;
+    };
+    class sp_fwa_50Rnd_762_mag_ball {
+        sp_fwa_50Rnd_762_mag_ball = 1;
+    };
+    class sp_fwa_50Rnd_762_mag_turret {
+        sp_fwa_50Rnd_762_mag_turret = 1;
+    };
+    class sp_fwa_100Rnd_762 {
+        sp_fwa_100Rnd_762 = 1;
+    };
+    class sp_fwa_100Rnd_762_mag {
+        sp_fwa_100Rnd_762_mag = 1;
+    };
+    class sp_fwa_100Rnd_762_mag_ball {
+        sp_fwa_100Rnd_762_mag_ball = 1;
+    };
+    class sp_fwa_100Rnd_762_mag_turret {
+        sp_fwa_100Rnd_762_mag_turret = 1;
+    };
+    class sp_fwa_200Rnd_762 {
+        sp_fwa_200Rnd_762 = 1;
+    };
+    class sp_fwa_200Rnd_762_mag {
+        sp_fwa_200Rnd_762_mag = 1;
+    };
+    class sp_fwa_200Rnd_762_mag_ball {
+        sp_fwa_200Rnd_762_mag_ball = 1;
+    };
+    class sp_fwa_200Rnd_762_mag_turret {
+        sp_fwa_200Rnd_762_mag_turret = 1;
+    };
+};
