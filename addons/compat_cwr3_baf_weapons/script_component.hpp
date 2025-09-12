344ac0d366285be53d6de8ba7387490e0464c840
Drofseh <Drofseh@users.noreply.github.com>
2025-09-12 11:48:28 -0700

    initial setup for weapons compat
    - just adding file/folder structure

---
+++ addons/compat_cwr3_baf_weapons/script_component.hpp
@@ -0,0 +1,5 @@
+#define COMPONENT compat_cwr3_baf_weapons
+#define COMPONENT_BEAUTIFIED CWR3 BAF Weapons Compatibility
+
+#include "\z\ace\addons\main\script_mod.hpp"
+#include "\z\ace\addons\main\script_macros.hpp"
