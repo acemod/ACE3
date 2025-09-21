89d8974f1cf9280336f6e378a04628a5e40c2e55
jpspider1 <jp.spider1@gmail.com>
2025-09-21 10:58:42 -0400

    ~ Adds: Wirecutter GVAR to SPE Barbed Wire objects

---
+++ addons/compat_spe/CfgVehicles/barbed_wire.hpp
@@ -0,0 +1,17 @@
+	class SPE_fortifications_base;
+	class Land_SPE_BarbedWire_01: SPE_fortifications_base
+	{
+        GVAR(isFence) = 1;
+	};
+	class Land_SPE_BarbedWire_02: SPE_fortifications_base
+	{
+        GVAR(isFence) = 1;
+	};
+	class Land_SPE_BarbedWire_03: SPE_fortifications_base
+	{
+        GVAR(isFence) = 1;
+	};
+	class Land_SPE_BarbedWire_04: SPE_fortifications_base
+	{
+        GVAR(isFence) = 1;
+	};
