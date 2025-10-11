d0f9203209c76351018b140b1144ad24eca322d9
PabstMirror <pabstmirror@gmail.com>
2025-10-10 21:52:35 -0500

    clean rhs compat

---
+++ addons/compat_rhs_afrf3/compat_rhs_afrf3_wardrobe/CfgWardrobe.hpp
@@ -0,0 +1,6 @@
+class ace_wardrobe {
+    class EGVAR(wardrobe,base);
+    class EGVAR(wardrobe,base_H_visor_up);
+    class EGVAR(wardrobe,base_H_visor_down);
+    #include "CfgWardrobe_Headgear.hpp"
+};
