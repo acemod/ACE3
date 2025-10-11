080e5e8ceafc0eeea1e96210fcc15db108bb2182
OverlordZorn <dennisAzorn@gmail.com>
2025-10-11 14:42:29 +0200

    rm import_base_classes

--- addons/compat_rhs_gref3/compat_rhs_gref3_wardrobe/CfgWardrobe.hpp
+++ addons/compat_rhs_gref3/compat_rhs_gref3_wardrobe/CfgWardrobe.hpp
@@ -1,5 +1,17 @@
 class ace_wardrobe {
-    IMPORT_BASE_CLASSES;
+    class ace_wardrobe_base;
+    class ace_wardrobe_base_H_visor_up;
+    class ace_wardrobe_base_H_visor_down;
+    class ace_wardrobe_base_H_goggles_on;
+    class ace_wardrobe_base_H_goggles_off;
+    class ace_wardrobe_base_H_mask_on;
+    class ace_wardrobe_base_H_mask_off;
+    class ace_wardrobe_base_U_sleeves_up;
+    class ace_wardrobe_base_U_sleeves_down;
+    class ace_wardrobe_base_U_gloves_on;
+    class ace_wardrobe_base_U_gloves_off;
+    class ace_wardrobe_base_U_jacket_open;
+    class ace_wardrobe_base_U_jacket_closed;
     
     #include "CfgWardrobe_Headgear.hpp"
 };
