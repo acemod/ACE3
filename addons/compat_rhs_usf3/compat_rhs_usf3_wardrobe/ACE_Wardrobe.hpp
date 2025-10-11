a1d35191e0ae4430295edd18d5ec6d79f5530b62
PabstMirror <pabstmirror@gmail.com>
2025-10-11 09:36:47 -0500

    newline, cleanup unused

--- addons/compat_rhs_usf3/compat_rhs_usf3_wardrobe/ACE_Wardrobe.hpp
+++ addons/compat_rhs_usf3/compat_rhs_usf3_wardrobe/ACE_Wardrobe.hpp
@@ -4,14 +4,6 @@ class ace_wardrobe {
     class ace_wardrobe_base_H_visor_down;
     class ace_wardrobe_base_H_goggles_on;
     class ace_wardrobe_base_H_goggles_off;
-    class ace_wardrobe_base_H_mask_on;
-    class ace_wardrobe_base_H_mask_off;
-    class ace_wardrobe_base_U_sleeves_up;
-    class ace_wardrobe_base_U_sleeves_down;
-    class ace_wardrobe_base_U_gloves_on;
-    class ace_wardrobe_base_U_gloves_off;
-    class ace_wardrobe_base_U_jacket_open;
-    class ace_wardrobe_base_U_jacket_closed;
 
     #include "ACE_Wardrobe_Goggles.hpp"
     #include "ACE_Wardrobe_Headgear.hpp"
