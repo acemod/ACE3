2414e431a5fe9033cd682f22b7347342b79d5b4b
PabstMirror <pabstmirror@gmail.com>
2025-10-18 22:33:27 -0500

    General - Cleanup CfgPatches `units[]` and `weapons[]`

--- addons/missionmodules/config.cpp
+++ addons/missionmodules/config.cpp
@@ -3,7 +3,7 @@
 class CfgPatches {
     class ADDON {
         name = COMPONENT_NAME;
-        units[] = {};
+        units[] = {"ACE_moduleAmbianceSound"};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {"ace_common"};
