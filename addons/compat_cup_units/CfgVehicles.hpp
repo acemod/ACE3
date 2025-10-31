f313a5e388b2bd5fc2fca8e57d473d68b7b11b6d
Andx <andx667@protonmail.com>
2025-10-31 17:14:21 +0100

    initial commit

--- addons/compat_cup_units/CfgVehicles.hpp
+++ addons/compat_cup_units/CfgVehicles.hpp
@@ -6,4 +6,77 @@ class CfgVehicles {
     class CUP_B_RUS_Patrol_bag_6E5_Summer: CUP_B_RUS_Patrol_bag_base {
          EGVAR(trenches,entrenchingTool) = 1;
     };
+
+    //Fire Protection
+    class ItemCore;
+
+    //Pilot and Tanker Coveralls
+    class CUP_U_B_CZ_Pilot_WDL: ItemCore {
+        ace_fire_protection = 0.5;
+    };
+    class CUP_U_B_GER_Fleck_Overalls_Pilot: ItemCore {
+        ace_fire_protection = 0.5;
+    };
+    class CUP_U_B_GER_Fleck_Overalls_Tank: ItemCore {
+        ace_fire_protection = 0.5;
+    };
+    class CUP_U_B_GER_Overalls_Tank: ItemCore {
+        ace_fire_protection = 0.5;
+    };
+    class CUP_U_B_USArmy_PilotOverall: ItemCore {
+        ace_fire_protection = 0.5;
+    };
+    class CUP_U_B_USMC_PilotOverall: ItemCore {
+        ace_fire_protection = 0.5;
+    };
+    class CUP_U_I_RACS_PilotOverall: ItemCore {
+        ace_fire_protection = 0.5;
+    };
+    class CUP_U_O_SLA_Overalls_Pilot: ItemCore {
+        ace_fire_protection = 0.5;
+    };
+    class CUP_U_O_SLA_Overalls_Tank: ItemCore {
+        ace_fire_protection = 0.5;
+    };
+
+    //USMC FROG https://en.wikipedia.org/wiki/Flame_Resistant_Organizational_Gear
+    class CUP_U_B_USMC_FROG1_DMARPAT: ItemCore {
+        ace_fire_protection = 0.2;
+    };
+    class CUP_U_B_USMC_FROG1_WMARPAT: ItemCore {
+        ace_fire_protection = 0.2;
+    };
+    class CUP_U_B_USMC_FROG2_DMARPAT: ItemCore {
+        ace_fire_protection = 0.2;
+    };
+    class CUP_U_B_USMC_FROG2_WMARPAT: ItemCore {
+        ace_fire_protection = 0.2;
+    };
+    class CUP_U_B_USMC_FROG3_DMARPAT: ItemCore {
+        ace_fire_protection = 0.2;
+    };
+    class CUP_U_B_USMC_FROG3_WMARPAT: ItemCore {
+        ace_fire_protection = 0.2;
+    };
+    class CUP_U_B_USMC_FROG4_DMARPAT: ItemCore {
+        ace_fire_protection = 0.2;
+    };
+    class CUP_U_B_USMC_FROG4_WMARPAT: ItemCore {
+        ace_fire_protection = 0.2;
+    };
+
+    //Civilian Clothes
+    class CUP_U_C_Fireman_01: ItemCore {
+        ace_fire_protection = 0.7;
+    };
+    class CUP_U_C_AirMedic_orange_01: ItemCore {
+        ace_fire_protection = 0.5;
+    };
+    class CUP_U_C_AirMedic_red_01: ItemCore {
+        ace_fire_protection = 0.5;
+    };
+    class CUP_U_C_AirMedic_yellow_01: ItemCore {
+        ace_fire_protection = 0.5;
+    };
 };
+
