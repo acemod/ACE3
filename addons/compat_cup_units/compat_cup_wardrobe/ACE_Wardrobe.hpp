467f1d86b91cb00ab5c1e222c0e83dbdf348eaf0
PabstMirror <pabstmirror@gmail.com>
2025-11-04 17:23:02 -0600

    Cleanup stringtables and unused

--- addons/compat_cup_units/compat_cup_wardrobe/ACE_Wardrobe.hpp
+++ addons/compat_cup_units/compat_cup_wardrobe/ACE_Wardrobe.hpp
@@ -4,14 +4,10 @@ class ace_wardrobe {
     class ace_wardrobe_base_H_visor_down;
     class ace_wardrobe_base_H_goggles_on;
     class ace_wardrobe_base_H_goggles_off;
-    class ace_wardrobe_base_H_mask_on;
-    class ace_wardrobe_base_H_mask_off;
     class ace_wardrobe_base_U_sleeves_up;
     class ace_wardrobe_base_U_sleeves_down;
     class ace_wardrobe_base_U_gloves_on;
     class ace_wardrobe_base_U_gloves_off;
-    class ace_wardrobe_base_U_jacket_open;
-    class ace_wardrobe_base_U_jacket_closed;
     class ace_wardrobe_base_U_hood_raised;
     class ace_wardrobe_base_U_hood_lowered;
 
