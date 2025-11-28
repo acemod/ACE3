a9a524f616344e2b38ac9b5530cd55a1c99baf52
Esmeray/Sky <22858781+Esmeray6@users.noreply.github.com>
2025-11-29 03:45:23 +0500

    Base patch files for compat_sogns

---
+++ addons/compat_sogns/config.cpp
@@ -0,0 +1,18 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"ace_common", "loadorder_f_vietnam"};
+        skipWhenMissingDependencies = 1;
+        author = ECSTRING(common,ACETeam);
+        authors[] = {"Sky"};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgWeapons.hpp"
