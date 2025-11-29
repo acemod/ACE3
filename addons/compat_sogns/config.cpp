641a04e9af7bd8017aab60e55578e731810971d5
Esmeray/Sky <22858781+Esmeray6@users.noreply.github.com>
2025-11-30 04:32:07 +0500

    ACE Compat SOGNS - Combine all SOGNS required addons into one

--- addons/compat_sogns/config.cpp
+++ addons/compat_sogns/config.cpp
@@ -8,14 +8,10 @@ class CfgPatches {
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {
             "ace_common",
-            "ace_main",
-            "loadorder_f_vietnam",
-            "characters_f_vietnam_05_c",
-            "air_f_vietnam_06_c",
-            "air_f_vietnam_04_c",
-            "armor_f_vietnam_05_c"
+            "loadorder_f_vietnam", // S.O.G. Prairie Fire
+            "data_f_vietnam_03" // S.O.G. Nickel Steel
         };
-        // skipWhenMissingDependencies = 1;
+        skipWhenMissingDependencies = 1;
         author = ECSTRING(common,ACETeam);
         authors[] = {"Sky"};
         url = ECSTRING(main,URL);
