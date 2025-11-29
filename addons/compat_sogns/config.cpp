259e98096820d0f1bf36938d43f2b1b1490747e8
Esmeray/Sky <22858781+Esmeray6@users.noreply.github.com>
2025-11-29 22:27:15 +0500

    Compat SOGNS - Add required addons for all patched weapon/vehicle

--- addons/compat_sogns/config.cpp
+++ addons/compat_sogns/config.cpp
@@ -6,8 +6,16 @@ class CfgPatches {
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"ace_common", "loadorder_f_vietnam"};
-        skipWhenMissingDependencies = 1;
+        requiredAddons[] = {
+            "ace_common",
+            "ace_main",
+            "loadorder_f_vietnam",
+            "characters_f_vietnam_05_c",
+            "air_f_vietnam_06_c",
+            "air_f_vietnam_04_c",
+            "armor_f_vietnam_05_c"
+        };
+        // skipWhenMissingDependencies = 1;
         author = ECSTRING(common,ACETeam);
         authors[] = {"Sky"};
         url = ECSTRING(main,URL);
