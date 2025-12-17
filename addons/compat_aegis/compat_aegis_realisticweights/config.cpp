35df12b6800c0fe9f0265e60252e63aa76014323
PoToo <70027258+SirPotoo@users.noreply.github.com>
2025-12-17 19:55:44 +0100

    Refactor CfgPatches structure in config.cpp

--- addons/compat_aegis/compat_aegis_realisticweights/config.cpp
+++ addons/compat_aegis/compat_aegis_realisticweights/config.cpp
@@ -1,26 +1,22 @@
-class CfgPatches
-{
-	class compat_aegis_realisticweights
-	{
-		name="ACE3 - Realistic Weights Aegis Patch";
-		units[]={};
-		weapons[]={};
-		requiredVersion=2.2;
-		requiredAddons[]=
-		{
+#include "script_component.hpp"
+
+class CfgPatches {
+	class SUBADDON {
+		name = COMPONENT NAME;
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = REQUIRED_VERSION;
+		requiredAddons[] = {
 			"ace_realisticweights",
 			"A3_Aegis_Weapons_F_Aegis"
 		};
 		skipWhenMissingDependencies = 1;
-		author="$STR_ace_common_ACETeam";
-		authors[]=
-		{
-			"PoToo"
-		};
-		url="$STR_ace_main_URL";
-		version=3.2;
-		versionStr="3.20.0.104";
-		versionAr[]={3,20,0,104};
+		author= ECSTRING(common,ACETeam);
+		authors[] = {};
+		url = ECSTRING(main,URL);
+		VERSION_CONFIG;
+
+        addonRootClass = QUOTE(ADDON);
 	};
 };
 
