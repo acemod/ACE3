2a46e59cab9d8f9d47d7620857f2d18b2df854ab
PabstMirror <pabstmirror@gmail.com>
2025-12-19 13:50:10 -0600

    remove tabs

--- addons/compat_aegis/compat_aegis_realisticweights/config.cpp
+++ addons/compat_aegis/compat_aegis_realisticweights/config.cpp
@@ -1,25 +1,23 @@
 #include "script_component.hpp"
 
 class CfgPatches {
-	class SUBADDON {
-		name = "ACE3 - Aegis Compatibility - Realistic Weights";
-		units[] = {};
-		weapons[] = {};
-		requiredVersion = REQUIRED_VERSION;
-		requiredAddons[] = {
-			"ace_realisticweights",
-			"A3_Aegis_Weapons_F_Aegis"
-		};
-		skipWhenMissingDependencies = 1;
-		author= ECSTRING(common,ACETeam);
-		authors[] = {"PoToo"};
-		url = ECSTRING(main,URL);
-		VERSION_CONFIG;
+    class SUBADDON {
+        name = "ACE3 - Aegis Compatibility - Realistic Weights";
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "ace_realisticweights",
+            "A3_Aegis_Weapons_F_Aegis"
+        };
+        skipWhenMissingDependencies = 1;
+        author= ECSTRING(common,ACETeam);
+        authors[] = {"PoToo"};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
 
         addonRootClass = QUOTE(ADDON);
-	};
+    };
 };
 
 #include "CfgWeapons.hpp"
-
-
