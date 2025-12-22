2b96b55995080a7ab49d3a2c98cef80d8feefc8b
OverlordZorn <dennisAzorn@gmail.com>
2025-12-22 18:58:40 +0100

    donate wardrobe vnx configs

---
+++ addons/compat_sogns/compat_sogns_wardrobe/ace_wardrobe.hpp
@@ -0,0 +1,12 @@
+#include "\z\ace\addons\wardrobe\script_macros_wardrobe.hpp"
+
+class ace_wardrobe {
+    class ace_wardrobe_base;
+    class ace_wardrobe_base_U_sleeves_up;
+    class ace_wardrobe_base_U_sleeves_down;
+    class ace_wardrobe_base_H_visor_up;
+    class ace_wardrobe_base_H_visor_down;
+
+    #include "ace_wardrobe_uniforms.hpp"
+    #include "ace_wardrobe_headgear.hpp"
+};
