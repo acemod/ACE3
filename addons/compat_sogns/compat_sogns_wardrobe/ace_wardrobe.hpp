265f0f22dc9847fc7e62f4770c039eb92cd63b3e
OverlordZorn <dennisAzorn@gmail.com>
2025-12-22 21:07:41 +0100

    rm double ref of macro file

--- addons/compat_sogns/compat_sogns_wardrobe/ace_wardrobe.hpp
+++ addons/compat_sogns/compat_sogns_wardrobe/ace_wardrobe.hpp
@@ -1,7 +1,4 @@
-#include "\z\ace\addons\wardrobe\script_macros_wardrobe.hpp"
-
 class ace_wardrobe {
-    class ace_wardrobe_base;
     class ace_wardrobe_base_U_sleeves_up;
     class ace_wardrobe_base_U_sleeves_down;
     class ace_wardrobe_base_H_visor_up;
