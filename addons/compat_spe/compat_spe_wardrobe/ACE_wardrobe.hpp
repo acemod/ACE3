d4c4b92515b1da69f3c279672dd4da42ab9ffb8c
OverlordZorn <dennisAzorn@gmail.com>
2025-12-22 21:09:51 +0100

    rm unused base classes

--- addons/compat_spe/compat_spe_wardrobe/ACE_wardrobe.hpp
+++ addons/compat_spe/compat_spe_wardrobe/ACE_wardrobe.hpp
@@ -4,14 +4,8 @@ class ace_wardrobe {
     class ace_wardrobe_base_U_sleeves_down;
     class ace_wardrobe_base_U_gloves_on;
     class ace_wardrobe_base_U_gloves_off;
-    class ace_wardrobe_base_U_jacket_open;
-    class ace_wardrobe_base_U_jacket_closed;
-    class ace_wardrobe_base_H_visor_up;
-    class ace_wardrobe_base_H_visor_down;
     class ace_wardrobe_base_H_goggles_on;
     class ace_wardrobe_base_H_goggles_off;
-    class ace_wardrobe_base_H_mask_on;
-    class ace_wardrobe_base_H_mask_off;
 
     #include "ACE_wardrobe_uniforms.hpp"
     #include "ACE_wardrobe_headgear.hpp"
