62924b581a77c6e7f9ad5e692f00d87852f94ae1
OverlordZorn <dennisAzorn@gmail.com>
2025-12-22 19:34:19 +0100

    init

---
+++ addons/compat_spe/compat_spe_wardrobe/ACE_wardrobe.hpp
@@ -0,0 +1,21 @@
+#include "\z\ace\addons\wardrobe\script_macros_wardrobe.hpp"
+
+class ace_wardrobe {
+    class ace_wardrobe_base;
+    class ace_wardrobe_base_U_sleeves_up;
+    class ace_wardrobe_base_U_sleeves_down;
+    class ace_wardrobe_base_U_gloves_on;
+    class ace_wardrobe_base_U_gloves_off;
+    class ace_wardrobe_base_U_jacket_open;
+    class ace_wardrobe_base_U_jacket_closed;
+    class ace_wardrobe_base_H_visor_up;
+    class ace_wardrobe_base_H_visor_down;
+    class ace_wardrobe_base_H_goggles_on;
+    class ace_wardrobe_base_H_goggles_off;
+    class ace_wardrobe_base_H_mask_on;
+    class ace_wardrobe_base_H_mask_off;
+
+    #include "ace_wardrobe_uniforms.hpp"
+    #include "ace_wardrobe_headgear.hpp"
+    #include "ace_wardrobe_goggles.hpp"
+};
---
+++ addons/compat_spe/compat_spe_wardrobe/config.cpp
@@ -0,0 +1,23 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class SUBADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "ace_wardrobe",
+            "WW2_SPE_Core_c_Core_c"
+        };
+        skipWhenMissingDependencies = 1;
+        author = ECSTRING(common,ACETeam);
+        authors[] = {"OverlordZorn [CVO]"};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+        addonRootClass = QUOTE(ADDON);
+    };
+};
+
+// ACE Wardrobe
+#include "ACE_Wardrobe.hpp"
