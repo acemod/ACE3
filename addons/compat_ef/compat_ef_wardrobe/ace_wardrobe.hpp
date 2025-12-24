b0d718a5fd36ed47346a3e1af354be9e3b852603
OverlordZorn <dennisAzorn@gmail.com>
2025-12-24 11:03:14 +0100

    donate

---
+++ addons/compat_ef/compat_ef_wardrobe/ace_wardrobe.hpp
@@ -0,0 +1,9 @@
+class ace_wardrobe {
+    class ace_wardrobe_base;
+    class ace_wardrobe_base_U_sleeves_up;
+    class ace_wardrobe_base_U_sleeves_down;
+    class ace_wardrobe_base_U_gloves_on;
+
+    #include "ace_wardrobe_uniforms.hpp"
+    #include "ace_wardrobe_headgear.hpp"
+};
