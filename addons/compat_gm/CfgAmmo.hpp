b3ffa2903166af35a27e74ea6ae8a3a458c1971f
Redzitus <79668535+Redzitus@users.noreply.github.com>
2026-01-31 13:52:46 -0300

    Refactor bullet properties in CfgAmmo.hpp

--- addons/compat_gm/CfgAmmo.hpp
+++ addons/compat_gm/CfgAmmo.hpp
@@ -42,82 +42,90 @@ class CfgAmmo
   };
 	class gm_bullet_545x39mm_base: gm_bullet_base
 	{
-  	ACE_ammoTempMuzzleVelocityShifts[] = {-26.55,-25.47,-22.85,-20.12,-16.98,-12.8,-7.64,-1.53,5.96,15.17,26.19};
-  	ACE_ballisticCoefficients[] = {0.168};
-	  ACE_barrelLengths[] = {254,414.02,508};
-  	ACE_bulletLength = 21.59;
-  	ACE_bulletMass = 3.42792;
-  	ACE_caliber = 5.588;
-  	ACE_dragModel = 7;
-  	ACE_muzzleVelocities[] = {780,880,920};
-  	ACE_standardAtmosphere = "ASM";
+  	    ACE_ammoTempMuzzleVelocityShifts[] = {-26.55,-25.47,-22.85,-20.12,-16.98,-12.8,-7.64,-1.53,5.96,15.17,26.19};
+  	    ACE_ballisticCoefficients[] = {0.168};
+        ACE_barrelLengths[] = {254,414.02,508};
+      	ACE_bulletLength = 21.59;
+  	    ACE_bulletMass = 3.42792;
+      	ACE_caliber = 5.588;
+      	ACE_dragModel = 7;
+  	    ACE_muzzleVelocities[] = {780,880,920};
+  	    ACE_standardAtmosphere = "ASM";
 	};
 	class gm_bullet_762x54mmR_base: gm_bullet_base
 	{
-  	ACE_ammoTempMuzzleVelocityShifts[] = {-26.55,-25.47,-22.85,-20.12,-16.98,-12.8,-7.64,-1.53,5.96,15.17,26.19};
-  	ACE_ballisticCoefficients[] = {0.411};
-	  ACE_barrelLengths[] = {406.4,508,604.5,736.6};
-	  ACE_bulletLength = 28.956;
-  	ACE_bulletMass = 9.7975952;
-	  ACE_caliber = 7.925;
-	  ACE_dragModel = 1;
-	  ACE_muzzleVelocities[] = {735,770,809,838};
-	  ACE_standardAtmosphere = "ICAO";
+      	ACE_ammoTempMuzzleVelocityShifts[] = {-26.55,-25.47,-22.85,-20.12,-16.98,-12.8,-7.64,-1.53,5.96,15.17,26.19};
+      	ACE_ballisticCoefficients[] = {0.411};
+    	ACE_barrelLengths[] = {406.4,508,604.5,736.6};
+    	ACE_bulletLength = 28.956;
+      	ACE_bulletMass = 9.7975952;
+    	ACE_caliber = 7.925;
+    	ACE_dragModel = 1;
+    	ACE_muzzleVelocities[] = {735,770,809,838};
+    	ACE_standardAtmosphere = "ICAO";
 	};
 	class gm_bullet_762x54mmR_B_T_7t2: gm_bullet_762x54mmR_base
 	{
-	  ACE_ballisticCoefficients[] = {0.395};
-    ACE_bulletMass = 9.6552;
+	    ACE_ballisticCoefficients[] = {0.395};
+        ACE_bulletMass = 9.6552;
 	};
 	class gm_bullet_762x54mmR_API_7bz3: gm_bullet_762x54mmR_base
 	{
-	  ACE_ballisticCoefficients[] = {0.401};
-	  ACE_bulletMass = 10.387265;
+	    ACE_ballisticCoefficients[] = {0.401};
+	    ACE_bulletMass = 10.387265;
 	};
 	class gm_bullet_762x54mmR_B_57N323S: gm_bullet_762x54mmR_base
 	{
-	  ACE_ballisticCoefficients[] = {0.374};
-	  ACE_bulletMass = 9.6031985;
+	    ACE_ballisticCoefficients[] = {0.374};
+	    ACE_bulletMass = 9.6031985;
 	};
 	class gm_bullet_762x51mm_base: gm_bullet_base
 	{
-  	ACE_ammoTempMuzzleVelocityShifts[] = {-26.55,-25.47,-22.85,-20.12,-16.98,-12.8,-7.64,-1.53,5.96,15.17,26.19};
-	  ACE_ballisticCoefficients[] = {0.2};
-  	ACE_barrelLengths[] = {419.1,431.8,457.2,482.6,508,533.4,558.8,584.2,609.6,635,660.4,685.8,711.2};
-  	ACE_bulletLength = 26.46;
-  	ACE_bulletMass = 9.52544;
-  	ACE_caliber = 7.823;
-  	ACE_dragModel = 7;
-  	ACE_muzzleVelocities[] = {780.593,785.47,790.956,802.843,816.559,817.778,828.446,836.371,843.077,843.991,853.745,853.44,860.45};
-  	ACE_standardAtmosphere = "ICAO";
+  	    ACE_ammoTempMuzzleVelocityShifts[] = {-26.55,-25.47,-22.85,-20.12,-16.98,-12.8,-7.64,-1.53,5.96,15.17,26.19};
+	    ACE_ballisticCoefficients[] = {0.2};
+  	    ACE_barrelLengths[] = {419.1,431.8,457.2,482.6,508,533.4,558.8,584.2,609.6,635,660.4,685.8,711.2};
+  	    ACE_bulletLength = 26.46;
+  	    ACE_bulletMass = 9.52544;
+  	    ACE_caliber = 7.823;
+  	    ACE_dragModel = 7;
+  	    ACE_muzzleVelocities[] = {780.593,785.47,790.956,802.843,816.559,817.778,828.446,836.371,843.077,843.991,853.745,853.44,860.45};
+  	    ACE_standardAtmosphere = "ICAO";
 	};
   	class gm_bullet_762x51mm_AP_DM151: gm_bullet_762x51mm_base
 	{
-  	ACE_bulletMass = 9.78464;
+  	    ACE_bulletMass = 9.78464;
 	};
     class gm_bullet_9x18mm_base;
-	  class gm_bullet_9x18mm_B_pst: gm_bullet_9x18mm_base
+	class gm_bullet_9x18mm_B_pst: gm_bullet_9x18mm_base
 	{
-  	ACE_ammoTempMuzzleVelocityShifts[] = {-2.655,-2.547,-2.285,-2.012,-1.698,-1.28,-0.764,-0.153,0.596,1.517,2.619};
-  	ACE_ballisticCoefficients[] = {0.135};
-  	ACE_barrelLengths[] = {50.8,76.2,101.6,127,152.4,177.8,203.2,228.6,254,279.4,304.8,330.2,355.6,381,406.4,431.8,457.2};
-  	ACE_bulletLength = 10.541;
-  	ACE_bulletMass = 6.1559;
-  	ACE_caliber = 9.27;
-  	ACE_dragModel = 1;
-  	ACE_muzzleVelocities[] = {253.289,271.272,294.132,296.57,301.142,313.334,317.297,321.259,324.002,325.526,329.489,327.965,337.109,335.28,338.938,329.794,327.66};
-  	ACE_standardAtmosphere = "ICAO";
+  	    ACE_ammoTempMuzzleVelocityShifts[] = {-2.655,-2.547,-2.285,-2.012,-1.698,-1.28,-0.764,-0.153,0.596,1.517,2.619};
+  	    ACE_ballisticCoefficients[] = {0.135};
+  	    ACE_barrelLengths[] = {50.8,76.2,101.6,127,152.4,177.8,203.2,228.6,254,279.4,304.8,330.2,355.6,381,406.4,431.8,457.2};
+  	    ACE_bulletLength = 10.541;
+  	    ACE_bulletMass = 6.1559;
+  	    ACE_caliber = 9.27;
+    	ACE_dragModel = 1;
+      	ACE_muzzleVelocities[] = {253.289,271.272,294.132,296.57,301.142,313.334,317.297,321.259,324.002,325.526,329.489,327.965,337.109,335.28,338.938,329.794,327.66};
+  	    ACE_standardAtmosphere = "ICAO";
 	};
 	class gm_bullet_9x19mm_base: gm_bullet_base
 	{
-  	ACE_ammoTempMuzzleVelocityShifts[] = {-2.655,-2.547,-2.285,-2.012,-1.698,-1.28,-0.764,-0.153,0.596,1.517,2.619};
-  	ACE_ballisticCoefficients[] = {0.165};
-  	ACE_barrelLengths[] = {101.6,127,228.6};
-  	ACE_bulletLength = 15.494;
-  	ACE_bulletMass = 8.0352;
-  	ACE_caliber = 9.017;
-  	ACE_dragModel = 1;
-   	ACE_muzzleVelocities[] = {340,370,400};
-    ACE_standardAtmosphere = "ASM";
+  	    ACE_ammoTempMuzzleVelocityShifts[] = {-2.655,-2.547,-2.285,-2.012,-1.698,-1.28,-0.764,-0.153,0.596,1.517,2.619};
+      	ACE_ballisticCoefficients[] = {0.165};
+      	ACE_barrelLengths[] = {101.6,127,228.6};
+      	ACE_bulletLength = 15.494;
+      	ACE_bulletMass = 8.0352;
+      	ACE_caliber = 9.017;
+      	ACE_dragModel = 1;
+       	ACE_muzzleVelocities[] = {340,370,400};
+        ACE_standardAtmosphere = "ASM";
 	};
+    class gm_bullet_9x19mm_AP_dm91: gm_bullet_9x19mm_base {
+        ACE_bulletMass = 5.7023;
+       	ACE_muzzleVelocities[] = {410,440,480};
+    };
+        gm_bullet_9x19mm_BSD_dm81: gm_bullet_9x19mm_base {
+        ACE_bulletMass = 9.52544;
+        ACE_muzzleVelocities[] = {220,250,290};
+    };
 };
