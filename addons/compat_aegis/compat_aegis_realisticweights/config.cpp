8343496648db0ed9d7533bd9d24c71775d4fb0c9
PabstMirror <pabstmirror@gmail.com>
2026-02-01 17:22:47 -0600

    use COMPONENT_NAME macro

--- addons/compat_aegis/compat_aegis_realisticweights/config.cpp
+++ addons/compat_aegis/compat_aegis_realisticweights/config.cpp
@@ -2,7 +2,7 @@
 
 class CfgPatches {
     class SUBADDON {
-        name = "ACE3 - Aegis Compatibility - Realistic Weights";
+        name = COMPONENT_NAME;
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
