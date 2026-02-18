ad53d90caf806a46ade1baf822fa8878bcc4b93f
OverlordZorn <dennisAzorn@gmail.com>
2026-02-18 19:40:36 +0100

    cleanup

--- addons/tagging/config.cpp
+++ addons/tagging/config.cpp
@@ -3,9 +3,7 @@
 class CfgPatches {
     class ADDON {
         name = COMPONENT_NAME;
-        units[] = {
-            "ACE_Item_SpraypaintBlack", "ACE_Item_SpraypaintRed", "ACE_Item_SpraypaintGreen", "ACE_Item_SpraypaintBlue", "ACE_Item_SpraypaintYellow", "ACE_Item_SpraypaintWhite"
-        };
+        units[] = {"ACE_Item_SpraypaintBlack", "ACE_Item_SpraypaintRed", "ACE_Item_SpraypaintGreen", "ACE_Item_SpraypaintBlue", "ACE_Item_SpraypaintYellow", "ACE_Item_SpraypaintWhite"};
         weapons[] = {
             "ACE_SpraypaintBlack", "ACE_SpraypaintRed", "ACE_SpraypaintGreen", "ACE_SpraypaintBlue", "ACE_SpraypaintYellow", "ACE_SpraypaintWhite",
             "ACE_ChalkBlack", "ACE_ChalkRed", "ACE_ChalkGreen", "ACE_ChalkBlue", "ACE_ChalkYellow", "ACE_ChalkWhite"
