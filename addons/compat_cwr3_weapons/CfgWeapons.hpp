122c568220df48390e5a99d7b9621c151afd26d0
Drofseh <Drofseh@users.noreply.github.com>
2026-02-21 16:22:53 -0800

    Add scope support

--- addons/compat_cwr3_weapons/CfgWeapons.hpp
+++ addons/compat_cwr3_weapons/CfgWeapons.hpp
@@ -1,2 +1,28 @@
 class CfgWeapons {
+
+    class ItemCore;
+    class optic_MRCO;
+    class CUP_optic_AN_PVS_4;
+
+    class cwr3_optic_xms: ItemCore {
+        ACE_ScopeHeightAboveRail = 3.09904;
+    };
+
+    class cwr3_optic_xms_dot: optic_MRCO {
+        ACE_ScopeHeightAboveRail = 3.10227;
+    };
+
+    class cwr3_optic_xms_cross: optic_MRCO {
+        ACE_ScopeHeightAboveRail = 3.10227;
+    };
+
+    class cwr3_optic_suit: ItemCore {
+        ACE_ScopeHeightAboveRail = 3.40244;
+    };
+
+    class cwr3_optic_iws: CUP_optic_AN_PVS_4 {
+        ACE_ScopeHeightAboveRail = 8.34381;
+    };
+
+
 };
