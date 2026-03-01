b3d682b93db44e7432b0409e6b4ec74f75f457be
Drofseh <Drofseh@users.noreply.github.com>
2026-03-01 00:29:26 -0800

    COMPAT CUP - add compat for ACE Tracers (#11302)
    Co-authored-by: PabstMirror <pabstmirror@gmail.com>

---
+++ addons/compat_cup_weapons/compat_cup_weapons_tracers/CfgAmmo.hpp
@@ -0,0 +1,174 @@
+class CfgAmmo {
+    class BulletBase;
+    class B_127x108_Ball;
+
+    class ShellBase;
+    class Sh_120mm_APFSDS;
+
+    class CUP_B_545x39_Ball: BulletBase {model = QPATHTOEF(tracers,ace_TracerWhite2.p3d);};
+    class CUP_B_545x39_Ball_Subsonic: CUP_B_545x39_Ball {model = QPATHTOEF(tracers,ace_TracerWhite2.p3d);};
+    class CUP_B_545x39_Ball_Tracer_Green: CUP_B_545x39_Ball {model = QPATHTOEF(tracers,ace_TracerGreen2.p3d);};
+    class CUP_B_545x39_Ball_Tracer_Red: CUP_B_545x39_Ball {model = QPATHTOEF(tracers,ace_TracerRed2.p3d);};
+    class CUP_B_545x39_Ball_Tracer_White: CUP_B_545x39_Ball {model = QPATHTOEF(tracers,ace_TracerWhite2.p3d);};
+    class CUP_B_545x39_Ball_Tracer_Yellow: CUP_B_545x39_Ball {model = QPATHTOEF(tracers,ace_TracerYellow2.p3d);};
+    class CUP_B_762x39_Ball: BulletBase {model = QPATHTOEF(tracers,ace_TracerWhite2.p3d);};
+    class CUP_B_762x39_Ball_Tracer_Green: CUP_B_762x39_Ball {model = QPATHTOEF(tracers,ace_TracerGreen2.p3d);};
+    class CUP_B_762x39_Ball_Tracer_Red: CUP_B_762x39_Ball {model = QPATHTOEF(tracers,ace_TracerRed2.p3d);};
+    class CUP_B_762x39_Ball_Tracer_Yellow: CUP_B_762x39_Ball {model = QPATHTOEF(tracers,ace_TracerYellow2.p3d);};
+
+    class CUP_B_762x51_noTracer;
+    class CUP_B_762x51_Tracer_Green: CUP_B_762x51_noTracer {model = QPATHTOEF(tracers,ace_TracerGreen2.p3d);};
+    class CUP_B_762x51_Tracer_Red: CUP_B_762x51_noTracer {model = QPATHTOEF(tracers,ace_TracerRed2.p3d);};
+    class CUP_B_762x51_Tracer_Yellow: CUP_B_762x51_noTracer {model = QPATHTOEF(tracers,ace_TracerYellow2.p3d);};
+    class CUP_B_762x51_Tracer_White: CUP_B_762x51_noTracer {model = QPATHTOEF(tracers,ace_TracerWhite2.p3d);};
+    class CUP_B_762x51_Red_Tracer_3RndBurst: CUP_B_762x51_noTracer {model = QPATHTOEF(tracers,ace_TracerRed2.p3d);};
+
+    class CUP_B_762x51_Tracer_Red_Splash;
+    class CUP_B_762x51_Tracer_White_Splash: CUP_B_762x51_Tracer_Red_Splash {model = QPATHTOEF(tracers,ace_TracerWhite2.p3d);};
+
+    class CUP_B_762x54_Ball_White_Tracer: BulletBase {model = QPATHTOEF(tracers,ace_TracerWhite2.p3d);};
+    class CUP_B_762x54_Ball_Red_Tracer: CUP_B_762x54_Ball_White_Tracer {model = QPATHTOEF(tracers,ace_TracerRed2.p3d);};
+    class CUP_B_762x54_Ball_Green_Tracer: CUP_B_762x54_Ball_White_Tracer {model = QPATHTOEF(tracers,ace_TracerGreen2.p3d);};
+    class CUP_B_762x54_Ball_Yellow_Tracer: CUP_B_762x54_Ball_White_Tracer {model = QPATHTOEF(tracers,ace_TracerYellow2.p3d);};
+
+    class CUP_B_86x70_Ball_noTracer: BulletBase {model = QPATHTOEF(tracers,ace_TracerRed2.p3d);};
+
+    class CUP_B_762x67_noTracer_Ball;
+    class CUP_B_762x67_Ball_Tracer_Red: CUP_B_762x67_noTracer_Ball {model = QPATHTOEF(tracers,ace_TracerRed2.p3d);};
+
+    class B_762x51_Ball;
+    class CUP_680x43_Ball_Tracer_Red: B_762x51_Ball {model = QPATHTOEF(tracers,ace_TracerRed2.p3d);};
+    class CUP_680x43_Ball_Tracer_Green: CUP_680x43_Ball_Tracer_Red {model = QPATHTOEF(tracers,ace_TracerGreen2.p3d);};
+    class CUP_680x43_Ball_Tracer_Yellow: CUP_680x43_Ball_Tracer_Red {model = QPATHTOEF(tracers,ace_TracerYellow2.p3d);};
+    class CUP_680x43_Ball_Tracer_White: CUP_680x43_Ball_Tracer_Red {model = QPATHTOEF(tracers,ace_TracerWhite2.p3d);};
+
+    class CUP_B_9x18_Ball: BulletBase {model = QPATHTOEF(tracers,ace_TracerWhite2.p3d);};
+    class CUP_B_9x18_SD: CUP_B_9x18_Ball {model = QPATHTOEF(tracers,ace_TracerGreen2.p3d);};
+    class CUP_B_9x18_Ball_Tracer_Green: CUP_B_9x18_Ball {model = QPATHTOEF(tracers,ace_TracerGreen2.p3d);};
+    class CUP_B_9x18_Ball_Tracer_Red: CUP_B_9x18_Ball {model = QPATHTOEF(tracers,ace_TracerRed2.p3d);};
+    class CUP_B_9x18_Ball_Tracer_Yellow: CUP_B_9x18_Ball {model = QPATHTOEF(tracers,ace_TracerYellow2.p3d);};
+    class CUP_B_9x18_Ball_White_Tracer: CUP_B_9x18_Ball {model = QPATHTOEF(tracers,ace_TracerWhite2.p3d);};
+
+    class CUP_B_9x19_Ball: BulletBase {model = QPATHTOEF(tracers,ace_TracerWhite2.p3d);};
+    class CUP_B_9x19_Ball_Tracer_Green: CUP_B_9x19_Ball {model = QPATHTOEF(tracers,ace_TracerGreen2.p3d);};
+    class CUP_B_9x19_Ball_Tracer_Red: CUP_B_9x19_Ball_Tracer_Green {model = QPATHTOEF(tracers,ace_TracerRed2.p3d);};
+    class CUP_B_9x19_Ball_Tracer_Yellow: CUP_B_9x19_Ball_Tracer_Green {model = QPATHTOEF(tracers,ace_TracerYellow2.p3d);};
+
+    class CUP_B_9x19_AP: BulletBase {model = QPATHTOEF(tracers,ace_TracerWhite2.p3d);};
+
+    class CUP_B_50AE_Ball: BulletBase {model = QPATHTOEF(tracers,ace_TracerRed2.p3d);};
+
+    class CUP_B_46x30_Ball: BulletBase {model = QPATHTOEF(tracers,ace_TracerWhite2.p3d);};
+    class CUP_B_46x30_Ball_Tracer_Red: CUP_B_46x30_Ball {model = QPATHTOEF(tracers,ace_TracerRed2.p3d);};
+    class CUP_B_46x30_Ball_Tracer_Green: CUP_B_46x30_Ball {model = QPATHTOEF(tracers,ace_TracerGreen2.p3d);};
+    class CUP_B_46x30_Ball_Tracer_Yellow: CUP_B_46x30_Ball {model = QPATHTOEF(tracers,ace_TracerYellow2.p3d);};
+
+    class B_570x28_Ball;
+    class CUP_B_570x28_Ball_Tracer_Red: B_570x28_Ball {model = QPATHTOEF(tracers,ace_TracerRed2.p3d);};
+    class CUP_B_570x28_Ball_Tracer_Green: B_570x28_Ball {model = QPATHTOEF(tracers,ace_TracerGreen2.p3d);};
+    class CUP_B_570x28_Ball_Tracer_Yellow: B_570x28_Ball {model = QPATHTOEF(tracers,ace_TracerYellow2.p3d);};
+
+    class B_45ACP_Ball;
+    class CUP_B_45ACP_Ball_Red: B_45ACP_Ball {model = QPATHTOEF(tracers,ace_TracerRed2.p3d);};
+
+    class B_127x107_Ball;
+    class CUP_B_127x107_Ball_Green_Tracer: B_127x107_Ball {model = QPATHTOEF(tracers,ace_TracerGreen2.p3d);};
+
+    class B_127x99_Ball;
+    class CUP_B_127x99_Ball_White_Tracer: B_127x99_Ball {model = QPATHTOEF(tracers,ace_TracerWhite2.p3d);};
+    class CUP_B_127x99_Ball_Red_Tracer: CUP_B_127x99_Ball_White_Tracer {model = QPATHTOEF(tracers,ace_TracerRed2.p3d);};
+    class CUP_B_127x99_Ball_Green_Tracer: CUP_B_127x99_Ball_White_Tracer {model = QPATHTOEF(tracers,ace_TracerGreen2.p3d);};
+    class CUP_B_127x99_Ball_Yellow_Tracer: CUP_B_127x99_Ball_White_Tracer {model = QPATHTOEF(tracers,ace_TracerYellow2.p3d);};
+
+    class CUP_B_127x99_Ball_Red_Tracer_Splash;
+    class CUP_B_127x99_Ball_M3P_Red_Tracer: CUP_B_127x99_Ball_Red_Tracer_Splash {model = QPATHTOEF(tracers,ace_TracerRed2.p3d);};
+
+    class CUP_B_145x115_AP_Green_Tracer: BulletBase {model = QPATHTOEF(tracers,ace_TracerGreen2.p3d);};
+
+    class CUP_B_19mm_HE: BulletBase {model = QPATHTOEF(tracers,ace_TracerWhite2.p3d);};
+
+    class CUP_B_20mm_AP_Tracer_Red: BulletBase {model = QPATHTOEF(tracers,ace_TracerRed2.p3d);};
+    class CUP_B_20mm_AP_Tracer_Green: CUP_B_20mm_AP_Tracer_Red {model = QPATHTOEF(tracers,ace_TracerGreen2.p3d);};
+    class CUP_B_20mm_AP_Tracer_Yellow: CUP_B_20mm_AP_Tracer_Red {model = QPATHTOEF(tracers,ace_TracerYellow2.p3d);};
+
+    class CUP_B_20mm_APHE_Tracer_Red: BulletBase {model = QPATHTOEF(tracers,ace_TracerRed2.p3d);};
+    class CUP_B_20mm_APHE_Tracer_Green: CUP_B_20mm_APHE_Tracer_Red {model = QPATHTOEF(tracers,ace_TracerGreen2.p3d);};
+    class CUP_B_20mm_APHE_Tracer_Yellow: CUP_B_20mm_APHE_Tracer_Red {model = QPATHTOEF(tracers,ace_TracerYellow2.p3d);};
+
+    class CUP_B_20mm_API_Tracer_Red: BulletBase {model = QPATHTOEF(tracers,ace_TracerRed2.p3d);};
+    class CUP_B_20mm_API_Tracer_Green: CUP_B_20mm_API_Tracer_Red {model = QPATHTOEF(tracers,ace_TracerGreen2.p3d);};
+    class CUP_B_20mm_API_Tracer_Yellow: CUP_B_20mm_API_Tracer_Red {model = QPATHTOEF(tracers,ace_TracerYellow2.p3d);};
+
+    class CUP_B_20mm_AA: BulletBase {model = QPATHTOEF(tracers,ace_TracerRed2.p3d);};
+
+    class CUP_B_23mm_APHE_Tracer_Green: BulletBase {model = QPATHTOEF(tracers,ace_TracerGreen2.p3d);};
+    class CUP_B_23mm_APHE_Tracer_Yellow: CUP_B_23mm_APHE_Tracer_Green {model = QPATHTOEF(tracers,ace_TracerYellow2.p3d);};
+    class CUP_B_23mm_APHE_Tracer_Red: CUP_B_23mm_APHE_Tracer_Green {model = QPATHTOEF(tracers,ace_TracerRed2.p3d);};
+
+    class CUP_B_23mm_AA: BulletBase {model = QPATHTOEF(tracers,ace_TracerGreen2.p3d);};
+
+    class CUP_B_23mm_APHE_Green_Tracer: BulletBase {model = QPATHTOEF(tracers,ace_TracerGreen2.p3d);};
+    class CUP_B_23mm_APHE_Yellow_Tracer: CUP_B_23mm_APHE_Green_Tracer {model = QPATHTOEF(tracers,ace_TracerYellow2.p3d);};
+
+    class CUP_B_30x113mm_M789_HEDP_Red_Tracer: BulletBase {model = QPATHTOEF(tracers,ace_TracerRed2.p3d);};
+    class CUP_B_30x113mm_M789_HEDP_Green_Tracer: CUP_B_30x113mm_M789_HEDP_Red_Tracer {model = QPATHTOEF(tracers,ace_TracerGreen2.p3d);};
+    class CUP_B_30x113mm_M789_HEDP_Yellow_Tracer: CUP_B_30x113mm_M789_HEDP_Red_Tracer {model = QPATHTOEF(tracers,ace_TracerYellow2.p3d);};
+    class CUP_B_30x113mm_M789_HEDP_White_Tracer: CUP_B_30x113mm_M789_HEDP_Red_Tracer {model = QPATHTOEF(tracers,ace_TracerWhite2.p3d);};
+
+    class CUP_B_30mm_HE_Base;
+    class CUP_B_30mm_HE_Red_Tracer: CUP_B_30mm_HE_Base {model = QPATHTOEF(tracers,ace_TracerRed2.p3d);};
+    class CUP_B_30mm_HE_Green_Tracer: CUP_B_30mm_HE_Base {model = QPATHTOEF(tracers,ace_TracerGreen2.p3d);};
+    class CUP_B_30mm_HE_Yellow_Tracer: CUP_B_30mm_HE_Base {model = QPATHTOEF(tracers,ace_TracerYellow2.p3d);};
+    class CUP_B_30mm_HE_White_Tracer: CUP_B_30mm_HE_Base {model = QPATHTOEF(tracers,ace_TracerWhite2.p3d);};
+
+    class CUP_B_30mm_HEIF_Base;
+    class CUP_B_30mm_HEIF_Red_Tracer: CUP_B_30mm_HEIF_Base {model = QPATHTOEF(tracers,ace_TracerRed2.p3d);};
+    class CUP_B_30mm_HEIF_Green_Tracer: CUP_B_30mm_HEIF_Base {model = QPATHTOEF(tracers,ace_TracerGreen2.p3d);};
+    class CUP_B_30mm_HEIF_Yellow_Tracer: CUP_B_30mm_HEIF_Base {model = QPATHTOEF(tracers,ace_TracerYellow2.p3d);};
+    class CUP_B_30mm_HEIF_White_Tracer: CUP_B_30mm_HEIF_Base {model = QPATHTOEF(tracers,ace_TracerWhite2.p3d);};
+
+    class CUP_B_30mm_AP_Base;
+    class CUP_B_30mm_AP_Red_Tracer: CUP_B_30mm_AP_Base {model = QPATHTOEF(tracers,ace_TracerRed2.p3d);};
+    class CUP_B_30mm_AP_Green_Tracer: CUP_B_30mm_AP_Base {model = QPATHTOEF(tracers,ace_TracerGreen2.p3d);};
+    class CUP_B_30mm_AP_Yellow_Tracer: CUP_B_30mm_AP_Base {model = QPATHTOEF(tracers,ace_TracerYellow2.p3d);};
+    class CUP_B_30mm_AP_White_Tracer: CUP_B_30mm_AP_Base {model = QPATHTOEF(tracers,ace_TracerWhite2.p3d);};
+
+    class CUP_B_30mm_APBC_Base;
+    class CUP_B_30mm_APBC_Red_Tracer: CUP_B_30mm_APBC_Base {model = QPATHTOEF(tracers,ace_TracerRed2.p3d);};
+    class CUP_B_30mm_APBC_Green_Tracer: CUP_B_30mm_APBC_Base {model = QPATHTOEF(tracers,ace_TracerGreen2.p3d);};
+    class CUP_B_30mm_APBC_Yellow_Tracer: CUP_B_30mm_APBC_Base {model = QPATHTOEF(tracers,ace_TracerYellow2.p3d);};
+    class CUP_B_30mm_APBC_White_Tracer: CUP_B_30mm_APBC_Base {model = QPATHTOEF(tracers,ace_TracerWhite2.p3d);};
+
+    class CUP_B_30mm_CAS_Red_Tracer: BulletBase {model = QPATHTOEF(tracers,ace_TracerRed2.p3d);};
+
+    class CUP_B_25mm_HE_White_Tracer: BulletBase {model = QPATHTOEF(tracers,ace_TracerWhite2.p3d);};
+    class CUP_B_25mm_HE_Red_Tracer: CUP_B_25mm_HE_White_Tracer {model = QPATHTOEF(tracers,ace_TracerRed2.p3d);};
+    class CUP_B_25mm_HE_Green_Tracer: CUP_B_25mm_HE_White_Tracer {model = QPATHTOEF(tracers,ace_TracerGreen2.p3d);};
+    class CUP_B_25mm_HE_Yellow_Tracer: CUP_B_25mm_HE_White_Tracer {model = QPATHTOEF(tracers,ace_TracerYellow2.p3d);};
+
+    class CUP_B_25mm_APFSDS_White_Tracer: BulletBase {model = QPATHTOEF(tracers,ace_TracerWhite2.p3d);};
+    class CUP_B_25mm_APFSDS_Red_Tracer: CUP_B_25mm_APFSDS_White_Tracer {model = QPATHTOEF(tracers,ace_TracerRed2.p3d);};
+    class CUP_B_25mm_APFSDS_Green_Tracer: CUP_B_25mm_APFSDS_White_Tracer {model = QPATHTOEF(tracers,ace_TracerGreen2.p3d);};
+    class CUP_B_25mm_APFSDS_Yellow_Tracer: CUP_B_25mm_APFSDS_White_Tracer {model = QPATHTOEF(tracers,ace_TracerYellow2.p3d);};
+
+    class CUP_B_25mm_APDS_White_Tracer: BulletBase {model = QPATHTOEF(tracers,ace_TracerWhite2.p3d);};
+    class CUP_B_25mm_APDS_Red_Tracer: CUP_B_25mm_APDS_White_Tracer {model = QPATHTOEF(tracers,ace_TracerRed2.p3d);};
+    class CUP_B_25mm_APDS_Green_Tracer: CUP_B_25mm_APDS_White_Tracer {model = QPATHTOEF(tracers,ace_TracerGreen2.p3d);};
+    class CUP_B_25mm_APDS_Yellow_Tracer: CUP_B_25mm_APDS_White_Tracer {model = QPATHTOEF(tracers,ace_TracerYellow2.p3d);};
+
+    class CUP_B_127x108_Ball_Green_Tracer: BulletBase {model = QPATHTOEF(tracers,ace_TracerGreen2.p3d);};
+
+    class CUP_B_127x108_AP_Green_Tracer: B_127x108_Ball {model = QPATHTOEF(tracers,ace_TracerGreen2.p3d);};
+
+    class CUP_Sh_3UBK9_HEAT: ShellBase {model = QPATHTOEF(tracers,ace_shell_tracer_green.p3d);};
+    class CUP_Sh_3UOF10_HEFRAG: CUP_Sh_3UBK9_HEAT {model = QPATHTOEF(tracers,ace_shell_tracer_green.p3d);};
+
+    class CUP_Sh_120_HE: ShellBase {model = QPATHTOEF(tracers,ace_shell_tracer_red.p3d);};
+
+    class CUP_Sh_120_SABOT: ShellBase {model = QPATHTOEF(tracers,ace_shell_tracer_red.p3d);};
+
+    class CUP_Sh_122_AT: Sh_120mm_APFSDS {model = QPATHTOEF(tracers,ace_shell_tracer_green.p3d);};
+
+    class CUP_Sh_122_HE;
+    class CUP_Sh_127_HE: CUP_Sh_122_HE {model = QPATHTOEF(tracers,ace_shell_tracer_white.p3d);};
+};
