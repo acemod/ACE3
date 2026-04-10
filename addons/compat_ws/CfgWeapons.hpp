3585c8c1c98172a97c109897b7b700a7c56731d3
Laid3acK <bal2.chris@orange.fr>
2026-04-10 15:51:31 +0200

    B_12Gauge_Slug_NoCartridge

--- addons/compat_ws/CfgWeapons.hpp
+++ addons/compat_ws/CfgWeapons.hpp
@@ -5,6 +5,8 @@ class CfgWeapons {
         ACE_barrelLength = 457;
         ACE_barrelTwist = 0.0;
         ACE_twistDirection = 0;
+        ACE_RailHeightAboveBore = 4.99572; // checkScopes.sqf
+        initSpeed = -0.940574; // 0.940574x488= 459m/s according to ACE_muzzleVelocities ASM conditions (15°C, 999.916hPa, 78%)
     };
 
     // Galat Arm
@@ -12,6 +14,7 @@ class CfgWeapons {
         ACE_barrelLength = 535;
         ACE_barrelTwist = 304.8;
         ACE_twistDirection = 1;
+        ACE_RailHeightAboveBore = 4.28793; // checkScopes.sqf
     };
 
     // SA-77
@@ -20,6 +23,7 @@ class CfgWeapons {
         ACE_barrelLength = 550;
         ACE_barrelTwist = 304.8;
         ACE_twistDirection = 1;
+        ACE_RailHeightAboveBore = 4.82762; // checkScopes.sqf
         EGVAR(overheating,allowSwapBarrel) = 1;
     };
     class LMG_S77_Compact_base_lxWS: LMG_S77_base_lxWS {
@@ -27,6 +31,10 @@ class CfgWeapons {
         ACE_barrelTwist = 304.8;
         ACE_twistDirection = 1;
         EGVAR(overheating,allowSwapBarrel) = 1;
+    };
+
+    class glaunch_GLX_base_lxWS: Rifle_Base_F {
+        ACE_RailHeightAboveBore = 4.88889; // checkScopes.sqf
     };
 
     // SLR
@@ -47,6 +55,7 @@ class CfgWeapons {
         ACE_barrelLength = 460;
         ACE_barrelTwist = 304.8;
         ACE_twistDirection = 1;
+        ACE_RailHeightAboveBore = 4.28793; // checkScopes.sqf
     };
     class arifle_VelkoR5_lxWS: arifle_Velko_base_lxWS {
         ACE_barrelLength = 332;
@@ -60,11 +69,25 @@ class CfgWeapons {
         ACE_barrelLength = 406.4;
         ACE_barrelTwist = 178.0;
         ACE_twistDirection = 1;
+        ACE_IronSightBaseAngle = 0; // arifle_SPAR_01_base_F 0.002856
+        ACE_RailHeightAboveBore = 6; // no value checkScopes.sqf, estimation based on arifle_MSBS65_UBS_F 5.1754, arifle_SPAR_01_base_F 3.20768
+        initSpeed = -0.971576; // Western Sahara 900
     };
+
     // 20 inch barrel for the marksman/autorifle
     class arifle_XMS_lxWS;
     class arifle_XMS_M_lxWS: arifle_XMS_lxWS {
         ACE_barrelLength = 508.0;
+        initSpeed = -1; // Western Sahara 925
+    };
+
+    class arifle_XMS_Shot_lxWS: arifle_XMS_Base_lxWS {
+        // initSpeed = -0.971576;
+        class UBS_lxWS: Rifle_Base_F {
+            ACE_barrelLength = 317; // https://modernfirearms.net/en/shotguns/u-s-a-shotguns/crye-six12-eng/
+            ACE_twistDirection = 0;
+            // initSpeed = 0; // tested with -2 and 1000 w/o effect on secondary UBS_lxWS muzzle velocity
+        };
     };
 
     // Headgear
@@ -89,5 +112,34 @@ class CfgWeapons {
         ace_hearing_protection = 0;
         ace_hearing_lowerVolume = 0;
         ace_hearing_hasEHP = 0;
+    };
+
+    class ItemCore;
+    class optic_r1_high_lxWS: ItemCore {
+        ACE_ScopeHeightAboveRail = 4.07748; // checkScopes.sqf
+    };
+
+    class optic_r1_low_lxWS: optic_r1_high_lxWS {
+        ACE_ScopeHeightAboveRail = 2.4921; // checkScopes.sqf
+    };
+
+    class optic_r1_low_khaki_lxWS: optic_r1_high_lxWS {
+        ACE_ScopeHeightAboveRail = 2.4921; // checkScopes.sqf
+    };
+
+    class optic_r1_low_sand_lxWS: optic_r1_high_lxWS {
+        ACE_ScopeHeightAboveRail = 2.4921; // checkScopes.sqf
+    };
+
+    class optic_r1_low_snake_lxWS: optic_r1_high_lxWS {
+        ACE_ScopeHeightAboveRail = 2.4921; // checkScopes.sqf
+    };
+
+    class optic_r1_low_arid_lxWS: optic_r1_high_lxWS {
+        ACE_ScopeHeightAboveRail = 2.4921; // checkScopes.sqf
+    };
+
+    class optic_r1_low_lush_lxWS: optic_r1_high_lxWS {
+        ACE_ScopeHeightAboveRail = 2.4921; // checkScopes.sqf
     };
 };
