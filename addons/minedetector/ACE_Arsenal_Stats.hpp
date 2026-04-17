e869534d242ac48d26a43cd82e0a6ba7b116930f
PabstMirror <pabstmirror@gmail.com>
2026-04-17 14:27:24 -0500

    use setting

--- addons/minedetector/ACE_Arsenal_Stats.hpp
+++ addons/minedetector/ACE_Arsenal_Stats.hpp
@@ -6,7 +6,7 @@ class EGVAR(arsenal,stats) {
         stats[] = {"radius"};
         displayName = CSTRING(arsenalStatRadius);
         showText = 1;
-        textStatement = QUOTE(private _radius = getNumber (configFile >> 'ACE_detector' >> 'detectors' >> (configName (_this select 1)) >> (_this select 0 select 0)); format [ARR_3('%1m (%2ft)',_radius,(_radius / 0.3048) toFixed 1)]);
+        textStatement = QUOTE(private _radius = getNumber (configFile >> 'ACE_detector' >> 'detectors' >> (configName (_this select 1)) >> (_this select 0 select 0)); _radius = _radius + ( round (GVAR(BoostRadius) * 10) / 10 ); format [ARR_3('%1m (%2ft)',_radius,(_radius / 0.3048) toFixed 1)]);
         condition = QUOTE(getNumber (configFile >> 'ACE_detector' >> 'detectors' >> (configName (_this select 1)) >> (_this select 0 select 0)) > 0);
         tabs[] = {{1}, {}};
     };
