441f79d6b93173b34a1194f1ee5e61437613478d
Laid3acK <bal2.chris@orange.fr>
2026-04-18 11:50:41 +0200

    B_12Gauge_Slug_NoCartridge

---
+++ addons/compat_ws/CfgMagazineWells.hpp
@@ -0,0 +1,12 @@
+class CfgMagazineWells {
+    class AA40_12Gauge { // Weapon initSpeed -1: ACE_barrelLength 762mm (12 Gauge SAAMI test barrel 30"), ACE_muzzleVelocities 610m/s (2000fps ICAO ASM, 15°C 59°F)
+        ADDON[] = {
+            "ACE_20Rnd_12Gauge_AA40_Slug_lxWS",
+            "ACE_20Rnd_12Gauge_AA40_Slug_Tan_lxWS",
+            "ACE_20Rnd_12Gauge_AA40_Slug_Snake_lxWS",
+            "ACE_8Rnd_12Gauge_AA40_Slug_lxWS",
+            "ACE_8Rnd_12Gauge_AA40_Slug_Tan_lxWS",
+            "ACE_8Rnd_12Gauge_AA40_Slug_Snake_lxWS"
+        };
+    };
+};
