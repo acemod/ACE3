acemod/ACE3

commit 9dc2c60bf69e98e85bd37c4146ed6082df5238fb

Explosives - Refactor (#10805)
* Explosives - minor refactor

* Update addons/explosives/XEH_postInit.sqf

Co-authored-by: PabstMirror <pabstmirror@gmail.com>

* Update fnc_startDefuse.sqf

* Minor cleanup

* Fix bugs

* Minor code improvements

* Transfer explosives ownership

* Fixes for ownership transfer for uniforms & vests

* Fix item passing

`EFUNC(common,hasItem))` doesn't update immediately near mission start

* Remove FUNC(dialingPhone)

* Fix accepted types

* Remove clackers on respawn

* Update addons/explosives/functions/fnc_getDetonators.sqf

Co-authored-by: PabstMirror <pabstmirror@gmail.com>

---------

Co-authored-by: PabstMirror <pabstmirror@gmail.com>

addons/explosives/functions/fnc_setupExplosive.sqf
@@ -4,111 +4,140 @@
  * Starts the setup process for the passed explosive. Player only.
  *
  * Arguments:
- * 0: Vehicle <OBJECT>
- * 1: Player Unit <OBJECT>
- * 2: Classname of explosive to place. (CfgMagazine class) <STRING>
+ * 0: Target (not used) <OBJECT>
+ * 1: Unit <OBJECT> (default: objNull)
+ * 2: Magazine classname of explosive to place <STRING> (default: "")
  *
  * Return Value:
  * None
  *
  * Example:
- * [player, "SatchelCharge_Remote_Mag"] call ACE_Explosives_fnc_SetupExplosive;
+ * [objNull, player, "SatchelCharge_Remote_Mag"] call ace_explosives_fnc_setupExplosive
  *
  * Public: Yes
  */
 
+#define TEST_LENGTH 0.1
 #define PLACE_RANGE_MAX 1
 #define PLACE_RANGE_MIN 0.025
 
-params ["_vehicle", "_unit", "_magClassname"];
-TRACE_3("params",_vehicle,_unit,_magClassname);
+params ["", ["_unit", objNull, [objNull]], ["_magClassname", "", [""]]];
+TRACE_2("params",_unit,_magClassname);
 
-//Get setup object vehicle and model:
-private _setupObjectClass = getText (configFile >> "CfgMagazines" >> _magClassname >> QGVAR(SetupObject));
-if (!isClass (configFile >> "CfgVehicles" >> _setupObjectClass)) exitWith {ERROR("Bad Vehicle");};
+if (!alive _unit || {_magClassname == ""}) exitWith {};
+
+// Get setup object vehicle and model
+private _configMagazine = configFile >> "CfgMagazines" >> _magClassname;
+private _setupObjectClass = getText (_configMagazine >> QGVAR(setupObject));
+
+if (!isClass (configFile >> "CfgVehicles" >> _setupObjectClass)) exitWith {
+    ERROR("Bad Vehicle");
+};
+
 private _p3dModel = getText (configFile >> "CfgVehicles" >> _setupObjectClass >> "model");
-if (_p3dModel == "") exitWith {ERROR("No Model");}; //"" - will crash game!
+
+ // "" will crash game!
+if (_p3dModel == "") exitWith {
+    ERROR("No Model");
+};
 
 [_unit, "forceWalk", QUOTE(ADDON), true] call EFUNC(common,statusEffect_set);
 [_unit, "blockThrow", QUOTE(ADDON), true] call EFUNC(common,statusEffect_set);
 
-//Show mouse buttons:
-[localize LSTRING(PlaceAction), LELSTRING(common,Cancel), localize LSTRING(ScrollAction)] call EFUNC(interaction,showMouseHint);
-_unit setVariable [QGVAR(placeActionEH), [_unit, "DefaultAction", {true}, {GVAR(placeAction) = PLACE_APPROVE;}] call EFUNC(common,AddActionEventHandler)];
-_unit setVariable [QGVAR(cancelActionEH), [_unit, "zoomtemp", {true}, {GVAR(placeAction) = PLACE_CANCEL;}] call EFUNC(common,AddActionEventHandler)];
+// Show mouse buttons
+[LLSTRING(PlaceAction), LELSTRING(common,Cancel), LLSTRING(ScrollAction)] call EFUNC(interaction,showMouseHint);
+_unit setVariable [QGVAR(placeActionEH), [_unit, "DefaultAction", {true}, {GVAR(placeAction) = PLACE_APPROVE}] call EFUNC(common,AddActionEventHandler)];
+_unit setVariable [QGVAR(cancelActionEH), [_unit, "zoomtemp", {true}, {GVAR(placeAction) = PLACE_CANCEL}] call EFUNC(common,AddActionEventHandler)];
 
-//Display to show virtual object:
+// Display to show virtual object
 (QGVAR(virtualAmmo) call BIS_fnc_rscLayer) cutRsc [QGVAR(virtualAmmo), "PLAIN", 0, false];
 ((uiNamespace getVariable [QGVAR(virtualAmmoDisplay), displayNull]) displayCtrl 800851) ctrlSetModel _p3dModel;
 
-//Make sure it has a trigger that works when attached (eg, no tripwires that only do pressurePlate)
-private _isAttachable = false;
-private _supportedTriggers = getArray (configFile >> "CfgMagazines" >> _magClassname >> "ACE_Triggers" >> "SupportedTriggers");
-{
-    if ((getNumber (configFile >> "ACE_Triggers" >> _x >> "isAttachable")) == 1) exitWith {_isAttachable = true;};
-} forEach _supportedTriggers;
-
+// Make sure it has a trigger that works when attached (e.g. no tripwires that only do pressurePlate)
+private _supportedTriggers = getArray (_configMagazine >> "ACE_Triggers" >> "SupportedTriggers");
+private _aceTriggers = configFile >> "ACE_Triggers";
+private _isAttachable = _supportedTriggers findIf {(getNumber (_aceTriggers >> _x >> "isAttachable")) == 1} != -1;
 
 GVAR(pfeh_running) = true;
 GVAR(placeAction) = PLACE_WAITING;
 GVAR(TweakedAngle) = 0;
 
-
 [{
     BEGIN_COUNTER(pfeh);
 
-    params ["_args", "_pfID"];
+    disableSerialization;
+
+    params ["_args", "_pfhID"];
     _args params ["_unit", "_magClassname", "_setupObjectClass", "_isAttachable"];
 
-    private _lookDirVector = ((positionCameraToWorld [0,0,0]) call EFUNC(common,positionToASL)) vectorFromTo ((positionCameraToWorld [0,0,10]) call EFUNC(common,positionToASL));
-    private _basePosASL = (eyePos _unit);
-    if (cameraView == "EXTERNAL") then {  //If external, show explosive over the right shoulder
-        _basePosASL = _basePosASL vectorAdd ((positionCameraToWorld [0.3,0,0]) vectorDiff (positionCameraToWorld [0,0,0]));
+    private _lookDirVector = ((positionCameraToWorld [0, 0, 0]) call EFUNC(common,positionToASL)) vectorFromTo ((positionCameraToWorld [0, 0, 10]) call EFUNC(common,positionToASL));
+    private _basePosASL = eyePos _unit;
+
+    // If external, show explosive over the right shoulder
+    if (cameraView == "EXTERNAL") then {
+        _basePosASL = _basePosASL vectorAdd ((positionCameraToWorld [0.3, 0, 0]) vectorDiff (positionCameraToWorld [0, 0, 0]));
     };
+
     if ((stance _unit) == "PRONE") then {
-        //If prone, lower base and increase up angle of look - Makes it much easier to attach to underside of vehicles
-        _basePosASL set [2, ((_basePosASL select 2) - 0.3)];
-        _lookDirVector = ((positionCameraToWorld [0,0,0]) call EFUNC(common,positionToASL)) vectorFromTo ((positionCameraToWorld [0,3,10]) call EFUNC(common,positionToASL));
+        // If prone, lower base and increase up angle of look - Makes it much easier to attach to underside of vehicles
+        _basePosASL vectorAdd [0, 0, -0.3];
+        _lookDirVector = ((positionCameraToWorld [0, 0, 0]) call EFUNC(common,positionToASL)) vectorFromTo ((positionCameraToWorld [0, 3, 10]) call EFUNC(common,positionToASL));
     };
+
     private _cameraAngle = (_lookDirVector select 0) atan2 (_lookDirVector select 1);
 
     private _testPositionIsValid = {
-        private _testBase = _basePosASL vectorAdd (_lookDirVector vectorMultiply (_this select 0));
+        private _testBase = _basePosASL vectorAdd (_lookDirVector vectorMultiply _this);
         private _return = true;
+
         {
-            private _testPos = _testBase vectorAdd [0.1 * (_x select 0) * (cos _cameraAngle), 0.1 * (_x select 0) * (sin _cameraAngle), 0.1 * (_x select 1)];
+            private _testPos = _testBase vectorAdd [(_x select 0) * (cos _cameraAngle), (_x select 0) * (sin _cameraAngle), _x select 1];
             #ifdef DEBUG_MODE_FULL
-            drawLine3D [(eyePos _unit) call EFUNC(common,ASLToPosition), (_testPos) call EFUNC(common,ASLToPosition), [1,0,0,1]];
+            drawLine3D [(eyePos _unit) call EFUNC(common,ASLToPosition), _testPos call EFUNC(common,ASLToPosition), [1, 0, 0, 1]];
             #endif
-            if ((lineIntersectsSurfaces [eyePos _unit, _testPos, _unit]) isNotEqualTo []) exitWith {_return = false;};
-        } forEach [[0,0], [-1,-1], [1,-1], [-1,1], [1,1]];
+
+            if ((lineIntersectsSurfaces [eyePos _unit, _testPos, _unit]) isNotEqualTo []) exitWith {
+                _return = false;
+            };
+        } forEach [[0, 0], [-TEST_LENGTH, -TEST_LENGTH], [TEST_LENGTH, -TEST_LENGTH], [-TEST_LENGTH, TEST_LENGTH], [TEST_LENGTH, TEST_LENGTH]];
+
         _return
     };
 
     private _distanceFromBase = PLACE_RANGE_MAX;
-    private _badPosition = !([_distanceFromBase] call _testPositionIsValid);
+    private _badPosition = !(_distanceFromBase call _testPositionIsValid);
     private _attachVehicle = objNull;
 
     if (_isAttachable && _badPosition) then {
-        _attachVehicle = objNull;
         private _testBase = _basePosASL vectorAdd _lookDirVector;
+
         {
-            private _testPos = _testBase vectorAdd [0.1 * (_x select 0) * (cos _cameraAngle), 0.1 * (_x select 0) * (sin _cameraAngle), 0.1 * (_x select 1)];
+            private _testPos = _testBase vectorAdd [(_x select 0) * (cos _cameraAngle), (_x select 0) * (sin _cameraAngle), _x select 1];
             private _intersectObject = ((lineIntersectsSurfaces [eyePos _unit, _testPos, _unit]) param [0, objNull]) param [3, objNull];
-            if (_intersectObject isNotEqualTo objNull) exitWith {_attachVehicle = _intersectObject};
-        } forEach [[0,0], [-1,-1], [1,-1], [-1,1], [1,1]];
-        if ((!isNull _attachVehicle) && {[PLACE_RANGE_MIN] call _testPositionIsValid} &&
-                {(_attachVehicle isKindOf "Car") || {_attachVehicle isKindOf "Tank"} || {_attachVehicle isKindOf "Air"} || {_attachVehicle isKindOf "Ship"}}) then {
+
+            if (!isNull _intersectObject) exitWith {
+                _attachVehicle = _intersectObject;
+            };
+        } forEach [[0, 0], [-TEST_LENGTH, -TEST_LENGTH], [TEST_LENGTH, -TEST_LENGTH], [-TEST_LENGTH, TEST_LENGTH], [TEST_LENGTH, TEST_LENGTH]];
+
+        if (
+            !isNull _attachVehicle &&
+            {(_attachVehicle isKindOf "Car") || {_attachVehicle isKindOf "Tank"} || {_attachVehicle isKindOf "Air"} || {_attachVehicle isKindOf "Ship"}} &&
+            {PLACE_RANGE_MIN call _testPositionIsValid}
+        ) then {
             private _min = PLACE_RANGE_MIN;
             private _max = PLACE_RANGE_MAX;
+
             for "_index" from 0 to 6 do {
                 _distanceFromBase = (_min + _max) / 2;
-                if ([_distanceFromBase] call _testPositionIsValid) then {
+
+                if (_distanceFromBase call _testPositionIsValid) then {
                     _min = _distanceFromBase;
                 } else {
                     _max = _distanceFromBase;
                 };
             };
+
             _badPosition = false;
             _distanceFromBase = ((_min + _max) / 2 + 0.075) min 1;
         } else {
@@ -120,97 +149,112 @@ GVAR(TweakedAngle) = 0;
 
     // Update mouse hint
     private _ctrlTextLMB = (uiNamespace getVariable [QEGVAR(interaction,mouseHint), displayNull]) displayCtrl 2420;
+
     if (_badPosition) then {
-        _ctrlTextLMB ctrlSetText localize LSTRING(BlockedAction);
+        _ctrlTextLMB ctrlSetText LLSTRING(BlockedAction);
     } else {
-        if (isNull _attachVehicle) then {
-            _ctrlTextLMB ctrlSetText localize LSTRING(PlaceAction);
-        } else {
-            _ctrlTextLMB ctrlSetText localize LSTRING(AttachAction);
-        };
+        _ctrlTextLMB ctrlSetText localize ([LSTRING(AttachAction), LSTRING(PlaceAction)] select (isNull _attachVehicle));
     };
 
-    //Don't allow Placing bellow terrain
-    if ((getTerrainHeightASL _virtualPosASL) > (_virtualPosASL select 2)) then {
-        _virtualPosASL set [2, (getTerrainHeightASL _virtualPosASL)];
+    private _terrainHeightASL = getTerrainHeightASL _virtualPosASL;
+
+    // Don't allow Placing below terrain
+    if (_terrainHeightASL > (_virtualPosASL select 2)) then {
+        _virtualPosASL set [2, _terrainHeightASL];
     };
 
-    //Don't allow placing in a bad position:
-    if (_badPosition && {GVAR(placeAction) == PLACE_APPROVE}) then {GVAR(placeAction) = PLACE_WAITING;};
+    // Don't allow placing in a bad position
+    if (_badPosition && {GVAR(placeAction) == PLACE_APPROVE}) then {
+        GVAR(placeAction) = PLACE_WAITING;
+    };
 
     if (_unit != ACE_player ||
         {!([_unit, objNull, ["isNotSwimming"]] call EFUNC(common,canInteractWith))} ||
-        {!(_magClassname in (magazines _unit))}
+        {!([_unit, _magClassname] call EFUNC(common,hasMagazine))}
     ) then {
         GVAR(placeAction) = PLACE_CANCEL;
     };
 
     if (GVAR(placeAction) != PLACE_WAITING) then {
-        [_pfID] call CBA_fnc_removePerFrameHandler;
+        _pfhID call CBA_fnc_removePerFrameHandler;
+
         GVAR(pfeh_running) = false;
 
         [_unit, "forceWalk", QUOTE(ADDON), false] call EFUNC(common,statusEffect_set);
         [_unit, "blockThrow", QUOTE(ADDON), false] call EFUNC(common,statusEffect_set);
+
         [] call EFUNC(interaction,hideMouseHint);
-        [_unit, "DefaultAction", (_unit getVariable [QGVAR(placeActionEH), -1])] call EFUNC(common,removeActionEventHandler);
-        [_unit, "zoomtemp", (_unit getVariable [QGVAR(cancelActionEH), -1])] call EFUNC(common,removeActionEventHandler);
+
+        [_unit, "DefaultAction", _unit getVariable [QGVAR(placeActionEH), -1]] call EFUNC(common,removeActionEventHandler);
+        [_unit, "zoomtemp", _unit getVariable [QGVAR(cancelActionEH), -1]] call EFUNC(common,removeActionEventHandler);
 
         (QGVAR(virtualAmmo) call BIS_fnc_rscLayer) cutText ["", "PLAIN"];
 
         if (GVAR(placeAction) == PLACE_APPROVE) then {
             private _placeAngle = 0;
-            private _expSetupVehicle = _setupObjectClass createVehicle (_virtualPosASL call EFUNC(common,ASLToPosition));
+            private _expSetupVehicle = _setupObjectClass createVehicle [0, 0, 0]; //(_virtualPosASL call EFUNC(common,ASLToPosition));
 
-            TRACE_1("Planting Mass",(getMass _expSetupVehicle));
-            //If the object is too heavy, it can kill a player if it colides
-            if ((getMass _expSetupVehicle) > 5) then {_expSetupVehicle setMass 5;};
+            TRACE_1("Planting Mass",getMass _expSetupVehicle);
+
+            // If the object is too heavy, it can kill a player if it collides
+            if (getMass _expSetupVehicle > 5) then {
+                _expSetupVehicle setMass 5;
+            };
 
             if (isNull _attachVehicle) then {
-                _placeAngle = _cameraAngle - GVAR(TweakedAngle) + 180;
-                _expSetupVehicle setPosASL _virtualPosASL;
+                _placeAngle = _cameraAngle - GVAR(tweakedAngle) + 180;
                 _expSetupVehicle setDir _placeAngle;
-                _placeAngle = _placeAngle + 180; //CfgAmmos seem to be 180 for some reason
+                _expSetupVehicle setPosASL _virtualPosASL;
+                _placeAngle = _placeAngle + 180; // CfgAmmos seem to be 180 for some reason
             } else {
                 private _modelOffset = _attachVehicle worldToModel (_virtualPosASL call EFUNC(common,ASLToPosition));
                 _placeAngle = _cameraAngle - (getDir _attachVehicle) + 180;
                 _expSetupVehicle attachTo [_attachVehicle, _modelOffset];
-                _expSetupVehicle setVectorDirAndUp [[0,0,-1],[(sin _placeAngle),(cos _placeAngle),0]];
+                _expSetupVehicle setVectorDirAndUp [[0, 0, -1], [sin _placeAngle, cos _placeAngle, 0]];
             };
 
-            TRACE_1("Place angel",_placeAngle);
+            TRACE_1("Place angle",_placeAngle);
 
             _expSetupVehicle setVariable [QGVAR(class), _magClassname, true];
-            _expSetupVehicle setVariable [QGVAR(Direction), _placeAngle, true];
+            _expSetupVehicle setVariable [QGVAR(direction), _placeAngle, true];
 
-            _unit removeMagazine _magClassname;
+            [_unit, _magClassname] call EFUNC(common,adjustMagazineAmmo);
             [_unit, "PutDown"] call EFUNC(common,doGesture);
-            _unit setVariable [QGVAR(PlantingExplosive), true];
-            [{_this setVariable [QGVAR(PlantingExplosive), false]}, _unit, 1.5] call CBA_fnc_waitAndExecute;
+            _unit setVariable [QGVAR(plantingExplosive), true];
+            [{_this setVariable [QGVAR(plantingExplosive), false]}, _unit, 1.5] call CBA_fnc_waitAndExecute;
 
+            // API
             [QGVAR(setup), [_expSetupVehicle, _magClassname, _unit]] call CBA_fnc_globalEvent;
         };
     } else {
         private _screenPos = worldToScreen (_virtualPosASL call EFUNC(common,ASLToPosition));
+
         if (_badPosition || {_screenPos isEqualTo []}) then {
             ((uiNamespace getVariable [QGVAR(virtualAmmoDisplay), displayNull]) displayCtrl 800851) ctrlShow false;
         } else {
-            //Show the model on the hud in aprox the same size/location as it will be placed:
-            ((uiNamespace getVariable [QGVAR(virtualAmmoDisplay), displayNull]) displayCtrl 800851) ctrlShow true;
+            private _display = uiNamespace getVariable [QGVAR(virtualAmmoDisplay), displayNull];
 
-            private _realDistance = ((_virtualPosASL call EFUNC(common,ASLToPosition)) distance (positionCameraToWorld [0,0,0])) / (([] call CBA_fnc_getFov) select 1);
-            _screenPos = [(_screenPos select 0), _realDistance, (_screenPos select 1)];
-            ((uiNamespace getVariable [QGVAR(virtualAmmoDisplay), displayNull]) displayCtrl 800851) ctrlSetPosition _screenPos;
+            // Show the model on the hud in aprox the same size/location as it will be placed
+            (_display displayCtrl 800851) ctrlShow true;
 
-            private _modelDir = [0,0,-1];
-            private _modelUp = [0,-1,0];
+            private _realDistance = ((_virtualPosASL call EFUNC(common,ASLToPosition)) distance (positionCameraToWorld [0, 0, 0])) / ((call CBA_fnc_getFov) select 1);
+            _screenPos = [_screenPos select 0, _realDistance, _screenPos select 1];
+            (_display displayCtrl 800851) ctrlSetPosition _screenPos;
+
+            private _modelDir = [0, 0, -1];
+            private _modelUp = [0, -1, 0];
+
             if (isNull _attachVehicle) then {
                 private _angle = acos (_lookDirVector select 2);
-                _modelUp = [0, (cos _angle), (sin _angle)];
-                _modelDir = [cos GVAR(TweakedAngle), sin GVAR(TweakedAngle), 0] vectorCrossProduct _modelUp;
+                _modelUp = [0, cos _angle, sin _angle];
+                _modelDir = [cos GVAR(tweakedAngle), sin GVAR(tweakedAngle), 0] vectorCrossProduct _modelUp;
             };
-            ((uiNamespace getVariable [QGVAR(virtualAmmoDisplay), displayNull]) displayCtrl 800851) ctrlSetModelDirAndUp [_modelDir, _modelUp];
+
+            (_display displayCtrl 800851) ctrlSetModelDirAndUp [_modelDir, _modelUp];
         };
     };
 
     END_COUNTER(pfeh);
 }, 0, [_unit, _magClassname, _setupObjectClass, _isAttachable]] call CBA_fnc_addPerFrameHandler;
+
+nil

addons/explosives/CfgAmmo.hpp
@@ -1,7 +1,5 @@
 class CfgAmmo {
-    // All explosive based Ammo classes. These are all listed in case they become required.
-
-    // GVAR(defuseTime) is the defuse time (default of 5 in ACE_Explosives_fnc_StartDefuse)
+    // GVAR(defuseTime) is the defuse time (default of 5 in ace_explosives_fnc_startDefuse)
     // GVAR(size) = 0; is small size
     // GVAR(size) = 1; is large size
     class ClaymoreDirectionalMine_Remote_Ammo;
@@ -67,7 +65,7 @@ class CfgAmmo {
         GVAR(size) = 0;
     };
 
-    // More sensitive pressure-fuze for the Vanilla AT Mine.
+    // More sensitive pressure-fuse for the Vanilla AT Mine
     class MineBase;
     class ATMine_Range_Ammo: MineBase {
         mineTrigger = "ACE_TankTriggerLight";
@@ -127,8 +125,8 @@ class CfgAmmo {
         GVAR(magazine) = "IEDUrbanBig_Remote_Mag";
         GVAR(size) = 1;
         soundTrigger[] = {"A3\Sounds_F\weapons\mines\mech_trigger_1", 0.8, 1, 40};
-        GVAR(ringtones)[] = {{QPATHTOF(Data\Audio\Cellphone_Ring.wss), 0.75, 3.16228, 1, 75}};
-        GVAR(callConnectTime)[] = {1,8};
+        GVAR(ringtones)[] = {{QPATHTOF(Data\Audio\Cellphone_Ring.wss), 0.75, 3.15, 1, 75}};
+        GVAR(callConnectTime)[] = {1, 8};
         GVAR(detonationTimingOffset) = 0.75;
     };
     class ACE_IEDUrbanBig_Command_Ammo: IEDUrbanBig_Remote_Ammo {
@@ -144,8 +142,8 @@ class CfgAmmo {
         GVAR(magazine) = "IEDUrbanSmall_Remote_Mag";
         GVAR(size) = 0;
         soundTrigger[] = {"A3\Sounds_F\weapons\mines\mech_trigger_1", 0.8, 1, 40};
-        GVAR(ringtones)[] = {{QPATHTOF(Data\Audio\Cellphone_Ring.wss), 0.75, 3.16228, 1, 75}};
-        GVAR(callConnectTime)[] = {1,8};
+        GVAR(ringtones)[] = {{QPATHTOF(Data\Audio\Cellphone_Ring.wss), 0.75, 3.15, 1, 75}};
+        GVAR(callConnectTime)[] = {1, 8};
         GVAR(detonationTimingOffset) = 0.75;
     };
     class ACE_IEDUrbanSmall_Command_Ammo: IEDUrbanSmall_Remote_Ammo {
@@ -161,8 +159,8 @@ class CfgAmmo {
         GVAR(magazine) = "IEDLandBig_Remote_Mag";
         GVAR(size) = 1;
         soundTrigger[] = {"A3\Sounds_F\weapons\mines\mech_trigger_1", 0.8, 1, 40};
-        GVAR(ringtones)[] = {{QPATHTOF(Data\Audio\Cellphone_Ring.wss), 0.75, 3.16228, 1, 75}};
-        GVAR(callConnectTime)[] = {1,8};
+        GVAR(ringtones)[] = {{QPATHTOF(Data\Audio\Cellphone_Ring.wss), 0.75, 3.15, 1, 75}};
+        GVAR(callConnectTime)[] = {1, 8};
         GVAR(detonationTimingOffset) = 0.75;
     };
     class ACE_IEDLandBig_Command_Ammo: IEDLandBig_Remote_Ammo {
@@ -178,8 +176,8 @@ class CfgAmmo {
         GVAR(magazine) = "IEDLandSmall_Remote_Mag";
         GVAR(size) = 0;
         soundTrigger[] = {"A3\Sounds_F\weapons\mines\mech_trigger_1", 0.8, 1, 40};
-        GVAR(ringtones)[] = {{QPATHTOF(Data\Audio\Cellphone_Ring.wss), 0.75, 3.16228, 1, 75}};
-        GVAR(callConnectTime)[] = {1,8};
+        GVAR(ringtones)[] = {{QPATHTOF(Data\Audio\Cellphone_Ring.wss), 0.75, 3.15, 1, 75}};
+        GVAR(callConnectTime)[] = {1, 8};
         GVAR(detonationTimingOffset) = 0.75;
     };
     class ACE_IEDLandSmall_Command_Ammo: IEDLandSmall_Remote_Ammo {

addons/explosives/CfgMagazines.hpp
@@ -3,7 +3,7 @@ class CfgMagazines {
     class ATMine_Range_Mag: CA_Magazine {
         useAction = 0;
         GVAR(placeable) = 1;
-        GVAR(setupObject) = "ACE_Explosives_Place_ATMine"; // CfgVehicle class for setup object
+        GVAR(setupObject) = "ACE_Explosives_Place_ATMine"; // CfgVehicles class for setup object
         class ACE_Triggers {
             SupportedTriggers[] = {"PressurePlate"};
             class PressurePlate {
@@ -140,7 +140,7 @@ class CfgMagazines {
     };
     class IEDLandBig_Remote_Mag: IEDUrbanBig_Remote_Mag {
         GVAR(setupObject) = "ACE_Explosives_Place_IEDLandBig";
-        picture = "\A3\Weapons_F\Data\UI\gear_mine_AT_CA.paa";  // Fix inconsistent picture
+        picture = "\A3\Weapons_F\Data\UI\gear_mine_AT_CA.paa"; // Fix inconsistent picture
         class ACE_Triggers: ACE_Triggers {
             class Command: Command {
                 ammo = "ACE_IEDLandBig_Command_Ammo";
@@ -154,7 +154,7 @@ class CfgMagazines {
     };
     class IEDUrbanSmall_Remote_Mag: DemoCharge_Remote_Mag {
         GVAR(setupObject) = "ACE_Explosives_Place_IEDUrbanSmall";
-        picture = "\A3\Weapons_F\Data\UI\gear_mine_AP_bouncing_CA.paa";  // Fix inconsistent picture
+        picture = "\A3\Weapons_F\Data\UI\gear_mine_AP_bouncing_CA.paa"; // Fix inconsistent picture
         class ACE_Triggers {
             SupportedTriggers[] = {"Command", "DeadmanSwitch", "Cellphone", "PressurePlate"};
             class Command {

addons/explosives/CfgVehicles.hpp
@@ -298,10 +298,10 @@ class CfgVehicles {
 
     #include "CfgModule.hpp"
 
-    // Add ACE_Clacker and ACE_DefusalKit to every explosive unit.
+    // Add ACE_Clacker and ACE_DefusalKit to every explosive unit
     #define MACRO_ADDMINEKIT \
-    items[] = {"FirstAidKit","ACE_Clacker","ACE_DefusalKit"}; \
-    respawnitems[] = {"FirstAidKit","ACE_Clacker","ACE_DefusalKit"};
+    items[] += {"ACE_Clacker","ACE_DefusalKit"}; \
+    respawnitems[] += {"ACE_Clacker","ACE_DefusalKit"};
 
     class B_Soldier_base_F; class B_soldier_exp_F: B_Soldier_base_F {MACRO_ADDMINEKIT};
     class B_Soldier_03_f; class B_engineer_F: B_Soldier_03_f {MACRO_ADDMINEKIT};
@@ -336,10 +336,10 @@ class CfgVehicles {
     class O_V_Soldier_base_F; class O_V_Soldier_Exp_hex_F: O_V_Soldier_base_F {MACRO_ADDMINEKIT};
     class O_V_Soldier_Exp_ghex_F: O_V_Soldier_Exp_hex_F {MACRO_ADDMINEKIT};
 
-    // Add ACE_Clacker to snipers and spotters for setting off Claymores
+    // Add ACE_Clacker to snipers and spotters for setting off claymores
     #define MACRO_ADDCLAYMOREKIT \
-    items[] = {"FirstAidKit","ACE_Clacker"}; \
-    respawnitems[] = {"FirstAidKit","ACE_Clacker"};
+    items[] += {"ACE_Clacker"}; \
+    respawnitems[] += {"ACE_Clacker"};
 
     class B_Soldier_sniper_base_F;
     class B_sniper_F: B_Soldier_sniper_base_F {MACRO_ADDCLAYMOREKIT};

commit 0c1fd4f65e08eff33b9b2a11681a3de776713ed6

Fix placing explosives while prone
Fix bug introduced in #10805

addons/explosives/functions/fnc_setupExplosive.sqf
@@ -61,7 +61,7 @@ private _isSticky = getNumber (_configMagazine >> QGVAR(isSticky)) == 1;
 
 GVAR(pfeh_running) = true;
 GVAR(placeAction) = PLACE_WAITING;
-GVAR(TweakedAngle) = 0;
+GVAR(tweakedAngle) = 0;
 
 [{
     BEGIN_COUNTER(pfeh);
@@ -81,7 +81,7 @@ GVAR(TweakedAngle) = 0;
 
     if ((stance _unit) == "PRONE") then {
         // If prone, lower base and increase up angle of look - Makes it much easier to attach to underside of vehicles
-        _basePosASL vectorAdd [0, 0, -0.3];
+        _basePosASL = _basePosASL vectorAdd [0, 0, -0.3];
         _lookDirVector = ((positionCameraToWorld [0, 0, 0]) call EFUNC(common,positionToASL)) vectorFromTo ((positionCameraToWorld [0, 3, 10]) call EFUNC(common,positionToASL));
     };
 