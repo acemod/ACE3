9697f4e5285cb3f6319b14fd124da42829646fa1
Laid3acK <bal2.chris@orange.fr>
2026-08-08 17:46:03 +0200

    B_12Gauge_Pellets_Submunition

---
+++ addons/compat_ws/CfgAmmo.hpp
@@ -0,0 +1,10 @@
+
+class CfgAmmo {
+    class B_12Gauge_Pellets_Submunition; // #00 Buckshot
+
+    class B_12gauge_Pellets_Cartridge_lxWS: B_12Gauge_Pellets_Submunition {
+        submunitionConeType[] = {"poissondisc", 8}; // #00 Buckshot
+        // submunitionConeType[] = {"poissondisc", 14}; // No2 Buckshot
+        // cartridge = "FxCartridge_12Gauge_Pellet_lxWS";
+    };
+};
