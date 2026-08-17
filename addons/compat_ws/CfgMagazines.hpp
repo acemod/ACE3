f48873d71c0c2500b4416572679823d3d7376e5b
PabstMirror <pabstmirror@gmail.com>
2026-08-17 12:33:24 -0500

    move to sub-addon

--- addons/compat_ws/CfgMagazines.hpp
+++ addons/compat_ws/CfgMagazines.hpp
@@ -1,12 +1,6 @@
 
 class CfgMagazines {
     class 11Rnd_45ACP_Mag;
-    class 20Rnd_12Gauge_AA40_Slug_lxWS;
-    class 20Rnd_12Gauge_AA40_Slug_Tan_lxWS;
-    class 20Rnd_12Gauge_AA40_Slug_Snake_lxWS;
-    class 8Rnd_12Gauge_AA40_Slug_lxWS;
-    class 8Rnd_12Gauge_AA40_Slug_Tan_lxWS;
-    class 8Rnd_12Gauge_AA40_Slug_Snake_lxWS;
 
     class 6rnd_Smoke_Mag_lxWS: 11Rnd_45ACP_Mag {
         initSpeed = 336; // 336x0.971576= 326m/s according to Crye Six12 ACE_muzzleVelocities ICAO conditions (15°C, 1013,25 hPa, 0%), 380
@@ -22,53 +16,5 @@ class CfgMagazines {
 
     class 2rnd_Smoke_Mag_lxWS: 6rnd_Smoke_Mag_lxWS {
         initSpeed = 380;
-    };
-
-    class ACE_20Rnd_12Gauge_AA40_Slug_lxWS: 20Rnd_12Gauge_AA40_Slug_lxWS {
-        author = ECSTRING(common,ACETeam);
-        displayName = CSTRING(20Rnd_12Gauge_AA40_Slug_lxWS_Name);
-        displayNameShort = CSTRING(20Rnd_12Gauge_Slug_NameShort);
-        ammo = "ACE_12Gauge_Slug_NoCartridge";
-        initSpeed = 610; // according to ACE_muzzleVelocities 12 Gauge SAAMI test barrel 30" ICAO conditions (15°C, 1013,25 hPa, 0%)
-    };
-
-    class ACE_20Rnd_12Gauge_AA40_Slug_Tan_lxWS: 20Rnd_12Gauge_AA40_Slug_Tan_lxWS {
-        author = ECSTRING(common,ACETeam);
-        displayName = CSTRING(20Rnd_12Gauge_AA40_Slug_Tan_lxWS_Name);
-        displayNameShort = CSTRING(20Rnd_12Gauge_Slug_NameShort);
-        ammo = "ACE_12Gauge_Slug_NoCartridge";
-        initSpeed = 610;
-    };
-
-    class ACE_20Rnd_12Gauge_AA40_Slug_Snake_lxWS: 20Rnd_12Gauge_AA40_Slug_Snake_lxWS {
-        author = ECSTRING(common,ACETeam);
-        displayName = CSTRING(20Rnd_12Gauge_AA40_Slug_Snake_lxWS_Name);
-        displayNameShort = CSTRING(20Rnd_12Gauge_Slug_NameShort);
-        ammo = "ACE_12Gauge_Slug_NoCartridge";
-        initSpeed = 610;
-    };
-
-    class ACE_8Rnd_12Gauge_AA40_Slug_lxWS: 8Rnd_12Gauge_AA40_Slug_lxWS {
-        author = ECSTRING(common,ACETeam);
-        displayName = CSTRING(8Rnd_12Gauge_AA40_Slug_lxWS_Name);
-        displayNameShort = CSTRING(20Rnd_12Gauge_Slug_NameShort);
-        ammo = "ACE_12Gauge_Slug_NoCartridge";
-        initSpeed = 610;
-    };
-
-    class ACE_8Rnd_12Gauge_AA40_Slug_Tan_lxWS: 8Rnd_12Gauge_AA40_Slug_Tan_lxWS {
-        author = ECSTRING(common,ACETeam);
-        displayName = CSTRING(8Rnd_12Gauge_AA40_Slug_Tan_lxWS_Name);
-        displayNameShort = CSTRING(20Rnd_12Gauge_Slug_NameShort);
-        ammo = "ACE_12Gauge_Slug_NoCartridge";
-        initSpeed = 610;
-    };
-
-    class ACE_8Rnd_12Gauge_AA40_Slug_Snake_lxWS: 8Rnd_12Gauge_AA40_Slug_Snake_lxWS {
-        author = ECSTRING(common,ACETeam);
-        displayName = CSTRING(8Rnd_12Gauge_AA40_Slug_Snake_lxWS_Name);
-        displayNameShort = CSTRING(20Rnd_12Gauge_Slug_NameShort);
-        ammo = "ACE_12Gauge_Slug_NoCartridge";
-        initSpeed = 610;
     };
 };
--- addons/compat_ws/compat_ws_ballistics/config.cpp
+++ addons/compat_ws/compat_ws_ballistics/config.cpp
@@ -1,22 +1,23 @@
 #include "script_component.hpp"
 
 class CfgPatches {
-    class ADDON {
+    class SUBADDON {
         name = COMPONENT_NAME;
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"data_f_lxWS_Loadorder", "ace_common", "ace_ballistics"};
+        requiredAddons[] = {
+            "ace_ballistics",
+            QUOTE(ADDON)
+        };
         skipWhenMissingDependencies = 1;
         author = ECSTRING(common,ACETeam);
-        authors[] = {"Mike"};
+        authors[] = {"Laid3acK"};
         url = ECSTRING(main,URL);
         VERSION_CONFIG;
+        addonRootClass = QUOTE(ADDON);
     };
 };
 
-#include "CfgEventHandlers.hpp"
-#include "CfgVehicles.hpp"
-#include "CfgWeapons.hpp"
-#include "CfgAmmo.hpp"
 #include "CfgMagazines.hpp"
+#include "CfgMagazineWells.hpp"
